94b236e74a52b66d2dd86536d323604826155a9e
Shafiqul Islam <Shafiqul Islam>
2016-11-12 16:31:30 +0600

    example test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,14 +6,26 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    protected $client;
+
+    protected function setUp()
+    {
+        $this->client = new GuzzleHttp\Client([
+            'base_uri' => 'http://api.sheba.new/v1/'
+        ]);
+    }
+
     /**
-     * A basic functional test example.
+     * A basic api test example.
      *
      * @return void
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel 5');
+        $response = $this->client->get('/');
+        $this->assertEquals(200, $response->getStatusCode());
+        $data = json_decode($response->getBody(), true);
+        $this->assertArrayHasKey('msg', $data);
+        $this->assertEquals("Success. This project will hold the api's", $data['msg']);
     }
 }
