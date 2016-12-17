d1abcb0c81733f3653a36554c01986358867e704
arnabrahman <arnabrahman@hotmail.com>
2016-12-17 19:09:49 +0600

    Cors middleware debug

--- app/Http/Middleware/Cors2MiddleWare.php
+++ app/Http/Middleware/Cors2MiddleWare.php
@@ -21,9 +21,26 @@ class Cors2MiddleWare {
             "http://admin.dev-sheba.xyz",
         ];
         if(in_array($request->server('HTTP_ORIGIN'), $domains)) {
-            return response()->json($domains);
+            $headers['Access-Control-Allow-Origin'] = $request->server('HTTP_ORIGIN');
         } else {
             return response()->json(['Unauthorized', 401]);
         }
+
+        // ALLOW OPTIONS METHOD
+        $headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT, DELETE';
+        $headers['Access-Control-Allow-Headers'] = 'Content-Type, X-Auth-Token, Origin, Authorization, X-Requested-With';
+
+        if ($request->getMethod() == "OPTIONS")
+        {
+            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+            return Response::make('OK', 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach ($headers as $key => $value)
+        {
+            $response->header($key, $value);
+        }
+        return $response;
     }
 }
