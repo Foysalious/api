121996f87e25d641a57c21cfac490c6f9f93413e
arnabrahman <arnabrahman@hotmail.com>
2016-12-25 19:20:16 +0600

    Rest append at the end for location list

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -11,7 +11,9 @@ class LocationController extends Controller
 {
     public function getAllLocations()
     {
-        $locations = Location::select('id', 'name')->orderBy('name')->get();
+        $locations = Location::select('id', 'name')->where('name', '<>', 'Rest')->orderBy('name')->get();
+        $rest = Location::select('id', 'name')->where('name','Rest')->first();
+        $locations->push($rest);
         return response()->json(['locations' => $locations, 'code' => 200, 'msg' => 'successful']);
     }
 }
