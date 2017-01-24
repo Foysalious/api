6a489da7cf5adcf5f30946e008bd0266eba90e12
arnabrahman <arnabrahman@hotmail.com>
2017-01-24 17:02:36 +0600

    facebook app redirect url change

--- config/services.php
+++ config/services.php
@@ -37,7 +37,7 @@ return [
     'facebook' => [
         'client_id' => '323841817978882',
         'client_secret' => '2e456b524617f8878dc4aeb7db93a128',
-        'redirect' => 'http://localhost/sheba_new_api/public/callback',
+        'redirect' => env('FB_APP_REDIRECT'),
     ],
 
 ];
