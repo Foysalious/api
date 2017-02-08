8919c2d6a5037c8bbaa021a802105b6633323288
arnabrahman <arnabrahman@hotmail.com>
2017-02-08 19:44:36 +0600

    career email send to hr with cover letter

--- app/Http/Controllers/CareerController.php
+++ app/Http/Controllers/CareerController.php
@@ -14,9 +14,10 @@ class CareerController extends Controller
         $slug = str_slug($request->input('name'), '_');
         Mail::raw($request->input('description'), function ($m) use ($request, $slug) {
             $m->from($request->input('email'), $request->input('name'));
-            $m->to('pasha.startern@gmail.com');
-            $m->subject('RESUME FOR ' . $request->input('jobTitle'));
+            $m->to('career@sheba.xyz');
+            $m->subject($request->input('jobTitle'));
             $m->attachData(file_get_contents($request->file('file')), 'RESUME_' . $slug . '.pdf');
+            $m->attachData(file_get_contents($request->file('cover')), 'COVER_' . $slug . '.pdf');
         });
         return response(['msg' => 'ok', 'code' => 200]);
     }
