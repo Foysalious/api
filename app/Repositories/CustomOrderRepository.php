64cb6bb21b5328df93244990ecc3a3640b5d97bc
arnabrahman <arnabrahman@hotmail.com>
2017-03-28 12:06:49 +0600

    isset remove form custom order

--- app/Repositories/CustomOrderRepository.php
+++ app/Repositories/CustomOrderRepository.php
@@ -22,21 +22,21 @@ class CustomOrderRepository
         $custom_order->customer_id = $customer->id;
         $custom_order->service_id = $service->id;
         $custom_order->service_variables = '';
-        if (isset($request->options)) {
+        if ($request->has('options')) {
             $custom_order->service_variables = $this->getServiceVariables($request->options, $service);
         }
         $custom_order->additional_info = $request->additional_info;
         $custom_order->location_id = $request->location_id;
-        $custom_order->schedule_date = isset($request->schedule_date) ? $request->schedule_date : '';
-        $custom_order->preferred_time = isset($request->preferred_time) ? $request->preferred_time : '';
-        if (!empty($request->address_id)) {
+        $custom_order->schedule_date = $request->has('schedule_date') ? $request->schedule_date : '';
+        $custom_order->preferred_time = $request->has('preferred_time') ? $request->preferred_time : '';
+        if ($request->has('address_id')) {
             $custom_order->delivery_address = (CustomerDeliveryAddress::find($request->address_id))->address;
-        } elseif (!empty($request->address)) {
+        } elseif ($request->has('address')) {
             $deliver_address = new CustomerDeliveryAddress();
             $deliver_address->address = $request->address;
 
             $deliver_address->customer_id = $customer->id;
-            if (!empty($request->created_by)) {
+            if ($request->has('created_by')) {
                 $deliver_address->created_by = $request->created_by;
                 $deliver_address->created_by_name = User::find($request->created_by)->name;
             } else {
@@ -46,10 +46,10 @@ class CustomOrderRepository
             $deliver_address->save();
             $custom_order->delivery_address = $request->address;
         }
-        $custom_order->sales_channel = isset($request->sales_channel) ? $request->sales_channel : 'Web';
-        $custom_order->job_name = isset($request->job_name) ? $request->job_name : '';
-        $custom_order->crm_id = isset($request->crm_id) ? $request->crm_id : '';
-        $custom_order->created_by = isset($request->created_by) ? $request->created_by : '';
+        $custom_order->sales_channel = $request->has('sales_channel') ? $request->sales_channel : 'Web';
+        $custom_order->job_name = $request->has('job_name') ? $request->job_name : '';
+        $custom_order->crm_id = $request->has('crm_id') ? $request->crm_id : '';
+        $custom_order->created_by = $request->has('created_by') ? $request->created_by : '';
         $custom_order->status = 'Open';
         if ($custom_order->save()) {
             return true;
