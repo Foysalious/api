545af428bc6c38ba3197aeefc8619d11af1917b4
arnabrahman <arnabrahman@hotmail.com>
2017-04-03 12:40:15 +0600

    location publication status check

--- app/Http/Controllers/LocationController.php
+++ app/Http/Controllers/LocationController.php
@@ -11,8 +11,11 @@ class LocationController extends Controller
 {
     public function getAllLocations()
     {
-        $locations = Location::select('id', 'name')->where('name', '<>', 'Rest')->orderBy('name')->get();
-        $rest = Location::select('id', 'name')->where('name','Rest')->first();
+        $locations = Location::select('id', 'name')->where([
+            ['name', '<>', 'Rest'],
+            ['publication_status', 1]
+        ])->orderBy('name')->get();
+        $rest = Location::select('id', 'name')->where('name', 'Rest')->first();
         $locations->push($rest);
         return response()->json(['locations' => $locations, 'code' => 200, 'msg' => 'successful']);
     }
