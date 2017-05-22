70b1ba345dcbdb2ec699d3d35d15c5a5d6f5d567
arnab <arnabrahman@hotmail.com>
2017-05-22 13:01:53 +0600

    Send referral request with email

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -94,9 +94,11 @@ $api->version('v1', function ($api) {
         $api->post('{customer}/email', 'App\Http\Controllers\CustomerController@modifyEmail');
         $api->post('{customer}/email-verification', 'App\Http\Controllers\CustomerController@checkEmailVerification');
         $api->post('{customer}/send-verification-link', 'App\Http\Controllers\CustomerController@sendVerificationLink');
+
         $api->get('{customer}/get-promo', 'App\Http\Controllers\PromotionController@getPromo');
         $api->post('{customer}/add-promo', 'App\Http\Controllers\PromotionController@addPromo');
         $api->post('{customer}/apply-promo', 'App\Http\Controllers\PromotionController@applyPromo');
+        $api->post('{customer}/send-referral-request-email', 'App\Http\Controllers\CustomerController@sendReferralRequestEmail');
 
         $api->post('{customer}/general-info', 'App\Http\Controllers\CustomerController@modifyGeneralInfo');
         $api->post('{customer}/order-list', 'App\Http\Controllers\OrderController@getNotClosedOrderInfo');
