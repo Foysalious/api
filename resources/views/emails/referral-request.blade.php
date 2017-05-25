7b6fb4aaf48e0b87d27ceb14aa30e1dad7e0849e
arnab <arnabrahman@hotmail.com>
2017-05-25 18:59:26 +0600

    referral request email env send from controller

--- resources/views/emails/referral-request.blade.php
+++ resources/views/emails/referral-request.blade.php
@@ -9,10 +9,8 @@
     <div class="content">
         <div class="title">
             <p>Please go to this link to register with Sheba & get 200 Tk on your first order
-                off: {{env('SHEBA_ACCOUNT_URL')}}/register?redirect_url={{env('SHEBA_FRONT_END_URL')}}
-                &referral_code={{$voucher->code}}
+                off: {{env('SHEBA_ACCOUNT_URL')}}/register?redirect_url={{$front}}&referral_code={{$voucher->code}}
             </p>
-
             <p style="color: red">If the above link is not clickable, try copying and pasting it into the address bar of
                 your web browser.</p>
         </div>
