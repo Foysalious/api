89c41aec1b3765b476462f1f6a7e21282238ac10
arnabrahman <arnabrahman@hotmail.com>
2017-08-08 11:52:53 +0600

    send customer referral code with login information

--- app/Repositories/ProfileRepository.php
+++ app/Repositories/ProfileRepository.php
@@ -60,6 +60,8 @@ class ProfileRepository
                 $info['bKash'] = $avatar->banking_info->bKash;
                 $info['verification_status'] = $avatar->verification_status;
                 $info['is_suspended'] = $avatar->is_suspended;
+            } elseif ($from == 'customer') {
+                $info['referral'] = $avatar->referral->code;
             }
             return $info;
         }
