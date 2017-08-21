23a3ef0c9e4067078c45bd922208a6c4a034669b
arnabrahman <arnabrahman@hotmail.com>
2017-08-21 13:21:46 +0600

    services of secondary category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -99,24 +99,44 @@ class CategoryController extends Controller
 
     public function getSecondaries($category, Request $request)
     {
-        $category = Category::where('id', $category)->with('children')->first();
+        $category = Category::find($category);
         if ($category != null) {
+            $children = $category->children;
             if (count($category->children) > 0) {
                 $category = collect($category)->only(['name', 'banner']);
-//                array_add($category->children, 'secondaries', $category->children);
-//                array_add($category->children, 'secondaries', $category->children);
-                return class_basename($request) == 'Request' ? response()->json(['category' => $category, 'msg' => 'successful', 'code' => 200]) : $category;
+                $category->put('secondaries', $children);
+                return class_basename($request) == 'Request' ? response()->json(['category' => $category->all(), 'msg' => 'successful', 'code' => 200]) : $category->all();
             } else
-                return response()->json(['msg' => 'no secondary categories found!', 'code' => 404]);
+                return class_basename($request) == 'Request' ? response()->json(['msg' => 'no secondary categories found!', 'code' => 404]) : null;
         } else {
-            return response()->json(['msg' => 'category not found', 'code' => 404]);
+            return class_basename($request) == 'Request' ? response()->json(['msg' => 'category not found', 'code' => 404]) : null;
         }
     }
 
     public function getSecondariesServices($category, Request $request)
     {
         $category = $this->api->get('categories/' . $category . '/secondaries');
-        dd(collect($category));
-        dd($this->categoryRepository->onlyChildrenWithServices($category->secondaries, 0, 2));
+        if ($category != null) {
+            $secondaries = $category['secondaries'];
+            if(count($secondaries)>0){
+                $offset = $request->offset != '' ? $request->offset : 0;
+                $limit = $request->limit != '' ? $request->limit : 2;
+                $location = $request->location != '' ? $request->location : 4;
+                $service_limit = $request->service != '' ? $request->services : 4;
+                $secondaries->load(['services' => function ($q) {
+                    $q->select('id', 'category_id', 'name', 'thumb', 'banner', 'slug', 'variable_type', 'variables', 'min_quantity')->published();
+                }]);
+                $secondaries = $this->categoryRepository->onlyChildrenWithServices($secondaries, $offset, $limit);
+                foreach ($secondaries as $child) {
+                    $child['slug'] = str_slug($child->name, '-');
+                    $services = $this->serviceRepository->getPartnerServicesAndPartners($child->services, $location, $service_limit);
+                    array_forget($child, 'services');
+                    $child['services'] = $services;
+                    $child['services'] = $this->serviceRepository->addServiceInfo($child['services']);
+                }
+                $category['secondaries']=$secondaries;
+                return class_basename($request) == 'Request' ? response()->json(['category' => $category, 'msg' => 'successful', 'code' => 200]) : $category;
+            }
+        }
     }
 }
