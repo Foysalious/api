d0cd426350bbb7725106f5a24edc301ae9179d5d
arnabrahman <arnabrahman@hotmail.com>
2017-09-14 14:46:59 +0600

    resource job list order by date & preferred time

--- app/Http/Controllers/ResourceJobController.php
+++ app/Http/Controllers/ResourceJobController.php
@@ -2,17 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\ResourceJobRepository;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
 class ResourceJobController extends Controller
 {
+    private $resourceJobRepository;
+
+    public function __construct()
+    {
+        $this->resourceJobRepository = new ResourceJobRepository();
+    }
+
     public function index($resource, Request $request)
     {
-        $resource = $request->resource;
-        $resource->load(['jobs' => function ($q) {
-            $q->select('id', 'resource_id', 'schedule_date', 'service_name', 'status', 'partner_order_id')->orderBy('schedule_date')->whereIn('status', ['Accepted', 'Schedule Due'])->with(['partner_order' => function ($q) {
+        $resource = $request->resource->load(['jobs' => function ($q) {
+            $q->select('id', 'resource_id', 'schedule_date', 'preferred_time', 'service_name', 'status', 'partner_order_id')->where('schedule_date', '<=', date('Y-m-d'))->whereIn('status', ['Accepted', 'Process', 'Schedule Due'])->with(['partner_order' => function ($q) {
                 $q->with('order.customer.profile');
             }]);
         }]);
@@ -26,8 +33,9 @@ class ResourceJobController extends Controller
                 array_forget($job, 'partner_order_id');
                 array_forget($job, 'resource_id');
             }
+            $jobs = $this->resourceJobRepository->rearrange($jobs);
             list($offset, $limit) = calculatePagination($request);
-            $jobs = $jobs->splice($offset, $limit)->all();
+            $jobs = array_slice($jobs, $offset, $limit);
             return api_response($request, $jobs, 200, ['jobs' => $jobs]);
         } else {
             return api_response($request, null, 404);
---
+++ app/Repositories/ResourceJobRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repositories;
+
+
+class ResourceJobRepository
+{
+
+    public function rearrange($jobs)
+    {
+        $process_job = $jobs->where('status', 'Process')->values()->all();
+        $other_jobs = $jobs->filter(function ($job) {
+            return $job->status != 'Process';
+        });
+        $other_jobs = $other_jobs->map(function ($item) {
+            return array_add($item, 'preferred_time_priority', constants('JOB_PREFERRED_TIMES_PRIORITY')[$item->preferred_time]);
+        });
+        $other_jobs = $other_jobs->sortBy(function ($job) {
+            return sprintf('%-12s%s', $job->schedule_date, $job->preferred_time_priority);
+        })->values()->all();
+        $jobs = array_merge($process_job, $other_jobs);
+    }
+}
