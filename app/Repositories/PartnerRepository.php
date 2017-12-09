5e317fc1fba146d7825f9b43ed758696ffd24b23
arnabrahman <arnabrahman@hotmail.com>
2017-12-09 16:42:42 +0600

    partner jobs history

--- app/Repositories/PartnerRepository.php
+++ app/Repositories/PartnerRepository.php
@@ -63,6 +63,8 @@ class PartnerRepository
             return array(constants('JOB_STATUSES')['Pending'], constants('JOB_STATUSES')['Not_Responded']);
         } elseif ($status == 'ongoing') {
             return array(constants('JOB_STATUSES')['Accepted'], constants('JOB_STATUSES')['Process']);
+        } elseif ($status == 'history') {
+            return array(constants('JOB_STATUSES')['Served']);
         }
     }
 
