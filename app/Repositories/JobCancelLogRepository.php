d05b9573e1e9ba2156cb1a8e87d4a34d9ddb064e
arnabrahman <arnabrahman@hotmail.com>
2018-01-18 16:08:21 +0600

    job cancel log user information save user information for web name change to customer-portal

--- app/Repositories/JobCancelLogRepository.php
+++ app/Repositories/JobCancelLogRepository.php
@@ -5,6 +5,7 @@ namespace App\Repositories;
 
 use App\Models\Job;
 use App\Models\JobCancelLog;
+use App\Sheba\UserRequestInformation;
 
 class JobCancelLogRepository
 {
@@ -23,13 +24,14 @@ class JobCancelLogRepository
 
     public function store($previous_status, $reason)
     {
-        $job_cancel = new JobCancelLog();
+        $job_cancel = new JobCancelLog((new UserRequestInformation(\request()))->getInformationArray());
         $job_cancel->job_id = $this->job->id;
         $job_cancel->from_status = $previous_status;
         $job_cancel->cancel_reason_details = $reason;
         $job_cancel->created_by_name = class_basename($this->created_by);
         $job_cancel->cancel_reason = class_basename($this->created_by) . ' Dependency';
         $job_cancel->log = 'Job has been cancelled by ' . class_basename($this->created_by);
+        $job_cancel->created_by_type = 'App/Models/' . class_basename($this->created_by);
         $job_cancel->save();
     }
 
