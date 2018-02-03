7921d4792dfae13eccc10429bbd81a77e1b6dab1
arnabrahman <arnabrahman@hotmail.com>
2018-02-03 13:47:50 +0600

    resource created by type bug fix

--- app/Repositories/ResourceJobRepository.php
+++ app/Repositories/ResourceJobRepository.php
@@ -14,7 +14,7 @@ use Carbon\Carbon;
 
 class ResourceJobRepository
 {
-    private $created_by_type = 'App/Models/Resource';
+    private $created_by_type = 'App\Models\Resource';
 
     public function rearrange($jobs)
     {
