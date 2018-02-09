969b67d9a5754f135ff56c72714173b1cec55f90
Arnab <arnabrahman@hotmail.com>
2018-02-09 20:50:36 +0600

    social profile class create

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -7,9 +7,9 @@ use App\Http\Controllers\FacebookAccountKit;
 use App\Models\Profile;
 use App\Repositories\FacebookRepository;
 use App\Repositories\ProfileRepository;
+use App\Sheba\SocialProfile;
 use Google_Client;
 use Illuminate\Http\Request;
-use Auth;
 use Illuminate\Validation\ValidationException;
 
 class GoogleController extends Controller
@@ -29,18 +29,15 @@ class GoogleController extends Controller
     public function login(Request $request)
     {
         try {
-            $from = implode(',', constants('FROM'));
-            $this->validate($request, ['id_token' => 'required', 'from' => "required|in:$from"]);
+            $this->validate($request, ['id_token' => 'required', 'from' => "required|in:" . implode(',', constants('FROM'))]);
             if ($payload = $this->getGooglePayload($request->id_token)) {
-                $profile = $this->profileRepository->ifExist($payload['email'], 'email');
-                if ($profile) {
+                $social_profile = new SocialProfile($payload);
+                $profile_info = $social_profile->getProfileInfo('google');
+                if ($profile = $this->profileRepository->getIfExist($profile_info['email'], 'email')) {
+                    $profile = $social_profile->updateProfileInformation($profile);
                     if (basename($profile->pro_pic) == 'default.jpg') {
-                        $profile->pro_pic = $this->profileRepository->uploadImage($profile, $payload['picture'], 'images/profiles/');
+                        $profile->pro_pic = $this->profileRepository->uploadImage($profile, $profile_info['pro_pic'], 'images/profiles/');
                     }
-                    if ($profile->google_id == null) {
-                        $profile->google_id = $payload['sub'];
-                    }
-                    $profile->email_verified = 1;
                     $profile->update();
                     $from = $this->profileRepository->getAvatar($request->from);
                     if ($profile->$from == null) {
@@ -64,27 +61,23 @@ class GoogleController extends Controller
     public function register(Request $request)
     {
         try {
-            $from = implode(',', constants('FROM'));
-            $this->validate($request, ['id_token' => 'required', 'email' => 'required|email', 'kit_code' => 'required', 'from' => "required|in:$from"]);
+            $this->validate($request, ['id_token' => 'required', 'kit_code' => 'required', 'from' => "required|in:" . implode(',', constants('FROM'))]);
             $payload = $this->getGooglePayload($request->id_token);
             $kit_data = $this->fbKit->authenticateKit($request->kit_code);
             if ($payload && $kit_data) {
-                $profile = $this->profileRepository->getIfExist($payload['email'], 'email');
-                if ($profile) {
-                    return api_response($request, null, 400, ['message' => 'Gmail already exists! Please login']);
+                $social_profile = new SocialProfile(array_merge($payload, ['mobile' => formatMobile($kit_data['mobile'])]));
+                $profile_info = $social_profile->getProfileInfo('google');
+
+                $email_profile = $this->profileRepository->getIfExist($profile_info['email'], 'email');
+                $mobile_profile = $this->profileRepository->getIfExist($profile_info['mobile'], 'mobile');
+                if ($email_profile || $mobile_profile) {
+                    $col = $email_profile ? 'Email' : 'Mobile';
+                    return api_response($request, null, 400, ['message' => $col . ' already exists! Please login']);
                 }
-                $profile = $this->profileRepository->getIfExist(formatMobile($kit_data['mobile']), 'mobile');
-                if ($profile) {
-                    return api_response($request, null, 400, ['message' => 'Mobile already exists! Please login']);
-                }
-                $profile = new Profile();
-                DB::transaction(function () use ($kit_data, &$profile, $payload) {
-                    $profile = $this->profileRepository->store([
-                        'email' => $payload['email'], 'mobile' => formatMobile($kit_data['mobile']), 'name' => trim($payload['name']), 'mobile_verified' => 1, 'email_verified' => 1, 'google_id' => $payload['sub']
-                    ]);
-                    $profile->pro_pic = $this->profileRepository->uploadImage($profile, $payload['picture'], 'images/profiles/');
-                    $profile->update();
-                });
+                $profile = $this->profileRepository->store($profile_info);
+                $profile->pro_pic = $this->profileRepository->uploadImage($profile, $profile_info['pro_pic'], 'images/profiles/');
+                $profile->update();
+                $from = $this->profileRepository->getAvatar($request->from);
                 if ($profile->$from == null) {
                     $this->profileRepository->registerAvatar($from, $request, $profile);
                 }
@@ -109,18 +102,5 @@ class GoogleController extends Controller
         } catch (\Throwable $e) {
             return null;
         }
-    }
-
-    public function create(array $data)
-    {
-        $profile = new Profile();
-        foreach ($data as $key => $value) {
-            $profile->$key = $value;
-        }
-        $profile->remember_token = str_random(255);
-        $profile->mobile_verified = 1;
-        $profile->email_verified = 1;
-        $profile->save();
-        return $profile;
     }
 }
