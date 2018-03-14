27fcc30ef996c59edd3134e064cd2c039e0637c5
arnabrahman <arnabrahman@hotmail.com>
2018-03-14 10:32:03 +0600

    localhost add in cors

--- app/Http/Middleware/Cors2MiddleWare.php
+++ app/Http/Middleware/Cors2MiddleWare.php
@@ -20,6 +20,8 @@ class Cors2MiddleWare
             "http://localhost",
             "http://localhost:8080",
             "http://localhost:8081",
+            "http://localhost:8082",
+            "http://localhost:8083",
             "http://192.168.1.109:8080",
             "http://192.168.1.108:8080",
             "http://dev-sheba.xyz",
