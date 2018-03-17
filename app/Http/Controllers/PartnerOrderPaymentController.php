3561c74911f24fd3e42f0589a0fb530fa54ae0da
arnabrahman <arnabrahman@hotmail.com>
2018-03-17 11:10:17 +0600

    partner collection date filter fix

--- app/Http/Controllers/PartnerOrderPaymentController.php
+++ app/Http/Controllers/PartnerOrderPaymentController.php
@@ -7,7 +7,6 @@ use App\Models\PartnerOrderPayment;
 use App\Models\Resource;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Validation\ValidationException;
 use DB;
 
 class PartnerOrderPaymentController extends Controller
@@ -25,8 +24,8 @@ class PartnerOrderPaymentController extends Controller
                     return $query->where(DB::raw('YEAR(partner_order_payments.created_at)'), $year);
                 })->when($month, function ($query) use ($month) {
                     return $query->where(DB::raw('MONTH(partner_order_payments.created_at)'), $month);
-                })->when($date, function ($query) use ($date) {
-                    return $query->where(DB::raw('MONTH(partner_order_payments.created_at)'), (Carbon::createFromDate(null, null, 31))->format('Y-m-d'));
+                })->when($date, function ($query) use ($date, $month, $year) {
+                    return $query->where(DB::raw('DATE(partner_order_payments.created_at)'), (Carbon::createFromDate($year, $month, $date))->format('Y-m-d'));
                 })
                 ->where('partners.id', $partner->id)->orderBy('partner_order_payments.id', 'desc')
                 ->select('partner_order_payments.id', 'partner_order_payments.created_at', 'partner_order_payments.created_by', 'partner_order_payments.amount', 'log', 'method', DB::raw('partner_orders.id as partner_order_id'))
@@ -61,12 +60,12 @@ class PartnerOrderPaymentController extends Controller
         $resources = $collections->groupBy('resource_name');
         $final = [];
         foreach ($resources as $resource) {
-            array_push($final,array(
-                'resource_name'=>$resource[0]->resource_name,
-                'resource_mobile'=>$resource[0]->resource_mobile,
-                'resource_picture'=> $resource[0]->resource_picture,
-                'total_collected_times'=>count($resource),
-                'amount'=>$resource->sum('amount')
+            array_push($final, array(
+                'resource_name' => $resource[0]->resource_name,
+                'resource_mobile' => $resource[0]->resource_mobile,
+                'resource_picture' => $resource[0]->resource_picture,
+                'total_collected_times' => count($resource),
+                'amount' => $resource->sum('amount')
             ));
         }
         return collect($final);
