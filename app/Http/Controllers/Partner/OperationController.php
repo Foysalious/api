aa08780971dc1b85a5f4be793a80d458043e05b7
arnabrahman <arnabrahman@hotmail.com>
2018-05-03 18:38:42 +0600

    dd remove

--- app/Http/Controllers/Partner/OperationController.php
+++ app/Http/Controllers/Partner/OperationController.php
@@ -137,7 +137,6 @@ class OperationController extends Controller
             $sentry->captureException($e);
             return api_response($request, $message, 400, ['message' => $message]);
         } catch (\Throwable $e) {
-            dd($e);
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
         }
