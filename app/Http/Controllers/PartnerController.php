e78d7796596cfc6d50dffcf1d26b571b24823e0e
arnabrahman <arnabrahman@hotmail.com>
2018-05-17 16:46:06 +0600

    review dd remove

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -280,7 +280,6 @@ class PartnerController extends Controller
             );
             return api_response($request, $info, 200, ['info' => $info]);
         } catch (\Throwable $e) {
-            dd($e);
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
         }
