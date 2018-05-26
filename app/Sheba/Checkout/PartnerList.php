8cdb5ab2b73de55e7159317a87658ae9ba079d98
arnabrahman <arnabrahman@hotmail.com>
2018-05-26 09:05:13 +0600

    pickup & destination calculate in partnerlist

--- app/Sheba/Checkout/PartnerList.php
+++ app/Sheba/Checkout/PartnerList.php
@@ -41,13 +41,13 @@ class PartnerList
         foreach ($services as $service) {
             $selected_service = Service::where('id', $service->id)->publishedForAll()->first();
             $selected_service['option'] = $service->option;
+            $selected_service['pick_up_location_id'] = isset($service->pick_up_location_id) ? $service->pick_up_location_id : null;
+            $selected_service['pick_up_location_type'] = isset($service->pick_up_location_type) ? $service->pick_up_location_type : null;
+            $selected_service['destination_location_id'] = isset($service->destination_location_id) ? $service->destination_location_id : null;
+            $selected_service['destination_location_type'] = isset($service->destination_location_type) ? $service->destination_location_type : null;
+            $selected_service['drop_off_date'] = isset($service->drop_off_date) ? $service->drop_off_date : null;
+            $selected_service['drop_off_time'] = isset($service->drop_off_time) ? $service->drop_off_time : null;
             if (in_array($selected_service->id, $this->rentCarServicesId)) {
-                $selected_service['pick_up_location_id'] = isset($service->pick_up_location_id) ? $service->pick_up_location_id : null;
-                $selected_service['pick_up_location_type'] = isset($service->pick_up_location_type) ? $service->pick_up_location_type : null;
-                $selected_service['destination_location_id'] = isset($service->destination_location_id) ? $service->destination_location_id : null;
-                $selected_service['destination_location_type'] = isset($service->destination_location_type) ? $service->destination_location_type : null;
-                $selected_service['drop_off_date'] = isset($service->drop_off_date) ? $service->drop_off_date : null;
-                $selected_service['drop_off_time'] = isset($service->drop_off_time) ? $service->drop_off_time : null;
                 $model = "App\\Models\\" . $service->pick_up_location_type;
                 $origin = $model::find($service->pick_up_location_id);
                 $selected_service['pick_up_address_geo'] = json_encode(array('lat' => $origin->lat, 'lng' => $origin->lng));
