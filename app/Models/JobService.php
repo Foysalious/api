042885e946a56a77ff190e45148e1dafc60d5aa0
arnabrahman <arnabrahman@hotmail.com>
2018-06-23 17:34:19 +0600

    rent a car info

--- app/Models/JobService.php
+++ app/Models/JobService.php
@@ -28,7 +28,7 @@ class JobService extends Model
             if ($job->carRentalJobDetail->destinationLocation) {
                 return $this->name . ' | ' . $job->carRentalJobDetail->pickUpLocation->name . ' to ' . $job->carRentalJobDetail->destinationLocation->name;
             } else {
-                return $this->name . ' from ' . $job->carRentalJobDetail->pickUpLocation->name;
+                return $this->name . ' | From ' . $job->carRentalJobDetail->pickUpLocation->name;
             }
         }
     }
