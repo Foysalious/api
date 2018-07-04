76190b1d2214129f0728b3375f9e7619ad0ab130
arnabrahman <arnabrahman@hotmail.com>
2018-07-04 08:41:34 +0600

    location test

---
+++ tests/LocationTest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class LocationTest extends TestCase
+{
+    public function testGetLocations()
+    {
+        dd( $this->json('GET', '/v1/locations'));
+        $this->json('GET', '/v1/locations')->seeJsonStructure([
+            'code',
+            'locations' => [
+                ['id', 'name']
+            ],
+            'msg'
+        ]);
+    }
+
+    public function testCurrentLocations()
+    {
+        $this->json('GET', '/v2/locations/current', ['lat' => 23.8270444, 'lng' => 90.3613735])->seeJsonStructure([
+            'code',
+            'location' => ['id', 'name'],
+            'message'
+        ]);
+    }
+
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,7 +7,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      *
      * @var string
      */
-    protected $baseUrl = 'http://api.sheba.new/v1';
+    protected $baseUrl;
 
     /**
      * Creates the application.
@@ -16,9 +16,11 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      */
     public function createApplication()
     {
-        $app = require __DIR__.'/../bootstrap/app.php';
+        $app = require __DIR__ . '/../bootstrap/app.php';
 
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        $this->baseUrl = env('APP_URL');
 
         return $app;
     }
