38841af8441ebd22cfeb67cd8b736ce3da911459
arnabrahman <arnabrahman@hotmail.com>
2018-07-14 11:37:36 +0600

    partner cancel request try catch fix

--- app/Sheba/CancelRequest/PartnerRequestor.php
+++ app/Sheba/CancelRequest/PartnerRequestor.php
@@ -9,17 +9,18 @@ class PartnerRequestor extends Requestor
         $this->notify();
     }
 
-    /**
-     * @throws \Exception
-     */
     protected function notify()
     {
-        $order = $this->job->partnerOrder->order;
-        $link = config('sheba.admin_url') . 'order/' . $order->id;
-        notify()->user($this->job->crm)->send([
-            "title" => $this->job->partnerOrder->partner->name . " requested to cancel a job: " . $order->code(),
-            "link"  => $link,
-            "type"  => notificationType('Danger')
-        ]);
+        try {
+            $order = $this->job->partnerOrder->order;
+            $link = config('sheba.admin_url') . 'order/' . $order->id;
+            notify()->user($this->job->crm)->send([
+                "title" => $this->job->partnerOrder->partner->name . " requested to cancel a job: " . $order->code(),
+                "link" => $link,
+                "type" => notificationType('Danger')
+            ]);
+        } catch (\Throwable $e) {
+            app('sentry')->captureException($e);
+        }
     }
 }
