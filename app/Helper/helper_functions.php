d36e7d01d8cbbf6e892846a62956139672da6004
Saiful Isalm <islam42555@gmail.com>
2018-07-21 23:52:13 +0600

    add missing month start end function

--- app/Helper/helper_functions.php
+++ app/Helper/helper_functions.php
@@ -282,3 +282,26 @@ if (!function_exists('ordinal')) {
             return $number . $ends[$number % 10];
     }
 }
+
+if (!function_exists('findStartEndDateOfAMonth')) {
+    /**
+     * @param $month
+     * @param $year
+     * @return array
+     */
+    function findStartEndDateOfAMonth($month = null, $year = null)
+    {
+        if ($month == 0 && $year != 0) {
+            $start_time = \Carbon\Carbon::now()->year($year)->month(1)->day(1)->hour(0)->minute(0)->second(0);
+            $end_time = \Carbon\Carbon::now()->year($year)->month(12)->day(31)->hour(23)->minute(59)->second(59);
+            return ['start_time' => $start_time, 'end_time' => $end_time, 'days_in_month' => 31];
+        } else {
+            if (empty($month)) $month = \Carbon\Carbon::now()->month;
+            if (empty($year)) $year = \Carbon\Carbon::now()->year;
+            $days_in_month = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+            $start_time = \Carbon\Carbon::now()->year($year)->month($month)->day(1)->hour(0)->minute(0)->second(0);
+            $end_time = \Carbon\Carbon::now()->year($year)->month($month)->day($days_in_month)->hour(23)->minute(59)->second(59);
+            return ['start_time' => $start_time, 'end_time' => $end_time, 'days_in_month' => $days_in_month];
+        }
+    }
+}
