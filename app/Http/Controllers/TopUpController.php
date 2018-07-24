4cc4d50cc59f11fa3e4e87b093a89fdfe23c955a
arnabrahman <arnabrahman@hotmail.com>
2018-07-24 11:31:09 +0600

    topup vendor agent commission set

--- app/Http/Controllers/TopUpController.php
+++ app/Http/Controllers/TopUpController.php
@@ -9,8 +9,8 @@ class TopUpController extends Controller
     public function getVendor(Request $request)
     {
         try {
-            $vendors = TopUpVendor::select('id', 'name', 'is_published')->get();
-            foreach ($vendors as $vendor){
+            $vendors = TopUpVendor::select('id', 'name', 'is_published', 'agent_commission')->get();
+            foreach ($vendors as $vendor) {
                 $asset_name = strtolower(trim(preg_replace('/\s+/', '_', $vendor->name)));
                 array_add($vendor, 'asset', $asset_name);
             }
@@ -23,21 +23,21 @@ class TopUpController extends Controller
 
     public function topUp(Request $request)
     {
-        try{
+        try {
             $this->validate($request, [
-                'mobile'            => 'required|string|mobile:bd',
-                'connection_type'   => 'required|in:prepaid,postpaid',
-                'vendor_id'         => 'required|exists:topup_vendors,id',
-                'amount'            => 'required|min:10|numeric'
+                'mobile' => 'required|string|mobile:bd',
+                'connection_type' => 'required|in:prepaid,postpaid',
+                'vendor_id' => 'required|exists:topup_vendors,id',
+                'amount' => 'required|min:10|numeric'
             ]);
 
-            if ($request->affiliate->wallet >= (double) $request->amount) {
+            if ($request->affiliate->wallet >= (double)$request->amount) {
                 $request->affiliate->doRecharge($request->vendor_id, $request->mobile, $request->amount, $request->connection_type);
                 return api_response($request, null, 200);
             } else {
                 return api_response($request, null, 403, ['message' => "You don't have sufficient balance to recharge."]);
             }
-        }catch (ValidationException $e) {
+        } catch (ValidationException $e) {
             $message = getValidationErrorMessage($e->validator->errors()->all());
             $sentry = app('sentry');
             $sentry->user_context(['request' => $request->all(), 'message' => $message]);
