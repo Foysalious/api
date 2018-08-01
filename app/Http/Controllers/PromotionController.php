be24bc2682e5be5bfe803a58120987eaf218dfdc
arnabrahman <arnabrahman@hotmail.com>
2018-08-01 16:45:23 +0600

    partner not available message in promotion auto apply

--- app/Http/Controllers/PromotionController.php
+++ app/Http/Controllers/PromotionController.php
@@ -161,7 +161,7 @@ class PromotionController extends Controller
         try {
             $partner_list = new PartnerList(json_decode($request->services), $request->date, $request->time, $request->location);
             $order_amount = $this->calculateOrderAmount($partner_list, $request->partner);
-            if (!$order_amount) return api_response($request, null, 403);
+            if (!$order_amount) return api_response($request, null, 403, ['message' => 'No partner available at this combination']);
             $voucherSuggester->init($request->customer, $partner_list->selected_services->first()->category_id, $request->partner, (int)$request->location, $order_amount, $request->sales_channel);
             if ($promo = $voucherSuggester->suggest()) {
                 $applied_voucher = array('amount' => (double)$promo['amount'], 'code' => $promo['voucher']->code, 'id' => $promo['voucher']->id);
