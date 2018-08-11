a0e259bd60bdaf98263b03c5a368c5731924a52d
Sabbir Ahmed <sabbir.sheba@gmail.com>
2018-08-11 15:45:44 +0600

    JobService Update moved to JobServiceActions.

--- app/Http/Controllers/JobServiceController.php
+++ app/Http/Controllers/JobServiceController.php
@@ -17,30 +17,17 @@ class JobServiceController extends Controller
     use ModificationFields;
     private $jobServiceRepository;
 
-    private $modifier_id;
-    private $time;
-    private $modifier_name;
-
     public function __construct()
     {
         $this->jobServiceRepository = new JobServiceRepository();
     }
-
-    private function setModifier($modifier)
-    {
-        $this->modifier_id = $modifier->id;
-        $this->modifier_name = substr(strrchr(get_class($modifier), '\\'), 1) . ' - ' . $modifier->profile->name;
-        $this->time = Carbon::now();
-    }
-
 
     public function update($partner, $job_service, Request $request)
     {
         try {
             $job_service = JobService::find($job_service);
             $invalid_job_statuses = [constants('JOB_STATUSES')['Pending'], constants('JOB_STATUSES')['Declined'], constants('JOB_STATUSES')['Not_Responded'], constants('JOB_STATUSES')['Served'], constants('JOB_STATUSES')['Cancelled']];
-            $job_service_old = clone $job_service;
-            $old_job = clone $job_service_old->job;
+            $old_job = clone $job_service->job;
             $old_job = $old_job->calculate(true);
 
             $request->merge(['status' => $old_job->status]);
@@ -56,31 +43,18 @@ class JobServiceController extends Controller
             if ($old_job->isCancelRequestPending()) {
                 return api_response($request, null, 400, ['message' => 'Unprocessable job. Cancel request pending.']);
             }
-            $data = $this->getJobServicePriceUpdateData($job_service, $request);
-            array_merge($data, [
-                'updated_by' => $this->modifier_id,
-                'updated_by_name' => $this->modifier_name,
-                'updated_at' => $this->time
-            ]);
+
             $this->setModifier($request->manager_resource);
+            $response = (new JobServiceActions)->update($job_service, $request, $old_job);
 
-            DB::transaction(function () use ($job_service, $request, $job_service_old, $old_job, $data) {
-                $job_service->update($data);
-                $this->saveServicePriceUpdateLog($job_service_old, $request);
-                $job = $job_service->job;
-                $job = $job->calculate(true);
-                try {
-                    notify()->customer($job->partnerOrder->order->customer)
-                        ->send($this->priceChangedNotificationData($job, $old_job->grossPrice));
-                } catch (\Throwable $exception) {
+            if ($response['code'] == 200) {
+                if ($request->has('discount')) $message = "$request->discount Tk have been successfully discounted.";
+                else $message = "Job service updated successfully";
 
-                }
-            });
-
-            if ($request->has('discount')) $message = "$request->discount Tk have been successfully discounted.";
-            else $message = "Job service updated successfully";
-
-            return api_response($request, null, 200, ['message' => $message]);
+                return api_response($request, null, 200, ['message' => $message]);
+            } else {
+                return api_response($request, null, 500);
+            }
         } catch (ValidationException $e) {
             $message = getValidationErrorMessage($e->validator->errors()->all());
             $sentry = app('sentry');
@@ -109,70 +83,6 @@ class JobServiceController extends Controller
                 return "Discount can't be greater than due";
             }
         }
-    }
-
-    private function getJobServicePriceUpdateData(JobService $job_service, Request $request)
-    {
-        $data = [
-            'quantity' => (float)$request->quantity,
-            'unit_price' => (float)$request->unit_price
-        ];
-        if ($request->has('discount') && (float)$request->discount != $job_service->discount) $data['discount'] = (float)$request->discount;
-        $this->getNewDiscount($job_service, $data);
-        return $data;
-    }
-
-    private function getNewDiscount(JobService $job_service, &$data)
-    {
-        if (isset($data['discount'])) {
-            $data['discount_percentage'] = '0.00';
-            $sheba_contribution_bdt = (float)$job_service->discount * ((float)$job_service->sheba_contribution / 100);
-            $data['sheba_contribution'] = (float)number_format($sheba_contribution_bdt / $data['discount'] * 100, 2);
-            $data['partner_contribution'] = 100 - $data['sheba_contribution'];
-        } elseif ($job_service->discount_percentage && $job_service->discount_percentage != "0.00") {
-            $data['discount'] = ($data['unit_price'] * $data['quantity'] * $job_service->discount_percentage) / 100;
-        }
-    }
-
-    private function saveServicePriceUpdateLog(JobService $job_service, Request $request)
-    {
-        $updated_data = [
-            'msg' => 'Service Price Updated',
-            'old_service_unit_price' => $job_service->unit_price,
-            'old_service_quantity' => $job_service->quantity,
-            'new_service_unit_price' => $request->unit_price,
-            'new_service_quantity' => $request->quantity,
-            'service_name' => $job_service->name
-        ];
-        $this->jobUpdateLog($job_service->job->id, json_encode($updated_data));
-    }
-
-    private function jobUpdateLog($job_id, $log)
-    {
-        $log_data = [
-            'job_id' => $job_id,
-            'log' => $log
-        ];
-        JobUpdateLog::create(array_merge((new UserRequestInformation(\request()))->getInformationArray(), $log_data, [
-            'created_by' => $this->modifier_id,
-            'created_by_name' => $this->modifier_name,
-            'created_at' => $this->time,
-            'updated_by' => $this->modifier_id,
-            'updated_by_name' => $this->modifier_name,
-            'updated_at' => $this->time,
-        ]));
-    }
-
-    private function priceChangedNotificationData(Job $job, $old_job_price)
-    {
-        $order = $job->partnerOrder->order;
-        return [
-            "title" => "Order: " . $order->code() . " Price changed. Old Price: " . $old_job_price . ", New Price: " . $job->grossPrice,
-            "link" => env('SHEBA_FRONT_END_URL') . "/orders/" . $order->id,
-            "type" => notificationType('Info'),
-            "event_type" => 'App\Models\Order',
-            "event_id" => $order->id
-        ];
     }
 
     public function destroy($partner, $job_service, Request $request)
--- app/Sheba/JobService/JobServiceActions.php
+++ app/Sheba/JobService/JobServiceActions.php
@@ -23,9 +23,81 @@ class JobServiceActions
         dd(5);
     }
 
-    public function update(JobService $job_service, Request $request)
+    public function update(JobService $job_service, Request $request, Job $old_job)
     {
+        try {
+            $job_service_old = clone $job_service;
 
+            $data = $this->getJobServicePriceUpdateData($job_service, $request);
+            $data = $this->withUpdateModificationField($data);
+
+            DB::transaction(function () use ($job_service, $request, $job_service_old, $old_job, $data) {
+                $job_service->update($data);
+                $this->saveServicePriceUpdateLog($job_service_old, $request);
+                $job = $job_service->job->calculate(true);
+                $this->updateJobDiscount($job);
+
+                try {
+                    notify()->customer($job->partnerOrder->order->customer)
+                        ->send($this->priceChangedNotificationData($job, $old_job->grossPrice));
+                } catch (\Throwable $exception) {
+
+                }
+            });
+
+            return ['code' => 200];
+        } catch (\Throwable $exception) {
+            app('sentry')->captureException($exception);
+            return ['code' => 500];
+        }
+    }
+
+    private function getJobServicePriceUpdateData(JobService $job_service, Request $request)
+    {
+        $data = [
+            'quantity' => (float)$request->quantity,
+            'unit_price' => (float)$request->unit_price
+        ];
+        if ($request->has('discount') && (float)$request->discount != $job_service->discount) $data['discount'] = (float)$request->discount;
+        $this->getNewDiscount($job_service, $data);
+        return $data;
+    }
+
+    private function getNewDiscount(JobService $job_service, &$data)
+    {
+        if (isset($data['discount'])) {
+            $data['discount_percentage'] = '0.00';
+            $sheba_contribution_bdt = (float)$job_service->discount * ((float)$job_service->sheba_contribution / 100);
+            $data['sheba_contribution'] = (float)number_format($sheba_contribution_bdt / $data['discount'] * 100, 2);
+            $data['partner_contribution'] = 100 - $data['sheba_contribution'];
+        } elseif ($job_service->discount_percentage && $job_service->discount_percentage != "0.00") {
+            $data['discount'] = ($data['unit_price'] * $data['quantity'] * $job_service->discount_percentage) / 100;
+        }
+    }
+
+    private function saveServicePriceUpdateLog(JobService $job_service, Request $request)
+    {
+        $updated_data = [
+            'msg' => 'Service Price Updated',
+            'old_service_unit_price' => $job_service->unit_price,
+            'old_service_quantity' => $job_service->quantity,
+            'new_service_unit_price' => $request->unit_price,
+            'new_service_quantity' => $request->quantity,
+            'service_name' => $job_service->name
+        ];
+        $this->jobUpdateLog($job_service->job->id, json_encode($updated_data));
+    }
+
+    private function priceChangedNotificationData(Job $job, $old_job_price)
+    {
+        $order = $job->partnerOrder->order;
+        return [
+            "title" => "Order: " . $order->code() . " Price changed. Old Price: " . $old_job_price . ", New Price: " . $job->grossPrice,
+            "link" => env('SHEBA_FRONT_END_URL') . "/orders/" . $order->id,
+            "type" => notificationType('Info'),
+            "event_type" => 'App\Models\Order',
+            "event_id" => $order->id
+        ];
     }
 
     public function delete(JobService $job_service)
