e050c28cb273b69e55546604a0d0d8d853768fe9
arnabrahman <arnabrahman@hotmail.com>
2018-09-01 09:56:50 +0600

    partner package upgrade billing type

--- app/Models/Partner.php
+++ app/Models/Partner.php
@@ -279,10 +279,10 @@ class Partner extends Model
         $this->subscriber()->getPackage($package)->subscribe($billing_type);
     }
 
-    public function subscriptionUpgrade($package)
+    public function subscriptionUpgrade($package, $billing_type)
     {
         $package = $package ? (($package) instanceof PartnerSubscriptionPackage ? $package : PartnerSubscriptionPackage::find($package)) : $this->partner->subscription;
-        $this->subscriber()->upgrade($package);
+        $this->subscriber()->upgrade($package, $billing_type);
     }
 
     public function runSubscriptionBilling()
