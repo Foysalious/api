fb453e2bb2ef989e7b836271be3a8f24ce54ac4d
arnabrahman <arnabrahman@hotmail.com>
2018-09-13 11:31:16 +0600

    referer check during ssl

--- app/Http/Controllers/SslController.php
+++ app/Http/Controllers/SslController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Sheba\PayCharge\PayCharge;
 
@@ -10,6 +11,13 @@ class SslController extends Controller
     public function validatePaycharge(Request $request)
     {
         try {
+            if (empty($request->headers->get('referer'))) {
+                $sentry = app('sentry');
+                $sentry->user_context(['request' => $request->headers->all()]);
+                $message = 'Referer not present in header';
+                $sentry->captureException(new \Exception($message));
+                return api_response($request, null, 400, ['message' => $message]);
+            };
             $pay_charge = new PayCharge('online');
             if ($response = $pay_charge->complete($request->tran_id)) {
                 return redirect($response['redirect_url'] . '?invoice_id=' . $request->tran_id);
