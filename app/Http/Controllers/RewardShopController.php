7b1a0585c6379411127b6d12f250b6304c9adf15
Sabbir Ahmed <sabbir.sheba@gmail.com>
2018-09-17 17:45:45 +0600

    [ADD] [SHEBA-2863] # product list fetched from database

--- app/Http/Controllers/RewardShopController.php
+++ app/Http/Controllers/RewardShopController.php
@@ -1,5 +1,6 @@
 <?php namespace App\Http\Controllers;
 
+use App\Models\RewardShopProduct;
 use Illuminate\Http\Request;
 
 class RewardShopController extends Controller
@@ -7,33 +8,14 @@ class RewardShopController extends Controller
     public function index(Request $request)
     {
         try{
-            $products = collect([
-                [
-                    'id' => 1,
-                    'name' => 'Visiting Card',
-                    'image' => 'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/services_images/thumbs/1482401445_hire_electrician.png',
-                    'required_point' => 1000
-                ],
-                [
-                    'id' => 2,
-                    'name' => 'T-Shirt',
-                    'image' => 'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/services_images/thumbs/1482401445_hire_electrician.png',
-                    'required_point' => 5000
-                ],
-                [
-                    'id' => 3,
-                    'name' => 'AC',
-                    'image' => 'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/services_images/thumbs/1482401445_hire_electrician.png',
-                    'required_point' => 10000
-                ],
-                [
-                    'id' => 3,
-                    'name' => 'Bike',
-                    'image' => 'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/services_images/thumbs/1482401445_hire_electrician.png',
-                    'required_point' => 50000
-                ]
-            ]);
-
+            $products = RewardShopProduct::published()->select('id', 'name', 'thumb', 'point')->get()->map(function ($product) {
+                return [
+                    'id' => $product->id,
+                    'name' => $product->name,
+                    'image' => $product->thumb,
+                    'required_point' => $product->point
+                ];
+            });
             return api_response($request, $products, 200, ['products' => $products, 'gift_points' => $request->partner->reward_point]);
         } catch (\Throwable $e) {
             app('sentry')->captureException($e);
