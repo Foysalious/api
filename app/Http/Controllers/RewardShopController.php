8030d852ef0a53c96657418b6166adf40f313a12
Sabbir Ahmed <sabbir.sheba@gmail.com>
2018-09-17 21:02:52 +0600

    [FIX] [SHEBA-2865] # Accidental dd() removed from RewardShopController

--- app/Http/Controllers/RewardShopController.php
+++ app/Http/Controllers/RewardShopController.php
@@ -51,7 +51,6 @@ class RewardShopController extends Controller
             $sentry->captureException($e);
             return api_response($request, $message, 400, ['message' => $message]);
         } catch (\Throwable $e) {
-            dd($e);
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
         }
