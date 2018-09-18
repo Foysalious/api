d56da1d8faf99e96554841378cc8b40bc5900b81
Sabbir Ahmed <sabbir.sheba@gmail.com>
2018-09-18 11:50:49 +0600

    [REFACTOR] [MW-94] # remove 'rewardOutLog' and rename 'rewardInLog' to 'storeLog'
    - RewardLog will contain only rewards 'In' log

--- app/Sheba/Repositories/RewardLogRepository.php
+++ app/Sheba/Repositories/RewardLogRepository.php
@@ -5,24 +5,13 @@ use App\Models\RewardLog;
 
 class RewardLogRepository extends BaseRepository
 {
-    public function rewardInLog(Reward $reward, $target_id, $log = null)
+    public function storeLog(Reward $reward, $target_id, $log = null)
     {
         $data = [
             'reward_id'     => $reward->id,
             'target_type'   => $reward->target_type,
             'target_id'     => $target_id,
             'log'           => $log
-        ];
-        RewardLog::create($this->withCreateModificationField($data));
-    }
-
-    public function rewardOutLog($target_type, $target_id, $log = null)
-    {
-        $data = [
-            'target_type'       => $target_type,
-            'target_id'         => $target_id,
-            'log'               => $log,
-            'transaction_type'  => 'Out'
         ];
         RewardLog::create($this->withCreateModificationField($data));
     }
