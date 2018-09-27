9cfd4797b82226779e7820d86862b5d548f83b90
Sabbir Ahmed <sabbir.sheba@gmail.com>
2018-09-27 18:18:30 +0600

    [UPDATE] # Make process_date in 24 hours format for GraphQL

--- app/GraphQL/Type/OrderType.php
+++ app/GraphQL/Type/OrderType.php
@@ -209,7 +209,7 @@ class OrderType extends GraphQlType
         $not_cancelled_job = $root->jobs->first();
         if ($not_cancelled_job && $not_cancelled_job->statusChangeLogs) {
             $process_log = $not_cancelled_job->statusChangeLogs->where('to_status', constants('JOB_STATUSES')['Process'])->first();
-            if ($process_log) return $process_log->created_at->format('Y-m-d h:i:s');
+            if ($process_log) return $process_log->created_at->format('Y-m-d H:i:s');
         }
         return null;
     }
