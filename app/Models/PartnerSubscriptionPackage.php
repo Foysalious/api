54fa38f92b2d69a063b660a687a2889d917ebaa3
Sabbir Ahmed <sabbir.sheba@gmail.com>
2018-10-03 22:13:27 +0600

    [FIX] [SHEBA-2904] # Return discount amount, if discount is not percentage

--- app/Models/PartnerSubscriptionPackage.php
+++ app/Models/PartnerSubscriptionPackage.php
@@ -38,7 +38,7 @@ class PartnerSubscriptionPackage extends Model implements SubscriptionPackage
         if ($running_discount = $this->runningDiscount($billing_type)) {
             if (in_array($billing_cycle, json_decode($running_discount->applicable_billing_cycles))) {
                 if ($running_discount->is_percentage) return $this->originalPrice($billing_type) * $running_discount->amount;
-                else $running_discount->amount;
+                else return $running_discount->amount;
             }
         } else {
             return 0;
