225193835875473660a6882c501dbb392dbb7180
Sabbir Ahmed <sabbir.sheba@gmail.com>
2018-10-04 19:32:07 +0600

    [FIX] [SHEBA-2904] # Last Requested Package Name Issue Fixed

--- app/Models/PartnerSubscriptionUpdateRequest.php
+++ app/Models/PartnerSubscriptionUpdateRequest.php
@@ -18,4 +18,19 @@ class PartnerSubscriptionUpdateRequest extends Model
     {
         return $query->where('partner_id', $partner_id);
     }
+
+    public function oldPackage()
+    {
+        return $this->hasOne(PartnerSubscriptionPackage::class, 'id', 'old_package_id');
+    }
+
+    public function newPackage()
+    {
+        return $this->hasOne(PartnerSubscriptionPackage::class, 'id', 'new_package_id');
+    }
+
+    public function Partner()
+    {
+        return $this->belongsTo(Partner::class);
+    }
 }
