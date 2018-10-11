e54d67f002fbbd5cad6cb59169a4d5b4a53677e6
Hasan Hafiz Pasha <mach.pasha@gmail.com>
2018-10-11 11:52:26 +0600

    [UPDATE] # Push notificaton - sound tag for wallet warning

--- app/Http/Controllers/ResourceScheduleController.php
+++ app/Http/Controllers/ResourceScheduleController.php
@@ -40,6 +40,6 @@ class ResourceScheduleController extends Controller
             "event_id" => (string)$job->partner_order->id,
             "action" => 'extend_time',
             "version" => $job->partner_order->getVersion(),
-        ], $topic, $channel, $sound);
+        ], $topic, $channel);
     }
 }
