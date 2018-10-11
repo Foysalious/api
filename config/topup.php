6bbc1f8d17a63580e614ede45c278884963ff962
arnabrahman <arnabrahman@hotmail.com>
2018-10-11 15:14:07 +0600

    [FIX] Topup recharge url from config

--- config/topup.php
+++ config/topup.php
@@ -6,6 +6,7 @@ return [
         'url' => env('ROBI_TOPUP_URL', 'http://202.134.12.103:9898/pretups/C2SReceiver'),
         'login_id' => env('ROBI_TOPUP_LOGIN', 'pretups'),
         'password' => env('ROBI_TOPUP_PASSWORD', 'pretups123'),
+        'gateway_code' => env('ROBI_TOPUP_GATEWAY_CODE', 'EXTGW'),
         'robi_pin' => env('ROBI_TOPUP_ROBI_PIN', '1972'),
         'airtel_pin' => env('ROBI_TOPUP_AIRTEL_PIN', '1972'),
         'robi_mid' => env('ROBI_TOPUP_ROBI_MID', '01849011359'),
