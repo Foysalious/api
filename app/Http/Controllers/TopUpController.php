bf4d9782deffe76d8ea0f8d00e21389cdb4a32d4
Hasan Hafiz Pasha <mach.pasha@gmail.com>
2018-10-18 17:26:02 +0600

    [REMOVE] # topup sentry

--- app/Http/Controllers/TopUpController.php
+++ app/Http/Controllers/TopUpController.php
@@ -53,9 +53,6 @@ class TopUpController extends Controller
             else return api_response($request, null, 500, ['message' => "Recharge Unsuccessful"]);
         } catch (ValidationException $e) {
             $message = getValidationErrorMessage($e->validator->errors()->all());
-            $sentry = app('sentry');
-            $sentry->user_context(['request' => $request->all(), 'message' => $message]);
-            $sentry->captureException($e);
             return api_response($request, $message, 400, ['message' => $message]);
         } catch (\Throwable $e) {
             app('sentry')->captureException($e);
