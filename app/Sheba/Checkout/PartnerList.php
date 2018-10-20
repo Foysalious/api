ebc3f601c4b1b0a6822405aff42a1319808d1f50
arnabrahman <arnabrahman@hotmail.com>
2018-10-20 15:28:39 +0600

    [ADD] SHEBA-2953 partner address

--- app/Sheba/Checkout/PartnerList.php
+++ app/Sheba/Checkout/PartnerList.php
@@ -259,6 +259,7 @@ class PartnerList
             $partner['total_jobs_of_category'] = $partner->jobs->first() ? $partner->jobs->first()->total_jobs_of_category : 0;
             $partner['contact_no'] = $this->getContactNumber($partner);
             $partner['subscription_type'] = $partner->subscription ? $partner->subscription->name : null;
+            $partner['address'] = $partner->address;
         }
     }
 
