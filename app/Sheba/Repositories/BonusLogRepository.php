d6f1f90df344737234b8ffc7e93d5fbc3a549fbf
Hasan Hafiz Pasha <mach.pasha@gmail.com>
2018-10-31 14:11:05 +0600

    [UPDATE] # Bonus log store on cash type bonus reward event

--- app/Sheba/Repositories/BonusLogRepository.php
+++ app/Sheba/Repositories/BonusLogRepository.php
@@ -7,8 +7,34 @@ class BonusLogRepository extends BaseRepository
     /**
      * @param $data
      */
-    public function store($data)
+    public function storeCreditLog($data)
+    {
+        $data['type'] = 'Credit';
+        $this->save($data);
+    }
+
+    /**
+     * @param $data
+     */
+    public function storeDebitLog($data)
+    {
+        $data['type'] = 'Debit';
+        $this->save($data);
+    }
+
+    /**
+     * @param $data
+     */
+    private function save($data)
     {
         BonusLog::create($this->withCreateModificationField($data));
+    }
+
+    public function insert($data)
+    {
+        foreach ($data as $key => $item) {
+            $data[$key] = $this->withCreateModificationField($item);
+        }
+        BonusLog::insert($data);
     }
 }
