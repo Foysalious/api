c59f44e3ed1e617643853383224369add8d80567
arnabrahman <arnabrahman@hotmail.com>
2018-11-01 15:08:29 +0600

    [Remove] dd

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -431,7 +431,6 @@ class JobController extends Controller
             $sentry->captureException($e);
             return api_response($request, $message, 400, ['message' => $message]);
         } catch (\Throwable $e) {
-            dd($e);
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
         }
