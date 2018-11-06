7ba6fb168a7bebecffc16c23f69b96bf6ccebbcc
Saiful Isalm <islam42555@gmail.com>
2018-11-06 20:51:37 +0600

    customer_favorite order detail

--- app/Http/Controllers/CustomerOrderController.php
+++ app/Http/Controllers/CustomerOrderController.php
@@ -37,7 +37,7 @@ class CustomerOrderController extends Controller
                 }
                 $q->with(['partnerOrders' => function ($q) use ($filter, $offset, $limit) {
                     $q->with(['partner.resources.profile', 'order' => function ($q) {
-                        $q->select('id', 'sales_channel');
+                        $q->select('id', 'sales_channel', 'favorite_id');
                     }, 'jobs' => function ($q) {
                         $q->with(['statusChangeLogs', 'resource.profile', 'jobServices', 'customerComplains', 'category', 'review' => function ($q) {
                             $q->select('id', 'rating', 'job_id');
@@ -132,6 +132,7 @@ class CustomerOrderController extends Controller
             'readable_status' => constants('JOB_STATUSES_SHOW')[$job->status]['customer'],
             'status' => $job->status,
             'is_on_premise' => (int) $job->isOnPremise(),
+            'customer_favorite' => $partnerOrder->order->favorite_id ? : null,
             'isRentCar' => $job->isRentCar(),
             'status_color' => constants('JOB_STATUSES_COLOR')[$job->status]['customer'],
             'partner_name' => $partnerOrder->partner->name,
