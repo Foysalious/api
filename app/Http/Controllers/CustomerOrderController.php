c1b42d9cd07ccbbde9c7bca9e16c39e0ee26e6e7
Saiful Isalm <islam42555@gmail.com>
2018-11-08 15:10:50 +0600

    customer favorite id

--- app/Http/Controllers/CustomerOrderController.php
+++ app/Http/Controllers/CustomerOrderController.php
@@ -131,7 +131,7 @@ class CustomerOrderController extends Controller
             'readable_status' => constants('JOB_STATUSES_SHOW')[$job->status]['customer'],
             'status' => $job->status,
             'is_on_premise' => (int) $job->isOnPremise(),
-            'customer_favorite' => empty($job->customerFavorite) ? 0 : 1,
+            'customer_favorite' => empty($job->customerFavorite) ? 0 : $job->customerFavorite->id,
             'isRentCar' => $job->isRentCar(),
             'status_color' => constants('JOB_STATUSES_COLOR')[$job->status]['customer'],
             'partner_name' => $partnerOrder->partner->name,
