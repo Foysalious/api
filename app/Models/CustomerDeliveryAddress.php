a10db379ab32ebb424e6d81ab589e64320d2a56f
Shafiqul Islam <shafiq.startern@gmail.com>
2018-11-13 17:14:06 +0600

    [ADD] [SHEBA-3097] # soft delete to deleivery address model

--- app/Models/CustomerDeliveryAddress.php
+++ app/Models/CustomerDeliveryAddress.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CustomerDeliveryAddress extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'customer_delivery_addresses';
 
     protected $fillable = ['name', 'address'];
@@ -14,6 +17,8 @@ class CustomerDeliveryAddress extends Model
     {
         return $this->belongsTo(Customer::class);
     }
+
+    protected $dates = ['deleted_at'];
 
     public function location()
     {
