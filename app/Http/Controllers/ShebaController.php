a785791123b0ccb4749c82fec26437061522dfc5
arnabrahman <arnabrahman@hotmail.com>
2018-11-14 11:30:23 +0600

    [FIX]eshop business slider query param

--- app/Http/Controllers/ShebaController.php
+++ app/Http/Controllers/ShebaController.php
@@ -66,7 +66,7 @@ class ShebaController extends Controller
     {
         try {
             $images = Slider::select('id', 'image_link', 'small_image_link', 'target_link', 'target_type', 'target_id');
-            if ($request->has('business') && $request->business) {
+            if ($request->has('is_business') && (int)$request->is_business) {
                 $images = $images->showBusiness()->map(function ($image) {
                     $image['target_type'] = $image['target_type'] ? explode('\\', $image['target_type'])[2] : null;
                     return $image;
