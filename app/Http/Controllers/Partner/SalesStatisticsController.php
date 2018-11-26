fa6f217e082067d437ebf3c518699cf34f137c46
Hasan Hafiz Pasha <mach.pasha@gmail.com>
2018-11-26 22:03:35 +0600

    [UPDATE] # Sales Stat - sales graph brackdown data fix

--- app/Http/Controllers/Partner/SalesStatisticsController.php
+++ app/Http/Controllers/Partner/SalesStatisticsController.php
@@ -27,16 +27,44 @@ class SalesStatisticsController extends Controller
                 $data['timeline'] = 'Oct 26 - Nov 1';
                 $data['sheba_payable'] = 4832.56;
                 $data['partner_collection'] = 483.56;
-                $data['sales_stat_breakdown'] = ['Sun' => 455.58, 'Mon' => 4552, 'Tue' => 45005, 'Wed' => 4505, 'Thu' => 455, 'Fri' => 4550, 'Sat' => 4550];
-                $data['order_stat_breakdown'] = ['Sun' => 455, 'Mon' => 4552, 'Tue' => 45005, 'Wed' => 4505, 'Thu' => 455, 'Fri' => 4550, 'Sat' => 4550];
+                $data['sales_stat_breakdown'] = [
+                    ['value' => 'Sun', 'amount' => 455.58],
+                    ['value' => 'Mon', 'amount' => 4552],
+                    ['value' => 'Tue', 'amount' => 45005],
+                    ['value' => 'Wed', 'amount' => 4505,],
+                    ['value' => 'Thu', 'amount' => 455],
+                    ['value' => 'Fri', 'amount' => 4550],
+                    ['value' => 'Sat', 'amount' => 455]
+                ];
+                $data['order_stat_breakdown'] = [
+                    ['value' => 'Sun', 'amount' => 455],
+                    ['value' => 'Mon', 'amount' => 4552],
+                    ['value' => 'Tue', 'amount' => 45005],
+                    ['value' => 'Wed', 'amount' => 4505],
+                    ['value' => 'Thu', 'amount' => 455],
+                    ['value' => 'Fri', 'amount' => 4550],
+                    ['value' => 'Sat', 'amount' => 455]
+                ];
             }
 
             if ($request->frequency == 'month') {
                 $data['timeline'] = 'October';
                 $data['sheba_payable'] = 4832.56;
                 $data['partner_collection'] = 483.56;
-                $data['sales_stat_breakdown'] = ['1' => 11.2, '2' => 11, '3' => 11.55, '4' => 11, '5' => 11, '6' => 11, '7' => 11, '8' => 11, '9' => 11, '10' => 11, '11' => 11, '12' => 11, '13' => 11, '14' => 11, '15' => 11, '16' => 11, '17' => 11, '18' => 11, '19' => 11, '20' => 11, '21' => 11, '22' => 11, '23' => 11, '24' => 11, '25' => 11, '26' => 113.56, '27' => 11, '28' => 11, '29' => 11, '30' => 11, '31' => 11.22];
-                $data['order_stat_breakdown'] = ['1' => 11, '2' => 11, '3' => 11, '4' => 11, '5' => 11, '6' => 11, '7' => 11, '8' => 11, '9' => 11, '10' => 11, '11' => 11, '12' => 11, '13' => 11, '14' => 11, '15' => 11, '16' => 11, '17' => 11, '18' => 11, '19' => 11, '20' => 11, '21' => 11, '22' => 11, '23' => 11, '24' => 11, '25' => 11, '26' => 113.56, '27' => 11, '28' => 11, '29' => 11, '30' => 11, '31' => 11.22];
+                $data['sales_stat_breakdown'] = [
+                    ['value' => 1, 'amount' => 11.22],
+                    ['value' => 2, 'amount' => 1121],
+                    ['value' => 3, 'amount' => 112.2],
+                    ['value' => 4, 'amount' => 11],
+                    ['value' => 5, 'amount' => 11]
+                ];
+                $data['order_stat_breakdown'] = [
+                    ['value' => 1, 'amount' => 10],
+                    ['value' => 2, 'amount' => 22],
+                    ['value' => 3, 'amount' => 11],
+                    ['value' => 4, 'amount' => 111],
+                    ['value' => 5, 'amount' => 101]
+                ];
             }
 
             if ($request->frequency == 'year') {
