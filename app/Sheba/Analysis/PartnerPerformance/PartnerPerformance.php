fba4f4324d2dfc9376125ff96012e689ed260821
Shafiqul Islam <shafiq.startern@gmail.com>
2018-11-27 18:45:20 +0600

    [WIP] [SHEBA-3163] dummy api

--- app/Sheba/Analysis/PartnerPerformance/PartnerPerformance.php
+++ app/Sheba/Analysis/PartnerPerformance/PartnerPerformance.php
@@ -30,210 +30,81 @@ class PartnerPerformance
 
     public function get()
     {
+        $completed = $this->getDataOf('successfully_completed');
+        $complain = $this->getDataOf('successfully_completed');
+        $timely_accepted = $this->getDataOf('successfully_completed');
+        $timely_processed = $this->getDataOf('successfully_completed');
+
         return [
-            'score' => 3.5,
+            'score' => ($completed['rate'] + $complain['rate'] + $timely_accepted['rate'] + $timely_processed['rate']) / 4,
             'performance_summary' => [
                 'total_order_taken' => 51,
-                'successfully_completed' => 39,
-                'order_without_complain' => 30,
-                'timely_order_taken' => 46,
-                'timely_job_start' => 15
+                'successfully_completed' => $completed['total_order'],
+                'order_without_complain' => $complain['total_order'],
+                'timely_order_taken' => $timely_accepted['total_order'],
+                'timely_job_start' => $timely_processed['total_order']
             ],
-            'successfully_completed' => [
-                'total_order' => 24,
-                'rate' => 49,
-                'last_week_rate' => 34,
-                'is_improved' => 1,
-                'last_week_rate_difference' => 15,
-                'last_weeks' => [
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ]
-                ]
+            'successfully_completed' => $completed,
+            'order_without_complain' => $complain,
+            'timely_order_taken' => $timely_accepted,
+            'timely_job_start' => $timely_processed
+        ];
+    }
+
+    private function getDataOf($of)
+    {
+        return [
+            'total_order' => 24,
+            'rate' => 49,
+            'last_week_rate' => 34,
+            'is_improved' => 1,
+            'last_week_rate_difference' => 15,
+            'previous_weeks' => $this->getPreviousWeeksData($of)
+        ];
+    }
+
+    private function getPreviousWeeksData($of)
+    {
+        return [
+            [
+                'name' => 'Week 41',
+                'date_range' => [
+                    'start' => '2018-11-11',
+                    'end' => '2018-11-18'
+                ],
+                'value' => '12',
             ],
-            'order_without_complain' => [
-                'total_order' => 30,
-                'rate' => 60,
-                'last_week_rate' => 54,
-                'is_improved' => 1,
-                'last_week_rate_difference' => 6,
-                'last_weeks' => [
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ]
-                ]
+            [
+                'name' => 'Week 41',
+                'date_range' => [
+                    'start' => '2018-11-11',
+                    'end' => '2018-11-18'
+                ],
+                'value' => '12',
             ],
-            'timely_order_taken' => [
-                'total_order' => 46,
-                'rate' => 93,
-                'last_week_rate' => 95,
-                'is_improved' => 0,
-                'last_week_rate_difference' => 2,
-                'last_weeks' => [
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ]
-                ]
+            [
+                'name' => 'Week 41',
+                'date_range' => [
+                    'start' => '2018-11-11',
+                    'end' => '2018-11-18'
+                ],
+                'value' => '12',
             ],
-            'timely_job_start' => [
-                'total_order' => 15,
-                'rate' => 30,
-                'last_week_rate' => 47,
-                'is_improved' => 0,
-                'last_week_rate_difference' => 17,
-                'last_weeks' => [
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ],
-                    [
-                        'name' => 'Week 41',
-                        'date_range' => [
-                            'start' => '2018-11-11',
-                            'end' => '2018-11-18'
-                        ],
-                        'value' => '12',
-                    ]
-                ]
+            [
+                'name' => 'Week 41',
+                'date_range' => [
+                    'start' => '2018-11-11',
+                    'end' => '2018-11-18'
+                ],
+                'value' => '12',
+            ],
+            [
+                'name' => 'Week 41',
+                'date_range' => [
+                    'start' => '2018-11-11',
+                    'end' => '2018-11-18'
+                ],
+                'value' => '12',
             ]
         ];
     }
