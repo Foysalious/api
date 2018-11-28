18017fc1ebfda585807ba79ee5203f7dc1752018
arnabrahman <arnabrahman@hotmail.com>
2018-11-28 12:58:28 +0600

    [REMOVE] dd

--- app/Sheba/Checkout/PartnerList.php
+++ app/Sheba/Checkout/PartnerList.php
@@ -141,7 +141,6 @@ class PartnerList
     {
         $this->partners = $this->findPartnersByService($partner_id);
         $this->partners->load('locations');
-        dd($this->partners->first()->locations->pluck('id'));
         return $this->partners->filter(function ($partner) {
             return $partner->locations->where('id', $this->location)->count() > 0;
         });
