4dd4ca4d643cbd262b1f3faaa3cbb0f5be3b76c7
Shafiqul Islam <shafiq.startern@gmail.com>
2018-12-01 17:35:06 +0000

    [FIX] missing function

--- app/Sheba/Helpers/TimeFrame.php
+++ app/Sheba/Helpers/TimeFrame.php
@@ -72,18 +72,23 @@ class TimeFrame
         return $this;
     }
 
-    public function forSomeWeekFromNow($week = 1, $week_start = null, $week_end = null)
+    public function forAWeek(Carbon $date, $week_start = null, $week_end = null)
     {
-        if($week == 0) return $this->forCurrentWeek($week_start);
-        else if($week > 0) $date = Carbon::today()->addWeeks($week);
-        else $date = Carbon::today()->subWeeks(abs($week));
-
         Carbon::setWeekStartsAt($week_start ?: Carbon::SUNDAY);
         Carbon::setWeekEndsAt($week_end ?: Carbon::SATURDAY);
 
         $this->start = $date->copy()->startOfWeek();
         $this->end = $date->endOfWeek();
         return $this;
+    }
+
+    public function forSomeWeekFromNow($week = 1, $week_start = null, $week_end = null)
+    {
+        if($week == 0) return $this->forCurrentWeek($week_start);
+        else if($week > 0) $date = Carbon::today()->addWeeks($week);
+        else $date = Carbon::today()->subWeeks(abs($week));
+
+        return $this->forAWeek($date);
     }
 
     public function hasDateBetween(Carbon $date)
