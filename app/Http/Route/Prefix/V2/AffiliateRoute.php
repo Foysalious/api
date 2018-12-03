9781ec15e6d6daf0925fdbd9a65c9f4186474553
Sakib Rahaman <sakib.rahaman@sheba.xyz>
2018-12-03 13:56:57 +0600

    [ADD] # Customer Delivery Address Api For Affiliate

--- app/Http/Route/Prefix/V2/AffiliateRoute.php
+++ app/Http/Route/Prefix/V2/AffiliateRoute.php
@@ -18,6 +18,7 @@ class AffiliateRoute
             $api->get('top-up/validate/ssl', 'SslController@validateTopUp');
             $api->get('top-up/ssl/balance', 'SslController@checkBalance');
             $api->get('top-up/history', 'AffiliateController@topUpHistory');
+            $api->get('customer-delivery-address', 'CustomerDeliveryAddressController@getDeliveryInfoForAffiliate');
         });
         $api->get('affiliates/faq', 'FaqController@getAffiliateFaqs');
     }
