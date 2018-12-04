f3f539c6d36c09d2f21af2aaebdfe4c4c1e745f1
Sakib Rahaman <sakib.rahaman@sheba.xyz>
2018-12-04 18:08:57 +0600

    [UPDATE] # Top Up Response Test

--- app/Sheba/TopUp/TopUp.php
+++ app/Sheba/TopUp/TopUp.php
@@ -77,6 +77,7 @@ class TopUp
         $topUpOrder->save();
 
         $commission = new CommissionFactory();
+        dd($commission->getByName($topUpOrder->agent_type));
         $commission->getByName($topUpOrder->agent_type)->setAgent($this->agent)->setTopUpOrder($topUpOrder)->setTopUpVendor($this->model)->disburse();
     }
 
