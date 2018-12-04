048498e169ab8473089a1ac3f42b310a46f3704d
irtezaasad <irtezaasad.sheba@gmail.com>
2018-12-04 16:56:37 +0600

    [UPDATE] customer info based on mobile 404 error updated

--- app/Http/Controllers/AffiliateController.php
+++ app/Http/Controllers/AffiliateController.php
@@ -576,7 +576,7 @@ class AffiliateController extends Controller
 
                 return api_response($request, $customer_name, 200, ['name' => $customer_name]);
             }
-            return api_response($request, [], 404,['addresses'=>[]]);
+            return api_response($request, [], 404, ['message' => 'User not found.']);
         }catch (ValidationException $e) {
             $message = getValidationErrorMessage($e->validator->errors()->all());
             return api_response($request, $message, 400, ['message' => $message]);
