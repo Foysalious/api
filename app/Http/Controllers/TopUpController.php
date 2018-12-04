d6292d8aa5739a503a3ee6b44ce2703d61595822
Sakib Rahaman <sakib.rahaman@sheba.xyz>
2018-12-04 20:48:55 +0600

    [UPDATE] # Top Up Response Test

--- app/Http/Controllers/TopUpController.php
+++ app/Http/Controllers/TopUpController.php
@@ -78,6 +78,7 @@ class TopUpController extends Controller
             $message = getValidationErrorMessage($e->validator->errors()->all());
             return api_response($request, $message, 400, ['message' => $message]);
         } catch (\Throwable $e) {
+            dd($e);
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
         }
