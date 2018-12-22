f031ebd52eb04e7e95ead4b7e0269ae0381dda88
Tonmoy <saddam.hossain@sheba.xyz>
2018-12-22 13:48:56 +0600

    [UPDATE] #partnerLocations

--- app/Models/HyperLocal.php
+++ app/Models/HyperLocal.php
@@ -26,15 +26,13 @@ class HyperLocal extends Eloquent
 
     public function scopeInsideCircle($query, $geo_info)
     {
-        return $query->where('geometry', 'geoWithin', [
-            '$centerSphere' => [
-                [
-                    (double)$geo_info->lng,
-                    (double)$geo_info->lat
-                ],
-                (double)$geo_info->radius * 1000
-//                / 6378.16
-            ]
+        return $query->where('geometry', 'nearSphere', [
+            '$geometry' => [
+                'type' => "Point",
+                'coordinates' => [(double)$geo_info->lng, (double)$geo_info->lat]
+            ],
+            '$minDistance' => 0,
+            '$maxDistance' => (double)($geo_info->radius * 1000) / 6378.16
         ]);
     }
 }
