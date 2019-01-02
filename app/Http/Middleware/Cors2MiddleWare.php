a3d4570101c255d839b6fb41156ab97dda2763f9
arnabrahman <arnabrahman@hotmail.com>
2019-01-02 16:08:02 +0600

    [ADD] cbl live link port in cors

--- app/Http/Middleware/Cors2MiddleWare.php
+++ app/Http/Middleware/Cors2MiddleWare.php
@@ -58,6 +58,7 @@ class Cors2MiddleWare
             "https://securepay.sslcommerz.com",
             "https://sandbox.thecitybank.com:4443",
             "https://epay.thecitybank.com:7788",
+            "https://epay.thecitybank.com:443",
             "http://bondhu.sheba.xyz",
             "https://bondhu.sheba.xyz",
             "https://api.sheba.xyz",
