087a9ad5fbd5b1c8fef9cc672b4c9fe136c0bb70
Sakib Rahaman <sakib.rahaman@sheba.xyz>
2019-01-02 19:41:26 +0600

    [UPDATE]# Eksheba Save Route

--- app/Http/Route/Prefix/V2/AffiliateRoute.php
+++ app/Http/Route/Prefix/V2/AffiliateRoute.php
@@ -23,6 +23,7 @@ class AffiliateRoute
             $api->get('customer-info', 'AffiliateController@getCustomerInfo');
             $api->post('top-up-test', 'TopUpController@topUpTest');
         });
+        $api->post('eksheba/save','EkshebaController@saveEkshebaData');
         $api->get('affiliates/faq', 'FaqController@getAffiliateFaqs');
     }
 }
