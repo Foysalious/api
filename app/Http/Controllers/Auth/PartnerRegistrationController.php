773682250803498324c98c51398304f0aae872f8
Sakib Rahaman <sakib.rahaman@sheba.xyz>
2019-01-17 16:28:22 +0600

    [FIX]# Register by resource, save resource name

--- app/Http/Controllers/Auth/PartnerRegistrationController.php
+++ app/Http/Controllers/Auth/PartnerRegistrationController.php
@@ -95,8 +95,9 @@ class PartnerRegistrationController extends Controller
                 return api_response($request, null, 403, ['message' => "Unauthorized."]);
             }
             $profile = $resource->profile;
-
-            if ($resource->partnerResources->count() > 0) return api_response($request, null, 403, ['message' => 'You already have a company!']);
+            $profile->name = $request->name;
+            $profile->save();
+            //if ($resource->partnerResources->count() > 0) return api_response($request, null, 403, ['message' => 'You already have a company!']);
 
             $data = $this->makePartnerCreateData($request);
             if ($partner = $this->createPartner($resource, $data)) {
