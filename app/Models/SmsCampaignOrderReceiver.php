d240c78e43ef5038c4c4d389030c7591305d8db7
Hasan Hafiz Pasha <mach.pasha@gmail.com>
2019-02-16 20:19:44 +0600

    [ADD][REFACTOR] # REFUND MOVE TO SEPARATE CLASS

--- app/Models/SmsCampaignOrderReceiver.php
+++ app/Models/SmsCampaignOrderReceiver.php
@@ -10,16 +10,4 @@ class SmsCampaignOrderReceiver extends Model
     {
         return $this->belongsTo(SmsCampaignOrder::class);
     }
-
-    public function refundIfFailed()
-    {
-        $amount_to_be_deducted = $this->sms_count * constants('SMS_CAMPAIGN.rate_per_sms');
-
-        $this->smsCampaignOrder->partner->creditWallet($amount_to_be_deducted);
-        $this->smsCampaignOrder->partner->walletTransaction([
-            'amount' => $amount_to_be_deducted,
-            'type' => 'Credit',
-            'log' => $amount_to_be_deducted . "BDT. has been credited for failing to sent a message in a campaign you have created"
-        ]);
-    }
 }
