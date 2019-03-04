fad3b3cb3bfbb3c53fe9ed36f3baef8ea4e0bf5e
miajee <saiful.sheba@gmail.com>
2019-03-04 19:20:58 +0600

    typo

--- app/Http/Controllers/Partner/CustomerSubscriptionController.php
+++ app/Http/Controllers/Partner/CustomerSubscriptionController.php
@@ -154,7 +154,7 @@ class CustomerSubscriptionController extends Controller
 
                 'customer_name' => $subscription_order->customer->profile->name,
                 'customer_mobile' => $subscription_order->customer->profile->mobile,
-                #'address' => $subscription_order->deliveryAddress->address,
+                'address' => $subscription_order->deliveryAddress->address,
                 'location_name' => $subscription_order->location->name,
                 #'ordered_for' => $subscription_order->deliveryAddress->name,
 
