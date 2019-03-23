6fa31275b09af338899966fc41c93202ce0bbccb
arnabrahman <arnabrahman@hotmail.com>
2019-03-23 15:54:17 +0600

    [ONGOING] SHEBA-3457 Vendor TopUp https://blabol.atlassian.net/browse/SHEBA-3457

--- app/Http/Route/Prefix/V1/Route.php
+++ app/Http/Route/Prefix/V1/Route.php
@@ -5,7 +5,7 @@ class Route
     public function set($api)
     {
         $api->group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers'], function ($api) {
-            $api->group(['prefix' => 'vendors','middleware' => ['vendor.auth']], function ($api) {
+            $api->group(['prefix' => 'vendors', 'middleware' => ['vendor.auth']], function ($api) {
                 $api->get('times', 'Vendor\ShebaController@getTimes');
                 $api->get('categories', 'Vendor\CategoryController@index');
                 $api->get('categories/{category}/secondaries', 'Vendor\CategoryController@get');
@@ -15,6 +15,8 @@ class Route
                 $api->get('orders/{order}/bills', 'Vendor\OrderController@getBills');
                 $api->post('orders', 'Vendor\OrderController@placeOrder');
                 $api->get('locations', 'Vendor\LocationController@index');
+//                $api->get('topup', 'TopUpController@topUp');
+                $api->post('top-up-test', 'TopUpController@topUpTest');
             });
             $api->get('categories', ['uses' => 'CategoryController@index']);
             $api->get('categories/{category}/secondaries', ['uses' => 'CategoryController@get']);
