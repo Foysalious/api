ac784ad87de37b0567b62e6acceb84bc16ebfb3a
miajee <saiful.sheba@gmail.com>
2019-03-26 12:32:47 +0600

    [ADD]  # Service Request (Info- call)  List

--- app/Http/Controllers/InfoCallController.php
+++ app/Http/Controllers/InfoCallController.php
@@ -11,21 +11,19 @@ class InfoCallController extends Controller
     {
         try {
             $customer = $request->customer;
-
-            $info = [
-                [
-                    'service_name' => 'ABC',
-                    'created_at' => 'March 20, 2019',
-                    'status' => 'In Progress',
-                ],
-                [
-                    'service_name' => 'XYZ',
-                    'created_at' => 'March 20, 2019',
-                    'status' => 'In Progress',
-
-                ],
-            ];
-            return api_response($request, $info, 200, ['info' => $info]);
+            $info_call_lists = collect([]);
+            foreach ($customer->infoCalls as $info_call) {
+                $info = [
+                    'id' => $info_call->id,
+                    'code' => $info_call->code(),
+                    'service_name' => $info_call->service_name,
+                    'status' => $info_call->status,
+                    'created_at' => $info_call->created_at->format('F j, Y'),
+                    #'created' => $info_call->created_at->format('d F Y'),
+                ];
+                $info_call_lists->push($info);
+            }
+            return api_response($request, $info_call_lists, 200, ['info_call_lists' => $info_call_lists]);
         } catch (\Throwable $e) {
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
@@ -39,11 +37,12 @@ class InfoCallController extends Controller
 
             $details = [
                 [
+                    'id' => 1,
                     'code' => '#A-9068947',
                     'created_at' => '18 March, 9.30AM',
                     'status' => 'In Progress',
                     'service_name' => 'ABC',
-                    'budget' => '1000',
+                    'estimated_budget' => '1000',
                 ]
 
             ];
