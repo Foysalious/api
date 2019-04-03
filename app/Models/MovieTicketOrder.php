4c9e8c0de4b06951edf0dd3ef5470793fa6570fc
Hasan Hafiz Pasha <pasha.startern@gmail.com>
2019-04-03 22:09:40 +0600

    [UPDATE] # TYPO

--- app/Models/MovieTicketOrder.php
+++ app/Models/MovieTicketOrder.php
@@ -1,12 +1,11 @@
-<?php
-
-namespace App\Models;
+<?php namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class MovieTicketOrder extends Model
 {
     protected $guarded = ['id'];
+
     public function isFailed()
     {
         return $this->status == 'Failed';
