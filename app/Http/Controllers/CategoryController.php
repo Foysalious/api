2d928bb4fc78ee589b33582065f505d8d4c0141a
miajee <saiful.sheba@gmail.com>
2019-04-10 13:05:40 +0600

    typo

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -167,10 +167,10 @@ class CategoryController extends Controller
 
 
             $best_deal_category = CategoryGroupCategory::where('category_group_id', self::BESTDEALID)->pluck('category_id')->toArray();
-            #dd(implode(',', $best_deal_category));
+            #dd(implode(',', $best_deal_category)->toArray());
             if ($location) {
                 $children = $category->load(['children' => function ($q) use ($best_deal_category, $location) {
-                    $q->whereNotIn('id', implode(',', $best_deal_category))
+                    $q->whereNotIn('id',  $best_deal_category)
                         ->whereHas('locations', function ($q) use ($location) {
                         $q->where('locations.id', $location->id);
                     });
