3f6a65ff753efd03c92d8d40d4f357584b760fa5
miajee <saiful.sheba@gmail.com>
2019-04-17 19:32:01 +0600

    [ADD]  # reorder Service group

--- app/Models/ServiceGroupService.php
+++ app/Models/ServiceGroupService.php
@@ -1,10 +1,10 @@
-<?php
-
-namespace App\Models;
+<?php namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class ServiceGroupService extends Model
 {
     protected $table = 'service_group_service';
+    protected $fillable = ['service_group_id', 'service_id', 'order'];
+    public $timestamps = false;
 }
