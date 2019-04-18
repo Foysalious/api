c7c2c8b3dd1ce1342c343f2d7369499fc7e38360
Hasan Hafiz Pasha <pasha.startern@gmail.com>
2019-04-18 20:43:25 +0600

    [UPDATE] # ORDER PLACEMENT MODEL UPDATE

--- app/Models/PosOrder.php
+++ app/Models/PosOrder.php
@@ -5,10 +5,35 @@ use Illuminate\Database\Eloquent\Model;
 class PosOrder extends Model
 {
     protected $guarded = ['id'];
+    /**
+     * @var int
+     */
+    private $totalServicePrice;
+    private $itemDiscounts;
+    /**
+     * @var int|number
+     */
+    private $totalPrice;
+    private $totalDiscount;
+    private $appliedDiscount;
+    /**
+     * @var float
+     */
+    private $grossAmount;
+    /**
+     * @var bool
+     */
+    private $isCalculated;
 
     public function calculate()
     {
+        $this->_calculateThisItems();
+        $this->totalDiscount = $this->itemDiscounts + $this->discount;
+        $this->appliedDiscount = (double)($this->totalDiscount > $this->amount) ? $this->amount : $this->discount;
+        $this->grossAmount = floatValFormat($this->totalPrice - $this->appliedDiscount);
+        $this->isCalculated = true;
 
+        return $this->_formatAllToTaka();
     }
 
     public function customer()
@@ -24,5 +49,40 @@ class PosOrder extends Model
     public function payments()
     {
         return $this->hasMany(PosOrderPayment::class);
+    }
+
+    private function _calculateThisItems()
+    {
+        $this->_initializeTotalsToZero();
+        foreach ($this->items as $item) {
+            /** @var PosOrderItem $item */
+            $item = $item->calculate();
+            $this->_updateTotalPriceAndCost($item);
+        }
+        return $this;
+    }
+
+    private function _initializeTotalsToZero()
+    {
+        $this->totalServicePrice = 0;
+    }
+
+    private function _updateTotalPriceAndCost(PosOrderItem $item)
+    {
+        $this->totalServicePrice += $item->servicePrice;
+        $this->totalPrice += $item->grossPrice;
+        $this->itemDiscounts += $item->discount;
+    }
+
+    private function _formatAllToTaka()
+    {
+        $this->totalDiscount = formatTaka($this->totalDiscount);
+        $this->appliedDiscount = formatTaka($this->appliedDiscount);
+        $this->grossAmount = formatTaka($this->grossAmount);
+        $this->totalServicePrice = formatTaka($this->totalServicePrice);
+        $this->totalPrice = formatTaka($this->totalPrice);
+        $this->itemDiscounts = formatTaka($this->itemDiscounts);
+
+        return $this;
     }
 }
--- app/Models/PosOrderItem.php
+++ app/Models/PosOrderItem.php
@@ -5,6 +5,23 @@ use Illuminate\Database\Eloquent\Model;
 class PosOrderItem extends Model
 {
     protected $guarded = ['id'];
+    /**
+     * @var number
+     */
+    private $servicePrice;
+    private $serviceDiscounts;
+    /**
+     * @var int|number
+     */
+    private $grossPrice;
+    /**
+     * @var number
+     */
+    private $unitPrice;
+    /**
+     * @var bool
+     */
+    private $isCalculated;
 
     public function order()
     {
@@ -14,5 +31,15 @@ class PosOrderItem extends Model
     public function service()
     {
         return $this->belongsTo(PartnerPosService::class);
+    }
+
+    public function calculate()
+    {
+        $this->servicePrice = formatTaka(($this->unit_price * $this->quantity));
+        $this->grossPrice = ($this->servicePrice > $this->discount) ? formatTaka($this->servicePrice - $this->discount) : 0;
+        $this->unitPrice = formatTaka($this->unit_price);
+        $this->isCalculated = true;
+
+        return $this;
     }
 }
