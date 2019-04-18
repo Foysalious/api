70988804a8b11ff6168a9941aa32d722932d7a89
arnabrahman <arnabrahman@hotmail.com>
2019-04-18 12:37:03 +0600

    [UPDATE] stock left

--- app/Http/Controllers/OfferGroupController.php
+++ app/Http/Controllers/OfferGroupController.php
@@ -65,7 +65,6 @@ class OfferGroupController extends Controller
             $message = getValidationErrorMessage($e->validator->errors()->all());
             return api_response($request, $message, 400, ['message' => $message]);
         } catch (\Throwable $e) {
-            dd($e);
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
         }
