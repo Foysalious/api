21f3c50c1e47c440f6955b9489e37884f9f48ee3
miajee <saiful.sheba@gmail.com>
2019-04-21 15:54:07 +0600

    [ADD]  #SignUp B2B

--- app/Http/Controllers/B2b/RegistrationController.php
+++ app/Http/Controllers/B2b/RegistrationController.php
@@ -23,15 +23,23 @@ class RegistrationController extends Controller
         try {
             $this->validate($request, [
                 'name' => 'required|string',
-                #'email' => 'required|email|unique:profiles',
+                'email' => 'required|email',
                 'password' => 'required|min:4',
                 'mobile' => 'required|string|mobile:bd',
             ]);
             $mobile = formatMobile($request->mobile);
-            $profile = $this->profileRepository->ifExist($mobile, 'mobile');
+            $email = $request->email;
+            $m_profile = $this->profileRepository->ifExist($mobile, 'mobile');
+            $e_profile = $this->profileRepository->ifExist($email, 'email');
 
-
-            if (!$profile) {
+            if ($m_profile && $e_profile) {
+                if ($m_profile->id == $e_profile->id)
+                    $member = $this->makeMember($m_profile);
+            } elseif ($m_profile) {
+                return api_response($request, null, 400, ['message' => 'Mobile already exists! Please login']);
+            } elseif ($e_profile) {
+                return api_response($request, null, 400, ['message' => 'Email already exists! Please login']);
+            } else {
                 $data = [
                     'mobile' => $mobile,
                     'name' => $request->name,
@@ -39,10 +47,7 @@ class RegistrationController extends Controller
                     'password' => bcrypt($request->password)
                 ];
                 $profile = $this->profileRepository->store($data);
-            } else {
                 $member = $this->makeMember($profile);
-                dd($member);
-                return api_response($request, null, 400, ['message' => 'Mobile already exists! Please login']);
             }
 
             return $profile ? api_response($request, $profile, 200, ['profile' => $profile]) : api_response($request, null, 404);
@@ -54,7 +59,6 @@ class RegistrationController extends Controller
             $sentry->captureException($e);
             return api_response($request, $message, 400, ['message' => $message]);
         } catch (\Throwable $e) {
-            dd($e);
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
         }
@@ -70,8 +74,4 @@ class RegistrationController extends Controller
         return $member;
     }
 
-    private function makeBussiness($member)
-    {
-
-    }
 }
