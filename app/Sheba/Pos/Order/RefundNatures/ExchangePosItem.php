bee58c140084ff95af4787c88cbb0d4106506d27
Hasan Hafiz Pasha <pasha.startern@gmail.com>
2019-04-25 16:08:34 +0600

    [UPDATE] # EXCHANGE LOGS

--- app/Sheba/Pos/Order/RefundNatures/ExchangePosItem.php
+++ app/Sheba/Pos/Order/RefundNatures/ExchangePosItem.php
@@ -1,5 +1,6 @@
 <?php namespace Sheba\Pos\Order\RefundNatures;
 
+use App\Models\PosOrder;
 use Sheba\Pos\Log\Supported\Types;
 use Sheba\Pos\Order\Creator;
 
@@ -7,12 +8,14 @@ class ExchangePosItem extends RefundNature
 {
     /** @var false|string */
     private $details;
+    /** @var PosOrder $old_order */
+    private $newOrder;
 
     public function update()
     {
         $creator = app(Creator::class);
         $this->data['previous_order_id'] = $this->order->id;
-        $creator->setData($this->data)->create();
+        $this->newOrder = $creator->setData($this->data)->create();
 
         $this->generateDetails();
         $this->saveLog();
@@ -32,15 +35,10 @@ class ExchangePosItem extends RefundNature
      */
     protected function generateDetails()
     {
-        /*$changes = [];
-        $this->services->each(function ($service) use (&$changes) {
-            $changes[$service->id]['qty'] = [
-                'new' => (double)$service->quantity,
-                'old' => (double)$this->old_services[$service->id],
-            ];
-        });
-        $details['orders']['changes'] = $changes;*/
-        $details = "";
+        $details['orders']['changes'] = [
+            'new' => $this->newOrder,
+            'old' => $this->order
+        ];
         $this->details = json_encode($details);
     }
 }
