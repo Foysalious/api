06978a24ccdb384860c8c03d43c58a6de1d8ac05
Hasan Hafiz Pasha <pasha.startern@gmail.com>
2019-05-08 19:48:20 +0600

    [ADD] # BEARER TOKEN ADD ON AUTHENTICATION

--- app/Sheba/Transport/Bus/ClientCalls/BdTickets.php
+++ app/Sheba/Transport/Bus/ClientCalls/BdTickets.php
@@ -3,6 +3,7 @@
 use Exception;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Redis;
 use Psr\Http\Message\ResponseInterface;
 
 class BdTickets extends ExternalApiClient
@@ -11,21 +12,26 @@ class BdTickets extends ExternalApiClient
     protected $baseUrl;
     protected $apiVersion;
     protected $bookingPort;
+    private $authorizationPort;
+    private $bearerToken;
 
     /**
      * BdTickets constructor.
      */
     public function __construct()
     {
-        $token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIrODgwMTY3ODI0MjkzNCIsImF1ZCI6InVua25vd24iLCJleHAiOjE1NTg2MDU2OTEsInVzZXIiOnsiaWQiOiI1YzcyM2Q0YTVhNzlmYjNmMDgyYmFlOTYiLCJyb2xlSWRlbnRpZmllcnMiOlsiUk9MRV9DVVNUT01FUiIsIlJPTEVfQVBJX0FHRU5UIl0sImZpcnN0TmFtZSI6IlNoZWJhIiwiZ2l2ZW5OYW1lIjoiLnh5eiIsImFsdEZpcnN0TmFtZSI6bnVsbCwiYWx0R2l2ZW5OYW1lIjpudWxsLCJnZW5kZXIiOm51bGwsInBob25lTnVtYmVyIjoiKzg4MDE2NzgyNDI5MzQiLCJzZWNvbmRhcnlQaG9uZU51bWJlciI6bnVsbCwiZW1haWwiOiJ0cEBzaGViYS54eXoiLCJwYXNzd29yZFVwZGF0ZWQiOjE1NTA5OTExNjMsImxvY2FsZSI6IkVOIiwiYWRkcmVzcyI6bnVsbCwic3RhdHVzIjoiUEhPTkVfVkVSSUZJQ0FUSU9OX05FRURFRCIsImVtYWlsVmVyaWZpY2F0aW9uU3RhdHVzIjoiUEVORElORyIsInVzZXJHcm91cERpc3BsYXkiOm51bGwsInVzZXJHcm91cHMiOm51bGwsImZpZWxkQWdlbnREZXRhaWxzIjpudWxsLCJhcHBEZXRhaWxzIjpudWxsLCJjb3Jwb3JhdGVDdXN0b21lckRldGFpbHMiOm51bGwsImZ1bGxOYW1lIjoiU2hlYmEgLnh5eiIsImFjY291bnROb25FeHBpcmVkIjp0cnVlLCJjcmVkZW50aWFsc05vbkV4cGlyZWQiOnRydWUsImFjY291bnROb25Mb2NrZWQiOnRydWUsIm5hbWVPclVzZXJOYW1lIjoiU2hlYmEgLnh5eiIsInVzZXJOaWNrIjoiU2hlYmEiLCJhdXRob3JpdGllcyI6WyJCT09LSU5HX0NSRUFURSIsIk9SREVSX0NSRUFURSIsIlNVUFBPUlRfVElDS0VUX0NSRUFURSIsIlNVUFBPUlRfVElDS0VUX0NSRUFURV9TRUxGIiwiQk9PS0lOR19DUkVBVEVfU0VMRiIsIkJPT0tJTkdfVVBEQVRFX1NFTEYiLCJCT09LSU5HX1ZJRVdfU0VMRiIsIkJPT0tJTkdfTElTVF9TRUxGIiwiQ0FSVF9DUkVBVEVfU0VMRiIsIkNBUlRfVVBEQVRFX1NFTEYiLCJDQVJUX0RFU1RST1lfU0VMRiIsIkNBUlRfVklFV19TRUxGIiwiQ0FSVF9MSVNUX1NFTEYiLCJPUkRFUl9DUkVBVEVfU0VMRiIsIk9SREVSX1VQREFURV9TRUxGIiwiTk9USUZJQ0FUSU9OX0NSRUFURSIsIk9SREVSX0RFU1RST1lfU0VMRiIsIk9SREVSX0xJU1RfU0VMRiIsIlBBWU1FTlRHQVRFV0FZX0JLQVNIX0FMTE9XIiwiUEFZTUVOVEdBVEVXQVlfUE9SVFdBTExFVF9BTExPVyIsIlBBWU1FTlRfUFVSQ0hBU0VfU0VMRiIsIlBBWU1FTlRfUkVDSEFSR0VfU0VMRiIsIlNVUFBPUlRfVElDS0VUX0xJU1RfU0VMRiIsIlNVUFBPUlRfVElDS0VUX1ZJRVdfU0VMRiIsIkFDQ09VTlRTX0NIRUNLX0JBTEFOQ0VfU0VMRiIsIkFDQ09VTlRTX1ZJRVdfVFJBTlNBQ1RJT05TX1NFTEYiLCJPUkRFUl9WSUVXX1NFTEYiLCJPUkRFUl9TRUFSQ0giLCJQUklDSU5HX1NIT1ciLCJPUkRFUl9WSUVXIiwiT1JERVJfTElTVCIsIk9SREVSX1VQREFURSIsIkJPT0tJTkdfVklFVyIsIkJPT0tJTkdfTElTVCIsIkJPT0tJTkdfVVBEQVRFIiwiVVNFUl9WSUVXIiwiQ0hFQ0tfQktBU0hfVFJBTlNBQ1RJT04iLCJCT09LSU5HX0NSRUFURV9PVEhFUlMiLCJCT09LSU5HX0RFU1RST1lfT1RIRVJTIiwiQk9PS0lOR19MSVNUX09USEVSUyIsIkJPT0tJTkdfVVBEQVRFX09USEVSUyIsIkJPT0tJTkdfVklFV19PVEhFUlMiLCJSRUZVTkRfUkVRVUVTVF9DUkVBVEUiXX0sImlhdCI6MTU1NzMwOTY5MX0.DeBTgtspxmB9codB6Hxg3SHVDpMvrfsNiUS3ERk-loDiiRKk5ynqaL5vWEkh1UCW4zpqeR6RQacSsww2P48f0g";
-        $this->client = (new Client(['headers' => [
-            'Content-Type' => 'application/json',
-            'Authorization' => 'Bearer ' . $token,
-        ]]));
-
+        $token = Redis::get('bdticket_bearer_token');
         $this->baseUrl = config('bus_transport.bdticket.base_url');
         $this->apiVersion = config('bus_transport.bdticket.api_version');
         $this->bookingPort = config('bus_transport.bdticket.booking_port');
+        $this->authorizationPort = config('bus_transport.bdticket.authorization_port');
+        $this->client = (new Client(['headers' => ['Content-Type' => 'application/json']]));
+
+        if (!$token) {
+            $token = $this->generateToken();
+            Redis::set('bdticket_bearer_token', $token);
+        }
+        $this->bearerToken = $token;
     }
 
     /**
@@ -86,8 +92,19 @@ class BdTickets extends ExternalApiClient
     {
         $options = [];
         // if (!is_null($data)) $options['form_params'] = $data;
+        $options['headers'] = ['Authorization' => 'Bearer ' . $this->bearerToken];
         $options['body'] = json_encode( $data);
 
         return $options;
+    }
+
+    private function generateToken()
+    {
+        $url    = $this->baseUrl . ':' . $this->authorizationPort . "/" . $this->apiVersion . "/auth";
+        $data   = ['email' => config('bus_transport.bdticket.login_email'), 'password' => config('bus_transport.bdticket.login_pass')];
+        $res    = $this->client->request('POST', $url, $this->getOptions($data));
+        $res    = json_decode($res->getBody()->getContents(), true);
+
+        return $res['data']['access']['token'];
     }
 }
