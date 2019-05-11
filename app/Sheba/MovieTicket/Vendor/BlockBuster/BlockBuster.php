e2696d8747e72b20db339be663e632381f6b8ed3
Hasan Hafiz Pasha <pasha.startern@gmail.com>
2019-05-11 13:56:27 +0600

    [UPDATE] # TYPO

--- app/Sheba/MovieTicket/Vendor/BlockBuster/BlockBuster.php
+++ app/Sheba/MovieTicket/Vendor/BlockBuster/BlockBuster.php
@@ -2,6 +2,7 @@
 
 use App\Models\MovieTicketVendor;
 use App\Models\MovieTicketVendorCommission;
+use Exception;
 use GuzzleHttp\Client;
 use Sheba\MovieTicket\Actions;
 use Sheba\MovieTicket\MovieTicket;
@@ -12,6 +13,7 @@ use Sheba\MovieTicket\TransactionGenerator;
 use Sheba\MovieTicket\Vendor\BlockBuster\KeyEncryptor;
 use GuzzleHttp\Exception\GuzzleException;
 use Sheba\MovieTicket\Vendor\Vendor;
+use SimpleXMLElement;
 
 class BlockBuster extends Vendor
 {
@@ -43,7 +45,7 @@ class BlockBuster extends Vendor
     }
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function init()
     {
@@ -58,28 +60,28 @@ class BlockBuster extends Vendor
     /**
      * @param $action
      * @return string
-     * @throws \Exception
+     * @throws Exception
      */
     public function generateURIForAction($action, $params = [])
     {
         switch ($action) {
             case Actions::GET_MOVIE_LIST:
-                $api_url = $this->apiUrl.'movie_list_running.php';
+                $api_url = $this->apiUrl . 'movie_list_running.php';
                 break;
             case Actions::GET_THEATRE_LIST:
-                $api_url =  $this->apiUrl.'movie_schedule.php';
+                $api_url = $this->apiUrl . 'movie_schedule.php';
                 break;
             case Actions::GET_THEATRE_SEAT_STATUS:
-                $api_url =  $this->apiUrl.'movie_schedule_theatre_seat_status.php';
+                $api_url = $this->apiUrl . 'movie_schedule_theatre_seat_status.php';
                 break;
             case Actions::REQUEST_MOVIE_TICKET_SEAT:
-                $api_url =  $this->apiUrl.'movie_seat_booking_request.php';
+                $api_url = $this->apiUrl . 'movie_seat_booking_request.php';
                 break;
             case Actions::UPDATE_MOVIE_SEAT_STATUS:
-                $api_url =  $this->apiUrl.'movie_ticket_confirm.php';
+                $api_url = $this->apiUrl . 'movie_ticket_confirm.php';
                 break;
             default:
-                throw new \Exception('Invalid Action');
+                throw new Exception('Invalid Action');
                 break;
         }
         return $api_url;
@@ -88,7 +90,7 @@ class BlockBuster extends Vendor
     private function addParamsToUrl($url, $params)
     {
         foreach ($params as $key => $value) {
-            $url .='&'.$key.'='.$value;
+            $url .= '&' . $key . '=' . $value;
         }
         return $url;
     }
@@ -98,18 +100,13 @@ class BlockBuster extends Vendor
      * @return MovieResponse
      * @throws GuzzleException
      */
-    function buyTicket(MovieTicketRequest $movieTicketRequest): MovieResponse
+    public function buyTicket(MovieTicketRequest $movieTicketRequest): MovieResponse
     {
         $this->init();
         $blockbuster_response = new BlockBusterResponse();
-        $response = $this->post(Actions::UPDATE_MOVIE_SEAT_STATUS,[
-            'trx_id' => $movieTicketRequest->getTrxId(),
-            'DTMSID'=>$movieTicketRequest->getDtmsId(),
-            'ticket_id'=>$movieTicketRequest->getTicketId(),
-            'ConfirmStatus'=>$movieTicketRequest->getConfirmStatus(),
-        ]);
+        $response = $this->post(Actions::UPDATE_MOVIE_SEAT_STATUS, ['trx_id' => $movieTicketRequest->getTrxId(), 'DTMSID' => $movieTicketRequest->getDtmsId(), 'ticket_id' => $movieTicketRequest->getTicketId(), 'ConfirmStatus' => $movieTicketRequest->getConfirmStatus(),]);
         $response->place = 'Blockbuster Movies, Jamuna Future Park';
-        $response->image_url =$movieTicketRequest->getImageUrl();
+        $response->image_url = $movieTicketRequest->getImageUrl();
         $blockbuster_response->setResponse($response);
         return $blockbuster_response;
     }
@@ -117,49 +114,49 @@ class BlockBuster extends Vendor
     /**
      * @param $action
      * @param array $params
-     * @return \SimpleXMLElement
+     * @return SimpleXMLElement
      * @throws GuzzleException
-     * @throws \Exception
+     * @throws Exception
      */
     public function get($action, $params = [])
     {
         try {
             $response = $this->httpClient->request('GET', $this->generateURIForAction($action, $params));
             $body = $response->getBody()->getContents();
-            return $this->isJson($body) ? $body :$this->parse($body);
+            return $this->isJson($body) ? $body : $this->parse($body);
         } catch (GuzzleException $e) {
             throw $e;
-        } catch (\Exception $e) {
+        } catch (Exception $e) {
             throw $e;
         }
     }
+
     /**
      * @param $action
      * @param array $body
      * @return mixed
      * @throws GuzzleException
-     * @throws \Exception
+     * @throws Exception
      */
     public function post($action, $body = [])
     {
         $body['username'] = $this->userName;
-        if(!isset($body['trx_id']))
-            $body['trx_id'] = 'SHEBA'.rand(0,32200);
-        if(isset($body['MovieID'])){
+        if (!isset($body['trx_id'])) $body['trx_id'] = 'SHEBA' . rand(0, 32200);
+        if (isset($body['MovieID'])) {
             $body['MovieID'] = $this->parseMovieIdToBlockBusterFormat($body['MovieID']);
         }
         try {
-            $ch_tt = curl_init($this->generateURIForAction($action,[]));
+            $ch_tt = curl_init($this->generateURIForAction($action, []));
             $post_data = json_encode($body);
             $header = $this->getHeaders();
             curl_setopt($ch_tt, CURLOPT_HTTPHEADER, $header);
             curl_setopt($ch_tt, CURLOPT_CUSTOMREQUEST, "POST");
             curl_setopt($ch_tt, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch_tt, CURLOPT_POSTFIELDS,$post_data);
+            curl_setopt($ch_tt, CURLOPT_POSTFIELDS, $post_data);
             curl_setopt($ch_tt, CURLOPT_FOLLOWLOCATION, 1);
             $result_tt = curl_exec($ch_tt);
             curl_close($ch_tt);
-            $result_tt=json_decode($result_tt);
+            $result_tt = json_decode($result_tt);
             return $this->formatResponse($action, $result_tt);
         } catch (GuzzleException $e) {
             throw $e;
@@ -168,18 +165,14 @@ class BlockBuster extends Vendor
 
     private function getHeaders()
     {
-        return array(
-            'Content-Type: application/json',
-            'secretecode: '.$this->secretCode,
-            'apikey: '.$this->apiKey
-        );
+        return array('Content-Type: application/json', 'secretecode: ' . $this->secretCode, 'apikey: ' . $this->apiKey);
     }
 
     /**
      * @param $action
      * @param $response
      * @return mixed|null
-     * @throws \Exception
+     * @throws Exception
      */
     private function formatResponse($action, $response)
     {
@@ -191,16 +184,16 @@ class BlockBuster extends Vendor
                 return $this->getTheatreListResponse($response);
                 break;
             case Actions::GET_THEATRE_SEAT_STATUS:
-                return$this->getTheatreSeatStatusResponse($response);
+                return $this->getTheatreSeatStatusResponse($response);
                 break;
             case Actions::REQUEST_MOVIE_TICKET_SEAT:
-                return$this->bookTicketResponse($response);
+                return $this->bookTicketResponse($response);
                 break;
             case Actions::UPDATE_MOVIE_SEAT_STATUS:
                 return $this->updateMovieTicketStatus($response);
                 break;
             default:
-                throw new \Exception('Invalid Action');
+                throw new Exception('Invalid Action');
                 break;
         }
     }
@@ -208,147 +201,110 @@ class BlockBuster extends Vendor
     /**
      * @param $response
      * @return mixed
-     * @throws \Exception
+     * @throws Exception
      */
     private function getMovieListResponse($response)
     {
-        if($response->api_validation && $response->api_validation->status==="ok") {
-            if($response->api_response->status === "ok")
-            {
-                $movies =  $response->api_response->movie_list;
-                foreach ($movies as  $index => $movie) {
-                    $data = [
-                        "id" => (int) $movie->MovieID,
-                        "MovieID" => $movie->MovieID,
-                        "MovieName" => $movie->MovieName,
-                        "DirName" => $movie->DirName,
-                        "ReleaseDate" => $movie->ReleaseDate,
-                        "MovieStartDate" => $movie->MovieStartDate,
-                        "MovieEndDate" => $movie->MovieEndDate,
-                        "MovieType" =>$movie->MovieType,
-                        "MovieStatus" => $movie->MovieStatus,
-                        "MovieTrailer" => $movie->MovieTrailer,
-                        "Status" =>$movie->Status,
-                        "Banner" => $movie->Banner,
-                        "BannerSmall" => $movie->BannerSmall
-                    ];
+        if ($response->api_validation && $response->api_validation->status === "ok") {
+            if ($response->api_response->status === "ok") {
+                $movies = $response->api_response->movie_list;
+                foreach ($movies as $index => $movie) {
+                    $data = ["id" => (int)$movie->MovieID, "MovieID" => $movie->MovieID, "MovieName" => $movie->MovieName, "DirName" => $movie->DirName, "ReleaseDate" => $movie->ReleaseDate, "MovieStartDate" => $movie->MovieStartDate, "MovieEndDate" => $movie->MovieEndDate, "MovieType" => $movie->MovieType, "MovieStatus" => $movie->MovieStatus, "MovieTrailer" => $movie->MovieTrailer, "Status" => $movie->Status, "Banner" => $movie->Banner, "BannerSmall" => $movie->BannerSmall];
                     $movies[$index] = $data;
                 }
                 return $movies;
             }
             return null;
         }
-        throw new \Exception('Server error');
+        throw new Exception('Server error');
     }
 
     /**
      * @param $response
      * @return mixed
-     * @throws \Exception
+     * @throws Exception
      */
     private function getTheatreListResponse($response)
     {
-        if($response && $response->api_validation && $response->api_validation->status === "ok"){
-            if($response->api_response->status === "ok") {
+        if ($response && $response->api_validation && $response->api_validation->status === "ok") {
+            if ($response->api_response->status === "ok") {
                 $schedules = $response->api_response->movie_schedule;
                 foreach ($schedules as $index => $schedule) {
-                    $data = [
-                        "id" => (int) $schedule->MovieID,
-                        "MovieID"=>  $schedule->MovieID,
-                        "MovieName" => $schedule->MovieName,
-                        "RequestDate"=> $schedule->RequestDate,
-                        "DTMID"=> $schedule->DTMID,
-                        "TheatreName"=> $schedule->TheatreName,
-                        "ShowTime"=> $schedule->ShowTime,
-                        "Slot"=> $schedule->Slot
-                    ];
+                    $data = ["id" => (int)$schedule->MovieID, "MovieID" => $schedule->MovieID, "MovieName" => $schedule->MovieName, "RequestDate" => $schedule->RequestDate, "DTMID" => $schedule->DTMID, "TheatreName" => $schedule->TheatreName, "ShowTime" => $schedule->ShowTime, "Slot" => $schedule->Slot];
                     $schedules[$index] = $data;
                 }
                 return $schedules;
             }
             return null;
         }
-        throw new \Exception('Server error');
+        throw new Exception('Server error');
     }
 
     /**
      * @param $response
      * @return null
-     * @throws \Exception
+     * @throws Exception
      */
     private function getTheatreSeatStatusResponse($response)
     {
-        if($response && $response->api_validation && $response->api_validation->status === "ok"){
-            if($response->api_response->status === "ok")
-            {
+        if ($response && $response->api_validation && $response->api_validation->status === "ok") {
+            if ($response->api_response->status === "ok") {
                 $seatStatus = $response->api_response->movie_schedule_theatre_seat_status;
-                $seat_classes = explode("|",$seatStatus->SeatClass);
-                $seat_classes_default = ['E_FRONT','E_REAR'];
-                $seat_prices = explode("|",$seatStatus->SeatClassTicketPrice);
+                $seat_classes = explode("|", $seatStatus->SeatClass);
+                $seat_classes_default = ['E_FRONT', 'E_REAR'];
+                $seat_prices = explode("|", $seatStatus->SeatClassTicketPrice);
 
                 $seats = array();
-                foreach($seat_classes_default as $index => $seat_class) {
-                    $key_of_total_seats = 'Total_'.str_replace("-","_",$seat_class).'_Seat';
-                    $key_of_available_seats = str_replace("-","_",$seat_class).'_Available_Seat';
-                    $original_price = round((float) $seat_prices[$index],2);
+                foreach ($seat_classes_default as $index => $seat_class) {
+                    $key_of_total_seats = 'Total_' . str_replace("-", "_", $seat_class) . '_Seat';
+                    $key_of_available_seats = str_replace("-", "_", $seat_class) . '_Available_Seat';
+                    $original_price = round((float)$seat_prices[$index], 2);
                     $total_price = $this->priceAfterShebaCommission($original_price);
-                    $seat = array(
-                        'class' => $seat_classes[$index],
-                        'price' => $total_price ,
-                        'total_seats' => (int) $seatStatus->{$key_of_total_seats},
-                        'available_seats' => (int) $seatStatus->{$key_of_available_seats}
-                    );
-                    array_push($seats,$seat);
+                    $seat = array('class' => $seat_classes[$index], 'price' => $total_price, 'total_seats' => (int)$seatStatus->{$key_of_total_seats}, 'available_seats' => (int)$seatStatus->{$key_of_available_seats});
+                    array_push($seats, $seat);
                 }
-                $status = array(
-                    'dtmsid' => $seatStatus->DTMSID,
-                    'dtmid' => $seatStatus->DTMID,
-                    'seats' => $seats
-                );
+                $status = array('dtmsid' => $seatStatus->DTMSID, 'dtmid' => $seatStatus->DTMID, 'seats' => $seats);
                 return $status;
-            }
-            else return $response->api_response;
+            } else return $response->api_response;
         }
-        throw new \Exception('Server error');
+        throw new Exception('Server error');
     }
 
     /**
      * @param $response
      * @return mixed
-     * @throws \Exception
+     * @throws Exception
      */
     private function bookTicketResponse($response)
     {
-        if($response && $response->api_validation && $response->api_validation->status === "ok"){
-            if($response->api_response->status === "ok") {
+        if ($response && $response->api_validation && $response->api_validation->status === "ok") {
+            if ($response->api_response->status === "ok") {
                 $response = $response->api_response->movie_seat_booking_request;
                 $response->cost = $this->priceAfterShebaCommission($response->cost);
                 return $response;
-            }
-            else
+            } else
                 return $response->api_response;
         }
-        throw new \Exception('Server error');
+        throw new Exception('Server error');
     }
 
     /**
      * @param $response
-     * @throws \Exception
+     * @return
+     * @throws Exception
      */
     private function updateMovieTicketStatus($response)
     {
-        if($response && $response->api_validation && $response->api_validation->status === "ok") {
-            if($response->api_response->status === "ok")
-                return $response->api_response->ticket_confirm_status;
-            else
+        if ($response && $response->api_validation && $response->api_validation->status === "ok") {
+            if ($response->api_response->status === "ok") return $response->api_response->ticket_confirm_status; else
                 return $response->api_response;
         }
-        throw new \Exception('Server error');
+        throw new Exception('Server error');
     }
 
     private function priceAfterShebaCommission($original_price)
     {
-        $price_without_sheba_commission = round((float) $original_price,2);
+        $price_without_sheba_commission = round((float)$original_price, 2);
         $sheba_commission = $this->shebaCommission($price_without_sheba_commission);
         $total_price = $price_without_sheba_commission + $sheba_commission;
         $total_price = ceil($total_price);
@@ -358,7 +314,7 @@ class BlockBuster extends Vendor
     private function shebaCommission($price)
     {
         $sheba_commission_percentage = $this->shebaCommissionPercentage();
-        return $price * ( $sheba_commission_percentage / 100);
+        return $price * ($sheba_commission_percentage / 100);
     }
 
     private function shebaCommissionPercentage()
