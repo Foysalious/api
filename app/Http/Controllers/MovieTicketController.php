28ccf65485bbb511fe4c969af6363e85a902203c
Hasan Hafiz Pasha <pasha.startern@gmail.com>
2019-05-12 20:01:12 +0600

    [UPDATE] # MOVIE TICKET ORDER DETAILS MODEL CALCULATE

--- app/Http/Controllers/MovieTicketController.php
+++ app/Http/Controllers/MovieTicketController.php
@@ -159,14 +159,21 @@ class MovieTicketController extends Controller
         try {
             $agent = $this->getAgent($request);
             $orders = MovieTicketOrder::where('agent_type', get_class($agent))->where('agent_id', $agent->id)->orderBy('created_at', 'desc')->get();
-            $histories = array();
+            $histories = [];
             foreach ($orders as $order) {
                 $reservation_details = json_decode($order->reservation_details);
                 if (isset($reservation_details->MovieName)) {
-                    $history = array('id' => $order->id, 'movie_title' => $reservation_details->MovieName, 'show_date' => $reservation_details->ShowDate, 'show_time' => $reservation_details->ShowTime, 'quantity' => $reservation_details->quantity, 'reserver_mobile' => $order->reserver_mobile, 'image_url' => isset($reservation_details->image_url) ? $reservation_details->image_url : null);
+                    $history = [
+                        'id' => $order->id,
+                        'movie_title' => $reservation_details->MovieName,
+                        'show_date' => $reservation_details->ShowDate,
+                        'show_time' => $reservation_details->ShowTime,
+                        'quantity' => $reservation_details->quantity,
+                        'reserver_mobile' => $order->reserver_mobile,
+                        'image_url' => isset($reservation_details->image_url) ? $reservation_details->image_url : null
+                    ];
                     array_push($histories, $history);
                 }
-
             }
             return api_response($request, $orders, 200, ['history' => $histories]);
         } catch (Throwable $e) {
@@ -178,10 +185,12 @@ class MovieTicketController extends Controller
     public function historyDetails($affiliate, $order, Request $request)
     {
         try {
-            $order = MovieTicketOrder::find((int)$order);
+            /** @var MovieTicketOrder $order */
+            $order = MovieTicketOrder::find((int)$order)->calculate();
+            $order->amount = (string)$order->getNetBill();
             removeRelationsAndFields($order);
             $order->reservation_details = json_decode($order->reservation_details);
-            $order->reservation_details->cost = round($order->amount);
+            $order->reservation_details->cost = round($order->getNetBill());
             return api_response($request, $order, 200, ['details' => $order]);
         } catch (Throwable $e) {
             app('sentry')->captureException($e);
