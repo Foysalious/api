3c902a5fbdd2c38da4362a2e4b9ed6eac8dfdf38
miajee <saiful.sheba@gmail.com>
2019-05-16 15:06:53 +0600

    [ADD]  # Form Template list

--- app/Http/Controllers/B2b/FormTemplateController.php
+++ app/Http/Controllers/B2b/FormTemplateController.php
@@ -1,12 +1,14 @@
 <?php namespace App\Http\Controllers\B2b;
 
-
 use App\Http\Controllers\Controller;
+use Sheba\ModificationFields;
+use App\Models\FormTemplate;
 use Illuminate\Http\Request;
 
 
 class FormTemplateController extends Controller
 {
+    use ModificationFields;
 
     public function store(Request $request)
     {
@@ -41,5 +43,30 @@ class FormTemplateController extends Controller
             ]
         ];
         return api_response($request, null, 200, ['form_template' => $data]);
+    }
+
+    public function index($business, Request $request)
+    {
+        try {
+            $business = $request->business;
+            $member = $request->manager_member;
+            $this->setModifier($member);
+            $form_templates = FormTemplate::where('owner_id', $business->id)->published()->orderBy('id', 'DESC')->get();
+            $templates = [];
+            foreach ($form_templates as $template) {
+
+                $template = [
+                    'id' => $template->id,
+                    'title' => $template->title,
+                    'long_description' => $template->long_description,
+                ];
+                array_push($templates, $template);
+            }
+            if (count($templates) > 0) return api_response($request, $templates, 200, ['templates' => $templates]);
+            else  return api_response($request, null, 404);
+        } catch (\Throwable $e) {
+            app('sentry')->captureException($e);
+            return api_response($request, null, 500);
+        }
     }
 }
