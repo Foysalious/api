252ab7709853aacd3d0b52ef4c6071195e224454
Hasan Hafiz Pasha <pasha.startern@gmail.com>
2019-05-21 12:09:14 +0600

    [FIX] # BOARDING POINT NULL CHECK

--- app/Sheba/Transport/Bus/Generators/VehicleList.php
+++ app/Sheba/Transport/Bus/Generators/VehicleList.php
@@ -78,7 +78,7 @@ class VehicleList
             $vehicles = $data['data'];
             foreach ($vehicles as  $vehicle) {
                 $vehicle = (object) $vehicle;
-                if ($vehicle->endCounter == null || $vehicle->arrivalTime == null) continue;
+                if (!$vehicle->endCounter || !$vehicle->arrivalTime || !$vehicle->boardingPoints || !$vehicle->droppingPoints) continue;
 
                 $start_time =  $vehicle->departureTime;
                 $end_time = $vehicle->arrivalTime;
