9611ae4849d6ee709f48d073525469740b4e792a
miajee <saiful.sheba@gmail.com>
2019-05-22 13:12:34 +0600

    [ADD]   # add comments and Issue

--- app/Models/InspectionItemIssue.php
+++ app/Models/InspectionItemIssue.php
@@ -16,4 +16,9 @@ class InspectionItemIssue extends Model
     {
         return $this->morphMany(Attachment::class, 'attachable');
     }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
 }
