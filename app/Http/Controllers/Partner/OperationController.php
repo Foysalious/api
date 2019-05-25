0358191dccf0cb7fe530cd611b90633208f22e16
Hasan Hafiz Pasha <pasha.startern@gmail.com>
2019-05-25 16:39:02 +0600

    [UPDATE] # rent a car - set base quantity and base prices

--- app/Http/Controllers/Partner/OperationController.php
+++ app/Http/Controllers/Partner/OperationController.php
@@ -60,16 +60,8 @@ class OperationController extends Controller
     public function store($partner, Request $request)
     {
         try {
-            $this->validate($request, [
-                'address' => "sometimes|required|string",
-                'locations' => "sometimes|required",
-                'working_schedule' => "sometimes|required",
-                'is_home_delivery_available' => "sometimes|required",
-                'is_on_premise_available' => "sometimes|required",
-                'delivery_charge' => "sometimes|required",
-            ]);
-            if (($request->has('is_home_delivery_available') && !$request->is_home_delivery_available) &&
-                ($request->has('is_on_premise_available') && !$request->is_on_premise_available)) {
+            $this->validate($request, ['address' => "sometimes|required|string", 'locations' => "sometimes|required", 'working_schedule' => "sometimes|required", 'is_home_delivery_available' => "sometimes|required", 'is_on_premise_available' => "sometimes|required", 'delivery_charge' => "sometimes|required",]);
+            if (($request->has('is_home_delivery_available') && !$request->is_home_delivery_available) && ($request->has('is_on_premise_available') && !$request->is_on_premise_available)) {
                 return api_response($request, null, 400, ['message' => "You have to select at least one delivery option"]);
             }
 
@@ -98,31 +90,14 @@ class OperationController extends Controller
                 if ($request->has('address')) $partner_info['address'] = $request->address;
                 if ($request->has('lat') && $request->has('lng')) {
                     $old_geo_informations = $partner->geo_informations;
-                    $partner_info['geo_informations'] = json_encode([
-                        'lat' => $request->lat,
-                        'lng' => $request->lng,
-                        'radius' => json_decode($partner->geo_informations)->radius ?: 1,
-                        //'radius' => $request->has('radius') ? ($request->radius / 1000) : (json_decode($partner->geo_informations)->radius ?: 1)
+                    $partner_info['geo_informations'] = json_encode(['lat' => $request->lat, 'lng' => $request->lng, 'radius' => json_decode($partner->geo_informations)->radius ?: 1,//'radius' => $request->has('radius') ? ($request->radius / 1000) : (json_decode($partner->geo_informations)->radius ?: 1)
                     ]);
 
-                    $geo_change_log_data = [
-                        'old_geo_informations' => $old_geo_informations,
-                        'new_geo_informations' => $partner_info['geo_informations'],
-                        'log' => 'Partner Geo Information Updated'
-                    ];
+                    $geo_change_log_data = ['old_geo_informations' => $old_geo_informations, 'new_geo_informations' => $partner_info['geo_informations'], 'log' => 'Partner Geo Information Updated'];
 
                     $partner->geoChangeLogs()->save(new PartnerGeoChangeLog($this->withCreateModificationField((new RequestIdentification())->set($geo_change_log_data))));
 
-                    $this->partnerLocationRepo->updateByPartnerId($partner->id, [
-                        'location' =>  array(
-                            'type' => 'Point',
-                            'coordinates' => [
-                                (double) $request->lng,
-                                (double) $request->lat
-                            ]
-                        ),
-                        'radius' => (double) $request->radius,
-                    ]);
+                    $this->partnerLocationRepo->updateByPartnerId($partner->id, ['location' => array('type' => 'Point', 'coordinates' => [(double)$request->lng, (double)$request->lat]), 'radius' => (double)$request->radius,]);
                 }
 
                 $partner->update($partner_info);
@@ -130,11 +105,7 @@ class OperationController extends Controller
                 if ($request->has('working_schedule')) {
                     $partner->workingHours()->delete();
                     foreach (json_decode($request->working_schedule) as $working_schedule) {
-                        $partner->workingHours()->save(new PartnerWorkingHour([
-                            'day' => $working_schedule->day,
-                            'start_time' => $working_schedule->start_time,
-                            'end_time' => $working_schedule->end_time
-                        ]));
+                        $partner->workingHours()->save(new PartnerWorkingHour(['day' => $working_schedule->day, 'start_time' => $working_schedule->start_time, 'end_time' => $working_schedule->end_time]));
                     }
                 }
 
@@ -171,9 +142,7 @@ class OperationController extends Controller
     public function saveCategories($partner, Request $request)
     {
         try {
-            $this->validate($request, [
-                'categories' => "required|string"
-            ]);
+            $this->validate($request, ['categories' => "required|string"]);
             $manager_resource = $request->manager_resource;
             $by = ["created_by" => $manager_resource->id, "created_by_name" => "Resource - " . $manager_resource->profile->name];
             $categories = json_decode($request->categories);
@@ -181,6 +150,7 @@ class OperationController extends Controller
             $categories->load('services');
             $partner = $request->partner;
             list($services, $category_partners) = $this->makeCategoryPartnerWithServices($partner, $categories, $by);
+
             DB::transaction(function () use ($partner, $category_partners, $services) {
                 $partner->categories()->sync($category_partners);
                 $partner_resources = PartnerResource::whereIn('id', $partner->handymanResources->pluck('pivot.id')->toArray())->get();
@@ -195,6 +165,7 @@ class OperationController extends Controller
                     $status_changer->change();
                 }
             });
+
             return api_response($request, $partner, 200);
         } catch (ValidationException $e) {
             $message = getValidationErrorMessage($e->validator->errors()->all());
@@ -213,19 +184,12 @@ class OperationController extends Controller
         $services = [];
         $category_partners = [];
         $location = (new PartnerRepository($partner))->getLocations()->pluck('id');
-        try{
+        try {
             foreach ($categories as $category) {
+                $current_category_partner = ['response_time_min' => constants('PARTNER_MINIMUM_RESPONSE_TIME'), 'response_time_max' => constants('PARTNER_MAXIMUM_RESPONSE_TIME'), 'commission' => $partner->commission, 'preparation_time_minutes' => 120, //$category->preparation_time_minutes,
+                    'category_id' => $category->id];
 
-                $current_category_partner = [
-                    'response_time_min' => constants('PARTNER_MINIMUM_RESPONSE_TIME'),
-                    'response_time_max' => constants('PARTNER_MAXIMUM_RESPONSE_TIME'),
-                    'commission' => $partner->commission,
-                    'preparation_time_minutes' => 120, //$category->preparation_time_minutes,
-                    'category_id' => $category->id
-                ];
-
-                if($partner->package_id === config('sheba.partner_lite_packages_id'))
-                    $current_category_partner['is_partner_premise_applied'] = true;
+                if ($partner->package_id === config('sheba.partner_lite_packages_id')) $current_category_partner['is_partner_premise_applied'] = true;
 
                 array_push($category_partners, array_merge($current_category_partner, $by));
                 $category->load(['services' => function ($q) use ($location) {
@@ -233,6 +197,7 @@ class OperationController extends Controller
                         $query->from('location_service')->whereIn('location_id', $location)->whereRaw('service_id=services.id');
                     })->publishedForAll();
                 }]);
+
                 foreach ($category->services as $service) {
                     if ($service->variable_type == 'Fixed') {
                         $options = null;
@@ -241,30 +206,32 @@ class OperationController extends Controller
                         $options = '';
                         foreach (json_decode($service->variables)->options as $key => $option) {
                             $input = explode(',', $option->answers);
-                            $output = implode(',', array_map(
-                                function ($value, $key) {
-                                    return sprintf("%s", $key);
-                                }, $input, array_keys($input)
-                            ));
+                            $output = implode(',', array_map(function ($value, $key) {
+                                return sprintf("%s", $key);
+                            }, $input, array_keys($input)));
                             $output = '[' . $output . '],';
                             $options .= $output;
                         }
                         $options = '[' . substr($options, 0, -1) . ']';
                         $price = ($service->variable_type == 'Options') ? json_encode(json_decode($service->variables)->prices) : "Custom";
                     }
-                    array_push($services, array_merge($by, [
-                        'description' => $service->description,
-                        'options' => $options,
-                        'prices' => $price,
-                        'is_published' => 1,
-                        'service_id' => $service->id
-                    ]));
+
+                    $service_data = ['description' => $service->description, 'options' => $options, 'prices' => $price, 'is_published' => 1, 'service_id' => $service->id];
+
+                    if (in_array($category->id, config('sheba.car_rental.secondary_category_ids'))) {
+                        $service_variables = json_decode($service->variables);
+                        if ($service_variables->base_prices && $service_variables->base_quantity) {
+                            $service_data += ['base_quantity' => json_encode($service_variables->base_quantity), 'base_prices' => json_encode($service_variables->base_prices)];
+                        }
+                    }
+
+                    array_push($services, array_merge($by, $service_data));
                 }
             }
-        }catch (\Throwable $exception){
-
+        } catch (\Throwable $exception) {
         }
-        return array($services, $category_partners);
+
+        return [$services, $category_partners];
     }
 
     public function isOnPremiseAvailable($partner, Request $request)
