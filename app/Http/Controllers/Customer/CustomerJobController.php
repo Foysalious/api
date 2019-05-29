812935146c2129ba41d93ce8f111be4b3131d030
arnabrahman <arnabrahman@hotmail.com>
2019-05-29 10:41:28 +0600

    Add promotion after order place [ADD]

--- app/Http/Controllers/Customer/CustomerJobController.php
+++ app/Http/Controllers/Customer/CustomerJobController.php
@@ -1,45 +1,69 @@
 <?php namespace App\Http\Controllers\Customer;
 
-
 use App\Http\Controllers\Controller;
-use App\Models\Job;
-use App\Models\Order;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Sheba\ModificationFields;
+use Sheba\Voucher\DTO\Params\CheckParamsForOrder;
+use Sheba\Voucher\PromotionList;
 use Sheba\Voucher\VoucherDiscount;
 
 class CustomerJobController extends Controller
 {
-    public function addVoucher($customer, $job, Request $request)
+    use ModificationFields;
+
+    public function addPromotion($customer, $job, Request $request, CheckParamsForOrder $order_params)
     {
-        $job = $request->job;
-        $voucher_code = strtoupper($request->voucher_code);
-        $partner = $request->partner_id;
-        $location = $request->location_id;
-        $customer_id = $job->customer_id;
-        $order_amount = $request->order_amount;
-        $sales_channel = $request->sales_channel;
-        $order_id = Order::find($request->order_id);
-        $category_id = $request->category_id;
+        try {
+            $this->validate($request, [
+                'code' => 'required|string',
+                'sales_channel' => 'required|string',
+            ]);
+            $job = $request->job;
+            $order = $job->partnerOrder->order;
+            $customer = $request->customer;
+            $order_params->setCategory($order->category)->setCustomer($customer)->setLocation($order->location)->setOrder($order);
+            $result = voucher(strtoupper($request->code))->checkForOrder($order_params)->reveal();
+            if ($result['is_valid']) {
+                $voucher = $result['voucher'];
+                $promo = (new PromotionList($customer))->add($voucher);
+                if (!$promo[0]) return api_response($request, null, 403, ['message' => $promo[1]]);
+                try {
+                    DB::transaction(function () use ($order, $voucher, $job) {
+                        $order->update($this->withUpdateModificationField(['voucher_id' => $voucher->id]));
+                        $voucherDiscount = new VoucherDiscount();
+                        $amount = $voucherDiscount->setVoucher($voucher)->calculate($order_amount);
+                        $discount_percentage = $voucher->is_amount_percentage ? $voucher->amount : null;
+                        $total_price = $job->partnerOrder->calculate(true)->totalPrice;
+                        $voucher_data = [
+                            'discount' => ($amount > $total_price) ? $total_price : $amount,
+                            'discount_percentage' => $discount_percentage,
+                            'sheba_contribution' => $voucher->sheba_contribution,
+                            'partner_contribution' => $voucher->partner_contribution
+                        ];
+                        $job->update($this->withUpdateModificationField($voucher_data));
+                    });
+                } catch (QueryException $e) {
+                    throw $e;
+                }
+                return api_response($request, 1, 200, ['voucher' => $voucher]);
+            } else {
+                return api_response($request, null, 403, ['message' => 'Invalid Promo']);
+            }
+        } catch (ValidationException $e) {
+            $message = getValidationErrorMessage($e->validator->errors()->all());
+            $sentry = app('sentry');
+            $sentry->user_context(['request' => $request->all(), 'message' => $message]);
+            $sentry->captureException($e);
+            return api_response($request, $message, 400, ['message' => $message]);
+        } catch (\Throwable $e) {
+            $sentry = app('sentry');
+            $sentry->user_context(['request' => $request->all()]);
+            $sentry->captureException($e);
+            return api_response($request, null, 500);
+        }
 
-        $voucher = voucher($voucher_code)->check($category_id, $partner, $location, $customer_id, $order_amount, $sales_channel, $order_id)->reveal();
-        if (!$voucher['is_valid']) return ['code' => 401, 'msg' => 'Voucher not valid.'];
 
-        $voucher = $voucher['voucher'];
-        $order->update($this->withUpdateModificationField(['voucher_id' => $voucher->id]));
-
-        $voucherDiscount = new VoucherDiscount();
-        $amount = $voucherDiscount->setVoucher($voucher)->calculate($order_amount);
-        $discount_percentage = $voucher->is_amount_percentage ? $voucher->amount : null;
-
-        $total_price = $job->partnerOrder->calculate(true)->totalPrice;
-        $voucher_data = [
-            'discount' => ($amount > $total_price) ? $total_price : $amount,
-            'discount_percentage' => $discount_percentage,
-            'sheba_contribution' => $voucher->sheba_contribution,
-            'partner_contribution' => $voucher->partner_contribution
-        ];
-        $job->update($this->withUpdateModificationField($voucher_data));
-
-        return ['code' => 200, 'msg' => 'Voucher Added Successfully'];
     }
 }
