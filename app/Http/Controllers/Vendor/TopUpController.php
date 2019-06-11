7e24d1e5fb90becf6ca449c15c457b72ea101be1
arnabrahman <arnabrahman@hotmail.com>
2019-06-11 15:55:12 +0600

    Initialize new top order request fro vendor [UPDATE] SHEBA-3629

--- app/Http/Controllers/Vendor/TopUpController.php
+++ app/Http/Controllers/Vendor/TopUpController.php
@@ -1,21 +1,20 @@
 <?php namespace App\Http\Controllers\Vendor;
 
 use App\Http\Controllers\Controller;
-use App\Models\Vendor;
 use App\Repositories\VendorRepository;
 use Dingo\Api\Routing\Helpers;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
+use Sheba\TopUp\Creator;
 use Validator;
 use Sheba\TopUp\Jobs\TopUpJob;
 use Sheba\TopUp\TopUpRequest;
-use Sheba\TopUp\Vendor\VendorFactory;
 
 class TopUpController extends Controller
 {
     use Helpers;
 
-    public function topUp(Request $request, VendorFactory $vendor, TopUpRequest $top_up_request)
+    public function topUp(Request $request, Creator $creator, TopUpRequest $top_up_request)
     {
         try {
             $this->validate($request, [
@@ -25,13 +24,15 @@ class TopUpController extends Controller
                 'amount' => 'required|min:10|max:1000|numeric'
             ]);
             $agent = $request->vendor;
-            $amount = (double)$request->amount;
-            if ($agent->wallet < $amount) return api_response($request, null, 403, ['message' => "You don't have sufficient balance to recharge."]);
-            $vendor = $vendor->getById($request->operator_id);
-            if (!$vendor->isPublished()) return api_response($request, null, 403, ['message' => 'Sorry, we don\'t support this operator at this moment']);
-            $top_up_request->setAmount($amount)->setMobile($request->mobile)->setType($request->connection_type);
-            dispatch((new TopUpJob($agent, $request->operator_id, $top_up_request)));
-            return api_response($request, null, 200, ['message' => "Recharge Request Successful"]);
+            $top_up_request->setAmount($request->amount)->setMobile($request->mobile)->setType($request->connection_type)->setAgent($agent)->setVendorId($request->vendor_id);
+            if ($top_up_request->hasError()) return api_response($request, null, 403, ['message' => $top_up_request->getErrorMessage()]);
+            $top_up_order = $creator->setTopUpRequest($top_up_request)->create();
+            if ($top_up_order) {
+                dispatch((new TopUpJob($agent, $request->vendor_id, $top_up_order)));
+                return api_response($request, null, 200, ['message' => "Recharge Request Successful", 'id' => $top_up_order->id]);
+            } else {
+                return api_response($request, null, 500);
+            }
         } catch (ValidationException $e) {
             $message = getValidationErrorMessage($e->validator->errors()->all());
             return api_response($request, $message, 400, ['message' => $message]);
@@ -53,7 +54,7 @@ class TopUpController extends Controller
                 $error = $validator->errors()->all()[0];
                 return api_response($request, $error, 400, ['msg' => $error]);
             }
-            $data= (new VendorRepository())->topUpHistory($request);
+            $data = (new VendorRepository())->topUpHistory($request);
             $response = ['data' => $data];
             return api_response($request, $response, 200, $response);
         } catch (\Throwable $e) {
