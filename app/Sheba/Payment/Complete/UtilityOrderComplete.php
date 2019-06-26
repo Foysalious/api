66dd71860f1cdb898a34f2c15bbcae32d5f9cd51
tonmoy <saddam.hossain@sheba.xyz>
2019-06-26 13:58:09 +0600

    [ADD] #profile auth mod

--- app/Sheba/Payment/Complete/UtilityOrderComplete.php
+++ app/Sheba/Payment/Complete/UtilityOrderComplete.php
@@ -4,7 +4,26 @@
 namespace Sheba\Payment\Complete;
 
 
-class UtilityOrderComplete
+class UtilityOrderComplete extends PaymentComplete
 {
 
+    public function complete()
+    {
+        $has_error = false;
+        try {
+            if ($this->payment->isComplete()) return $this->payment;
+            $this->paymentRepository->setPayment($this->payment);
+            $payable = $this->payment->payable;
+            $this->setModifier($customer = $payable->user);
+            $this->payment->transaction_details = null;
+            $this->completePayment();
+        } catch (RequestException $e) {
+            $this->failPayment();
+            throw $e;
+        }
+        if ($has_error) {
+            $this->completePayment();
+        }
+        return $this->payment;
+    }
 }
