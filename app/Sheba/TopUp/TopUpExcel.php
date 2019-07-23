ba531c890a16ff2203ba78a47aa4c2dac5b28cae
tonmoy <saddam.hossain@sheba.xyz>
2019-07-23 16:32:12 +0600

    [UPDATE] #bulk topup name add

--- app/Sheba/TopUp/TopUpExcel.php
+++ app/Sheba/TopUp/TopUpExcel.php
@@ -5,13 +5,15 @@ class TopUpExcel
     const SHEET = "data";
 
     const MOBILE_COLUMN = "A";
-    const VENDOR_COLUMN = "B";
-    const TYPE_COLUMN = "C";
-    const AMOUNT_COLUMN = "D";
-    const STATUS_COLUMN = "E";
-    const MESSAGE_COLUMN = "F";
+    const NAME_COLUMN = "B";
+    const VENDOR_COLUMN = "C";
+    const TYPE_COLUMN = "D";
+    const AMOUNT_COLUMN = "E";
+    const STATUS_COLUMN = "F";
+    const MESSAGE_COLUMN = "G";
 
     const MOBILE_COLUMN_TITLE = "mobile";
+    const NAME_COLUMN_TITLE = "name";
     const VENDOR_COLUMN_TITLE = "operator";
     const TYPE_COLUMN_TITLE = "connection_type";
     const AMOUNT_COLUMN_TITLE = "amount";
