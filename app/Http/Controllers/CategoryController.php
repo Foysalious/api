d7986ab6b7319a03511e0b941e5be3d3051efa3d
arnabrahman <arnabrahman@hotmail.com>
2019-07-25 16:47:23 +0600

    ⬆️ Category group list optimize
    Type(s): UPDATE

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -155,7 +155,7 @@ class CategoryController extends Controller
                 ->parent()->orderBy('order');
             if ($with) {
                 $categories->with(['children' => function ($q) use ($location_id, $best_deal_category_ids) {
-                    $q->select('id', 'name', 'thumb', 'parent_id')
+                    $q->select('id', 'name', 'thumb', 'parent_id', 'app_thumb')
                         ->whereHas('locations', function ($q) use ($location_id) {
                             $q->select('locations.id')->where('locations.id', $location_id);
                         })->whereHas('services', function ($q) use ($location_id) {
