cfde4afdbf3ef85aa7bccaee079b2469a5b1419b
tonmoy <saddam.hossain@sheba.xyz>
2019-07-25 16:32:32 +0600

    [UPDATE] #transaction invoice

--- app/Http/Controllers/ShebaController.php
+++ app/Http/Controllers/ShebaController.php
@@ -277,7 +277,6 @@ class ShebaController extends Controller
             $sentry->captureException($e);
             return api_response($request, $message, 400, ['message' => $message]);
         } catch (\Throwable $e) {
-            dd($e);
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
         }
