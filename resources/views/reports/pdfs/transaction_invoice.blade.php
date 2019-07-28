47da56c7e40f03c08551cd76ad46e9f718e9de1b
arnabrahman <arnabrahman@hotmail.com>
2019-07-28 18:31:50 +0600

    ⬆️ Payment Transaction pdf
    Type(s): UPDATE

--- resources/views/reports/pdfs/transaction_invoice.blade.php
+++ resources/views/reports/pdfs/transaction_invoice.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <title> Invoice </title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
 <body>
@@ -47,31 +48,54 @@
     </tr>
     <tr>
         <td>
-            <table style="width: 100%;color: #636363;" cellpadding="5">
+            <table style="width: 100%;color: #636363;" class="invoice-table" cellpadding="5">
                 <tr>
-                    <td><i class="fa fa-money"></i></td>
+                    <td><i class="material-icons">account_balance_wallet</i></td>
                     <td>Payment amount</td>
                     <td align="right">BDT {{$amount}}</td>
                 </tr>
                 <tr>
-                    <td><i class="fa fa-money"></i></td>
+                    <td><i class="material-icons">info</i></td>
                     <td>Payment purpose</td>
                     <td align="right">{{$description}}</td>
                 </tr>
                 <tr>
-                    <td><i class="fa fa-money"></i></td>
+                    <td><i class="material-icons">event_available</i></td>
                     <td>Payment time</td>
                     <td align="right">{{$created_at}}</td>
                 </tr>
                 <tr>
-                    <td><i class="fa fa-money"></i></td>
+                    <td><i class="material-icons">style</i></td>
                     <td>Payment type</td>
                     <td align="right">{{$method}}</td>
                 </tr>
             </table>
         </td>
     </tr>
+    <tr>
+        <td>
+            <p class="terms"><strong>Terms and Note:</strong> If needed, it can take a maximum of 15 days to get your refund. You will only get return by the way you pay.</p>
+        </td>
+    </tr>
     </tbody>
 </table>
+
+<style>
+    .invoice-table tr {
+        color: #689ab8;
+        font-size: 14px;
+    }
+    .invoice-table tr i {
+        font-size: 14px;
+        position: relative;
+        top: 2px
+    }
+    .terms {
+        font-size: 12px;
+        padding-top: 20px;
+        border-top: 1px solid #e1e1e1;
+        color: #4a4a4a;
+    }
+</style>
 </body>
 </html>
