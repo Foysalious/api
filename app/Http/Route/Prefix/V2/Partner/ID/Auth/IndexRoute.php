53f95e37f6cad949bb397fa62cba9c91e3bd0cd0
miajee <saiful.sheba@gmail.com>
2019-08-01 13:40:09 +0600

    route

--- app/Http/Route/Prefix/V2/Partner/ID/Auth/IndexRoute.php
+++ app/Http/Route/Prefix/V2/Partner/ID/Auth/IndexRoute.php
@@ -48,6 +48,8 @@ class IndexRoute
                 });
                 $api->group(['prefix' => 'services'], function ($api) {
                     $api->get('/', 'Pos\ServiceController@index');
+                    $api->get('/', 'Pos\ServiceController@index');
+                    $api->get('/units', 'Pos\ServiceController@getUnits');
                     $api->post('/', 'Pos\ServiceController@store');
                     $api->group(['prefix' => '{service}'], function ($api) {
                         $api->get('/', 'Pos\ServiceController@show');
