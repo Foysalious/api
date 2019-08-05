ccbbfed66f9fb2c55d5c6a7d6c01c12a20590f9f
Hasan Hafiz Pasha <pasha.startern@gmail.com>
2019-08-05 10:59:34 +0600

    ➕ POS CUSTOMER EXTRA NEEDED INFORMATION
    Type(s): ADD

--- app/Http/Controllers/Pos/CustomerController.php
+++ app/Http/Controllers/Pos/CustomerController.php
@@ -1,11 +1,16 @@
 <?php namespace App\Http\Controllers\Pos;
 
 use App\Http\Controllers\Controller;
+
 use App\Models\PartnerPosCustomer;
+use App\Models\PosOrder;
+
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
+
 use Sheba\Pos\Customer\Creator;
+
 use Throwable;
 
 class CustomerController extends Controller
@@ -39,9 +44,28 @@ class CustomerController extends Controller
     public function show($partner, $customer, Request $request)
     {
         try {
+            /** @var PartnerPosCustomer $customer */
             $customer = PartnerPosCustomer::find((int)$customer);
             if (!$customer) return api_response($request, null, 404, ['message' => 'Customer Not Found.']);
-            return api_response($request, $customer, 200, ['customer' => $customer->details()]);
+
+            $data = $customer->details();
+            $data['customer_since'] = $customer->created_at->format('Y-m-d');
+            $data['customer_since_formatted'] = $customer->created_at->diffForHumans();
+
+            $total_purchase_amount = 0.00;
+            $total_due_amount = 0.00;
+            PosOrder::where('customer_id', $customer->id)->get()->each(function ($order) use (&$total_purchase_amount, &$total_due_amount) {
+                /** @var PosOrder $order */
+                $order = $order->calculate();
+                $total_purchase_amount += $order->getTotalBill();
+                $total_due_amount += $order->getDue();
+            });
+
+            $data['total_purchase_amount'] = $total_purchase_amount;
+            $data['total_due_amount'] = $total_due_amount;
+            $data['total_used_promo'] = 0.00;
+
+            return api_response($request, $customer, 200, ['customer' => $data]);
         } catch (Throwable $e) {
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
