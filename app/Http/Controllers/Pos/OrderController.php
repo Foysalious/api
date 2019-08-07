d3cccc28a473a9f901674868405930cf530ee310
Hasan Hafiz Pasha <pasha.startern@gmail.com>
2019-08-07 13:12:55 +0600

    ⬆️  CUSTOMER NOT FOUND ON POS ORDER - SEND EMAIL
    Type(s): UPDATE

--- app/Http/Controllers/Pos/OrderController.php
+++ app/Http/Controllers/Pos/OrderController.php
@@ -112,7 +112,6 @@ class OrderController extends Controller
         }
     }
 
-
     public function store($partner, Request $request, Creator $creator, ProfileCreator $profileCreator, PosCustomerCreator $posCustomerCreator, PartnerRepository $partnerRepository, PaymentLinkCreator $paymentLinkCreator, PaymentLinkOrderAdapter $paymentLinkOrderAdapter)
     {
         try {
@@ -312,14 +311,20 @@ class OrderController extends Controller
      * EMAIL TO CUSTOMER ABOUT POS ORDER BILLS
      *
      * @param Request $request
+     * @param Updater $updater
      * @return JsonResponse
      */
-    public function sendEmail(Request $request)
+    public function sendEmail(Request $request, Updater $updater)
     {
         try {
             $this->setModifier($request->manager_resource);
             /** @var PosOrder $order */
             $order = PosOrder::with('items')->find($request->order)->calculate();
+
+            if ($request->has('customer_id') && is_null($order->customer_id)) {
+                $requested_customer = PosCustomer::find($request->customer_id);
+                $order = $updater->setOrder($order)->setData(['customer_id' => $requested_customer->id])->update();
+            }
 
             if (!$order) return api_response($request, null, 404, ['msg' => 'Order not found']);
             if (!$order->customer) return api_response($request, null, 404, ['msg' => 'Customer not found']);
