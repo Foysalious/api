91b68bf36ee1fe8f1731ef068681f0ecc230ca22
arnabrahman <arnabrahman@hotmail.com>
2019-08-08 17:35:44 +0600

    ⬆️ Send payment link sms message
    Type(s): UPDATE

--- app/Jobs/Partner/PaymentLink/SendPaymentLinkSms.php
+++ app/Jobs/Partner/PaymentLink/SendPaymentLinkSms.php
@@ -4,7 +4,6 @@ use App\Jobs\Job;
 use App\Models\Payment;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 use Sheba\PaymentLink\PaymentLinkTransformer;
 use Sheba\Repositories\Interfaces\PaymentLinkRepositoryInterface;
 use Sheba\Sms\Sms;
@@ -22,7 +21,7 @@ class SendPaymentLinkSms extends Job implements ShouldQueue
     {
         $this->payment = $payment;
         $this->paymentLink = $paymentLink;
-        $this->sms = new Sms();//app(Sms::class);
+        $this->sms = new Sms();
         $this->paymentLinkRepository = app(PaymentLinkRepositoryInterface::class);
     }
 
@@ -42,12 +41,13 @@ class SendPaymentLinkSms extends Job implements ShouldQueue
             $formatted_collected_amount = number_format($this->payment->payable->amount, 2);
             $name = $this->payment->payable->getName();
             $payment_receiver = $this->paymentLink->getPaymentReceiver();
-            $log = "$formatted_collected_amount TK has been collected from " . $name . " by link- " . $this->paymentLink->getLinkID() . ". Please see the sManager app for detail information.";
+            $log = "$formatted_collected_amount TK has been collected from " . $name . " by link-" . $this->paymentLink->getLinkID() . ". Please see the sManager app for detail information.";
             $this->sms->shoot($payment_receiver->getMobile(), $log);
             $target = $this->paymentLink->getTarget();
             $variable = "paid $formatted_collected_amount TK";
             if ($target) $variable = "placed an order, ID: {$target->id}. $formatted_collected_amount TK has been paid";
-            $log = "You have successfully $variable To {$payment_receiver->name} through {$this->payment->paymentDetails()->last->readable_method}." . $money_receipt ? " Money receipt: $money_receipt" : '';
+            $log = "You have successfully $variable To {$payment_receiver->name} through {$this->payment->paymentDetails->last()->readable_method}.";
+            $log .= $money_receipt ? " Money receipt: $money_receipt" : '';
             $this->sms->shoot($this->payment->payable->getMobile(), $log);
         }
     }
