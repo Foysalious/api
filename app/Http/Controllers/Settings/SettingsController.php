ab4c094fd4b29de61f63c7c2d702b06902500c15
miajee <saiful.sheba@gmail.com>
2019-08-20 16:25:19 +0600

    When To Rate Sheba App [ADD]  [SHEBA-3814]

--- app/Http/Controllers/Settings/SettingsController.php
+++ app/Http/Controllers/Settings/SettingsController.php
@@ -88,7 +88,7 @@ class SettingsController extends Controller
                     'is_bkash_saved' => $customer->profile->bkash_agreement_id ? 1 : 0
                 ],
                 'pending_order' => $customer->partnerOrders->where('closed_and_paid_at', null)->where('cancelled_at', null)->count(),
-                'show_app_rating' => ($reviews->count() >= 3) ? 1 : 0
+                'has_rated_customer_app' => ($customer->has_rated_customer_app == 1) ? 1 : (($reviews->count() >= 3) ? 0 : 1)
             ];
             return api_response($request, $settings, 200, ['settings' => $settings]);
         } catch (\Throwable $e) {
