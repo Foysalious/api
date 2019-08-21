c0475d8ec8a5e87687812a325d0351346d3ac192
miajee <saiful.sheba@gmail.com>
2019-08-21 16:36:37 +0600

    typo

--- app/Http/Controllers/CustomerNotificationController.php
+++ app/Http/Controllers/CustomerNotificationController.php
@@ -18,7 +18,7 @@ class CustomerNotificationController extends Controller
             $notifications = [];
             $notification_1 = [
                 'type_id' => 154919,
-                'type' => 'Order',
+                'type' => 'Job',
                 'text' => 'Pure Milk',
                 'image' => 'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/categories_images/thumbs/1549878615_dairy_milk_and_ghee.png',
                 'statuses' => [
@@ -41,7 +41,7 @@ class CustomerNotificationController extends Controller
             ];
             $notification_2 = [
                 'type_id' => 154919,
-                'type' => 'Order',
+                'type' => 'Job',
                 'text' => 'Pure Milk',
                 'image' => 'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/categories_images/thumbs/1549878615_dairy_milk_and_ghee.png',
                 'statuses' => [
