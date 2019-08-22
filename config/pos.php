23f9bd933a827e1b5230ead85344753bea691274
shovan-sheba <shovan@sheba.xyz>
2019-08-22 19:13:38 +0600

    ➕  POS WARRANTY ADD AND EDIT Type(s): ADD

--- config/pos.php
+++ config/pos.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-    'payment_method' => ['cod', 'bkash', 'online', 'others', 'payment_link']
+    'payment_method' => ['cod', 'bkash', 'online', 'others', 'payment_link'],
+    'warranty_unit' => [
+        'day' => ['bn' => 'দিন', 'en' => 'day'], 'week' => ['bn' => 'সপ্তাহ', 'en' => 'week'], 'month' => ['bn' => 'মাস', 'en' => 'month'], 'year' => ['bn' => 'বছর', 'en' => 'year']
+    ]
 ];
