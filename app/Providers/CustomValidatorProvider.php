9398b43cc0a10d98567ae9fceb73463692dc9d66
Hasan Hafiz Pasha <pasha.startern@gmail.com>
2019-08-25 22:03:30 +0600

    🚧  VOUCHER FOR PARTNER
    Type(s): WIP
    
    Issue(s):
    - JIRA: SHEBA-3800

--- app/Providers/CustomValidatorProvider.php
+++ app/Providers/CustomValidatorProvider.php
@@ -15,6 +15,14 @@ class CustomValidatorProvider extends ServiceProvider
     public function boot()
     {
         Validator::extend('mobile', $this->validatorNamespace . 'MobileNumberValidator@validate');
+        Validator::extend('is_variables_present', $this->validatorNamespace . 'SmsTemplateValidator@validate');
+        Validator::extend('resolution', $this->validatorNamespace . 'ImageResolutionValidator@validate');
+        Validator::extend('aspect', $this->validatorNamespace . 'ImageAspectRatioValidator@validate');
+        Validator::extend('after_or_equal', $this->validatorNamespace . 'CustomDateValidator@afterOrEqual');
+        Validator::extend('before_or_equal', $this->validatorNamespace . 'CustomDateValidator@beforeOrEqual');
+        Validator::extend('after_or_equal_not_if', $this->validatorNamespace . 'CustomDateValidator@afterOrEqualIf');
+        Validator::extend('unique_and_valid_app_version', $this->validatorNamespace . 'AppVersionValidator@uniqueAndValidVersion');
+        Validator::extend('date_if_not', $this->validatorNamespace . 'CustomDateValidator@dateIfNot');
     }
 
     /**
