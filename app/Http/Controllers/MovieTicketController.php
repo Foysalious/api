285de2e7be39e603be0774243d04eff9dd8db7c1
tonmoy <saddam.hossain@sheba.xyz>
2019-08-26 14:54:00 +0600

    [UPDATE] #Movie&Busticket api

--- app/Http/Controllers/MovieTicketController.php
+++ app/Http/Controllers/MovieTicketController.php
@@ -287,10 +287,11 @@ class MovieTicketController extends Controller
      */
     private function getAgent(Request $request)
     {
+        $type = $request->type;
         if ($request->affiliate) return $request->affiliate;
         elseif ($request->customer) return $request->customer;
         elseif ($request->partner) return $request->partner;
-        elseif ($request->type) return $request->get($request->type);
+        elseif ($type) return $request->$type;
         throw new Exception('Invalid Agent');
     }
 
