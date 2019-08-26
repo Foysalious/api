afc7efe72c94571076744a275d5ad44572635e38
miajee <saiful.sheba@gmail.com>
2019-08-26 12:49:32 +0600

    Gift Card [ADD] [SHEBA-3817]

--- app/Sheba/Payment/Complete/GiftCardPurchaseComplete.php
+++ app/Sheba/Payment/Complete/GiftCardPurchaseComplete.php
@@ -2,9 +2,10 @@
 
 namespace Sheba\Payment\Complete;
 
-use App\Models\GiftCard;
-use App\Models\GiftCardPurchase;
 use Illuminate\Database\QueryException;
+use App\Models\GiftCardPurchase;
+use App\Models\Bonus;
+use Carbon\Carbon;
 use DB;
 
 class GiftCardPurchaseComplete extends PaymentComplete
@@ -15,13 +16,17 @@ class GiftCardPurchaseComplete extends PaymentComplete
             $this->paymentRepository->setPayment($this->payment);
             DB::transaction(function () {
                 $gift_card_purchase = GiftCardPurchase::find($this->payment->payable->type_id);
+                $gift_card = $gift_card_purchase->giftCard;
                 $gift_card_purchase->status = 'successful';
                 $gift_card_purchase->update();
-                $this->payment->payable->user->rechargeWallet($gift_card_purchase->credits_purchased, [
-                    'amount' => $gift_card_purchase->credits_purchased,
-                    'transaction_details' => $this->payment->transaction_details,
-                    'type' => 'Credit',
-                    'log' => 'Credit Purchase'
+                Bonus::create([
+                    'user_type' => get_class($this->payment->payable->user),
+                    'user_id' => $this->payment->payable->user->id,
+                    'type' => 'cash',
+                    'amount' => (double)$gift_card->credit,
+                    'log' => "$gift_card->credit tk gift card purchase",
+                    'status' => 'valid',
+                    'valid_till' => Carbon::now()->addMonth(6),
                 ]);
                 $this->completePayment();
             });
