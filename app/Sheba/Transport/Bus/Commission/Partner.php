7ec4ead0bc32458b990b024ce6cdcc05437fed4f
tonmoy <saddam.hossain@sheba.xyz>
2019-09-01 14:34:57 +0600

    [UPDATE] #Transport ticket agent commission

--- app/Sheba/Transport/Bus/Commission/Partner.php
+++ app/Sheba/Transport/Bus/Commission/Partner.php
@@ -4,7 +4,29 @@ use Sheba\Transport\Bus\BusTicketCommission;
 
 class Partner extends BusTicketCommission
 {
-    public function disburse(){}
+    public function disburse()
+    {
+        $this->storeAgentsCommission();
+        if ($this->agent->ambassador) {
+            $vendor_ambassador_commission = $this->vendorCommission->ambassador_amount;
+            if ($vendor_ambassador_commission > 0) {
+                $this->storeAmbassadorCommission();
+                $this->storeAmbassadorWalletTransaction();
+            }
+        }
+    }
 
+    private function storeAmbassadorCommission()
+    {
+        $this->transportTicketOrder->ambassador_amount = $this->calculateAmbassadorCommissionForMovieTicket();
+        $this->transportTicketOrder->save();
+    }
+
+    private function storeAmbassadorWalletTransaction()
+    {
+        $this->agent->ambassador->creditWallet($this->transportTicketOrder->ambassador_amount);
+        $log = "{$this->agent->profile->name} gifted {$this->transportTicketOrder->ambassador_amount} Tk. for {$this->transportTicketOrder->amount} Tk. transport ticket purchase";
+        $this->agent->ambassador->walletTransaction(['amount' => $this->transportTicketOrder->ambassador_amount, 'type' => 'Credit', 'log' => $log]);
+    }
     public function refund(){}
 }
