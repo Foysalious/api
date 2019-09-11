106e5edfcb5331e03b3451fbc5b50efd47c2837e
tonmoy <saddam.hossain@sheba.xyz>
2019-09-11 13:06:31 +0600

    [UPDATE] #partner subscription purchase

--- app/Sheba/Payment/Adapters/Payable/RechargeAdapter.php
+++ app/Sheba/Payment/Adapters/Payable/RechargeAdapter.php
@@ -1,5 +1,6 @@
 <?php namespace Sheba\Payment\Adapters\Payable;
 
+use App\Models\Partner;
 use App\Models\Payable;
 use App\Sheba\Payment\Rechargable;
 use Carbon\Carbon;
@@ -23,7 +24,7 @@ class RechargeAdapter implements PayableAdapter
         $payable->user_type = get_class($this->user);
         $payable->amount = (double)$this->amount;
         $payable->completion_type = 'wallet_recharge';
-        $payable->success_url = config('sheba.front_url') . '/profile/credit';
+        $payable->success_url = ($this->user instanceof Partner)?config('sheba.partners_url'):config('sheba.front_url') . '/profile/credit';
         $payable->created_at = Carbon::now();
         $payable->save();
         return $payable;
