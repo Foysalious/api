d55737347b6b2b3d400fee47670e9e929d64f101
Saiful <saiful.sheba@gmail.com>
2019-09-22 18:00:29 +0600

    ➕ Design
    Type(s): ADD

--- resources/views/reports/pdfs/subscription_order_invoice.blade.php
+++ resources/views/reports/pdfs/subscription_order_invoice.blade.php
@@ -18,8 +18,11 @@
         }
     </style>
 </head>
+
 <body>
+
 <table align="center" style="max-width: 800px;margin: auto;min-width: 600px;font-family: Arial, sans-serif">
+    <tbody>
     <tr>
         <td>
             <table class="table table-striped" style="width: 100%;">
@@ -45,12 +48,18 @@
         <td>
             <table class="table table-striped" style="width: 100%">
                 <tr class="table-light">
-                    <td colspan="3" style="border-left: 4px">
+                    <td colspan="3" style="border-left: 4px; border-top: none">
                         @if(isset($customer))
                             <div style="padding: 10px 20px">
-                                <span style="color: #B0BEC5;">Bill to</span><br>
+                                <span style="color: #B0BEC5;">Bill to:</span>
                                 <span style="color: #383d46;letter-spacing: -0.05px;font-weight: 700;">{{ucfirst($customer['name'])}}</span><br>
                                 <span style="color: #9b9b9b">{{$customer['mobile']}}</span>
+                            </div>
+                            <div class="">
+                                <span style="color: #B0BEC5;">Bill Number:</span>
+                                <span style="color: #383d46;letter-spacing: -0.05px;font-weight: 700;">{{$subscription_code}}</span><br>
+                                <span style="color: #B0BEC5;">Date Of Bill:</span>
+                                <span style="color: #9b9b9b">{{$bill_pay_date}}</span>
                             </div>
                         @endif
                     </td>
@@ -59,63 +68,69 @@
         </td>
     </tr>
     @if(isset($orders))
-        <table class="table table-striped" style="width: 100%;color: #929292"
-               cellspacing="5">
-            <thead class="thead-dark">
-            <tr>
-                <th>Service Name</th>
-                <th>Order Code</th>
-                <th>QTY</th>
-                <th>Unit Price</th>
-                <th align="right">Price</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($orders as $order)
-                <tr>
-                    <td style="color: #383d46"> {{ $order['service_name'] }} <br></td>
-                    <td style="color: #383d46">{{$order['id']}}</td>
-                    <td style="color: #383d46">{{$order['service_quantity']}}</td>
-                    <td style="color: #383d46">{{$order['service_unit_price']}}</td>
-                    <td align="right" style="color: #383d46"><span
-                                style="width: 13px"></span> {{number_format(($order['total']), 2)}}</td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-
-        <tbody>
         <tr>
-            <td colspan="4" align="right" style="color: #9b9b9b">Total Service Price</td>
-            <td align="right" style="color: #383d46"><span
-                        style="width: 13px"></span> {{number_format($original_price,2)}}</td>
+            <td>
+                <table class="table table-striped" style="width: 100%;color: #929292"
+                       cellspacing="5">
+                    <thead class="thead-dark">
+                    <tr>
+                        <th>Service Name</th>
+                        <th>Order Code</th>
+                        <th>QTY</th>
+                        <th>Unit Price</th>
+                        <th align="right">Price</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($orders as $order)
+                        <tr>
+                            <td style="color: #383d46"> {{ $order['service_name'] }} <br></td>
+                            <td style="color: #383d46">{{$order['id']}}</td>
+                            <td style="color: #383d46">{{$order['service_quantity']}}</td>
+                            <td style="color: #383d46">{{$order['service_unit_price']}}</td>
+                            <td align="right" style="color: #383d46"><span
+                                        style="width: 13px"></span> {{number_format(($order['total']), 2)}}</td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                <table>
+                    <tbody>
+                    <tr>
+                        <td colspan="4" align="right" style="color: #9b9b9b">Total Service Price</td>
+                        <td align="right" style="color: #383d46"><span
+                                    style="width: 13px"></span> {{number_format($original_price,2)}}</td>
+                    </tr>
+                    <tr>
+                        <td colspan="4" align="right" style="color: #9b9b9b">Discount</td>
+                        <td align="right" style="color: #383d46"><span
+                                    style="width: 13px"></span> {{number_format($discount,2)}}</td>
+                    </tr>
+                    <tr>
+                        <td colspan="4" align="right" style="color: #9b9b9b">Subtotal</td>
+                        <td align="right" style="color: #383d46"><span
+                                    style="width: 13px"></span> {{number_format($subtotal,2)}}</td>
+                    </tr>
+                    <tr>
+                        <td colspan="4" align="right" style="color: #9b9b9b">Grand Total</td>
+                        <td align="right" style="color: #383d46"><span
+                                    style="width: 13px"></span> {{number_format($subtotal,2)}}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </td>
         </tr>
-        <tr>
-            <td colspan="4" align="right" style="color: #9b9b9b">Discount</td>
-            <td align="right" style="color: #383d46"><span
-                        style="width: 13px"></span> {{number_format($discount,2)}}</td>
-        </tr>
-        <tr>
-            <td colspan="4" align="right" style="color: #9b9b9b">Subtotal</td>
-            <td align="right" style="color: #383d46"><span
-                        style="width: 13px"></span> {{number_format($subtotal,2)}}</td>
-        </tr>
-        <tr>
-            <td colspan="4" align="right" style="color: #9b9b9b">Grand Total</td>
-            <td align="right" style="color: #383d46"><span
-                        style="width: 13px"></span> {{number_format($subtotal,2)}}</td>
-        </tr>
-        </tbody>
     @endif
     <tr>
         <td>
             <p class="terms">
-                <strong>*</strong>
+                <strong>Terms and Note:</strong>
                 If needed, it can take a maximum of 15 days to get your refund. You will only get return
                 by the way you pay.
             </p>
         </td>
     </tr>
+    </tbody>
 </table>
 </body>
 </html>
