253b189b7dd3f0db6f1024240cf17d5f9db5d3d8
Saiful <saiful.sheba@gmail.com>
2019-09-22 20:24:12 +0600

    ➕ Invoice subscription order design
    Type(s): ADD

--- resources/views/reports/pdfs/subscription_order_invoice.blade.php
+++ resources/views/reports/pdfs/subscription_order_invoice.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
         .terms {
             font-size: 12px;
@@ -25,41 +26,52 @@
     <tbody>
     <tr>
         <td>
-            <table class="table table-striped" style="width: 100%;">
-                @if(isset($partner))
-                    <tr>
-                        <td width="120"><img style="max-width: 80px" src="{{$partner['image']}}" alt=""></td>
-                        <td colspan="3">
-                            <div style="text-align: left;padding: 10px 20px;">
-                                <span style="color: #383d46;letter-spacing: -0.05px;font-weight: 700;">{{ucfirst($partner['name'])}}</span><br>
-                            </div>
-                            <div class="p-2 bd-highlight">
-                                <span style="color: #9b9b9b;">{{$partner['mobile']}}</span><br>
-                                <span style="color: #9b9b9b;">{{$partner['email']}}</span><br>
-                                <span style="color: #9b9b9b;">{{$partner['address']}}</span>
-                            </div>
-                        </td>
-                    </tr>
-                @endif
-            </table>
+            <div style="width:100%;background-color:#F2F3F7;padding:14px; padding-top: 42px">
+                <table class="" style="width: 100%;">
+                    @if(isset($partner))
+                        <tr>
+                            <td width="120"><img style="max-width: 80px;padding-right: unset" src="{{$partner['image']}}" alt=""></td>
+                            <td colspan="3">
+                                <div class="d-flex justify-content-between">
+                                    <div style="text-align: left;padding: 10px 20px;padding-left: 0%">
+                                        <span style="color: #383d46;letter-spacing: -0.05px;font-weight: 700;">{{ucfirst($partner['name'])}}</span><br>
+                                    </div>
+                                    <div style="text-align: right">
+                                        <span style="color: #9b9b9b;">{{$partner['mobile']}}</span>
+                                        <span style="padding-left: 10px"> <i class="fa fa-phone" aria-hidden="true"></i></span><br>
+                                        <span style="color: #9b9b9b;">{{$partner['email']}}</span>
+                                        <span style="padding-left: 10px"><i class="fa fa-envelope"
+                                                                            aria-hidden="true"></i></span><br>
+                                        <span style="color: #9b9b9b;">{{$partner['address']}}</span>
+                                        <span style="padding-left: 10px"><i class="fa fa-map-marker"
+                                                                            aria-hidden="true"></i></span>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    @endif
+                </table>
+            </div>
         </td>
     </tr>
     <tr>
         <td>
             <table class="table table-striped" style="width: 100%">
                 <tr class="table-light">
-                    <td colspan="3" style="border-left: 4px; border-top: none">
+                    <td style="border-left: 4px; border-top: none">
                         @if(isset($customer))
-                            <div style="padding: 10px 20px">
-                                <span style="color: #B0BEC5;">Bill to:</span>
-                                <span style="color: #383d46;letter-spacing: -0.05px;font-weight: 700;">{{ucfirst($customer['name'])}}</span><br>
-                                <span style="color: #9b9b9b">{{$customer['mobile']}}</span>
-                            </div>
-                            <div class="">
-                                <span style="color: #B0BEC5;">Bill Number:</span>
-                                <span style="color: #383d46;letter-spacing: -0.05px;font-weight: 700;">{{$subscription_code}}</span><br>
-                                <span style="color: #B0BEC5;">Date Of Bill:</span>
-                                <span style="color: #9b9b9b">{{$bill_pay_date}}</span>
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <span style="color: #B0BEC5;">Bill to:</span>
+                                    <span style="color: #383d46;letter-spacing: -0.05px;font-weight: 700;">{{ucfirst($customer['name'])}}</span><br>
+                                    <span style="padding-left:45px;color: #9b9b9b">{{$customer['mobile']}}</span>
+                                </div>
+                                <div>
+                                    <span style="color: #B0BEC5;">Bill Number:</span>
+                                    <span style="color: #383d46;letter-spacing: -0.05px;font-weight: 700;">{{$subscription_code}}</span><br>
+                                    <span style="color: #B0BEC5;">Date Of Bill:</span>
+                                    <span style="color: #9b9b9b">{{$bill_pay_date}}</span>
+                                </div>
                             </div>
                         @endif
                     </td>
@@ -72,7 +84,7 @@
             <td>
                 <table class="table table-striped" style="width: 100%;color: #929292"
                        cellspacing="5">
-                    <thead class="thead-dark">
+                    <thead style="background-color: #7B83A5; color: white">
                     <tr>
                         <th>Service Name</th>
                         <th>Order Code</th>
@@ -94,26 +106,27 @@
                     @endforeach
                     </tbody>
                 </table>
-                <table>
-                    <tbody>
+                <table class="float-right">
+                    <tbody style="font-weight: bold">
                     <tr>
-                        <td colspan="4" align="right" style="color: #9b9b9b">Total Service Price</td>
+                        <td colspan="4">Total Service Price</td>
                         <td align="right" style="color: #383d46"><span
-                                    style="width: 13px"></span> {{number_format($original_price,2)}}</td>
+                                    style="width: 13px ; padding-left: 60px"></span> {{number_format($original_price,2)}}
+                        </td>
                     </tr>
                     <tr>
-                        <td colspan="4" align="right" style="color: #9b9b9b">Discount</td>
+                        <td colspan="4">Discount</td>
                         <td align="right" style="color: #383d46"><span
                                     style="width: 13px"></span> {{number_format($discount,2)}}</td>
                     </tr>
                     <tr>
-                        <td colspan="4" align="right" style="color: #9b9b9b">Subtotal</td>
+                        <td colspan="4">Subtotal</td>
                         <td align="right" style="color: #383d46"><span
                                     style="width: 13px"></span> {{number_format($subtotal,2)}}</td>
                     </tr>
-                    <tr>
-                        <td colspan="4" align="right" style="color: #9b9b9b">Grand Total</td>
-                        <td align="right" style="color: #383d46"><span
+                    <tr style="background-color: #1b4280;color: white">
+                        <td colspan="4">Grand Total</td>
+                        <td align="right"><span
                                     style="width: 13px"></span> {{number_format($subtotal,2)}}</td>
                     </tr>
                     </tbody>
@@ -121,9 +134,10 @@
             </td>
         </tr>
     @endif
+    <hr>
     <tr>
         <td>
-            <p class="terms">
+            <p class="terms" style="border-top: none">
                 <strong>Terms and Note:</strong>
                 If needed, it can take a maximum of 15 days to get your refund. You will only get return
                 by the way you pay.
