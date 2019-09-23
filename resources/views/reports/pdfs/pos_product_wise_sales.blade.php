d7083ee733c49173d19176540b4e285fc7b14a57
tonmoy <saddam.hossain@sheba.xyz>
2019-09-23 15:14:33 +0600

    ⬆️  POS REPORT PDF
    Type(s): UPDATE

--- resources/views/reports/pdfs/pos_product_wise_sales.blade.php
+++ resources/views/reports/pdfs/pos_product_wise_sales.blade.php
@@ -2,14 +2,19 @@
 <html lang="en" >
 <head>
     <title>Product wise sales report</title>
-    <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <style>
+    <style type="text/css">
         @font-face {
-            font-family: "Shonar Bangla";
-            src: {{storage_path("fonts/Shonar Bangla.ttf")}} format("truetype"); /* IE9*/
+            font-family: Shonar-Bangla;
+            src: {{storage_path("fonts/Shonar-Bangla.ttf")}} format("truetype"); /* IE9*/
+            font-weight: bold;
+        }
+
+        @font-face {
+            font-family: Siyamrupali;
+            src: {{storage_path("fonts/Siyamrupali.ttf")}} format("truetype"); /* IE9*/
             font-weight: bold;
         }
 
@@ -24,8 +29,11 @@
             page-break-after: always;
         }
 
+        .break-before {
+            page-break-before: auto;
+        }
         body {
-            font-family: kalpurush, 'Shonar Bangla', DejaVu Sans, Roboto, sans-serif;
+            font-family: Siyamrupali, DejaVu Sans, 'Roboto', sans-serif !important;
             color: #4a4a4a;
         }
 
@@ -61,6 +69,24 @@
         .table-head {
             background-color: #ededed;
         }
+
+        @page {
+            margin: 40px;
+            padding: 2cm;
+        }
+
+        @media print {
+
+            .table {
+                page-break-inside: auto !important;
+            }
+
+            .table tbody tr {
+                page-break-inside: avoid !important;
+                page-break-after: auto !important;
+            }
+
+        }
     </style>
 </head>
 <body align="center">
@@ -75,48 +101,38 @@
             </div>
         </td>
     </tr>
-    <tr>
-        <td>
-            <table class="table table-bordered">
-                <thead>
-                <tr class="table-head">
-                    <th> পণ্যের নাম</th>
-                    <th> বিক্রির পরিমাণ</th>
-                    <th> বিক্রয়মূল্য</th>
-                    <th> গড় বিক্রয়মূল্য</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php $totalQuantity = $totalPrice = 0;?>
-                @foreach($data as $item)
-                    <tr>
-                        <?php $totalPrice += (float)$item['total_price'];
-                        $totalQuantity += (float)$item['total_quantity']; ?>
-                        <td>{{$item['service_name']}}</td>
-                        <td>{{convertNumbersToBangla((float)$item['total_quantity'])}}</td>
-                        <td>{{convertNumbersToBangla((float)$item['total_price'])}}</td>
-                        <td>{{convertNumbersToBangla((float)$item['avg_price'])}}</td>
-                    </tr>
-                @endforeach
-                <tr class="table-head">
-                    <td><span class="font-weight-bold"> মোট</span></td>
-                    <td><span class="font-weight-bold">{{convertNumbersToBangla((float)$totalQuantity)}}</span></td>
-                    <td><span class="font-weight-bold">{{convertNumbersToBangla((float)$totalPrice)}}</span></td>
-                    <td></td>
-                </tr>
-                </tbody>
-            </table>
-        </td>
-    </tr>
     </tbody>
-    <tfoot>
-    <tr>
-        <td style="width: 100%;text-align: center">
-
-            <span>Powered By sManager</span>
-        </td>
-    </tr>
-    </tfoot>
 </table>
+<div>
+    <table class="table table-bordered">
+        <thead>
+        <tr class="table-head">
+            <th> পণ্যের নাম</th>
+            <th> বিক্রির পরিমাণ</th>
+            <th> বিক্রয়মূল্য</th>
+            <th> গড় বিক্রয়মূল্য</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php $totalQuantity = $totalPrice = 0;?>
+        @foreach($data as $item)
+            <tr>
+                <?php $totalPrice += (float)$item['total_price'];
+                $totalQuantity += (float)$item['total_quantity']; ?>
+                <td>{{$item['service_name']}}</td>
+                <td>{{convertNumbersToBangla((float)$item['total_quantity'])}}</td>
+                <td>{{convertNumbersToBangla((float)$item['total_price'])}}</td>
+                <td>{{convertNumbersToBangla((float)$item['avg_price'])}}</td>
+            </tr>
+        @endforeach
+        <tr style="page-break-after: always" class="table-head">
+            <td><span class="font-weight-bold"> মোট</span></td>
+            <td><span class="font-weight-bold">{{convertNumbersToBangla((float)$totalQuantity)}}</span></td>
+            <td><span class="font-weight-bold">{{convertNumbersToBangla((float)$totalPrice)}}</span></td>
+            <td></td>
+        </tr>
+        </tbody>
+    </table>
+</div>
 </body>
 </html>
