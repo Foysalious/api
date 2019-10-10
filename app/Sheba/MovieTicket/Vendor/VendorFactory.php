15df7e4d261e9ea93237d65865bf67bd7d2486e0
Pasha <pasha.startern@gmail.com>
2019-10-10 20:27:55 +0600

    ⬆️  REFORMAT CODE MOVIE VENDORS
    Type(s): UPDATE

--- app/Sheba/MovieTicket/Vendor/VendorFactory.php
+++ app/Sheba/MovieTicket/Vendor/VendorFactory.php
@@ -1,65 +1,33 @@
 <?php namespace Sheba\MovieTicket\Vendor;
 
 use App\Models\MovieTicketVendor;
+use Exception;
 use ReflectionClass;
 use Sheba\MovieTicket\Vendor\BlockBuster\BlockBuster;
 
 class VendorFactory
 {
     const BlockBuster = 1;
-
-    private $classes = [
-        BlockBuster::class
-    ];
-
-    /**
-     * @param $id
-     * @return Vendor
-     * @throws \Exception
-     */
-    public function getById($id)
-    {
-        if(!in_array($id, $this->getConstants())) {
-            throw new \Exception('Invalid Vendor');
-        }
-        return app($this->classes[$id - 1])->setModel($this->getModel($id));
-    }
+    private $classes = [BlockBuster::class];
 
     /**
      * @param $name
      * @return Vendor
-     * @throws \Exception
+     * @throws Exception
      */
     public function getByName($name)
     {
-        if(!in_array($name, array_keys($this->getConstants()))) {
-            throw new \Exception('Invalid Vendor');
+        if (!in_array($name, array_keys($this->getConstants()))) {
+            throw new Exception('Invalid Vendor');
         }
         $id = $this->getConstants()[$name];
         return app($this->classes[$id - 1])->setModel($this->getModel($id));
     }
 
-    /**
-     * @param $name
-     * @return Vendor
-     * @throws \Exception
-     */
-    public function getIdByName($name)
+    private function getConstants()
     {
-        if(!in_array($name, array_keys($this->getConstants()))) {
-            throw new \Exception('Invalid Vendor');
-        }
-        return $this->getConstants()[$name];
-    }
-
-    /**
-     * @param $mobile
-     * @return Vendor
-     * @throws \Exception
-     */
-    public function getByMobile($mobile)
-    {
-        return $this->getById(1);
+        $oClass = new ReflectionClass(__CLASS__);
+        return $oClass->getConstants();
     }
 
     public function getModel($id)
@@ -67,9 +35,39 @@ class VendorFactory
         return MovieTicketVendor::find($id);
     }
 
-    private function getConstants()
+    /**
+     * @param $name
+     * @return Vendor
+     * @throws Exception
+     */
+    public function getIdByName($name)
     {
-        $oClass = new ReflectionClass(__CLASS__);
-        return $oClass->getConstants();
+        if (!in_array($name, array_keys($this->getConstants()))) {
+            throw new Exception('Invalid Vendor');
+        }
+        return $this->getConstants()[$name];
+    }
+
+    /**
+     * @param $mobile
+     * @return Vendor
+     * @throws Exception
+     */
+    public function getByMobile($mobile)
+    {
+        return $this->getById(1);
+    }
+
+    /**
+     * @param $id
+     * @return Vendor
+     * @throws Exception
+     */
+    public function getById($id)
+    {
+        if (!in_array($id, $this->getConstants())) {
+            throw new Exception('Invalid Vendor');
+        }
+        return app($this->classes[$id - 1])->setModel($this->getModel($id));
     }
 }
