e6d7e49390f8bc77dce52d5f0fd6acff7edd6b7a
arnabrahman <arnabrahman@hotmail.com>
2019-10-23 17:08:25 +0600

    ⬆️ Bid route
    Type(s): UPDATE

--- app/Http/Route/Prefix/V2/Partner/ID/Auth/BidRoute.php
+++ app/Http/Route/Prefix/V2/Partner/ID/Auth/BidRoute.php
@@ -6,6 +6,10 @@ class BidRoute
     {
         $api->group(['prefix' => 'bids'], function ($api) {
             $api->post('/', 'Partner\BidController@store');
+
+            $api->group(['prefix' => '{bid}'], function ($api) {
+                $api->post('/', 'Partner\BidController@takeAction');
+            });
             $api->group(['prefix' => '{procurement}'], function ($api) {
                 $api->post('convert', 'Partner\ProcurementController@convertToBid');
             });
