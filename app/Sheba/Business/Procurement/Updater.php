0df08a1c38f99ee575903f6e0985606edb90a93b
Saiful <saiful.sheba@gmail.com>
2019-10-31 18:21:23 +0600

    ⬆️  Procurement Status Change Log
    Type(s): UPDATE

--- app/Sheba/Business/Procurement/Updater.php
+++ app/Sheba/Business/Procurement/Updater.php
@@ -1,10 +1,8 @@
 <?php namespace App\Sheba\Business\Procurement;
 
 use App\Models\Procurement;
-use DB;
-use Sheba\Repositories\Interfaces\ProcurementItemFieldRepositoryInterface;
-use Sheba\Repositories\Interfaces\ProcurementItemRepositoryInterface;
-use Sheba\Repositories\Interfaces\ProcurementQuestionRepositoryInterface;
+use Illuminate\Database\QueryException;
+use Sheba\Business\ProcurementStatusChangeLog\Creator;
 use Sheba\Repositories\Interfaces\ProcurementRepositoryInterface;
 
 class Updater
@@ -12,12 +10,14 @@ class Updater
 
     private $status;
     private $procurement;
+    private $statusLogCreator;
     private $procurementRepository;
 
 
-    public function __construct(ProcurementRepositoryInterface $procurement_repository)
+    public function __construct(ProcurementRepositoryInterface $procurement_repository, Creator $creator)
     {
         $this->procurementRepository = $procurement_repository;
+        $this->statusLogCreator = $creator;
     }
 
     public function setProcurement(Procurement $procurement)
@@ -34,6 +34,13 @@ class Updater
 
     public function updateStatus()
     {
-        $this->procurementRepository->update($this->procurement, ['status' => $this->status]);
+        try {
+            $previous_status = $this->procurement->status;
+            $procurement = $this->procurementRepository->update($this->procurement, ['status' => $this->status]);
+            $this->statusLogCreator->setProcurement($this->procurement)->setPreviousStatus($previous_status)->setStatus($this->status)->create();
+        } catch (QueryException $e) {
+            throw  $e;
+        }
+        return $procurement;
     }
 }
