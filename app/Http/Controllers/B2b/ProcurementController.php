26003324f2dc1f365079a9277d10a62d6c71cbb4
Saiful <saiful.sheba@gmail.com>
2019-11-06 14:39:07 +0600

    ⬆️  Asdf
    Type(s): UPDATE

--- app/Http/Controllers/B2b/ProcurementController.php
+++ app/Http/Controllers/B2b/ProcurementController.php
@@ -287,7 +287,7 @@ class ProcurementController extends Controller
             list($offset, $limit) = calculatePagination($request);
             $procurements = Procurement::order()->with(['bids' => function ($q) {
                 $q->select('id', 'procurement_id', 'bidder_id', 'bidder_type', 'status', 'price');
-            }]);
+            }])->orderBy('id', 'DESC');
             $total_procurement = $procurements->get()->count();
 
             if ($request->has('status')) {
