95dc53e6027bfbf40713213f62418bc8ab74d6a8
arnabrahman <arnabrahman@hotmail.com>
2019-11-06 16:39:13 +0600

    ⬆️Barikoi api
    Type(s): UPDATE

--- app/Http/Controllers/GeocodeController.php
+++ app/Http/Controllers/GeocodeController.php
@@ -7,18 +7,18 @@ use Sheba\Location\Geo;
 
 class GeocodeController extends Controller
 {
-
     public function reverseGeocode(Request $request)
     {
-        $b = new BarikoiClient();
-        $geo = new Geo();
-        $geo->setLat($request->lat)->setLng($request->lng);
-        $address = $b->getAddress($geo);
-        if (!$address) return api_response($request, 500, null);
-        if ($address) return api_response($request, 200, $address,
-            [
-                'location' => ['address' => $address]
-            ]
-        );
+        try {
+            $b = new BarikoiClient();
+            $geo = new Geo();
+            $geo->setLat($request->lat)->setLng($request->lng);
+            $address = $b->getAddress($geo);
+            if (!$address) return api_response($request, null, 404);
+            if ($address) return api_response($request, $address, 200, ['location' => ['address' => $address]]);
+        } catch (\Throwable $e) {
+            app('sentry')->captureException($e);
+            return api_response($request, null, 500);
+        }
     }
 }
