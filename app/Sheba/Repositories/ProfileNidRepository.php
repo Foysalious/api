4e6bf21436242b7620f8d17637220ee9df851101
raqibulHasanReal <raqibul@sheba.xyz>
2019-11-12 20:01:18 +0600

    [WIP]#NID SUBMIT api

--- app/Sheba/Repositories/ProfileNidRepository.php
+++ app/Sheba/Repositories/ProfileNidRepository.php
@@ -5,9 +5,19 @@ use Sheba\Repositories\Interfaces\ProfileNidRepositoryInterface;
 
 class ProfileNidRepository extends BaseRepository implements ProfileNidRepositoryInterface
 {
-    public function store(Request $request)
+    public function saveImage(Request $request)
     {
+        $data = [];
+        if ($request->hasFile('nid_image')) {
+            $data['nid_image'] = $this->_saveNIdImage($request);
+        }
+        return $data;
+    }
 
+    private function _saveNIdImage(Request $request)
+    {
+        list($avatar, $avatar_filename) = $this->makeThumb($request->file('profile_image'), $request->name);
+        return $this->saveImageToCDN($avatar, getResourceAvatarFolder(), $avatar_filename);
     }
 
 }
