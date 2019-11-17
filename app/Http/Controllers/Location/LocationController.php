9ffb54624fe9f621df4a754eb4892a0108467c77
arnabrahman <arnabrahman@hotmail.com>
2019-11-17 12:40:56 +0600

    ⬆️ City list
    Type(s): UPDATE

---
+++ app/Http/Controllers/Location/LocationController.php
@@ -0,0 +1,38 @@
+<?php namespace App\Http\Controllers\Location;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LocationController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        try {
+            $cities = [
+                [
+                    'id' => 1,
+                    'name' => 'Dhaka',
+                    'image' => "https://cdn-shebadev.s3.ap-south-1.amazonaws.com/sheba_xyz/jpg/dhaka.jpg",
+                    'center' => [
+                        'lat' => 23.788994076131,
+                        'lng' => 90.410852011945
+                    ]
+                ],
+                [
+                    'id' => 2,
+                    'name' => 'Chittagong',
+                    'image' => "https://cdn-shebadev.s3.ap-south-1.amazonaws.com/sheba_xyz/jpg/chittagong.jpg",
+                    'center' => [
+                        'lat' => 23.788994076131,
+                        'lng' => 90.410852011945
+                    ]
+                ]
+            ];
+            return api_response($request, $cities, 200, ['cities' => $cities]);
+        } catch (\Throwable $e) {
+            return api_response($request, null, 500);
+        }
+    }
+
+}
