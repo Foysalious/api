406897b13089e7b77b4ade784f9844b5bfdd5df4
tonmoy <saddam.hossain@sheba.xyz>
2019-11-19 15:53:46 +0600

    ⬆️  POS ORDER CREATE
    Type(s): UPDATE

--- app/Sheba/Pos/Validators/OrderCreateValidator.php
+++ app/Sheba/Pos/Validators/OrderCreateValidator.php
@@ -10,19 +10,31 @@ class OrderCreateValidator extends Validator
     private $stockManager;
     /** @var PosServiceRepository $posServiceRepo */
     private $posServiceRepo;
+    /** @var array $services */
+    private $services;
 
     public function __construct(StockManager $stock_manager, PosServiceRepositoryInterface $pos_service_repo)
     {
-        $this->stockManager = $stock_manager;
+        $this->stockManager   = $stock_manager;
         $this->posServiceRepo = $pos_service_repo;
     }
-
-    /** @var array $services */
-    private $services;
 
     public function hasError()
     {
         if ($this->isOutOfStock()) return ['code' => 421, 'msg' => 'Product out of stock.'];
+    }
+
+    private function isOutOfStock()
+    {
+        $is_out_of_stock = false;
+        foreach ($this->services as $service) {
+            $original_service      = isset($service['id']) ? $this->posServiceRepo->find($service['id']) : $this->posServiceRepo->defaultInstance($service);
+            $is_stock_maintainable = $this->stockManager->setPosService($original_service)->isStockMaintainable();
+            if ($is_stock_maintainable) {
+                $is_out_of_stock = $service['quantity'] > $original_service->stock;
+                if ($is_out_of_stock) return true;
+            }
+        }
     }
 
     /**
@@ -31,18 +43,5 @@ class OrderCreateValidator extends Validator
     public function setServices(array $services)
     {
         $this->services = $services;
-    }
-
-    private function isOutOfStock()
-    {
-        $is_out_of_stock = false;
-        foreach ($this->services as $service) {
-            $original_service = $this->posServiceRepo->find($service['id']);
-            $is_stock_maintainable = $this->stockManager->setPosService($original_service)->isStockMaintainable();
-            if ($is_stock_maintainable) {
-                $is_out_of_stock = $service['quantity'] > $original_service->stock;
-                if ($is_out_of_stock) return true;
-            }
-        }
     }
 }
