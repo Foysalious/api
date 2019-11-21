ee066e129311ccd48219d0210b6a3b79d06ff5d0
tonmoy <saddam.hossain@sheba.xyz>
2019-11-21 17:37:49 +0600

    ⬆️  AFFILIATE JOIN CLAN AND REGISTRATION API MAKE AMBASSADOR
    Type(s): UPDATE

--- app/Sheba/Repositories/AffiliateRepository.php
+++ app/Sheba/Repositories/AffiliateRepository.php
@@ -2,6 +2,7 @@
 
 use App\Models\Affiliate;
 use App\Models\AffiliateTransaction;
+use Sheba\Voucher\Creator\BroadcastPromo;
 
 class AffiliateRepository extends BaseRepository
 {
@@ -11,14 +12,29 @@ class AffiliateRepository extends BaseRepository
         $this->walletTransaction($affiliate, $log_data);
     }
 
+    public function walletTransaction(Affiliate $affiliate, $data)
+    {
+        $affiliate->transactions()->save(new AffiliateTransaction($this->withCreateModificationField($data)));
+    }
+
     public function debitWallet(Affiliate $affiliate, $amount, $log_data)
     {
         $affiliate->update(['wallet' => $affiliate->wallet - $amount]);
         $this->walletTransaction($affiliate, $log_data);
     }
 
-    public function walletTransaction(Affiliate $affiliate, $data)
+    public function makeAmbassador(Affiliate $affiliate)
     {
-        $affiliate->transactions()->save(new AffiliateTransaction($this->withCreateModificationField($data)));
+        $voucher = (new BroadcastPromo($affiliate, null, false))
+            ->setVoucherCode($this->getCodeByPhone($affiliate))
+            ->saveVoucher()
+            ->getVoucher();
+
+        $affiliate->update(["is_ambassador" => 1, 'ambassador_code' => $voucher->code]);
+    }
+
+    public function getCodeByPhone(Affiliate $affiliate)
+    {
+        return preg_replace("/\+88|\+/", '', $affiliate->profile->mobile);
     }
 }
