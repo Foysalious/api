0fdb7b3d3785220aa9fb2f6e2b9b36f02162a448
shovan <shovan.sheba@gmail.com>
2019-12-02 15:58:38 +0600

    [add] relation between bank and bank_user

--- app/Models/BankUser.php
+++ app/Models/BankUser.php
@@ -10,5 +10,10 @@ class BankUser extends Model
     {
         return $this->belongsTo(Profile::class);
     }
+    public function bank()
+    {
+        return $this->belongsTo(Bank::class);
+    }
+
 
 }
