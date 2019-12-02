a393828dc68bd10bbe510fb86644a620ca1741c3
arnabrahman <arnabrahman@hotmail.com>
2019-12-02 13:47:59 +0600

    ⬆️ Order loop 2
    Type(s): UPDATE

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Models\Order;
+
 Route::get('/', function () {
+    $orders = Order::whereHas('partnerOrders', function ($q) {
+        $q->where('partner_id', null);
+    })->whereRaw('created_at + INTERVAL 15 MINUTE <= NOW()')->with('partnerOrders.partnerOrderRequests')->get();
+    foreach ($orders as $order) {
+        dd($order);
+    }
     return ['code' => 200, 'message' => "Success. This project will hold the api's"];
 });
 
