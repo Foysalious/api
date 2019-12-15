389bf2025a215d9b2ce4ab66c530b1a1b11d75e0
arnabrahman <arnabrahman@hotmail.com>
2019-12-15 16:56:49 +0600

    ⬆️ Subscription partner list show reason
    Type(s): UPDATE

--- app/Http/Controllers/Subscription/CustomerSubscriptionController.php
+++ app/Http/Controllers/Subscription/CustomerSubscriptionController.php
@@ -52,6 +52,7 @@ class CustomerSubscriptionController extends Controller
             $partner_list->filterPartnerByAvailability();
             $partner_list->removeShebaHelpDesk();
             $partners = $partner_list->partners;
+            if ($request->has('show_reason')) return api_response($request, null, 200, ['reason' => $partner_list->getNotShowingReason()]);
             if ($partners->count() > 0) {
                 $partner_list->addPricing();
                 $partner_list->addInfo();
@@ -64,7 +65,6 @@ class CustomerSubscriptionController extends Controller
                 $partners = $partner_list->partners;
                 return api_response($request, $partners, 200, ['partners' => $partners->values()->all()]);
             }
-            if ($request->has('show_reason')) return api_response($request, null, 200, ['reason' => $partner_list->getNotShowingReason()]);
             return api_response($request, null, 404, ['message' => 'No partner found.']);
         } catch (HyperLocationNotFoundException $e) {
             app('sentry')->captureException($e);
@@ -190,10 +190,10 @@ class CustomerSubscriptionController extends Controller
 
                 if ($subscription_order->partner) {
                     $orders_list["partner"] = [
-                        "id"        => $subscription_order->partner_id,
-                        "name"      => $service_details->name,
-                        "mobile"    => $subscription_order->partner->mobile,
-                        "logo"      => $service_details->logo
+                        "id" => $subscription_order->partner_id,
+                        "name" => $service_details->name,
+                        "mobile" => $subscription_order->partner->mobile,
+                        "logo" => $service_details->logo
                     ];
                 }
                 $subscription_orders_list->push($orders_list);
@@ -293,56 +293,56 @@ class CustomerSubscriptionController extends Controller
 
             $subscription_order_details = [
                 "subscription_code" => $subscription_order->code(),
-                "category_id"       => $service->category->id,
-                "category_name"     => $service->category->name,
-                'service_id'        => $service->id,
-                "service_name"      => $service->name,
-                "app_thumb"         => $service->app_thumb,
-                'description'       => $service->description,
-                "variables"         => $variables,
-                "total_quantity"    => $service_details->total_quantity,
-                'quantity'          => (double)$service_details_breakdown->quantity,
-                'is_weekly'         => $service_subscription->is_weekly,
-                'is_monthly'        => $service_subscription->is_monthly,
-                'min_weekly_qty'    => $service_subscription->min_weekly_qty,
-                'min_monthly_qty'   => $service_subscription->min_monthly_qty,
-                "partner_id"        => $subscription_order->partner_id,
+                "category_id" => $service->category->id,
+                "category_name" => $service->category->name,
+                'service_id' => $service->id,
+                "service_name" => $service->name,
+                "app_thumb" => $service->app_thumb,
+                'description' => $service->description,
+                "variables" => $variables,
+                "total_quantity" => $service_details->total_quantity,
+                'quantity' => (double)$service_details_breakdown->quantity,
+                'is_weekly' => $service_subscription->is_weekly,
+                'is_monthly' => $service_subscription->is_monthly,
+                'min_weekly_qty' => $service_subscription->min_weekly_qty,
+                'min_monthly_qty' => $service_subscription->min_monthly_qty,
+                "partner_id" => $subscription_order->partner_id,
 
-                "partner_name"      => property_exists($service_details, 'name') ? $service_details->name : null,
-                "logo"              => property_exists($service_details, 'logo') ? $service_details->logo : null,
+                "partner_name" => property_exists($service_details, 'name') ? $service_details->name : null,
+                "logo" => property_exists($service_details, 'logo') ? $service_details->logo : null,
 
-                "contact_person"    => $partner ? $partner->getContactPerson() : null,
-                "partner_slug"      => $partner ? $partner->sub_domain : null,
-                "partner_mobile"    => $partner ? $partner->getContactNumber() : null,
-                "partner_address"   => $partner ? $partner->address : null,
-                "avg_rating"        => $partner ? (double)$partner->reviews()->avg('rating') : 0.00,
-                "total_rating"      => $partner ? $partner->reviews->count() : null,
+                "contact_person" => $partner ? $partner->getContactPerson() : null,
+                "partner_slug" => $partner ? $partner->sub_domain : null,
+                "partner_mobile" => $partner ? $partner->getContactNumber() : null,
+                "partner_address" => $partner ? $partner->address : null,
+                "avg_rating" => $partner ? (double)$partner->reviews()->avg('rating') : 0.00,
+                "total_rating" => $partner ? $partner->reviews->count() : null,
 
-                'customer_name'     => $subscription_order->customer->profile->name,
-                'customer_mobile'   => $subscription_order->customer->profile->mobile,
-                'address_id'        => $delivery_address->id,
-                'address'           => $delivery_address->address,
-                'location_name'     => $subscription_order->location ? $subscription_order->location->name : "",
-                'ordered_for'       => $delivery_address->name,
-                "billing_cycle"     => $subscription_order->billing_cycle,
-                "total_orders"      => $subscription_order->orders->count(),
-                "completed_orders"  => $served_orders->count(),
-                "orders_left"       => $subscription_order->orders->count() - $served_orders->count(),
-                "preferred_time"    => $schedules->first()->time,
-                "next_order"        => empty($next_order) ? null : $next_order,
-                "days_left"         => Carbon::today()->diffInDays(Carbon::parse($subscription_order->billing_cycle_end)),
-                'original_price'    => $service_details->original_price,
-                'discount'          => $service_details->discount,
-                'total_price'       => $subscription_order->totalPrice,
-                "paid_on"           => $subscription_order->isPaid() ? $subscription_order->paid_at->format('M-j, Y') : null,
-                'is_paid'           => $subscription_order->isPaid(),
-                "orders"            => $format_partner_orders,
-                'schedule_dates'    => $schedule_dates,
-                'paid'              => $subscription_order->paid,
-                'due'               => $subscription_order->due,
-                "subscription_additional_info"  => $subscription_order->additional_info,
-                "subscription_status"           => $subscription_order->status,
-                "subscription_period"           => Carbon::parse($subscription_order->billing_cycle_start)->format('M j') . ' - ' . Carbon::parse($subscription_order->billing_cycle_end)->format('M j'),
+                'customer_name' => $subscription_order->customer->profile->name,
+                'customer_mobile' => $subscription_order->customer->profile->mobile,
+                'address_id' => $delivery_address->id,
+                'address' => $delivery_address->address,
+                'location_name' => $subscription_order->location ? $subscription_order->location->name : "",
+                'ordered_for' => $delivery_address->name,
+                "billing_cycle" => $subscription_order->billing_cycle,
+                "total_orders" => $subscription_order->orders->count(),
+                "completed_orders" => $served_orders->count(),
+                "orders_left" => $subscription_order->orders->count() - $served_orders->count(),
+                "preferred_time" => $schedules->first()->time,
+                "next_order" => empty($next_order) ? null : $next_order,
+                "days_left" => Carbon::today()->diffInDays(Carbon::parse($subscription_order->billing_cycle_end)),
+                'original_price' => $service_details->original_price,
+                'discount' => $service_details->discount,
+                'total_price' => $subscription_order->totalPrice,
+                "paid_on" => $subscription_order->isPaid() ? $subscription_order->paid_at->format('M-j, Y') : null,
+                'is_paid' => $subscription_order->isPaid(),
+                "orders" => $format_partner_orders,
+                'schedule_dates' => $schedule_dates,
+                'paid' => $subscription_order->paid,
+                'due' => $subscription_order->due,
+                "subscription_additional_info" => $subscription_order->additional_info,
+                "subscription_status" => $subscription_order->status,
+                "subscription_period" => Carbon::parse($subscription_order->billing_cycle_start)->format('M j') . ' - ' . Carbon::parse($subscription_order->billing_cycle_end)->format('M j'),
             ];
 
             $location_service = LocationService::where('location_id', $subscription_order->location_id)->where('service_id', $service->id)->first();
@@ -355,7 +355,7 @@ class CustomerSubscriptionController extends Controller
                 "variable_type" => $service->variable_type
             ];
             $resource = new Item($selected_service, new ServiceV2MinimalTransformer($location_service, $price_calculation));
-            $price_discount_data  = $manager->createData($resource)->toArray();
+            $price_discount_data = $manager->createData($resource)->toArray();
 
             $resource = new Item($service->category, new ServiceV2DeliveryChargeTransformer($delivery_charge, $job_discount_handler));
             $delivery_charge_discount_data = $manager->createData($resource)->toArray();
