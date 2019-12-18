6c1e2d2cc6329b7c27980d4753b9dedf945bf3f7
arnabrahman <arnabrahman@hotmail.com>
2019-12-18 20:01:53 +0600

    ⬆️ Delivery address save
    Type(s): UPDATE

--- app/Http/Controllers/PushSubscriptionController.php
+++ app/Http/Controllers/PushSubscriptionController.php
@@ -52,7 +52,7 @@ class PushSubscriptionController extends Controller
             'event_type' => "bid",
             "title" => "Test notification",
             'message' => "Test notification",
-            'link' => config('sheba.business_url') . "/dashboard/procurement/316/quotation?id=155"
+            'link' => "https://b2b.dev-sheba.xyz/dashboard/fleet-management/requests/151/details"
         ], 233, "App\Models\Partner"));
     }
 }
