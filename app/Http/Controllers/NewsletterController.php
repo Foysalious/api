4de00e17ddf27131429b7f0531b1a5854a5cf7ae
Saiful <saiful.sheba@gmail.com>
2019-12-26 09:33:07 +0600

    ➕ Email Subscribe Newsletter
    Type(s): ADD
    
    Issue(s):
    - JIRA: SHEBA-4228

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -1,6 +1,7 @@
 <?php namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
 use Sheba\Dal\Newsletter\Model as Newsletter;
 use Illuminate\Http\Request;
 use Sheba\Newsletter\Creator;
@@ -9,11 +10,20 @@ class NewsletterController extends Controller
 {
     public function create(Request $request, Creator $creator)
     {
-        $this->validate($request, [
-            'email' => 'required|email',
-            'portal_name' => 'required|string|in:' . implode(',', config('sheba.portals'))
-        ]);
-        $creator->setEmail($request->email)->setPortalName($request->portal_name)->setIp($request->ip);
-        $creator->store();
+        try {
+            $this->validate($request, [
+                'email' => 'required|email',
+                'portal_name' => 'required|string|in:' . implode(',', config('sheba.portals'))
+            ]);
+            $creator->setEmail($request->email)->setPortalName($request->portal_name)->setIp($request->ip());
+            $creator->store();
+            return api_response($request, 1, 200);
+        } catch (ValidationException $e) {
+            $message = getValidationErrorMessage($e->validator->errors()->all());
+            return api_response($request, $message, 400, ['message' => $message]);
+        } catch (\Throwable $e) {
+            app('sentry')->captureException($e);
+            return api_response($request, null, 500);
+        }
     }
 }
