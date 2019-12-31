15efec47531a7f713e7a74e9086c125ec7e82eb7
shovan <shovan.sheba@gmail.com>
2019-12-31 11:38:19 +0600

    [update] pos category item  name wise sorting(asc)

--- app/Http/Controllers/Pos/CategoryController.php
+++ app/Http/Controllers/Pos/CategoryController.php
@@ -47,7 +47,7 @@ class CategoryController extends Controller
                             ->select($this->getSelectColumnsOfServiceDiscount());
 
                         $discounts_query->where($updated_after_clause);
-                    }])->select($this->getSelectColumnsOfService());
+                    }])->select($this->getSelectColumnsOfService())->orderBy('name','asc');
 
                 }])->select($this->getSelectColumnsOfCategory());
             if ($request->has('updated_after')) {
