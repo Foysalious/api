5e693ffad2b1f6360b7f4ca98ed6398997c77a2f
arnabrahman <arnabrahman@hotmail.com>
2020-01-05 11:22:17 +0600

    ⬆️ Service content
    Type(s): UPDATE

--- app/Sheba/Service/ServiceQuestion.php
+++ app/Sheba/Service/ServiceQuestion.php
@@ -20,19 +20,23 @@ class ServiceQuestion
         $options = (json_decode($this->service->variables))->options;
         foreach ($options as &$option) {
             $option->answers = explode(',', $option->answers);
-            $option->contents = [
-                'image' => 'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/bulk/jpg/Services/74/600.jpg',
-                'description' => [
-                    "We have more than 300 services",
-                    "Verified experts all arround the country"
-                ],
-                'images' => [
-                    'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/bulk/jpg/Services/74/600.jpg',
-                    'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/bulk/jpg/Services/74/600.jpg',
-                    'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/bulk/jpg/Services/74/600.jpg',
-                    'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/bulk/jpg/Services/74/600.jpg'
-                ]
-            ];
+            $contents = [];
+            foreach ($option->answers as $answer) {
+                array_push($contents, [
+                    'image' => 'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/bulk/jpg/Services/74/600.jpg',
+                    'description' => [
+                        "We have more than 300 services",
+                        "Verified experts all arround the country"
+                    ],
+                    'images' => [
+                        'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/bulk/jpg/Services/74/600.jpg',
+                        'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/bulk/jpg/Services/74/600.jpg',
+                        'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/bulk/jpg/Services/74/600.jpg',
+                        'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/bulk/jpg/Services/74/600.jpg'
+                    ]
+                ]);
+            }
+            $option->contents = $contents;
         }
         return $options;
     }
