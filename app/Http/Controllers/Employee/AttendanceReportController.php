d7b6e938927d547f1b44ad5f768c5fdfe28f2bf1
Pasha <pasha.startern@gmail.com>
2020-01-13 20:46:08 +0600

    ⬆️  DUMMY TRANSFORMAR REPORTS
    Type(s): UPDATE

--- app/Http/Controllers/Employee/AttendanceReportController.php
+++ app/Http/Controllers/Employee/AttendanceReportController.php
@@ -1,12 +1,20 @@
 <?php namespace App\Http\Controllers\Employee;
 
 use App\Http\Controllers\Controller;
+use App\Transformers\Business\Attendance\ReportTransformer;
 use Illuminate\Http\Request;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Serializer\ArraySerializer;
 
 class AttendanceReportController extends Controller
 {
     public function index(Request $request)
     {
-        dd($request->all());
+        /*$manager = new Manager();
+        $manager->setSerializer(new ArraySerializer());
+        $announcements = new Collection($announcements, new ReportTransformer());
+        $announcements = $manager->createData($announcements)->toArray()['data'];
+        return api_response($request, $announcements, 200, ['announcements' => $announcements]);*/
     }
 }
