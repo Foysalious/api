8db64eb9673e86f4e1ff6b1f8c1890468a669bb9
arnabrahman <arnabrahman@hotmail.com>
2020-01-14 12:37:23 +0600

    ⬆️ Service controller cleanup
    Type(s): UPDATE

--- app/Http/Controllers/Service/ServiceController.php
+++ app/Http/Controllers/Service/ServiceController.php
@@ -8,20 +8,13 @@ use App\Models\LocationService;
 use App\Models\Service;
 use App\Transformers\Service\ServiceTransformer;
 use Illuminate\Http\Request;
-use Illuminate\Support\Collection;
 use League\Fractal\Manager;
 use League\Fractal\Resource\Item;
-use Sheba\Checkout\DeliveryCharge;
-use Sheba\JobDiscount\JobDiscountHandler;
-use Sheba\LocationService\PriceCalculation;
-use Sheba\LocationService\UpsellCalculation;
 
 class ServiceController extends Controller
 {
-    /**
-     * @todo Code refactor
-     */
-    public function show($service, Request $request, ServiceTransformer $service_transformer, PriceCalculation $price_calculation, UpsellCalculation $upsell_calculation, DeliveryCharge $delivery_charge, JobDiscountHandler $job_discount_handler)
+
+    public function show($service, Request $request, ServiceTransformer $service_transformer)
     {
         $this->validate($request, ['lat' => 'required|numeric', 'lng' => 'required|numeric']);
         $hyperLocation = HyperLocal::insidePolygon((double)$request->lat, (double)$request->lng)->with('location')->first();
@@ -39,28 +32,5 @@ class ServiceController extends Controller
         return api_response($request, $data, 200, ['service' => $data]);
     }
 
-    /**
-     * @param PriceCalculation $price_calculation
-     * @param $prices
-     * @param UpsellCalculation $upsell_calculation
-     * @param LocationService $location_service
-     * @return Collection
-     */
-    private function formatOptionWithPrice(PriceCalculation $price_calculation, $prices,
-                                           UpsellCalculation $upsell_calculation, LocationService $location_service)
-    {
-        $options = collect();
-        foreach ($prices as $key => $price) {
-            $option_array = explode(',', $key);
-            $options->push([
-                'option' => collect($option_array)->map(function ($key) {
-                    return (int)$key;
-                }),
-                'price' => $price_calculation->setOption($option_array)->getUnitPrice(),
-                'upsell_price' => $upsell_calculation->setOption($option_array)->getAllUpsellWithMinMaxQuantity()
-            ]);
-        }
-        return $options;
-    }
 
 }
