36119c1781469b542b155134e13a0a7016feb7a0
arnabrahman <arnabrahman@hotmail.com>
2020-01-14 20:19:52 +0600

    ⬆️ Attendance ongoing
    Type(s): UPDATE

--- app/Http/Controllers/Employee/AttendanceController.php
+++ app/Http/Controllers/Employee/AttendanceController.php
@@ -2,35 +2,28 @@
 
 use App\Http\Controllers\Controller;
 use App\Models\BusinessMember;
-use App\Transformers\Business\Attendance\ReportTransformer;
 use Illuminate\Http\Request;
-use League\Fractal\Manager;
-use League\Fractal\Resource\Collection;
-use League\Fractal\Serializer\ArraySerializer;
-use Sheba\Business\Attendance\AttendanceAction;
+use Sheba\Business\AttendanceActionLog\AttendanceAction;
 use Sheba\Dal\Attendance\EloquentImplementation;
 use Sheba\Dal\AttendanceActionLog\Actions;
+use Sheba\ModificationFields;
 
 class AttendanceController extends Controller
 {
-    public function index(Request $request)
-    {
-        /*$manager = new Manager();
-        $manager->setSerializer(new ArraySerializer());
-        $announcements = new Collection($announcements, new ReportTransformer());
-        $announcements = $manager->createData($announcements)->toArray()['data'];
-        return api_response($request, $announcements, 200, ['announcements' => $announcements]);*/
-    }
+    use ModificationFields;
 
     public function takeAction(Request $request, EloquentImplementation $attendance_repository, AttendanceAction $attendance_action)
     {
-        $this->validate($request, ['action' => 'string|in:' . implode(',', Actions::get())]);
+        $this->validate($request, ['action' => 'required|string|in:' . implode(',', Actions::get())]);
         $auth_info = $request->auth_info;
         $business_member = $auth_info['business_member'];
         /** @var BusinessMember $business_member */
         $business_member = BusinessMember::find($business_member['id']);
-        $business_member->attendanceOfToday();
-        $attendance_action->setBusinessMember($business_member)->setActionName($request->action_name)->doAction();
-        $attendance_repository = $attendance_repository->where('business_member_id', $business_member['id']);
+        $this->setModifier($business_member->member);
+        $attendance_action->setBusinessMember($business_member)->setAction($request->action);
+        if (!$attendance_action->canTakeThisAction()) return api_response($request, null, 403);
+        $action = $attendance_action->doAction();
+        if ($action) return api_response($request, $action, 200);
+        return api_response($request, null, 500);
     }
 }
---
+++ app/Sheba/Business/AttendanceActionLog/Creator.php
@@ -0,0 +1,79 @@
+<?php namespace Sheba\Business\AttendanceActionLog;
+
+
+use Sheba\Dal\Attendance\Model as Attendance;
+use Sheba\Dal\AttendanceActionLog\EloquentImplementation;
+
+class Creator
+{
+    private $attendanceActionLogRepository;
+    private $action;
+    private $deviceId;
+    /** @var Attendance */
+    private $attendance;
+    private $ip;
+    private $userAgent;
+
+    public function __construct(EloquentImplementation $attendance_action_log_repository)
+    {
+        $this->attendanceActionLogRepository = $attendance_action_log_repository;
+    }
+
+    /**
+     * @param mixed $action
+     * @return Creator
+     */
+    public function setAction($action)
+    {
+        $this->action = $action;
+        return $this;
+    }
+
+    /**
+     * @param mixed $deviceId
+     * @return Creator
+     */
+    public function setDeviceId($deviceId)
+    {
+        $this->deviceId = $deviceId;
+        return $this;
+    }
+
+    /**
+     * @param mixed $ip
+     * @return Creator
+     */
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+        return $this;
+    }
+
+    /**
+     * @param mixed $userAgent
+     * @return Creator
+     */
+    public function setUserAgent($userAgent)
+    {
+        $this->userAgent = $userAgent;
+        return $this;
+    }
+
+    /**
+     * @param Attendance $attendance
+     * @return Creator
+     */
+    public function setAttendance($attendance)
+    {
+        $this->attendance = $attendance;
+        return $this;
+    }
+
+    public function create()
+    {
+        $this->attendanceActionLogRepository->create([
+            'attendance_id' => $this->attendance->id,
+            'action' => $this->action
+        ]);
+    }
+}
