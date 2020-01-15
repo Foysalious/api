f3c3f6a0df5279658c67f2b8171d7f7fe4bfd969
shovan <shovan.sheba@gmail.com>
2020-01-15 14:36:44 +0600

    [update] stop sms send after pos order create

--- app/Http/Controllers/Pos/OrderController.php
+++ app/Http/Controllers/Pos/OrderController.php
@@ -195,7 +195,7 @@ class OrderController extends Controller
             if ($error = $creator->hasError()) return $error;
             $order = $creator->create();
             $order = $order->calculate();
-            if ($partner->wallet >= 1) $this->sendCustomerSms($order);
+           /* if ($partner->wallet >= 1) $this->sendCustomerSms($order);*/
             $this->sendCustomerEmail($order);
             $order->payment_status      = $order->getPaymentStatus();
             $order->client_pos_order_id = $request->client_pos_order_id;
