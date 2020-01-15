b1e26234ac1b6b0683a6350bea068afec607b8c6
arnabrahman <arnabrahman@hotmail.com>
2020-01-15 20:23:48 +0600

    ⬆️ Address filter location and preferred partner 404
    Type(s): UPDATE

--- app/Http/Controllers/Customer/CustomerPartnerController.php
+++ app/Http/Controllers/Customer/CustomerPartnerController.php
@@ -11,6 +11,7 @@ class CustomerPartnerController extends Controller
 
     public function getPreferredPartners($customer, Request $request, Recommended $recommended, Geo $geo, ServiceRequest $service_request)
     {
+        return api_response($request, null, 404);
         $this->validate($request, ['services' => 'required|string', 'lat' => 'required|numeric', 'lng' => 'required|numeric']);
         $service_request_object = $service_request->setServices(json_decode($request->services, 1))->get();
         $geo->setLat($request->lat)->setLng($request->lng);
--- app/Http/Controllers/CustomerDeliveryAddressController.php
+++ app/Http/Controllers/CustomerDeliveryAddressController.php
@@ -93,10 +93,9 @@ class CustomerDeliveryAddressController extends Controller
             $location = null;
             $customer_delivery_addresses = $customer->delivery_addresses()->select('id', 'location_id', 'address', 'name', 'geo_informations', 'flat_no')->get();
 
-            /*$hyper_location = HyperLocal::insidePolygon((double)$request->lat, (double)$request->lng)->first();
-            if ($hyper_location) $location = $hyper_location->location;*/
-
-            $location = Location::find(4);
+            $hyper_location = HyperLocal::insidePolygon((double)$request->lat, (double)$request->lng)->first();
+            if ($hyper_location) $location = $hyper_location->location;
+            
             if ($location == null) return api_response($request, null, 404, ['message' => "No address at this location"]);
 
             $customer_order_addresses = $customer->orders()->selectRaw('delivery_address,count(*) as c')->groupBy('delivery_address')->orderBy('c', 'desc')->get();
