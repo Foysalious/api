7d15464b068c9565cf9f1d6cfd5d4fcb08a29fac
Pasha <pasha.startern@gmail.com>
2020-01-21 14:24:44 +0600

    ⬆️  STATUS CALCULATOR DATA - DUMMY
    Type(s): UPDATE

--- app/Sheba/Business/AttendanceActionLog/StatusCalculator.php
+++ app/Sheba/Business/AttendanceActionLog/StatusCalculator.php
@@ -1,6 +1,32 @@
 <?php namespace Sheba\Business\AttendanceActionLog;
 
+use Sheba\Dal\Attendance\Model as Attendance;
+use Sheba\Dal\Attendance\Statuses;
 
 class StatusCalculator
 {
+    /** @var Attendance $attendance */
+    private $attendance;
+    private $action;
+
+    public function __construct()
+    {
+    }
+
+    public function setAttendance(Attendance $attendance)
+    {
+        $this->attendance = $attendance;
+        return $this;
+    }
+
+    public function setAction($action)
+    {
+        $this->action = $action;
+        return $this;
+    }
+
+    public function calculate()
+    {
+        return Statuses::ON_TIME;
+    }
 }
