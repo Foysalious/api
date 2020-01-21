6b063e8b81e9c869b7b35a5accd2bbd25e98205e
arnabrahman <arnabrahman@hotmail.com>
2020-01-21 09:32:56 +0600

    ⬆️ Preferred partner not found response
    Type(s): UPDATE

--- app/Http/Controllers/Customer/CustomerPartnerController.php
+++ app/Http/Controllers/Customer/CustomerPartnerController.php
@@ -10,6 +10,7 @@ class CustomerPartnerController extends Controller
 {
     public function getPreferredPartners($customer, Request $request, Recommended $recommended, Geo $geo, ServiceRequest $service_request)
     {
+        return api_response($request, null, 404);
         $this->validate($request, [
             'services' => 'required|string',
             'lat' => 'required|numeric', 'lng' => 'required|numeric'
