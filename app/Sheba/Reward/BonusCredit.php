cd6b3f02759731991e58b671f24c9a2026e07a90
arnabrahman <arnabrahman@hotmail.com>
2020-01-23 13:39:21 +0600

    ⬆️ WIP
    Type(s): UPDATE

--- app/Sheba/Reward/BonusCredit.php
+++ app/Sheba/Reward/BonusCredit.php
@@ -1,71 +1,75 @@
 <?php namespace Sheba\Reward;
 
+use Sheba\HasWallet;
 use Sheba\ModificationFields;
 use Sheba\Payment\PayableType;
 use Sheba\Repositories\BonusLogRepository;
+use DB;
 
 class BonusCredit
 {
     use ModificationFields;
     private $user;
     /** @var PayableType */
-    private $payable_type;
-
+    private $payableType;
     private $logRepo;
-    private $spent_model;
+    private $log;
+
     public function __construct(BonusLogRepository $log_repo)
     {
         $this->logRepo = $log_repo;
     }
 
-    public function setUser($user)
+    public function setUser(HasWallet $user)
     {
         $this->user = $user;
         return $this;
     }
 
-    public function setPayableType(PayableType $payable_type)
+    public function setLog($log)
     {
-        $this->payable_type = $payable_type;
+        $this->log = $log;
         return $this;
     }
 
-    public function deduct($amount, $log = '')
+    public function setPayableType(PayableType $payable_type)
+    {
+        $this->payableType = $payable_type;
+        return $this;
+    }
+
+    public function deduct($amount)
     {
         $original_amount = $amount;
-        $bonuses = $this->user->bonuses()->valid()->orderBy('valid_till')->get();
-        foreach ($bonuses as $bonus) {
-            if ($amount == 0) break;
-            elseif ($bonus->amount >= $amount) {
-                if ($bonus->amount != $amount) {
-                    $this->createNewBonus($amount, $bonus);
-                    $bonus->amount = $amount;
+        DB::transaction(function () use (&$original_amount, &$amount) {
+            $bonuses = $this->user->bonuses()->valid()->orderBy('valid_till')->get();
+            foreach ($bonuses as $bonus) {
+                if ($amount == 0) break;
+                elseif ($bonus->amount >= $amount) {
+                    if ($bonus->amount != $amount) {
+                        $this->createNewBonus($amount, $bonus);
+                        $bonus->amount = $amount;
+                    }
+                    $amount = 0;
+                } elseif ($bonus->amount < $amount) {
+                    $amount = $amount - $bonus->amount;
                 }
-                $amount = 0;
-            } elseif ($bonus->amount < $amount) {
-                $amount = $amount - $bonus->amount;
+                $this->updateExistingBonus($bonus);
             }
-            $this->updateExistingBonus($bonus);
-        }
 
-        if ($amount < $original_amount) $this->saveLog($original_amount - $amount, $log);
+            if ($amount < $original_amount) $this->saveLog($original_amount - $amount);
+        });
 
         return $amount;
     }
 
-    public function setSpentModel($spent_on)
-    {
-        $this->spent_model = $spent_on;
-        return $this;
-    }
-
-    private function saveLog($amount, $log = '')
+    private function saveLog($amount)
     {
         $data = $this->getSpentInfo();
         $data['user_type'] = get_class($this->user);
         $data['user_id'] = $this->user->id;
         $data['amount'] = $amount;
-        $data['log'] = $log;
+        $data['log'] = $this->log;
         $data['valid_till'] = null;
         $this->logRepo->storeDebitLog($data);
     }
@@ -74,8 +78,8 @@ class BonusCredit
     private function getSpentInfo()
     {
         return [
-            'spent_on_type' => get_class($this->payable_type),
-            'spent_on_id' => $this->payable_type->id
+            'spent_on_type' => get_class($this->payableType),
+            'spent_on_id' => $this->payableType->id
         ];
     }
 
@@ -89,7 +93,7 @@ class BonusCredit
 
     private function setSpentInfo($bonus)
     {
-        if ($this->payable_type) {
+        if ($this->payableType) {
             $data = $this->getSpentInfo();
             $bonus->spent_on_type = $data['spent_on_type'];
             $bonus->spent_on_id = $data['spent_on_id'];
