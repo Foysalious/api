2c05180edd58b67bd88374d43f649e6836163807
Shafiqul Islam <shafiq.startern@gmail.com>
2020-01-25 15:09:12 +0600

    ⚗ Sonarqube issues
    Type(s): EXPERIMENTAL

--- app/Exceptions/ApiValidationException.php
+++ app/Exceptions/ApiValidationException.php
@@ -5,9 +5,5 @@ use Throwable;
 
 class ApiValidationException extends Exception
 {
-    public function __construct($message = "", $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
 
-    }
 }
