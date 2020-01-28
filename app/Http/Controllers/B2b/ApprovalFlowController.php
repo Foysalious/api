0b15783e2c3d01c357fbb15d90c8b902ab610ed9
Saiful <saiful.sheba@gmail.com>
2020-01-28 16:02:17 +0600

    ➕ Approval Flow Create
    Type(s): ADD
    
    Issue(s):
    - JIRA: SHEBA-4342

--- app/Http/Controllers/B2b/ApprovalFlowController.php
+++ app/Http/Controllers/B2b/ApprovalFlowController.php
@@ -1,19 +1,27 @@
 <?php namespace App\Http\Controllers\B2b;
 
+use Illuminate\Validation\ValidationException;
+use Sheba\Business\ApprovalFlow\Creator;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB as DBTransaction;
-use Illuminate\Validation\ValidationException;
-use Sheba\Dal\TripRequestApprovalFlow\Model as TripRequestApprovalFlow;
+
 
 class ApprovalFlowController extends Controller
 {
-    public function createApprovalFlow(Request $request)
+    public function createApprovalFlow(Request $request, Creator $creator)
     {
         try {
             $this->validate($request, [
-                'status' => 'required|string|in:accept,reject'
+                'title' => 'required|string',
+                'business_department_id' => 'required|integer',
+                'business_member_ids' => 'required'
             ]);
+            $approval_flow = $creator->setMember($request->member)
+                ->setTitle($request->title)
+                ->setBusinessDepartmentId($request->business_department_id)
+                ->setBusinessMemberIds($request->business_member_ids)
+                ->store();
+            return api_response($request, $approval_flow, 200, ['id' => $approval_flow->id]);
         } catch (ValidationException $e) {
             $message = getValidationErrorMessage($e->validator->errors()->all());
             $sentry = app('sentry');
--- app/Sheba/Business/ApprovalFlow/Creator.php
+++ app/Sheba/Business/ApprovalFlow/Creator.php
@@ -1,21 +1,69 @@
 <?php namespace Sheba\Business\ApprovalFlow;
 
+use Illuminate\Support\Facades\DB;
+use Sheba\Dal\TripRequestApprovalFlow\Model as TripRequestApprovalFlow;
+use Sheba\Dal\TripRequestApprovalFlow\TripRequestApprovalFlowRepositoryInterface;
+use Sheba\ModificationFields;
+
 class Creator
 {
+    use ModificationFields;
+    private $member;
+    private $businessMembers;
+    private $superAdmins;
+    private $title;
+    private $businessDepartmentId;
+    private $businessMemberIds;
+    private $data = [];
+    private $approvalFlowRepo;
+
+    public function __construct(TripRequestApprovalFlowRepositoryInterface $approval_flow_repo)
+    {
+        $this->approvalFlowRepo = $approval_flow_repo;
+    }
+
     public function setMember($member)
     {
         $this->member = $member;
         return $this;
     }
-    public function setBusinessMember($business_member)
+
+    public function setTitle($title)
     {
-        $this->businessMember = $business_member;
+        $this->title = $title;
         return $this;
     }
 
-    public function setSuperAdmins($super_admins)
+    public function setBusinessDepartmentId($business_department_id)
     {
-        $this->superAdmins = $super_admins;
+        $this->businessDepartmentId = $business_department_id;
         return $this;
     }
+
+    public function setBusinessMemberIds($business_member_ids)
+    {
+        $this->businessMemberIds = json_decode($business_member_ids);
+        return $this;
+    }
+
+    public function makeData()
+    {
+        $this->data = [
+            'title' => $this->title,
+            'business_department_id' => $this->businessDepartmentId
+        ];
+    }
+
+    public function store()
+    {
+        $this->makeData();
+        DB::beginTransaction();
+        $this->setModifier($this->member);
+        $approval_flow = $this->approvalFlowRepo->create($this->data);
+        $approval_flow->approvers()->sync($this->businessMemberIds);
+        DB::commit();
+        return $approval_flow;
+
+    }
+
 }
