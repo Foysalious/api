dbabcaff86e459dd0f40e22ff7557728799e8118
shovan <shovan.sheba@gmail.com>
2020-02-04 20:00:33 +0600

    [update] slider image link changed

--- config/partner.php
+++ config/partner.php
@@ -56,8 +56,8 @@ return [
         ],
         'description' => 'ঘরে-বাইরে যেকোনো সময় দ্রুত পেমেন্ট সংগ্রহ করার জন্য আপনার বিকাশ, নগদ, মাস্টার কার্ড অথবা অন্যান্য একাউন্টের QR কোডের ছবি আপলোড করুন।',
         'slider_image' => [
-            'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/partners/logos/1519727255_express_solution.jpg',
-            'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/images/partners/logos/1519727255_express_solution.jpg'
+            'https://cdn-shebadev.s3.ap-south-1.amazonaws.com/partner_assets/assets/images/home_v3/qr_banner_01.jpg',
+            'https://cdn-shebadev.s3.ap-south-1.amazonaws.com/partner_assets/assets/images/home_v3/qr_banner_02.jpg'
         ]
     ]
 ];
