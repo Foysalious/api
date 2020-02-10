5e18721632340e600e3a95830de9fc7478cbfb30
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-02-10 18:35:00 +0600

    [UPDATE] #Weekends included to holiday API

--- app/Http/Controllers/Employee/HolidayController.php
+++ app/Http/Controllers/Employee/HolidayController.php
@@ -11,13 +11,14 @@ use Sheba\Dal\BusinessHoliday\Contract as BusinessHolidayRepoInterface;
 
 class HolidayController extends Controller
 {
-    public function getHolidays(Request $request, BusinessHolidayRepoInterface $business_holiday_repo)
+    public function getHolidays(Request $request, BusinessHolidayRepoInterface $business_holiday_repo,BusinessWeekendRepoInterface $business_weekend_repo)
     {
         $business_member = $this->getBusinessMember($request);
         if (!$business_member) return api_response($request, null, 404);
 
         $business_holidays = $business_holiday_repo->getAllByBusiness($business_member->business);
-        return api_response($request, null,200, ['holidays' => $business_holidays]);
+        $weekend = $business_weekend_repo->weekendDates($business_member->business);
+        return api_response($request, null,200, ['holidays' => $business_holidays, 'weekends' => $weekend]);
     }
 
     private function getBusinessMember(Request $request)
