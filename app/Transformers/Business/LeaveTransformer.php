86a2aa7c71b1c7dbd7c4477b64e459bdd5739e9e
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-02-12 12:57:58 +0600

    [UPDATE] #change start and end date format

--- app/Transformers/Business/LeaveTransformer.php
+++ app/Transformers/Business/LeaveTransformer.php
@@ -7,14 +7,12 @@ class LeaveTransformer extends TransformerAbstract
 {
     public function transform(LeaveModel $leave)
     {
-        $start_date = new \DateTime($leave->start_date);
-        $end_date = new \DateTime($leave->end_date);
         return [
             'title' => $leave->title,
             'leave_type' => $leave->leaveType->title,
             'start_date' => $leave->start_date,
             'end_date' => $leave->end_date,
-            'total_days' => $start_date->diff($end_date)->format("%r%a"),
+            'total_days' => $leave->total_days,
             'status' => $leave->status,
             'requested_on' => $leave->created_at
         ];
