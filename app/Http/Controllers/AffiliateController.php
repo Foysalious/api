89e094a895cd3bd1671c98a3c3358cdab3ca74a8
shovan <shovan.sheba@gmail.com>
2020-02-12 18:42:18 +0600

    [fix] discard dd()

--- app/Http/Controllers/AffiliateController.php
+++ app/Http/Controllers/AffiliateController.php
@@ -1202,7 +1202,6 @@ GROUP BY affiliate_transactions.affiliate_id', [$affiliate->id, $agent_id]));
             $message = getValidationErrorMessage($e->validator->errors()->all());
             return api_response($request, $message, 400, ['message' => $message]);
         } catch (\Throwable $e) {
-            dd($e);
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
         }
