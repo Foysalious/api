40d9327bb8f87ca4cd4c4ee0385f67f69118c7db
Saiful <saiful.sheba@gmail.com>
2020-02-13 19:18:28 +0600

    ⬆️  Partner_Wallet_Setting Create With Vendor Invite
    Type(s): UPDATE

--- app/Sheba/Repositories/PartnerWalletSettingRepository.php
+++ app/Sheba/Repositories/PartnerWalletSettingRepository.php
@@ -1,8 +1,13 @@
 <?php namespace App\Sheba\Repositories;
 
-use App\Repositories\BaseRepository;
+use App\Models\PartnerWalletSetting;
+use Sheba\Repositories\BaseRepository;
 
 class PartnerWalletSettingRepository extends BaseRepository
 {
-
+    public function __construct(PartnerWalletSetting $partnerWalletSetting)
+    {
+        parent::__construct();
+        $this->setModel($partnerWalletSetting);
+    }
 }
