3ca3b8f68f25deb0d9956a322d5ae763b091f317
Saiful <saiful.sheba@gmail.com>
2020-02-18 16:43:26 +0600

    ➕ Typo
    Type(s): ADD

--- app/Http/Controllers/Customer/CustomerController.php
+++ app/Http/Controllers/Customer/CustomerController.php
@@ -1,7 +1,9 @@
 <?php namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
+use App\Models\HyperLocal;
 use App\Models\Job;
+use App\Models\Location;
 use App\Models\LocationService;
 use App\Models\Review;
 use Carbon\Carbon;
@@ -23,7 +25,11 @@ class CustomerController extends Controller
                                   JobDiscountHandler $job_discount_handler, UpsellCalculation $upsell_calculation, ServiceQuestion $service_question)
     {
         $customer = $request->customer;
-        $reviews = Review::where([['customer_id', $customer->id], ['rating', '>=', 4]])->select('id', 'category_id', 'job_id', 'rating', 'partner_id')
+        if ($request->has('lat')) {
+            $hyper_location = HyperLocal::insidePolygon((double)$request->lat, (double)$request->lng)->with('location')->first();
+            if (!is_null($hyper_location)) $location = $hyper_location->location_id;
+        }
+        /*$reviews = Review::where([['customer_id', $customer->id], ['rating', '>=', 4]])->select('id', 'category_id', 'job_id', 'rating', 'partner_id')
             ->with(['category' => function ($q) {
                 $q->select('id', 'name', 'thumb', 'app_thumb', 'banner', 'app_banner', 'frequency_in_days');
             }, 'job' => function ($q) {
@@ -38,8 +44,34 @@ class CustomerController extends Controller
                         $q->select('id', 'location_id');
                     }]);
                 }]);
-            }])->where('created_at', '>=', Carbon::now()->subMonths(6)->toDateTimeString())->orderBy('id', 'desc');
-        if ($request->has('category_id')) $reviews = $reviews->where('category_id', $request->category_id);
+            }])->where('created_at', '>=', Carbon::now()->subMonths(6)->toDateTimeString())->orderBy('id', 'desc');*/
+        $reviews = Review::where([['customer_id', $customer->id], ['rating', '>=', 4]])->select('id', 'category_id', 'job_id', 'rating', 'partner_id')
+            ->whereHas('category', function ($q) use ($location) {
+                $q->published()->select('id', 'name', 'thumb', 'app_thumb', 'banner', 'app_banner', 'frequency_in_days', 'publication_status')
+                    ->whereHas('locations', function ($q) use ($location) {
+                        $q->where('locations.id', $location);
+                    });
+            })->whereHas('job', function ($q) use ($location) {
+                $q->select('id', 'category_id', 'partner_order_id')->with('category')->with(['jobServices' => function ($q) use ($location) {
+                    $q->select('id', 'job_id', 'service_id', 'quantity', 'option', 'variable_type', 'created_at')
+                        ->whereHas('service', function ($q) use ($location) {
+                            $q->published()->select('id', 'name', 'min_quantity', 'thumb', 'app_thumb', 'banner', 'app_banner', 'variables', 'variable_type', 'publication_status')
+                                ->whereHas('locations', function ($q) use ($location) {
+                                    $q->where('locations.id', $location);
+                                });
+                        });
+                }, 'partnerOrder' => function ($q) {
+                    $q->select('id', 'order_id', 'partner_id')->with(['partner' => function ($q) {
+                        $q->select('id', 'name', 'logo');
+                    }, 'order' => function ($q) {
+                        $q->select('id', 'location_id');
+                    }]);
+                }]);
+            })->where('created_at', '>=', Carbon::now()->subMonths(6)->toDateTimeString())->orderBy('id', 'desc')->toSql();
+
+        if ($request->has('category_id')) {
+            $reviews = $reviews->where('category_id', $request->category_id);
+        }
         $reviews = $reviews->get();
         if (count($reviews) == 0) return api_response($request, null, 404);
         $final = collect();
