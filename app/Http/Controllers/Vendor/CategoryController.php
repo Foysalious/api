d48e3a0002d337bbd1917288bba0fce8d5866722
arnabrahman <arnabrahman@hotmail.com>
2020-02-24 10:49:35 +0600

    ⬆️ Category null check for vendor services api
    Type(s): Update

--- app/Http/Controllers/Vendor/CategoryController.php
+++ app/Http/Controllers/Vendor/CategoryController.php
@@ -56,21 +56,17 @@ class CategoryController extends Controller
 
     public function getServices($category, Request $request)
     {
-        try {
-            $this->validate($request, [
-                'location' => 'sometimes|numeric',
-                'lat' => 'sometimes|numeric',
-                'lng' => 'required_with:lat'
-            ]);
-            $category = $this->api->get('/v1/categories/' . $category . '/services?location=' . $request->location . '&lat=' . $request->lat . '&lng=' . $request->lng);
-            $services = $category['services'];
-            $fractal = new Manager();
-            $fractal->setSerializer(new CustomSerializer());
-            $resource = new Collection($services, new ServiceTransformer());
-            return response()->json($fractal->createData($resource)->toArray());
-        } catch (\Throwable $e) {
-            app('sentry')->captureException($e);
-            return response()->json(['data' => null]);
-        }
+        $this->validate($request, [
+            'location' => 'sometimes|numeric',
+            'lat' => 'sometimes|numeric',
+            'lng' => 'required_with:lat'
+        ]);
+        $category = $this->api->get('/v1/categories/' . $category . '/services?location=' . $request->location . '&lat=' . $request->lat . '&lng=' . $request->lng);
+        if (!$category || !isset($category['services'])) return response()->json(['data' => null]);
+        $services = $category['services'];
+        $fractal = new Manager();
+        $fractal->setSerializer(new CustomSerializer());
+        $resource = new Collection($services, new ServiceTransformer());
+        return response()->json($fractal->createData($resource)->toArray());
     }
 }
