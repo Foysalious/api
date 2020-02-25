6860cb9cb5cec649d9497c46460c907e91736770
arnabrahman <arnabrahman@hotmail.com>
2020-02-25 16:42:07 +0600

    ⬆️ Deploy script
    Type(s): Update

--- deploy.sh
+++ deploy.sh
@@ -7,18 +7,21 @@
 # git commit -m "Make deploy.sh executable."
 # git push
 
-# Sentry release version create
-VERSION=$(sentry-cli releases propose-version)
-# Create a release
-sentry-cli releases new -p api $VERSION
-# Associate commits with the release
-sentry-cli releases set-commits --auto $VERSION
 
 sudo git fetch origin
 branch_name="$(git symbolic-ref --short -q HEAD 2>/dev/null)"
 reset="sudo git reset --hard origin/"
 reset_branch="$reset$branch_name"
 eval $reset_branch
+
+# Sentry release version create
+VERSION=$(sentry-cli releases propose-version)
+# Create a release
+sentry-cli releases new -p api $VERSION
+# Associate commits with the release
+sentry-cli releases set-commits --auto $VERSION
+# Finalize release
+sentry-cli releases finalize "$VERSION"
 
 sudo composer install --ignore-platform-reqs --no-interaction
 sudo php artisan config:clear
