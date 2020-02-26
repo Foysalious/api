0ad3f0a8da88a9b7443bfee3d1626947c0b4f6c5
tonmoy <saddam.hossain@sheba.xyz>
2020-02-26 09:55:14 +0600

    [UPDATE] #CORS

--- app/Http/Middleware/Cors2MiddleWare.php
+++ app/Http/Middleware/Cors2MiddleWare.php
@@ -90,7 +90,8 @@ class Cors2MiddleWare
             "https://banks.sheba.xyz",
             "http://stage.sheba.xyz",
             "https://stage.sheba.xyz",
-            "https://admin.stage.sheba.xyz"
+            "https://admin.stage.sheba.xyz",
+            "http://smanager.sheba.test"
         ];
         // ALLOW OPTIONS METHOD
         $headers['Access-Control-Allow-Credentials'] = 'true';
