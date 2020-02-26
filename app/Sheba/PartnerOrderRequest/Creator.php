dbfbd97a6e162bb6d4e0fc2e11a1e814034fb896
Pasha <pasha.startern@gmail.com>
2020-02-26 19:21:18 +0600

    🚑  NOTIFICATION EVENT TYPE & LINK CHANGE
    Type(s): HOTFIX

--- app/Sheba/PartnerOrderRequest/Creator.php
+++ app/Sheba/PartnerOrderRequest/Creator.php
@@ -95,7 +95,9 @@ class Creator
         $this->pushNotificationHandler->send([
             "title" => 'New Order',
             "message" => "প্রিয় $partner->name আপনার একটি নতুন অর্ডার রয়েছে, অনুগ্রহ করে ম্যানেজার অ্যাপ থেকে অর্ডারটি একসেপ্ট করুন",
-            "sound" => "notification_sound"
+            "sound" => "notification_sound",
+            "event_type" => 'PartnerOrder',
+            "link" => "new_order"
         ], $topic, $channel, $sound);
     }
 
