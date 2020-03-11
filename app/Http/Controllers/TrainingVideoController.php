c61e4ff5ce24231c619a8f9aa0af63f370798fbf
Arafat <syedyeaminarafat@gmail.com>
2020-03-11 12:47:23 +0600

    [UPDATE] #training_video api updated

--- app/Http/Controllers/TrainingVideoController.php
+++ app/Http/Controllers/TrainingVideoController.php
@@ -8,6 +8,7 @@ class TrainingVideoController extends Controller
         if($request->has('screen'))
         {
             $data=$video_repository->getByScreen($request->screen);
+
         }
         else
         {
