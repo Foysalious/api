f689783ef62b51ec8770c7e0bb17d133b9c2977f
shovan <shovan.sheba@gmail.com>
2020-03-12 20:05:27 +0600

    [update]whitelisted okwallet ip

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -84,7 +84,7 @@ class WalletController extends Controller
             return api_response($request, $message, 400, ['message' => $message]);
 
         } catch (Throwable $e) {
-
+            dd($e);
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
         }
