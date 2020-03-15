735265fd509b3835356dc6910bd13d3b4644ccfe
shovan <shovan.sheba@gmail.com>
2020-03-15 13:17:24 +0600

    [update] change share_pdf output format

--- app/Http/Controllers/Pos/DueTrackerController.php
+++ app/Http/Controllers/Pos/DueTrackerController.php
@@ -34,7 +34,8 @@ class DueTrackerController extends Controller
             if (($request->has('share_pdf')) && ($request->share_pdf == 1)){
                 $data['start_date'] = $request->has("start_date") ? $request->start_date : null;
                 $data['end_date'] = $request->has("end_date") ? $request->end_date : null;
-                $data['pdf_link'] = (new PdfHandler())->setName("due tracker")->setData($data)->setViewFile('due_tracker_due_list')->save();
+                $pdf_link = (new PdfHandler())->setName("due tracker")->setData($data)->setViewFile('due_tracker_due_list')->save();
+                return api_response($request, null, 200, ['message' => 'PDF download successful','link'    => $pdf_link]);
             }
             return api_response($request, $data, 200, ['data' => $data]);
         } catch (InvalidPartnerPosCustomer $e) {
@@ -66,7 +67,8 @@ class DueTrackerController extends Controller
             if (($request->has('share_pdf')) && ($request->share_pdf == 1)){
                 $data['start_date'] = $request->has("start_date") ? $request->start_date : null;
                 $data['end_date'] = $request->has("end_date") ? $request->end_date : null;
-                $data['pdf_link'] = (new PdfHandler())->setName("due tracker by customer")->setData($data)->setViewFile('due_tracker_due_list_by_customer')->save();
+                $pdf_link = (new PdfHandler())->setName("due tracker")->setData($data)->setViewFile('due_tracker_due_list')->save();
+                return api_response($request, null, 200, ['message' => 'PDF download successful','link'  => $pdf_link]);
             }
             return api_response($request, $data, 200, ['data' => $data]);
         } catch (InvalidPartnerPosCustomer $e) {
