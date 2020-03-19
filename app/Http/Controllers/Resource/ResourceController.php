da36bf255ac0bd39c63667e688263df12d6f5eba
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-03-19 20:53:07 +0600

    [UPDATE] #Resource order reschedule slot list API

--- app/Http/Controllers/Resource/ResourceController.php
+++ app/Http/Controllers/Resource/ResourceController.php
@@ -1,5 +1,7 @@
 <?php namespace App\Http\Controllers\Resource;
 
+use App\Models\Category;
+use App\Models\Partner;
 use App\Models\Resource;
 use App\Transformers\CustomSerializer;
 use App\Transformers\Resource\ResourceProfileTransformer;
@@ -7,6 +9,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use League\Fractal\Manager;
 use League\Fractal\Resource\Item;
+use Sheba\Schedule\ScheduleSlot;
 
 class ResourceController extends Controller
 {
@@ -19,5 +22,21 @@ class ResourceController extends Controller
             $profile = $fractal->createData($resource)->toArray()['data'];
 
             return api_response($request, $profile, 200, ['profile' => $profile]);
+    }
+
+    public function getSchedules(Request $request, ScheduleSlot $slot)
+    {
+        $this->validate($request, [
+            'limit' => 'sometimes|required|numeric:min:1'
+        ]);
+        //TODO: Need to get resource_id, category_id and partner_id from Auth Middleware
+        $resource = Resource::find(44994);
+        $slot->setCategory(Category::find(14));
+        $slot->setPartner(Partner::find($resource->firstPartner()->id));
+        $slot->setLimit($request->limit);
+        $dates = $slot->getSchedulesByResource($resource);
+
+        return api_response($request, $dates, 200, ['dates' => $dates]);
+
     }
 }
