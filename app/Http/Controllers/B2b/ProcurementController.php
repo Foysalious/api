1f8d6797c8133027b147cfc2578412793ab41da6
Fazal Mahmud Niloy <fazal@sheba.xyz>
2020-03-19 13:40:18 +0600

    [ADD] # New PDF design for Invoice document added

--- app/Http/Controllers/B2b/ProcurementController.php
+++ app/Http/Controllers/B2b/ProcurementController.php
@@ -401,7 +401,7 @@ class ProcurementController extends Controller
             'company_evaluation' => $company_evaluation ? $company_evaluation->fields ? $company_evaluation->fields : null : null,
         ];
 
-
+        return view('pdfs.invoice', compact('procurement_details'));
         return App::make('dompdf.wrapper')
             ->loadView('pdfs.procurement_details', compact('procurement_details'))
             ->download("procurement_details.pdf");
