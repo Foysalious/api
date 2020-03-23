866da2536308e80a3f3370fba0d55bfc089a6b35
Saiful <saiful.sheba@gmail.com>
2020-03-23 14:12:58 +0600

    ⬆️  Status update for approval_request
    Type(s): UPDATE

--- app/Http/Controllers/Employee/ApprovalRequestController.php
+++ app/Http/Controllers/Employee/ApprovalRequestController.php
@@ -1,18 +1,40 @@
 <?php namespace App\Http\Controllers\Employee;
 
-use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
-#use Sheba\Business\ApprovalRequest\Updater;
-use App\Http\Controllers\Controller;
 use Sheba\Business\ApprovalRequest\Updater;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class ApprovalRequestController extends Controller
 {
     public function updateStatus(Request $request, Updater $updater)
     {
         try {
-            dd(1233, $request->all());
-            #return api_response($request, $approval_request, 200, ['id' => $approval_request->id]);
+            $type = $request->type;
+            $type_ids = json_decode($request->type_id);
+            $business_member = $request->auth_info['business_member'];
+            $business_member_id = $business_member['id'];
+            $member_id = $business_member['member_id'];
+            #$model = "App\\Models\\" . ucfirst(camel_case($type));
+            $model = 'Sheba\\Dal\\' . ucfirst(camel_case($type)) . '\\Model';
+            $models = $model::whereIn('id', $type_ids)->get();
+            foreach ($models as $model) {
+                #dd($model as $leave);
+                $model_status = $model->status;#check leave status rejected ki na??
+                $approval_requests = $model->requests->where('status', 'rejected');#check approval_requests status rejected ki na??
+                #dd($approval_requests->isEmpty());
+
+                $approval_request = $model->requests->where('approver_id', $business_member_id)->first();#null check korte hobe
+                dump($approval_request);
+                $updater->setMember($member_id)
+                    ->setBusinessMember($business_member_id)
+                    ->setApprovalRequest($approval_request)
+                    ->setStatus($request->status);
+                if ($error = $updater->hasError())
+                    return api_response($request, $error, 400, ['message' => $error]);
+                $updater->change();
+            }
+            return api_response($request, null, 200);
         } catch (ValidationException $e) {
             $message = getValidationErrorMessage($e->validator->errors()->all());
             $sentry = app('sentry');
@@ -20,6 +42,7 @@ class ApprovalRequestController extends Controller
             $sentry->captureException($e);
             return api_response($request, $message, 400, ['message' => $message]);
         } catch (\Throwable $e) {
+            dd($e);
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
         }
--- app/Sheba/Business/ApprovalRequest/Updater.php
+++ app/Sheba/Business/ApprovalRequest/Updater.php
@@ -1,5 +1,7 @@
 <?php namespace Sheba\Business\ApprovalRequest;
 
+use App\Models\BusinessMember;
+use App\Models\Member;
 use Sheba\Dal\ApprovalRequest\ApprovalRequestRepositoryInterface;
 use Sheba\Dal\ApprovalRequest\Model as ApprovalRequest;
 use Sheba\Dal\ApprovalRequest\Status;
@@ -17,48 +19,50 @@ class Updater
     private $member;
     private $businessMember;
     private $statuses;
+    private $status;
     private $data;
 
-    public function __construct(ApprovalRequestRepositoryInterface $approval_request_repo, Status $status)
+    public function __construct(ApprovalRequestRepositoryInterface $approval_request_repo, Status $statuses)
     {
         $this->approvalRequestRepo = $approval_request_repo;
-        $this->statuses = $status;
+        #$this->statuses = $statuses;
     }
 
     public function hasError()
     {
-        if (!in_array($this->data['status'], $this->statuses)) return "Invalid Status!";
-        if ($this->approvalRequest->business_member_id != $this->businessMember->id) return "You are not authorized to  change the Status!";
+        #if (!in_array($this->status, $this->statuses)) return "Invalid Status!";
+        if ($this->approvalRequest->approver_id != $this->businessMember->id) return "You are not authorized to  change the Status!";
         return false;
     }
 
     public function setMember($member)
     {
-        $this->member = $member;
+        $this->member = Member::findOrFail((int)$member);
         return $this;
     }
 
     public function setBusinessMember($business_member)
     {
-        $this->businessMember = $business_member;
+        $this->businessMember = BusinessMember::findOrFail((int)$business_member);
         return $this;
     }
 
-    public function setApprovalRequest($approval)
+    public function setApprovalRequest(ApprovalRequest $approval)
     {
-        $this->approvalRequest = ApprovalRequest::findOrFail($approval);
+        $this->approvalRequest = $approval;
         return $this;
     }
 
-    public function setData($data)
+    public function setStatus($status)
     {
-        $this->data = $data;
+        $this->status = $status;
+        return $this;
     }
 
     public function change()
     {
         $this->setModifier($this->member);
-        $data = ['status' => $this->data['status']];
+        $data = ['status' => $this->status];
         $this->approvalRequestRepo->update($this->approvalRequest, $this->withUpdateModificationField($data));
     }
 }
