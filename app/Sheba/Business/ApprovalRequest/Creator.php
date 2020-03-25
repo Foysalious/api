f458a9f1457ddc3b9849ab3cbc26c036d804c553
Pasha <pasha.startern@gmail.com>
2020-03-26 00:59:44 +0600

    ⬆️  LEAVE REQUEST 0.0.2
    Type(s): UPDATE
    
    What:
    NOTIFICATION

--- app/Sheba/Business/ApprovalRequest/Creator.php
+++ app/Sheba/Business/ApprovalRequest/Creator.php
@@ -1,8 +1,10 @@
 <?php namespace Sheba\Business\ApprovalRequest;
 
 use Sheba\Dal\ApprovalRequest\ApprovalRequestRepositoryInterface;
+use Sheba\Dal\ApprovalRequest\Model as ApprovalRequest;
 use Sheba\Dal\ApprovalRequest\Status;
 use Sheba\ModificationFields;
+use Sheba\PushNotificationHandler;
 
 class Creator
 {
@@ -13,19 +15,25 @@ class Creator
     private $approverId;
     private $requestableType;
     private $requestableId;
+    /** @var PushNotificationHandler $pushNotificationHandler*/
+    private $pushNotificationHandler;
+    private $member;
 
     /**
      * Creator constructor.
      * @param ApprovalRequestRepositoryInterface $approval_request_repo
+     * @param PushNotificationHandler $push_notification_handler
      */
-    public function __construct(ApprovalRequestRepositoryInterface $approval_request_repo)
+    public function __construct(ApprovalRequestRepositoryInterface $approval_request_repo, PushNotificationHandler $push_notification_handler)
     {
         $this->approvalRequestRepo = $approval_request_repo;
+        $this->pushNotificationHandler = $push_notification_handler;
     }
 
     public function setBusinessMember($business_member)
     {
-        $this->setModifier($business_member->member);
+        $this->member = $business_member->member;
+        $this->setModifier($this->member);
         return $this;
     }
 
@@ -52,16 +60,34 @@ class Creator
 
     public function create()
     {
-        $approval_requests = [];
         foreach ($this->approverId as $approver_id) {
-            $approval_requests[] = $this->withCreateModificationField([
+            $data = $this->withCreateModificationField([
                 'requestable_type' => $this->requestableType,
                 'requestable_id' => $this->requestableId,
                 'status' => Status::PENDING,
                 'approver_id' => $approver_id
             ]);
+            $approval_request = $this->approvalRequestRepo->create($data);
+            $this->sendPushToApprover($approval_request);
         }
+    }
 
-        $this->approvalRequestRepo->insert($approval_requests);
+    /**
+     * @param ApprovalRequest $approval_request
+     */
+    public function sendPushToApprover(ApprovalRequest $approval_request)
+    {
+        $topic = config('sheba.push_notification_topic_name.employee') . (int)$this->member->id;
+        $channel = config('sheba.push_notification_channel_name.employee');
+        $notification_data = [
+            "title" => 'New Leave Request Arrived',
+            "message" => "Leave Request Arrived Message",
+            "event_type" => 'leave_request',
+            "event_id" => $approval_request->id,
+            "sound" => "notification_sound",
+            "channel_id" => $channel,
+            "click_action" => "FLUTTER_NOTIFICATION_CLICK"
+        ];
+        $this->pushNotificationHandler->send($notification_data, $topic, $channel);
     }
 }
