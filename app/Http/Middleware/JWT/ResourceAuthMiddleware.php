e0a14661f03a09556e47611f56c9a9823ff1b3f3
arnabrahman <arnabrahman@hotmail.com>
2020-03-27 17:59:20 +0600

    ⬆️ Resource auth middleware
    Type(s): Update

---
+++ app/Http/Middleware/JWT/ResourceAuthMiddleware.php
@@ -0,0 +1,44 @@
+<?php namespace App\Http\Middleware\JWT;
+
+
+use Illuminate\Http\Request;
+use Sheba\Auth\Auth;
+use Sheba\Auth\AuthenticationFailedException;
+use Sheba\Auth\JWTAuth;
+use Closure;
+
+class ResourceAuthMiddleware
+{
+    private $auth;
+    private $JWTAuth;
+
+
+    public function __construct(Auth $auth, JWTAuth $JWT_auth)
+    {
+        $this->auth = $auth;
+        $this->JWTAuth = $JWT_auth;
+        $this->auth->setStrategy($this->JWTAuth)->setRequest(request());
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        try {
+            $auth_user = $this->auth->authenticate();
+            if ($auth_user->getResource()) {
+                $request->merge(['auth_user' => $auth_user]);
+                return $next($request);
+            }
+            return api_response($request, null, 403, ["message" => "You're not authorized to access this user."]);
+        } catch (AuthenticationFailedException $e) {
+            return api_response($request, null, 403, ["message" => "You're not authorized to access this user."]);
+        }
+
+    }
+}
