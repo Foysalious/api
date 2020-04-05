8e2b8cf1c577a749bd1eb7bf407f6d871b28dc07
Pasha <pasha.startern@gmail.com>
2020-04-05 21:38:15 +0600

    ⬆️  COMMON FUNCTION MOVE TO MODEL
    Type(s): UPDATE

--- app/Transformers/Business/LeaveBalanceTransformer.php
+++ app/Transformers/Business/LeaveBalanceTransformer.php
@@ -1,7 +1,6 @@
 <?php namespace App\Transformers\Business;
 
 use App\Models\BusinessMember;
-use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use League\Fractal\TransformerAbstract;
 use Sheba\Helpers\TimeFrame;
@@ -53,37 +52,10 @@ class LeaveBalanceTransformer extends TransformerAbstract
             array_push($single_employee_leave_balance, [
                 'title' => $leave_type['title'],
                 'allowed_leaves' => (int)$leave_type['total_days'],
-                'used_leaves' => $this->getNumberOfUsedLeaveDaysByType($leave_type['id'])
+                'used_leaves' => $this->businessMember->getCountOfUsedLeaveDaysByTypeOnAFiscalYear($leave_type['id'])
             ]);
         }
 
         return $single_employee_leave_balance;
-    }
-
-    /**
-     * @param $leave_type_id
-     * @return int
-     */
-    private function getNumberOfUsedLeaveDaysByType($leave_type_id)
-    {
-        /**
-         * STATIC NOW, NEXT SPRINT COMES FROM DB
-         */
-        $business_fiscal_start_month = 7;
-        $used_days = 0;
-        $this->timeFrame->forAFiscalYear(Carbon::now(), $business_fiscal_start_month);
-
-        $leaves = $this->businessMember->leaves()->accepted()->between($this->timeFrame)->with('leaveType')->whereHas('leaveType', function ($leave_type) use (&$leave_lefts, $leave_type_id) {
-            return $leave_type->where('id', $leave_type_id);
-        })->get();
-
-        $leaves->each(function ($leave) use (&$used_days) {
-            $start_date = $leave->start_date->lt($this->timeFrame->start) ? $this->timeFrame->start : $leave->start_date;
-            $end_date = $leave->end_date->gt($this->timeFrame->end) ? $this->timeFrame->end : $leave->end_date;
-
-            $used_days += $end_date->diffInDays($start_date) + 1;
-        });
-
-        return (int)$used_days;
     }
 }
