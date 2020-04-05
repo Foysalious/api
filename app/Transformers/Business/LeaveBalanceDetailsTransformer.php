ae851c23bc0d2b82a031282af079374cc61b4f6f
Rashad Akhtar <rashad@sheba.xyz>
2020-04-05 21:55:35 +0600

    Refactor - Leave Balance Details

--- app/Transformers/Business/LeaveBalanceDetailsTransformer.php
+++ app/Transformers/Business/LeaveBalanceDetailsTransformer.php
@@ -1,47 +1,73 @@
 <?php namespace App\Transformers\Business;
 
+use App\Models\BusinessMember;
 use League\Fractal\TransformerAbstract;
 use Sheba\Dal\LeaveType\Model as LeaveType;
+use Sheba\Helpers\TimeFrame;
 
 class LeaveBalanceDetailsTransformer extends TransformerAbstract
 {
     private $leave_types;
+    /** @var TimeFrame $timeFrame */
+    private $timeFrame;
+    /** @var BusinessMember $businessMember */
+    private $businessMember;
 
-    public function __construct($leave_types_data)
+    /**
+     * LeaveBalanceDetailsTransformer constructor.
+     * @param $leave_types
+     * @param TimeFrame $time_frame
+     */
+    public function __construct($leave_types, TimeFrame $time_frame)
     {
-        $this->leave_types = $leave_types_data;
+        $this->leave_types = $leave_types;
+        $this->timeFrame = $time_frame;
     }
 
+    /**
+     * @param $business_member
+     * @return array
+     */
     public function transform($business_member)
     {
+        $this->businessMember = $business_member;
         $member = $business_member->member;
         $profile = $member->profile;
         $role = $business_member->role;
         $leaves = $business_member->leaves;
 
         return [
-            'employee_name' => $profile->name, 'employee_id' => $member->id, 'designation' => $role ? $role->name : null, 'department' => $role ? $role->businessDepartment->name : null, 'leave_balance' => $this->calculate($leaves), 'leaves' => $this->fetchLeaves($leaves),
+            'employee_name' => $profile->name, 'employee_id' => $business_member->id, 'designation' => $role ? $role->name : null, 'department' => $role ? $role->businessDepartment->name : null, 'leave_balance' => $this->calculate(), 'leaves' => $this->fetchLeaves($leaves),
         ];
     }
 
-    private function calculate($leaves)
+    /**
+     * @return array
+     */
+    private function calculate()
     {
         $single_employee_leave_balance = [];
         foreach ($this->leave_types as $leave_type) {
-            $total_leave = $leaves->where('leave_type_id', $leave_type['id'])->sum('total_days');
             array_push($single_employee_leave_balance, [
-                'title' => $leave_type['title'], 'total_used_leaves' => (int)$total_leave, 'allowed_leaves' => $leave_type['total_days']
+                'title' => $leave_type['title'],
+                'allowed_leaves' => (int)$leave_type['total_days'],
+                'used_leaves' => $this->businessMember->getCountOfUsedLeaveDaysByTypeOnAFiscalYear($leave_type['id'])
             ]);
         }
 
         return $single_employee_leave_balance;
     }
 
+    /**
+     * @param $leaves
+     * @return array
+     */
     private function fetchLeaves($leaves)
     {
         $all_leaves = [];
         foreach ($leaves as $leave) {
             array_push($all_leaves, [
+                'id' => $leave->id,
                 'date' => $leave->created_at->format('d/m/Y'),
                 'leave_type' => $this->getLeaveTypeTitle($leave),
                 'leave_days' => (int)$leave->total_days,
@@ -52,6 +78,10 @@ class LeaveBalanceDetailsTransformer extends TransformerAbstract
         return $all_leaves;
     }
 
+    /**
+     * @param $leave
+     * @return mixed
+     */
     private function getLeaveTypeTitle($leave)
     {
         $leave_type = LeaveType::findOrFail($leave->leave_type_id);
