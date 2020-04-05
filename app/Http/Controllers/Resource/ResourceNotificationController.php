a94f989eae5f1d72cdf972c4bf2f0ff72902c8e7
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-04-05 22:02:20 +0600

    [UPDATE] #Notification test for job_alert and payment

--- app/Http/Controllers/Resource/ResourceNotificationController.php
+++ app/Http/Controllers/Resource/ResourceNotificationController.php
@@ -12,10 +12,10 @@ class ResourceNotificationController extends Controller
 {
     public function test(Request $request, PushNotificationHandler $pushNotificationHandler)
     {
+        $this->validate($request, ['job_id' => 'required']);
         /** @var AuthUser $auth_user */
         $auth_user = $request->auth_user;
         $resource = $auth_user->getResource();
-
         $topic = config('sheba.push_notification_topic_name.resource') . $resource->id;
         $channel = config('sheba.push_notification_channel_name.resource');
 
@@ -27,6 +27,32 @@ class ResourceNotificationController extends Controller
                 "event_type" => 'job_assign',
                 "event_id" => $request->job_id,
                 "sound" => "notification_sound",
+                "channel_id" => $channel,
+                "click_action" => "FLUTTER_NOTIFICATION_CLICK"
+            ], $topic, $channel);
+        }
+
+        if($request->has('payment')) {
+            $job = Job::find($request->job_id);
+            $pushNotificationHandler->send([
+                "title" => 'Online Payment',
+                "message" => 'আপনার অর্ডার ' . $job->partnerOrder->order->code() . ' টি Online Payment এর মাধ্যমে পরিশোধ করা হয়েছে ',
+                "event_type" => 'online_payment',
+                "event_id" => $job->id,
+                "sound" => "notification_sound",
+                "channel_id" => $channel,
+                "click_action" => "FLUTTER_NOTIFICATION_CLICK"
+            ], $topic, $channel);
+        }
+
+        if($request->has('job_alert')) {
+            $job = Job::find($request->job_id);
+            $pushNotificationHandler->send([
+                "title"     => "তৈরি হয়ে নিন",
+                "message" => 'আপনার অর্ডার' . $job->partnerOrder->order->code() . 'টি আর ১৫ মিনিট এর মধ্যে শুরু হয়ে যাবে',
+                "event_type"=> "job_alert",
+                "event_id"  => $job->id,
+                "sound"     => 'notification_sound',
                 "channel_id" => $channel,
                 "click_action" => "FLUTTER_NOTIFICATION_CLICK"
             ], $topic, $channel);
