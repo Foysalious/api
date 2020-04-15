15c033d6bc5e8f8a1000eaf497dcf76a536f2c60
arnabrahman <arnabrahman@hotmail.com>
2020-04-15 23:36:18 +0600

    ⬆️ Payment validity set to 10 minutes
    Type(s): UPDATE

--- app/Sheba/Payment/Methods/Wallet.php
+++ app/Sheba/Payment/Methods/Wallet.php
@@ -23,7 +23,7 @@ class Wallet extends PaymentMethod
             $payment->transaction_id = $invoice;
             $payment->gateway_transaction_id = $invoice;
             $payment->status = 'initiated';
-            $payment->valid_till = Carbon::tomorrow();
+            $payment->valid_till = $this->getValidTill();
             $this->setModifier($payable->user);
             $payment->fill((new RequestIdentification())->get());
             $this->withCreateModificationField($payment);
