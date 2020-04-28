d19267a07a984db4c0bb829a86653c5949354a34
Shafiqul Islam <shafiq.startern@gmail.com>
2020-04-29 03:19:44 +0600

    ➕ Port Wallet payment integrated
    Type(s): ADD

--- config/sheba.php
+++ config/sheba.php
@@ -143,6 +143,8 @@ return [
     'category_groups' => [
         'trending' => env('TRENDING_CATEGORY_GROUP', 10)
     ],
-    'payout_token'=>env('SHEBA_PAYOUT_TOKEN','ShebaAdminPanelToken!@#$!@#'),
     'resource_max_withdraw_limit' => 1000
+    'address' => 'House #63 (1st Floor), Road #04, Block-C, Banani, Dhaka 1213',
+    'email' => 'info@sheba.xyz'
+    'payout_token'=>env('SHEBA_PAYOUT_TOKEN','ShebaAdminPanelToken!@#$!@#'),
 ];
