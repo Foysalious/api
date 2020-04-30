3a0914b050f3bbb8689b5b72cc9a4fd49f18397c
Shafiqul Islam <shafiq.startern@gmail.com>
2020-05-01 00:52:09 +0600

    ⬆️ Payable can have multiple payments
    Type(s): UPDATE

--- app/Sheba/Payment/Methods/PortWallet/Service.php
+++ app/Sheba/Payment/Methods/PortWallet/Service.php
@@ -123,11 +123,11 @@ class Service
             ]
         ];
 
-        if ($payable->amount >= config('sheba.min_order_amount_for_emi')) {
+        if ($payable->amount >= config('sheba.min_order_amount_for_emi') || $payable->emi_month) {
             $data['emi']['enable'] = 1;
-            /*if ($payable->emi_month) {
-                $data['emi']['tenure'] = (int)$payable->emi_month;
-            }*/
+            if ($payable->emi_month){
+                // $data['emi']['tenure'] = [(int)$payable->emi_month];
+            }
         }
 
         return $data;
