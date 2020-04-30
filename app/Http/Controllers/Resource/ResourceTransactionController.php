b0ed678f3d1ca52b7be3816ebb9ab0c30efc36b7
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-04-30 12:01:11 +0600

    [UPDATE] #Resource Wallet Moved to Wallet Controller

--- app/Http/Controllers/Resource/ResourceTransactionController.php
+++ app/Http/Controllers/Resource/ResourceTransactionController.php
@@ -1,6 +1,5 @@
 <?php namespace App\Http\Controllers\Resource;
 
-use Dingo\Blueprint\Annotation\Resource;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -18,16 +17,5 @@ class ResourceTransactionController extends Controller
         list($offset, $limit) = calculatePagination($request);
         $transactions = ResourceTransaction::where('resource_id', $resource->id)->select('id', 'type', 'amount', 'log', 'created_at')->get()->splice($offset, $limit);
         return api_response($request, $transactions, 200, ['transactions' => $transactions]);
-    }
-
-    public function getWallet(Request $request)
-    {
-        $wallet = [
-            'balance' => 1000,
-            'max_withdrawal_limit' => 600,
-            'tag' => 'approval_pending',
-            'message' => 'আপনার সর্বশেষ ব্যালেন্স উত্তোলনের রিকুয়েস্ট এখনো অপেক্ষমাণ আছে, তাই আপনি এখন রিকুয়েস্ট করতে পারবেন না।'
-        ];
-        return api_response($request, $wallet, 200, ['wallet' => $wallet]);
     }
 }
--- app/Http/Route/Prefix/V2/Resource/AuthRoute.php
+++ app/Http/Route/Prefix/V2/Resource/AuthRoute.php
@@ -36,7 +36,7 @@ class AuthRoute
             $api->group(['prefix' => 'transactions'], function ($api) {
                 $api->get('/', 'Resource\ResourceTransactionController@index');
             });
-            $api->get('wallet', 'Resource\ResourceTransactionController@getWallet');
+            $api->get('wallet', 'Resource\ResourceWalletController@getWallet');
             $api->post('withdrawals', 'Resource\ResourceWithdrawalRequestController@store');
         });
     }
