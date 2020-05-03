f2e4af5947c3c90857e83bc0f13305dbf5a423fa
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-05-03 10:24:39 +0600

    [UPDATE] #Reward Show Route

--- app/Http/Route/Prefix/V2/Resource/AuthRoute.php
+++ app/Http/Route/Prefix/V2/Resource/AuthRoute.php
@@ -40,8 +40,10 @@ class AuthRoute
             $api->post('withdrawals', 'Resource\ResourceWithdrawalRequestController@store');
             $api->group(['prefix' => 'rewards'], function ($api) {
                 $api->get('/', 'Resource\ResourceRewardController@index');
-                $api->get('{reward}', 'Resource\ResourceRewardController@show');
                 $api->get('history', 'Resource\ResourceRewardController@history');
+                $api->group(['prefix' => '{reward}'], function ($api) {
+                    $api->get('/', 'Resource\ResourceRewardController@show');
+                });
             });
         });
     }
