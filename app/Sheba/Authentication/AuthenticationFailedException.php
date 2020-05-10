c569ed376fdf2bf4c51d652599396038666836a0
arnabrahman <arnabrahman@hotmail.com>
2020-05-10 13:51:22 +0600

    ⬆️ JWT auth exception message and code
    Type(s): Update

--- app/Sheba/Authentication/AuthenticationFailedException.php
+++ app/Sheba/Authentication/AuthenticationFailedException.php
@@ -1,11 +1,12 @@
 <?php namespace Sheba\Authentication;
 
 use App\Exceptions\ApiValidationException;
+use Throwable;
 
 class AuthenticationFailedException extends ApiValidationException
 {
-    public function report()
+    public function __construct($message = "Authentication failed.", $code = 401, Throwable $previous = null)
     {
-        \Log::debug('User not found');
+        parent::__construct($message, $code, $previous);
     }
 }
