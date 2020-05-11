d6be08e9e5dfa067d5c2c5b59c210bfd16ac17c7
Saiful <saiful.sheba@gmail.com>
2020-05-11 16:35:57 +0600

    [ADD]Holiday Update

--- app/Sheba/Business/Holiday/CreateRequest.php
+++ app/Sheba/Business/Holiday/CreateRequest.php
@@ -4,6 +4,10 @@
 class CreateRequest
 {
     private $business;
+    private $member;
+    private $startDate;
+    private $endDate;
+    private $holidayName;
 
     /**
      * @return mixed
@@ -22,4 +26,77 @@ class CreateRequest
         $this->business = $business;
         return $this;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getMember()
+    {
+        return $this->member;
+    }
+
+    /**
+     * @param mixed $member
+     * @return CreateRequest
+     */
+    public function setMember($member)
+    {
+        $this->member = $member;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getHolidayName()
+    {
+        return $this->holidayName;
+    }
+
+    /**
+     * @param $title
+     * @return $this
+     */
+    public function setHolidayName($title)
+    {
+        $this->holidayName = $title;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * @param $start_date
+     * @return $this
+     */
+    public function setStartDate($start_date)
+    {
+        $this->startDate = $start_date . ' ' . '00:00:00';
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEndDate()
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * @param $end_date
+     * @return $this
+     */
+    public function setEndDate($end_date)
+    {
+        $this->endDate = $end_date . ' ' . '23:59:59';
+        return $this;
+    }
+
 }
