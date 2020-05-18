180c3a46e7ca203379de6b69494e499c407040ec
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-05-18 15:57:48 +0600

    [UPDATE] #Presets and Categories Filtered

--- app/Http/Controllers/ComplainController.php
+++ app/Http/Controllers/ComplainController.php
@@ -41,14 +41,18 @@ class ComplainController extends Controller
                 'for' => 'required|in:customer,partner'
             ]);
             $job = Job::find($job);
-            $accessor = $this->accessorRepo->findByNameWithPublishedCategoryAndPreset(ucwords($request->for));
+            if (!empty($job)) {
+                $accessor = $this->accessorRepo->findByNameWithPublishedCategoryAndPresetFilteredByCategoryAndStatus(ucwords($request->for), $job->category_id, $this->getJobStatus($job));
+            } else {
+                $accessor = $this->accessorRepo->findByNameWithPublishedCategoryAndPreset(ucwords($request->for));
+            }
             $final_complains = collect();
             $final_presets = collect();
-            $presets = $this->getCategoryWisePresets($job, $accessor);
+            $presets = $accessor->complainPresets;
             foreach ($presets as $preset) {
                 $final_presets->push(collect($preset)->only(['id', 'name', 'category_id']));
             }
-            $categories = $this->getJobWiseCategory($job, $accessor);
+            $categories = $accessor->complainCategories;
             foreach ($categories as $category) {
                 $final = collect($category)->only(['id', 'name']);
                 $final->put('presets', $final_presets->where('category_id', $category->id)->values()->all());
@@ -67,7 +71,7 @@ class ComplainController extends Controller
         }
     }
 
-    private function getJobWiseCategory($job, $accessor)
+    /* private function getJobWiseCategory($job, $accessor)
     {
         if (!empty($job)) {
             $job_status = $this->getJobStatus($job);
@@ -84,7 +88,7 @@ class ComplainController extends Controller
                     return $job->category_id == $cat->id;
                 })->count() > 0;
         }) : $accessor->complainPresets;
-    }
+    } */
 
     private function getJobStatus($job)
     {
