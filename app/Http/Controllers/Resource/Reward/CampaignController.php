c874f3b6ffbfebb0650a7e4a80169802245ccf2d
arnabrahman <arnabrahman@hotmail.com>
2020-05-20 20:53:24 +0600

    ⬆️ Reward campaign info
    Type(s): Update

--- app/Http/Controllers/Resource/Reward/CampaignController.php
+++ app/Http/Controllers/Resource/Reward/CampaignController.php
@@ -1,34 +1,21 @@
 <?php namespace App\Http\Controllers\Resource\Reward;
 
-
 use App\Http\Controllers\Controller;
 use App\Models\RewardCampaign;
 use Illuminate\Http\Request;
+use Sheba\Authentication\AuthUser;
+use Sheba\Resource\Reward\Info\Campaign;
 
 class CampaignController extends Controller
 {
-    public function show(RewardCampaign $campaign, Request $request)
+    public function show($campaign, Request $request, Campaign $campaign_info)
     {
-        $info = [
-            "id" => $campaign->id,
-            "name" => $campaign->reward->name,
-            "short_description" => $campaign->reward->short_description,
-            "type" => $campaign->reward->type,
-            "amount" => $campaign->reward->amount,
-            "start_time" => $campaign->reward->start_time->toDateTimeString(),
-            "end_time" => $campaign->reward->end_time->toDateTimeString(),
-            "created_at" => $campaign->reward->created_at->toDateTimeString(),
-            "progress" => [
-                "is_completed" => 0,
-                "target" => 5,
-                "completed" => 2
-            ],
-            "rules" => [
-                "নুন্যতম ১০ টি অর্ডার সিডিউল ডিউ ছাড়া সার্ভ করতে হবে",
-                "শুধু মাত্র সার্ভ অর্ডার গণনার আন্তর্ভুক্ত হবে",
-                "বিজয়ী গন আগামী ৯ অক্টোবর রাত ১২ টার পরে তাদের গিফ্‌ট পয়েন্ট বুঝে পাবেন।"
-            ]
-        ];
-        return api_response($request, null, 200, ['info' => $info]);
+        /** @var AuthUser $auth_user */
+        $auth_user = $request->auth_user;
+        $resource = $auth_user->getResource();
+        $campaign = RewardCampaign::find($campaign);
+        if (!$campaign) return api_response($request, null, 404);
+        $campaign_info->setReward($campaign->reward)->setRewardable($resource);
+        return api_response($request, null, 200, ['info' => $campaign_info->getInfo()]);
     }
 }
--- app/Sheba/Resource/Reward/Info/Info.php
+++ app/Sheba/Resource/Reward/Info/Info.php
@@ -27,6 +27,8 @@ abstract class Info
     {
         return [
             "id" => $this->reward->id,
+            "reward_type" => $this->reward->isCampaign() ? 'campaign' : 'action',
+            "reward_type_id" => $this->reward->detail_id,
             "name" => $this->reward->name,
             "short_description" => $this->reward->short_description,
             "type" => $this->reward->type,
@@ -34,6 +36,11 @@ abstract class Info
             "start_time" => $this->reward->start_time->toDateTimestring(),
             "end_time" => $this->reward->end_time->toDateTimestring(),
             "created_at" => $this->reward->created_at->toDateTimestring(),
+            "rules" => [
+                "নুন্যতম ১০ টি অর্ডার সিডিউল ডিউ ছাড়া সার্ভ করতে হবে",
+                "শুধু মাত্র সার্ভ অর্ডার গণনার আন্তর্ভুক্ত হবে",
+                "বিজয়ী গন আগামী ৯ অক্টোবর রাত ১২ টার পরে তাদের গিফ্‌ট পয়েন্ট বুঝে পাবেন।"
+            ],
             'progress' => $this->getProgress()
         ];
     }
