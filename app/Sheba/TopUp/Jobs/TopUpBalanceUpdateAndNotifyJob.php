1eecd828d979102c79f1d3b744718c7cd79480a6
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-05-31 17:10:03 +0600

    [UPDATE] #TopUp Gateway Balance Moved

--- app/Sheba/TopUp/Jobs/TopUpBalanceUpdateAndNotifyJob.php
+++ app/Sheba/TopUp/Jobs/TopUpBalanceUpdateAndNotifyJob.php
@@ -7,46 +7,61 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Sheba\Dal\TopUpGateway\Model as TopUpGateway;
+use Sheba\TopUp\Gateway\Gateway;
+use Sheba\TopUp\Gateway\GatewayFactory;
 
 class TopUpBalanceUpdateAndNotifyJob extends Job implements ShouldQueue
 {
     use InteractsWithQueue, SerializesModels;
 
-    const QUEUE_NAME = 'topup:high';
-
     /** @var TopUpGateway */
     protected $topUpGateway;
     protected $balance;
+    /** @var TopUpOrder */
+    protected $topup_order;
+    /**  @var Gateway */
+    protected $topUpGatewayFactory;
 
-    public function __construct(TopUpOrder $topup_order, $vendor)
+    public function __construct(TopUpOrder $topup_order)
     {
-        $this->balance = $vendor->getBalance();
-        $this->topUpGateway = $this->getGatewayModel($topup_order->gateway);
-        $this->connection = 'topup';
-        $this->queue = self::QUEUE_NAME;
+        $this->topup_order = $topup_order;
+        $this->topUpGatewayFactory = $this->getTopUpGatewayFactory();
+        $this->balance = $this->getBalance();
+        $this->topUpGateway = $this->getGatewayModel();
     }
 
     public function handle()
     {
         if ($this->attempts() < 2) {
-            if($this->checkIfLessThanThreshold($this->topUpGateway, $this->balance)) {
-                $this->sendSmsToGatewaySmsReceivers($this->topUpGateway, $this->balance);
-            }
             $this->topUpGateway->update([
                 'balance' => $this->balance
             ]);
+            if($this->checkIfLessThanThreshold($this->topUpGateway, $this->balance)) {
+                $this->sendSmsToGatewaySmsReceivers($this->topUpGateway, $this->balance);
+            }
         }
     }
 
-    private function getGatewayModel($gateway)
+    private function getTopUpGatewayFactory()
     {
-        return TopUpGateway::where('name', $gateway)->first();
+        $gateway_factory = new GatewayFactory();
+        $gateway_factory->setGatewayName($this->topup_order->gateway)->setVendorId($this->topup_order->vendor_id);
+        return $gateway_factory->get();
+    }
+
+    private function getGatewayModel()
+    {
+        return TopUpGateway::where('name', $this->topup_order->gateway)->first();
+    }
+
+    private function getBalance()
+    {
+        return $this->topUpGatewayFactory->getBalance();
     }
 
     private function checkIfLessThanThreshold($gateway, $balance)
     {
         $threshold = $gateway->threshold;
-        dd($gateway, $balance, $threshold);
         return (double) $balance < (double) $threshold;
     }
 
