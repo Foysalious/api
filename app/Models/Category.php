79df7bc90aeae1e5a543b712c0260491898c3c37
Irteza Asad <irtezaasad.sheba@gmail.com>
2020-06-08 15:17:35 +0600

    Category isRentMaster fixed [UPDATE]

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -211,7 +211,7 @@ class Category extends Model
 
     public function isRentMaster()
     {
-        return in_array($this->id, array_map('intval', explode(',', env('RENT_A_CAR_CATEGORY_ID')))) ? 1 : 0;
+        return in_array($this->id, array_map('intval', explode(',', config('sheba.car_rental.master_category_id')))) ? 1 : 0;
     }
 
 
