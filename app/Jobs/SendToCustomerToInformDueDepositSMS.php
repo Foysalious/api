bb701f2aedfefce23d04307f7583e90b47eb87fc
shovan <shovan.sheba@gmail.com>
2020-06-10 23:05:50 +0600

    [fix] check wallet balance before send sms

--- app/Jobs/SendToCustomerToInformDueDepositSMS.php
+++ app/Jobs/SendToCustomerToInformDueDepositSMS.php
@@ -2,10 +2,13 @@
 
 namespace App\Jobs;
 use App\Repositories\SmsHandler;
+use App\Repositories\SmsHandler as SmsHandlerRepo;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Sheba\FraudDetection\TransactionSources;
 use Sheba\PartnerWallet\PartnerTransactionHandler;
+use Sheba\Transactions\Wallet\WalletTransactionHandler;
 
 
 class SendToCustomerToInformDueDepositSMS extends Job implements ShouldQueue
@@ -35,29 +38,43 @@ class SendToCustomerToInformDueDepositSMS extends Job implements ShouldQueue
     public function handle()
     {
         try {
-            if ($this->data['type'] == 'due') {
-                $sms = (new SmsHandler('inform-due'))->setVendor('infobip')->send($this->data['mobile'], [
-                    'customer_name' => $this->data['customer_name'],
-                    'partner_name' => $this->data['partner_name'],
-                    'amount' => $this->data['amount'],
-                    'payment_link' => $this->data['payment_link']
-                ]);
-                $log = " BDT has been deducted for sending due details";
-            } else {
-                $sms = (new SmsHandler('inform-deposit'))->setVendor('infobip')->send($this->data['mobile'], [
-                    'customer_name' => $this->data['customer_name'],
-                    'partner_name' => $this->data['partner_name'],
-                    'amount' => $this->data['amount'],
-                ]);
-                $log = " BDT has been deducted for sending deposit details";
-            }
+            list($sms, $log) = $this->getSms();
             $sms_cost = $sms->getCost();
-            $partner_transaction_handler = new PartnerTransactionHandler($this->partner);
-            $partner_transaction_handler->debit($sms_cost, $sms_cost . $log, null, null);
+            if ((double)$this->partner->wallet > (double)$sms_cost) {
+                /** @var WalletTransactionHandler $walletTransactionHandler */
+                $sms->shoot();
+                (new WalletTransactionHandler())->setModel($this->partner)->setAmount($sms_cost)->setType('debit')->setLog($sms_cost . $log)->setTransactionDetails([])->setSource(TransactionSources::SMS)->store();
+            }
 
         } catch (\Throwable $e) {
             app('sentry')->captureException($e);
         }
 
+    }
+
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    private function getSms()
+    {
+
+        if ($this->data['type'] == 'due') {
+            $sms = (new SmsHandlerRepo('inform-due'))->setVendor('infobip')->setMobile($this->data['mobile'])->setMessage([
+                'customer_name' => $this->data['customer_name'],
+                'partner_name' => $this->data['partner_name'],
+                'amount' => $this->data['amount'],
+                'payment_link' => $this->data['payment_link']
+            ]);
+            $log = " BDT has been deducted for sending due details";
+        } else {
+            $sms = (new SmsHandlerRepo('inform-deposit'))->setVendor('infobip')->setMobile($this->data['mobile'])->setMessage([
+                'customer_name' => $this->data['customer_name'],
+                'partner_name' => $this->data['partner_name'],
+                'amount' => $this->data['amount'],
+            ]);
+            $log = " BDT has been deducted for sending deposit details";
+        }
+        return [$sms, $log];
     }
 }
