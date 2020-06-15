662c097a632f85cdbb426e5f0d93d2bd1fada18c
arnabrahman <arnabrahman@hotmail.com>
2020-06-15 11:33:48 +0600

    ⬆️ Bkash client url
    Type(s): Update

--- app/Http/Middleware/Cors2MiddleWare.php
+++ app/Http/Middleware/Cors2MiddleWare.php
@@ -101,7 +101,8 @@ class Cors2MiddleWare
             "http://payment-link-web.sheba.test:3031",
             "https://v3.sheba.xyz",
             "https://www.smanager.xyz",
-            "https://v2.sheba.xyz"
+            "https://v2.sheba.xyz",
+            "https://bkash-client.dev-sheba.xyz"
         ];
         // ALLOW OPTIONS METHOD
         $headers['Access-Control-Allow-Credentials'] = 'true';
@@ -109,7 +110,7 @@ class Cors2MiddleWare
         $headers['Access-Control-Allow-Headers'] = 'Content-Type, X-Auth-Token, Origin, Authorization, X-Requested-With, Portal-Name, User-Id';
         $headers['Access-Control-Allow-Origin'] = '*';
         if (!in_array($request->server('HTTP_ORIGIN'), $domains)) {
-            return response()->json(['message' => 'Unauthorized domain :'.$request->server('HTTP_ORIGIN'), 'code' => 401])->withHeaders($headers);
+            return response()->json(['message' => 'Unauthorized domain :' . $request->server('HTTP_ORIGIN'), 'code' => 401])->withHeaders($headers);
         }
 
         // ALLOW OPTIONS METHOD
