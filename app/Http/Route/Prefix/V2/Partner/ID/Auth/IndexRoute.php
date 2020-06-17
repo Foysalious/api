abb3c2f957de818543bee211c37e19f943ed7163
Arafat <syedyeaminarafat@gmail.com>
2020-06-17 19:14:30 +0600

    [UPDATE] #check if bondhu profile exists for partner resource

--- app/Http/Route/Prefix/V2/Partner/ID/Auth/IndexRoute.php
+++ app/Http/Route/Prefix/V2/Partner/ID/Auth/IndexRoute.php
@@ -293,6 +293,8 @@ class IndexRoute
                 $api->get('check-verification', 'Partner\ProfileController@checkVerification');
                 $api->post('submit-data-for-verification', 'Partner\ProfileController@submitDataForVerification');
                 $api->post('verification-message-seen-status', 'Partner\ProfileController@updateSeenStatus');
+                $api->get('check-first-time-user', 'Partner\ProfileController@checkFirstTimeUser');
+
             });
             (new IncomeExpenseRoute())->set($api);
             (new BidRoute())->set($api);
