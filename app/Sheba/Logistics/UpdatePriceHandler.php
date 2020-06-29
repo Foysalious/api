39852f42376f03b1dec3dca5313ece4bb5fd5ffe
arnabrahman <arnabrahman@hotmail.com>
2020-06-29 14:27:48 +0600

    ➕ Don't hit logistics service if request come from logistics server
    Type(s): ADD

--- app/Sheba/Logistics/UpdatePriceHandler.php
+++ app/Sheba/Logistics/UpdatePriceHandler.php
@@ -27,6 +27,7 @@ class UpdatePriceHandler
      */
     public function update()
     {
+        if (request()->header('origin') == config('logistics.api_url')) return;
         /** @var Job $job */
         $job = $this->partnerOrder->lastJob();
         $logistic_order = $job->getCurrentLogisticOrder();
