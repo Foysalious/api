98cdbb2832354b900a013ac75f4d2c5742a5a36b
Pasha <pasha.startern@gmail.com>
2020-07-04 13:52:16 +0600

    Requester inject and push
    Type(s): UPDATE

--- app/Http/Controllers/B2b/ProcurementPaymentController.php
+++ app/Http/Controllers/B2b/ProcurementPaymentController.php
@@ -1,11 +1,12 @@
 <?php namespace App\Http\Controllers\B2b;
 
-
 use App\Http\Controllers\Controller;
 use App\Models\Procurement;
 use App\Sheba\Business\Procurement\Updater;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Sheba\Business\Procurement\OrderClosedHandler;
+use Sheba\Business\Procurement\RequestHandler;
 use Sheba\Business\ProcurementPayment\Creator;
 use Sheba\ModificationFields;
 use Sheba\Repositories\Interfaces\ProcurementRepositoryInterface;
@@ -15,17 +16,38 @@ class ProcurementPaymentController extends Controller
 {
     use ModificationFields;
 
-    public function adjustPayment($business, $procurement, Request $request, Creator $payment_creator, Updater $procurement_updater, ProcurementRepositoryInterface $procurement_repository, OrderClosedHandler $procurement_order_close_handler)
+    /**
+     * @param $business
+     * @param $procurement
+     * @param Request $request
+     * @param Creator $payment_creator
+     * @param Updater $procurement_updater
+     * @param ProcurementRepositoryInterface $procurement_repository
+     * @param OrderClosedHandler $procurement_order_close_handler
+     * @param RequestHandler $request_handler
+     * @return JsonResponse
+     */
+    public function adjustPayment($business, $procurement, Request $request,
+                                  Creator $payment_creator, Updater $procurement_updater,
+                                  ProcurementRepositoryInterface $procurement_repository,
+                                  OrderClosedHandler $procurement_order_close_handler,
+                                  RequestHandler $request_handler)
     {
         $this->validate($request, ['payment_method' => 'required|string', 'sheba_collection' => 'required|numeric']);
         $this->setModifier($request->manager_member);
+
         /** @var Procurement $procurement */
-        $procurement = $procurement_repository->where('id', $procurement)->where('owner_id', $business)
+        $procurement = $procurement_repository
+            ->where('id', $procurement)->where('owner_id', $business)
             ->where('owner_type', 'like', '%business')->first();
+
         if (!$procurement) return api_response($request, 1, 404);
         $procurement->calculate();
-        if ((double)$request->sheba_collection > $procurement->due) return api_response($request, 1, 403, ['message' => "Can't collect more than due"]);
-        DB::transaction(function () use ($request, $procurement_updater, $procurement, $payment_creator, $procurement_order_close_handler) {
+
+        if ((double)$request->sheba_collection > $procurement->due)
+            return api_response($request, 1, 403, ['message' => "Can't collect more than due"]);
+
+        DB::transaction(function () use ($request, $procurement_updater, $procurement, $payment_creator, $procurement_order_close_handler, $request_handler) {
             $payment_creator->setAmount($request->sheba_collection)
                 ->setPaymentMethod($request->payment_method)
                 ->setPaymentType('Debit')
@@ -37,9 +59,18 @@ class ProcurementPaymentController extends Controller
                 ->setAttachment($request->attachment)
                 ->setAttachmentId($request->transaction_id)
                 ->create();
-            $procurement_updater->setProcurement($procurement)->setShebaCollection($procurement->sheba_collection + $request->sheba_collection)->update();
-            $procurement_order_close_handler->setProcurement($procurement->fresh())->run();
+
+            $procurement_updater
+                ->setProcurement($procurement)
+                ->setRequestHandler($request_handler)
+                ->setShebaCollection($procurement->sheba_collection + $request->sheba_collection)
+                ->update();
+
+            $procurement_order_close_handler
+                ->setProcurement($procurement->fresh())
+                ->run();
         });
+
         return api_response($request, 1, 200);
     }
 }
