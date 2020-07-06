be84ec8220ed65c80227c4bb95951c4cdb32aa37
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-07-06 20:29:16 +0600

    ⬆️ SSL Balance Update
    Type(s): UPDATE

--- app/Sheba/TopUp/Gateway/Ssl.php
+++ app/Sheba/TopUp/Gateway/Ssl.php
@@ -34,8 +34,4 @@ class Ssl implements Gateway
         return self::SHEBA_COMMISSION;
     }
 
-    public function getBalance()
-    {
-        return $this->ssl->getBalance()->available_credit;
-    }
 }
