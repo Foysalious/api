c78281dc5c00ca9f67d2ab90add7d69ce40e3d17
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-07-07 18:45:34 +0600

    🚑 Delivery Location
    Type(s): HOTFIX

--- app/Sheba/Resource/Jobs/JobList.php
+++ app/Sheba/Resource/Jobs/JobList.php
@@ -208,7 +208,7 @@ class JobList
             $formatted_job->put('category_id', $job->category_id);
             $formatted_job->put('category_name', $job->category->name);
             $formatted_job->put('delivery_address', $job->partnerOrder->order->deliveryAddress ? $job->partnerOrder->order->deliveryAddress->address : $job->partnerOrder->order->delivery_address);
-            $formatted_job->put('location', $job->partnerOrder->order->location ? $job->partnerOrder->order->location->name : null);
+            $formatted_job->put('location', $job->partnerOrder->order->deliveryAddress && $job->partnerOrder->order->deliveryAddress->location ? $job->partnerOrder->order->deliveryAddress->location->name : null);
             $formatted_job->put('delivery_mobile', $job->partnerOrder->order->deliveryAddress ? $job->partnerOrder->order->deliveryAddress->mobile :  $job->partnerOrder->order->delivery_mobile);
             $formatted_job->put('start_time', Carbon::parse($job->preferred_time_start)->format('h:i A'));
             $formatted_job->put('services', $this->jobInfo->formatServices($job->jobServices));
