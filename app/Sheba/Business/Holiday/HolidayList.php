77e099e27336561d1b97c00475df3f3d11cdfdf5
Rashad Akhtar <rashad@sheba.xyz>
2020-07-10 19:36:52 +0600

    [UPDATE] # Holiday List Api

--- app/Sheba/Business/Holiday/HolidayList.php
+++ app/Sheba/Business/Holiday/HolidayList.php
@@ -25,7 +25,10 @@ class HolidayList
             $diff_in_days = $holiday->start_date->diffInDays($holiday->end_date);
             array_push($holiday_list, [
                 'id' => $holiday->id,
-                'date' => $diff_in_days === 0 ? $holiday->start_date->format('d M, Y') : $holiday->start_date->format('d M, Y') . ' - ' . $holiday->end_date->format('d M, Y'),
+                'start_date' => $holiday->start_date->format('d/m/Y'),
+                'end_date' => $holiday->end_date->format('d/m/Y'),
+                'day_difference' => $holiday->start_date->diffInDays($holiday->end_date),
+//                'date' => $diff_in_days === 0 ? $holiday->start_date->format('d M, Y') : $holiday->start_date->format('d M, Y') . ' - ' . $holiday->end_date->format('d M, Y'),
                 'total_days' => $diff_in_days === 0 ? ($diff_in_days + 1).' day' : ($diff_in_days + 1).' days',
                 'name' => $holiday->title
             ]);
