ce570d62c8dbcb22367b743c23442ef9943d971b
shovan <shovan.sheba@gmail.com>
2020-07-14 02:28:00 +0600

    ⬆️ Manager-app new notification system support
    Type(s): UPDATE

--- app/Sheba/PushNotificationHandler.php
+++ app/Sheba/PushNotificationHandler.php
@@ -10,6 +10,7 @@ class PushNotificationHandler
 {
     public function send($notification_data, $topic, $channel = null, $sound = 'default')
     {
+        $str_topic = $topic;
         $notificationBuilder = new PayloadNotificationBuilder($notification_data['title']);
         $notificationBuilder->setBody($notification_data['message'])->setSound($sound)->setChannelId($channel);
         $notification = $notificationBuilder->build();
@@ -22,6 +23,11 @@ class PushNotificationHandler
 
         if (config('sheba.send_push_notifications')) {
             $topicResponse = FCM::sendToTopic($topic, null, $notification, $data);
+            if (strpos($str_topic, config('sheba.push_notification_topic_name.manager')) == 0) {
+                $str_topic = str_replace(config('sheba.push_notification_topic_name.manager'), config('sheba.push_notification_topic_name.manager_new'), $str_topic);
+                $topic = (new Topics())->topic($str_topic);
+                $topicResponse = FCM::sendToTopic($topic, null, null, $data);
+            }
         }
 
         //$topicResponse->isSuccess();
