268bad85d861c3d2e4240cc25f313c7f57661e67
shovan <shovan.sheba@gmail.com>
2020-07-15 13:46:52 +0600

    ⬆️ Claim list added in loan details api
    Type(s): UPDATE

--- app/Sheba/Loan/Loan.php
+++ app/Sheba/Loan/Loan.php
@@ -368,15 +368,15 @@ class Loan
         return true;
     }
 
-    public function claimList($request)
+    public function claimList($loan_id)
     {
-        $claims = (new LoanClaim())->getAll($request->loan_id);
-        $pending_claim = (new LoanClaim())->getPending($request->loan_id);
+        $claims = (new LoanClaim())->getAll($loan_id);
+        $pending_claim = (new LoanClaim())->getPending($loan_id);
         $data['claim_list'] = [];
         $data['pending_claim'] = null;
-        $data['can_claim'] = 1;
-        $data['should_pay'] = 0;
-        list($data['can_claim'],$data['should_pay']) = $this->canClaimShouldPay($request);
+        //$data['can_claim'] = 1;
+       // $data['should_pay'] = 0;
+       // list($data['can_claim'],$data['should_pay']) = $this->canClaimShouldPay($request);
 
 
         if($pending_claim){
@@ -664,6 +664,7 @@ class Loan
         $loan                   = (new PartnerLoanRequest($request));
         $details                = $loan->details();
         $details['next_status'] = $loan->getNextStatus($loan_id);
+        $details['claims'] = $this->claimList($loan_id);
         return $details;
     }
 
