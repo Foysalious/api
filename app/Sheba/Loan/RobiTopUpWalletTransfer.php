f8f684971f0d30b3f163f231c74abc929553d8d5
Arafat <yeamin@sheba.xyz>
2020-07-19 14:13:37 +0600

    ⬆️ Robi topup balance transfer from resource for micro loan
    Type(s): UPDATE

---
+++ app/Sheba/Loan/RobiTopUpWalletTransfer.php
@@ -0,0 +1,66 @@
+<?php namespace Sheba\Loan;
+
+
+use App\Models\Affiliate;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+Class RobiTopUpWalletTransfer
+{
+    private $affiliate, $amount, $type;
+
+    public function setAffiliate(Affiliate $affiliate)
+    {
+        $this->affiliate = $affiliate;
+        return $this;
+    }
+
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+        return $this;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    private function transfer()
+    {
+        $this->affiliate->robi_topup_wallet += $this->amount;
+        $this->affiliate->update();
+    }
+
+    private function storeTransactionRecord()
+    {
+        $formatted_data = [
+            'type' => ucfirst($this->type),
+            'log' => $this->getLog(),
+            'created_at' => Carbon::now(),
+            'balance' => $this->getCalculatedBalance(),
+            'amount' => $this->amount
+        ];
+    }
+
+    public function process($data = [])
+    {
+        DB::transaction(function () use ($data, &$transaction) {
+            $this->transfer();
+        });
+    }
+
+    private function getLog()
+    {
+        return "Robi topup balance transfer for MICRO loan";
+    }
+
+    private function getCalculatedBalance()
+    {
+        $last_inserted_transaction = $this->affiliate->orderBy('id', 'desc')->first();
+        $last_inserted_balance = $last_inserted_transaction ? $last_inserted_transaction->balance : 0.00;
+        return strtolower($this->type) == 'credit' ? $last_inserted_balance + $this->amount : $last_inserted_balance - $this->amount;
+    }
+
+}
