8721a64e43eef748307f4c12c60602620360d8eb
Irteza Asad <irtezaasad.sheba@gmail.com>
2020-07-19 18:39:16 +0600

    ⬆️ cars api code organize
    Type(s): UPDATE

--- app/Http/Controllers/RentACar/RentACarController.php
+++ app/Http/Controllers/RentACar/RentACarController.php
@@ -77,7 +77,46 @@ class RentACarController extends Controller
 
             $car_prices = $service_model->category->isRentACarOutsideCity()
                 ? $this->getOptionUnitPricesOfServiceForOutsideCity($service_model, $service->getPickupThana(), $service->getDestinationThana())
-                : $location_service ? json_decode($location_service->prices, true) : [];;
+                : $location_service ? json_decode($location_service->prices, true) : [];
+
+            $cars = [];
+            $price_calculation = $this->resolvePriceCalculation($service_model->category);
+
+
+
+            foreach ($car_types as $key => $car) {
+                $option = [$key];
+                $price_calculation->setService($service_model)->setOption($option)->setQuantity($service->getQuantity());
+                $service_model->category->isRentACarOutsideCity() ? $price_calculation->setPickupThanaId($service->getPickupThana()->id)->setDestinationThanaId($service->getDestinationThana()->id) : $price_calculation->setLocationService($location_service);
+                $original_price = $price_calculation->getTotalOriginalPrice();
+                $discount_calculation->setService($service_model)->setLocationService($location_service)->setOriginalPrice($original_price)->calculate();
+                $discounted_price =  $discount_calculation->getDiscountedPrice();
+                $unit_price = $car_prices[$key] ? (double) $car_prices[$key] : null;
+                $surcharge = $price_calculation->getSurcharge();
+                $surcharge_amount =  $surcharge
+                    ? $surcharge->is_amount_percentage
+                        ? ($unit_price / 100) * $surcharge->amount
+                        : $surcharge->amount
+                    : null;
+                $answer = [
+                    'name' => $car,
+                    'image' => 'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/sheba_xyz/png/'.$variables['helpers']['assets'][$key].'.png',
+                    'number_of_seats' => $variables['helpers']['capacity'][$key],
+                    'info' => $variables['helpers']['descriptions'][$key],
+                    'discounted_price' => $discounted_price,
+                    'original_price' => $original_price,
+                    'discount' => $discount_calculation->getDiscount(),
+                    'quantity' => $service->getQuantity(),
+                    'is_surcharge_applied' => !!($surcharge) ? 1 : 0,
+                    'surcharge_percentage' => $surcharge ? $surcharge->amount : null,
+                    'surcharge_amount' => $surcharge_amount,
+                    'unit_price' => $unit_price,
+                    'sheba_contribution' => $discount_calculation->getShebaContribution(),
+                    'partner_contribution' => $discount_calculation->getPartnerContribution(),
+                    'is_discount_percentage' => $discount_calculation->getIsDiscountPercentage()
+                ];
+                $cars[] = $answer;
+            }
 
 
 
@@ -90,108 +129,6 @@ class RentACarController extends Controller
             return api_response($request, null, 400, ['message' => 'Please try with inside city for this location.', 'code' => 702]);
         }
 
-    }
-
-    public function getCarsForOutsideCity($service, $discount_calculation)
-    {
-        $service_model = $service->getService();
-        $variables = json_decode($service->getService()->variables, true);
-        $car_types = $this->getCarTypes($variables);
-        $car_prices = $this->getOptionUnitPricesOfServiceForOutsideCity($service_model, $service->getPickupThana(), $service->getDestinationThana());
-        $cars = [];
-        $surcharge = $this->getSurchargeOfService($service_model);
-
-        $location_service = LocationService::where([['location_id', $service->getHyperLocal()->location_id], ['service_id', $service->getServiceId()]])->first();
-
-        $price_calculation = $this->resolvePriceCalculation($service_model->category);
-
-
-
-        foreach ($car_types as $key => $car) {
-            $option = [$key];
-            $price_calculation->setService($service_model)->setOption($option)->setQuantity($service->getQuantity());
-            $price_calculation->setPickupThanaId($service->getPickupThana()->id)->setDestinationThanaId($service->getDestinationThana()->id);
-            $original_price = $price_calculation->getTotalOriginalPrice();
-            $discount_calculation->setService($service_model)->setLocationService($location_service)->setOriginalPrice($original_price)->calculate();
-            $discounted_price =  $discount_calculation->getDiscountedPrice();
-            $surcharge_amount = $surcharge
-                ? $surcharge->is_amount_percentage
-                    ? ($discounted_price / 100) * $surcharge->amount
-                    : $surcharge->amount
-                : null;
-
-
-            $answer = [
-                'name' => $car,
-                'image' => 'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/sheba_xyz/png/'.$variables['helpers']['assets'][$key].'.png',
-                'number_of_seats' => $variables['helpers']['capacity'][$key],
-                'info' => $variables['helpers']['descriptions'][$key],
-                'discounted_price' => $discounted_price,
-                'original_price' => $original_price,
-                'discount' => $discount_calculation->getDiscount(),
-                'quantity' => $service->getQuantity(),
-                'is_surcharge_applied' => !!($surcharge) ? 1 : 0,
-                'surcharge_percentage' => $surcharge ? $surcharge->amount : null,
-                'surcharge_amount' => $surcharge_amount,
-                'unit_price' => $car_prices[$key] ? (double) $car_prices[$key] : null,
-                'sheba_contribution' => $discount_calculation->getShebaContribution(),
-                'partner_contribution' => $discount_calculation->getPartnerContribution(),
-                'is_discount_percentage' => $discount_calculation->getIsDiscountPercentage()
-            ];
-            $cars[] = $answer;
-        }
-
-        return $cars;
-    }
-
-    public function getCarsForInsideCity($service, $discount_calculation)
-    {
-        $service_model = $service->getService();
-        $variables = json_decode($service->getService()->variables, true);
-        $car_types = $this->getCarTypes($variables);
-
-        $cars = [];
-        $surcharge = $this->getSurchargeOfService($service_model);
-
-        $location_service = LocationService::where([['location_id', $service->getHyperLocal()->location_id], ['service_id', $service->getServiceId()]])->first();
-        $car_prices = $location_service ? json_decode($location_service->prices, true) : [];
-
-
-        $price_calculation = $this->resolvePriceCalculation($service_model->category);
-
-        foreach ($car_types as $key => $car) {
-            $option = [$key];
-            $price_calculation->setService($service_model)->setOption($option)->setQuantity($service->getQuantity());
-            $price_calculation->setLocationService($location_service);
-            $original_price = $price_calculation->getTotalOriginalPrice();
-            $discount_calculation->setService($service_model)->setLocationService($location_service)->setOriginalPrice($original_price)->calculate();
-            $discounted_price =  $discount_calculation->getDiscountedPrice();
-            $surcharge_amount = $surcharge
-                ? $surcharge->is_amount_percentage
-                    ? ($discounted_price / 100) * $surcharge->amount
-                    : $surcharge->amount
-                : null;
-            $answer = [
-                'name' => $car,
-                'image' => 'https://s3.ap-south-1.amazonaws.com/cdn-shebaxyz/sheba_xyz/png/'.$variables['helpers']['assets'][$key].'.png',
-                'number_of_seats' => $variables['helpers']['capacity'][$key],
-                'info' => $variables['helpers']['descriptions'][$key],
-                'discounted_price' => $discounted_price,
-                'original_price' => $original_price,
-                'discount' => $discount_calculation->getDiscount(),
-                'quantity' => $service->getQuantity(),
-                'is_surcharge_applied' => !!($surcharge) ? 1 : 0,
-                'surcharge_percentage' => $surcharge ? $surcharge->amount : null,
-                'surcharge_amount' => $surcharge_amount,
-                'unit_price' => $car_prices[$key],
-                'sheba_contribution' => $discount_calculation->getShebaContribution(),
-                'partner_contribution' => $discount_calculation->getPartnerContribution(),
-                'is_discount_percentage' => $discount_calculation->getIsDiscountPercentage()
-            ];
-            $cars[] = $answer;
-        }
-
-        return $cars;
     }
 
     public function getCarTypes($variables)
@@ -208,11 +145,6 @@ class RentACarController extends Controller
             ['destination_thana_id', $destination_thana->id]
         ])->first();
         return $car_rental_prices ? json_decode($car_rental_prices->prices, true) : null;
-    }
-
-    public function getSurchargeOfService($service)
-    {
-        return ServiceSurcharge::where('service_id', $service->id)->first();
     }
 
     public function getPickupAndDestinationThana(Request $request, FromGeo $fromGeo)
