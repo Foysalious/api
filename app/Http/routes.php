98639b1ac84daa35155d038fa79f44fe859a01a0
arnabrahman <arnabrahman@hotmail.com>
2020-07-23 12:50:11 +0600

    ⬆️ Test
    Type(s): UPDATE

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Models\Customer;
+use App\Models\PartnerOrder;
+use Sheba\AutoSpAssign\Initiator;
+
 Route::get('/', function () {
+    $p_order = PartnerOrder::find(183985);
+    $customer = Customer::find(6582);
+    /** @var Initiator $initiator */
+    $initiator = app(Initiator::class);
+    $initiator->setPartnerIds([32486, 2969, 3836, 2281])->setCustomer($customer)->setPartnerOrder($p_order)->initiate();
     return ['code' => 200, 'message' => "Success. This project will hold the api's"];
 });
 $api = app('Dingo\Api\Routing\Router');
