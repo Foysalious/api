799a3a2ec824ad188840ebf9f1c7afd5812af3f1
Arafat <yeamin@sheba.xyz>
2020-07-26 16:32:04 +0600

    ⬆️ Sms sending report
    Type(s): UPDATE

--- app/Http/Controllers/LoanReportController.php
+++ app/Http/Controllers/LoanReportController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
+use Sheba\Loan\ExcelReport\IPDCSmsSendingReport;
 use Sheba\Loan\ExcelReport\LoanDisbursementReport;
-
 
 class LoanReportController extends Controller
 {
@@ -18,15 +18,42 @@ class LoanReportController extends Controller
     public function loanDisbursementReport(Request $request, LoanDisbursementReport $report)
     {
         try {
-            $this->validate($request, [
-                'start_date' => 'required',
-                'end_date' => 'required',
-            ]);
+            $this->validate($request, $this->reportValidator());
             return $report->setDates($request)->get();
         }
         catch (ValidationException $e) {
             $message = getValidationErrorMessage($e->validator->errors()->all());
             return api_response($request, $message, 400, ['data' => $message]);
         }
+    }
+
+    /**
+     * @param Request $request
+     * @param IPDCSmsSendingReport $report
+     * @return bool|\Illuminate\Http\JsonResponse
+     * @throws \Sheba\Reports\Exceptions\NotAssociativeArray
+     */
+    public function ipdcSmsSendingReport(Request $request, IPDCSmsSendingReport $report)
+    {
+        try {
+            $this->validate($request, $this->reportValidator());
+            return $report->setDates($request)->get();
+        }
+        catch (ValidationException $e) {
+            $message = getValidationErrorMessage($e->validator->errors()->all());
+            return api_response($request, $message, 400, ['data' => $message]);
+        }
+
+    }
+
+    /**
+     * @return string[]
+     */
+    private function reportValidator()
+    {
+        return [
+            'start_date' => 'required',
+            'end_date' => 'required',
+        ];
     }
 }
--- app/Http/Route/Prefix/V2/Partner/ID/Auth/LoanRoute.php
+++ app/Http/Route/Prefix/V2/Partner/ID/Auth/LoanRoute.php
@@ -11,7 +11,7 @@ class LoanRoute
             $api->post('/password/reset','Auth\PasswordController@resetPasswordForBank');
         });
 
-        $api->group(['prefix'=>'loans','middleware'=>'jwtGlobalAuth'], function ($api) {
+        $api->group(['prefix'=>'loans', 'middleware'=>'jwtGlobalAuth'], function ($api) {
             $api->post('/upload-retailer-list','LoanController@uploadRetailerList');
             $api->get('/dashboard','LoanController@getDashboardData');
             $api->post('/strategic-partner-dashboard','LoanController@strategicPartnerDashboard');
@@ -36,6 +36,7 @@ class LoanRoute
             $api->post('/{loan_id}/upload-documents','LoanController@uploadDocuments');
             $api->post('/{loan_id}/update-claim-status','LoanController@claimStatusUpdate');
             $api->get('loan-disbursement-report','LoanReportController@loanDisbursementReport');
+            $api->get('ipdc-sms-sending-report','LoanReportController@ipdcSmsSendingReport');
 
         });
 
