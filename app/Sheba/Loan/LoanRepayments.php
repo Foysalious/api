3721c2835841ff72cc400bc8d111a0db5434147b
tonmoy <shtonmoy007@gmail.com>
2020-07-28 12:35:24 +0600

    ⬆️  LOAN ROUTE

---
+++ app/Sheba/Loan/LoanRepayments.php
@@ -0,0 +1,110 @@
+<?php
+
+
+namespace Sheba\Loan;
+
+
+use App\Models\Partner;
+use App\Models\Resource;
+use App\Sheba\Loan\DLSV2\Exceptions\InsufficientWalletCreditForRepayment;
+use App\Sheba\Loan\DLSV2\LoanClaim;
+use App\Sheba\Loan\DLSV2\Repayment;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Sheba\FraudDetection\TransactionSources;
+use Sheba\ModificationFields;
+use Sheba\Transactions\Wallet\WalletTransactionHandler;
+
+class LoanRepayments
+{
+    use ModificationFields;
+    /** @var Partner $partner */
+    private $partner;
+    /** @var Resource $resource */
+    private $resource;
+    /**
+     * @var LoanRepository
+     */
+    private $repo;
+
+    public function __construct() {
+        $this->repo           = new LoanRepository();
+    }
+
+    /**
+     * @param Partner $partner
+     * @return LoanRepayments
+     */
+    public function setPartner($partner)
+    {
+        $this->partner = $partner;
+        return $this;
+    }
+
+    /**
+     * @param Resource $resource
+     * @return LoanRepayments
+     */
+    public function setResource($resource)
+    {
+        $this->resource = $resource;
+        return $this;
+    }
+    /**
+     * @param $request
+     * @throws InsufficientWalletCreditForRepayment
+     */
+    public function repaymentFromWallet($request)
+    {
+        $last_claim = (new LoanClaim())->setLoan($request->loan_id)->lastClaim();
+        $this->balanceCheck($request->amount);
+        DB::transaction(function () use ($last_claim, $request) {
+            $this->debitFromWallet($request->loan_id, $request->amount);
+            (new Repayment())->setLoan($request->loan_id)->setClaim($last_claim->id)->setAmount($request->amount)->repaymentFromWallet();
+        });
+    }
+
+    /**
+     * @param $amount
+     * @return bool
+     * @throws InsufficientWalletCreditForRepayment
+     */
+    private function balanceCheck($amount)
+    {
+
+        if ((double)$this->partner->wallet < $amount)
+            throw new InsufficientWalletCreditForRepayment();
+        return true;
+
+    }
+
+    private function debitFromWallet($loan_id, $amount)
+    {
+        $this->setModifier($this->resource);
+        (new WalletTransactionHandler())->setModel($this->partner)->setAmount($amount)->setSource(TransactionSources::LOAN_REPAYMENT)->setType('debit')->setLog("$amount BDT has been collected from {$this->resource->profile->name} as Loan Repayment  for  loan: $loan_id")->store();
+        return true;
+    }
+
+    public function repaymentList($loan_id, $all = false, $month = null, $year = null)
+    {
+        $repayments = !$all ? (new Repayment())->getByYearAndMonth($loan_id, $month, $year) : (new Repayment())->getAll($loan_id);
+        $last_claim = (new LoanClaim())->setLoan($loan_id)->lastClaim();
+
+        $data['repayment_list'] = [];
+
+        foreach ($repayments as $repayment) {
+            array_push($data['repayment_list'], [
+                'id'          => $repayment->id,
+                'claim_id'    => $repayment->loan_claim_request_id,
+                'amount'      => (int)$repayment->debit == 0 ? $repayment->credit : $repayment->debit,
+                'amount_type' => (int)$repayment->debit == 0 ? 'credit' : 'debit',
+                'log'         => $repayment->log,
+                'created_at'  => Carbon::parse($repayment->created_at)->format('Y-m-d H:i:s')
+            ]);
+        }
+        $data['credit_amount'] = $this->repo->find($loan_id)->loan_amount;
+        $data['due_amount']    = $last_claim ? $this->getDue($last_claim->id) : 0;
+
+        return $data;
+    }
+}
---
+++ app/Sheba/Payment/Complete/LoanRepaymentComplete.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Sheba\Payment\Complete;
+
+
+class LoanRepaymentComplete extends PaymentComplete
+{
+
+    protected function saveInvoice()
+    {
+        // TODO: Implement saveInvoice() method.
+    }
+
+    public function complete()
+    {
+        // TODO: Implement complete() method.
+    }
+}
