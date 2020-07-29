7a59a05f792e5771caa49671e33e82f982830c62
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-07-29 12:29:40 +0600

    ♻️ Accept Jon Inside Order Request
    Type(s): REFACTORING

--- app/Http/Controllers/Resource/ResourceOrderController.php
+++ app/Http/Controllers/Resource/ResourceOrderController.php
@@ -14,7 +14,7 @@ use Sheba\Order\OrderCreateRequest;
 
 class ResourceOrderController extends Controller
 {
-    public function placeOrder(Request $request, Geo $geo, OrderCreateRequest $orderCreateRequest, AcceptJobAndAssignResource $acceptJobAndAssignResource)
+    public function placeOrder(Request $request, Geo $geo, OrderCreateRequest $orderCreateRequest)
     {
         $request->merge(['mobile' => formatMobile($request->mobile)]);
         $this->validate($request, [
@@ -36,15 +36,10 @@ class ResourceOrderController extends Controller
         $location = Location::find($request->location_id);
         $geo_info = json_decode($location->geo_informations);
         $geo->setLat($geo_info->lat)->setLng($geo_info->lng);
-        $orderCreateRequest->setGeo($geo)->setServices($request->services)->setDate($request->date)->setTime($request->time)->setPartnerId($request->partner);
-        if ($request->assign_resource) $orderCreateRequest->setResource($resource);
-        $orderCreateRequest->setMobile($request->mobile)->setName($request->name)->setAddress($request->address)->setAdditionalInformation($request->additional_information)->setSalesChannel($request->sales_channel)->setPaymentMethod($request->payment_method);
-        $response = $orderCreateRequest->create();
-        if ($request->assign_resource && $response->hasSuccess()) {
-            $job = Job::find($response->getResponse()->job_id);
-            $partner = Partner::find($request->partner);
-            $acceptJobAndAssignResource->setJob($job)->setPartner($partner)->setResource($resource)->setRequest($request)->acceptJobAndAssignResource();
-        }
+        $response = $orderCreateRequest->setGeo($geo)->setServices($request->services)->setDate($request->date)->setTime($request->time)
+            ->setPartnerId($request->partner)->setMobile($request->mobile)->setName($request->name)->setAddress($request->address)
+            ->setAdditionalInformation($request->additional_information)->setSalesChannel($request->sales_channel)
+            ->setPaymentMethod($request->payment_method)->setAssignResource($request->assign_resource)->setRequest($request)->setResource($resource)->create();
         return api_response($request, null, $response->getCode(), ['message' => $response->getMessage()]);
     }
 }
