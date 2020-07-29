722c679f07962b876f9e34a8edb48acd27f80e31
Arafat <yeamin@sheba.xyz>
2020-07-29 10:43:42 +0600

    ⬆️  NOTIFICATION LIST

---
+++ app/Repositories/BankUserNotificationRepository.php
@@ -0,0 +1,16 @@
+<?php namespace App\Repositories;
+
+Class BankUserNotificationRepository
+{
+    public function getBankUserNotifications($model, $offset, $limit)
+    {
+        $notifications = $model->notifications()->select('id', 'title', 'event_type', 'event_id', 'type', 'is_seen', 'created_at')->orderBy('id', 'desc')->skip($offset)->limit($limit)->get();
+        if (count($notifications) > 0)
+            $notifications = $notifications->map(function ($notification) {
+                array_add($notification, 'time', $notification->created_at->diffForHumans());
+                return $notification;
+            });
+
+        return $notifications;
+    }
+}
