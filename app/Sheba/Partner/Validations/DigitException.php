d3148559942acc774d119512eb75a4e91fb941e3
arnabrahman <arnabrahman@hotmail.com>
2020-08-06 12:41:44 +0600

    🔀 Master
    Type(s): MERGE

---
+++ app/Sheba/Partner/Validations/DigitException.php
@@ -0,0 +1,13 @@
+<?php namespace Sheba\Partner\Validations;
+
+
+use Throwable;
+
+class DigitException extends \Exception
+{
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        $message = 'Date of birth is required for 13 digit nid number';
+        parent::__construct($message, $code, $previous);
+    }
+}
---
+++ app/Sheba/Partner/Validations/InvalidVendorException.php
@@ -0,0 +1,19 @@
+<?php namespace Sheba\Partner\Validations;
+
+use Sheba\Exceptions;
+use Throwable;
+
+class InvalidVendorException extends \Exception
+{
+    /**
+     * InvalidVendorException constructor.
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        $message = "Vendor is invalid,Please set a valid vendor";
+        parent::__construct($message, $code, $previous);
+    }
+}
