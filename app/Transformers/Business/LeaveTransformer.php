26220ebde49d49333e8b490c6e739675be0f7a26
Pasha <pasha.startern@gmail.com>
2020-08-07 23:54:33 +0600

    SUBSTITUTE AND APPROVAR LISTS
    Type(s): ADD

--- app/Transformers/Business/LeaveTransformer.php
+++ app/Transformers/Business/LeaveTransformer.php
@@ -1,17 +1,30 @@
 <?php namespace App\Transformers\Business;
 
+use App\Models\BusinessMember;
+use App\Models\Member;
+use App\Models\Profile;
 use App\Transformers\AttachmentTransformer;
 use League\Fractal\TransformerAbstract;
-use Sheba\Dal\Leave\Model as LeaveModel;
+use Sheba\Dal\ApprovalRequest\ApprovalRequestPresenter as ApprovalRequestPresenter;
+use Sheba\Dal\Leave\Model as Leave;
 use Sheba\Dal\Leave\LeaveStatusPresenter as LeaveStatusPresenter;
+use Sheba\Dal\LeaveType\Model as LeaveType;
 
 class LeaveTransformer extends TransformerAbstract
 {
     protected $defaultIncludes = ['attachments'];
 
-    public function transform(LeaveModel $leave)
+    public function transform(Leave $leave)
     {
+        /** @var LeaveType $leave_type */
         $leave_type = $leave->leaveType;
+        /** @var BusinessMember $substitute_business_member */
+        $substitute_business_member = $leave->substitute;
+        /** @var Member $member */
+        $substitute_member = $substitute_business_member ? $substitute_business_member->member : null;
+        /** @var Profile $profile */
+        $leave_substitute = $substitute_member ? $substitute_member->profile : null;
+
         return [
             'title' => $leave->title,
             'leave_type' => $leave_type->title,
@@ -21,6 +34,10 @@ class LeaveTransformer extends TransformerAbstract
             'status' => LeaveStatusPresenter::statuses()[$leave->status],
             'requested_on' => $leave->created_at,
             'note' => $leave->note,
+            'substitute' => $substitute_business_member ? [
+                'name' => $leave_substitute->name
+            ] : null,
+            'approvers' => $this->getApprover($leave)
         ];
     }
 
@@ -30,5 +47,25 @@ class LeaveTransformer extends TransformerAbstract
         return $collection->getData() ? $collection : $this->item(null, function () {
             return [];
         });
+    }
+
+    /**
+     * @param Leave $leave
+     * @return array
+     */
+    private function getApprover(Leave $leave)
+    {
+        $approvers = [];
+        foreach ($leave->requests as $approval_request) {
+            $business_member = $approval_request->approver;
+            $member = $business_member->member;
+            $profile = $member->profile;
+            array_push($approvers, [
+                'name' => $profile->name,
+                'status' => ApprovalRequestPresenter::statuses()[$approval_request->status]
+            ]);
+        }
+
+        return $approvers;
     }
 }
