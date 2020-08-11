3df62c0fd5b20f28dad83b705cd3447466a87543
Saiful <saiful.sheba@gmail.com>
2020-08-11 15:50:47 +0600

    [UPDATE]Substitute Notification Message Change

--- app/Sheba/Business/Leave/Creator.php
+++ app/Sheba/Business/Leave/Creator.php
@@ -252,10 +252,11 @@ class Creator
         $leave_applicant = $member->profile->name;
         $topic = config('sheba.push_notification_topic_name.employee') . (int)$substitute_business_member->member->id;
         $channel = config('sheba.push_notification_channel_name.employee');
-
+        $start_date = $leave->start_date->format('d/m/Y');
+        $end_date = $leave->end_date->format('d/m/Y');
         $notification_data = [
-            "title" => 'Substitute Setup',
-            "message" => "$leave_applicant choose you a substitute.",
+            "title" => 'Leave substitute',
+            "message" => "You have been chosen as $leave_applicant's substitute from $start_date to $end_date",
             "event_type" => 'substitute',
             "event_id" => $leave->id,
             "sound" => "notification_sound",
