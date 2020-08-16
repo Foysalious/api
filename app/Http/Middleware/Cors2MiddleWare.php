6b1e10a2eb79362dc4c270f3fce54519f4909655
tonmoy <shtonmoy007@gmail.com>
2020-08-16 18:31:01 +0600

    ➕ RETAILER CORS
    Type(s): ADD

--- app/Http/Middleware/Cors2MiddleWare.php
+++ app/Http/Middleware/Cors2MiddleWare.php
@@ -110,7 +110,8 @@ class Cors2MiddleWare
             "https://v2.sheba.xyz",
             "https://retailers.dev-sheba.xyz",
             "https://bkash-client.dev-sheba.xyz",
-            "https://business.stage.sheba.xyz"
+            "https://business.stage.sheba.xyz",
+            "https://partners.smanager.xyz"
         ];
         // ALLOW OPTIONS METHOD
         $headers['Access-Control-Allow-Credentials'] = 'true';
