0ec28b01157c464fc64f84c6f3d872488026328a
tonmoy <shtonmoy007@gmail.com>
2020-08-19 19:32:57 +0600

    ✅ NAGAD FU$$
    Type(s): TEST

--- app/Sheba/Payment/Methods/Nagad/NagadClient.php
+++ app/Sheba/Payment/Methods/Nagad/NagadClient.php
@@ -1,9 +1,6 @@
 <?php namespace Sheba\Payment\Methods\Nagad;
 
 
-use Exception;
-use GuzzleHttp\Client;
-use Sheba\Payment\Methods\Nagad\Exception\EncryptionFailed;
 use Sheba\Payment\Methods\Nagad\Response\CheckoutComplete;
 use Sheba\Payment\Methods\Nagad\Response\Initialize;
 use Sheba\TPProxy\TPProxyClient;
@@ -19,7 +16,7 @@ class NagadClient
     private $privateKey;
     private $contextPath;
 
-    public function __construct(Client $client)
+    public function __construct(TPProxyClient $client)
     {
         $this->client      = $client;
         $this->baseUrl     = config('nagad.base_url');
@@ -32,48 +29,48 @@ class NagadClient
     /**
      * @param $transactionId
      * @return Initialize
-     * @throws EncryptionFailed
+     * @throws TPProxyServerError|Exception\EncryptionFailed
      */
     public function init($transactionId)
     {
-        $url  = "$this->baseUrl/$this->contextPath/api/dfs/check-out/initialize/$this->merchantId/$transactionId";
-        $data = Inputs::init($transactionId);
-        $resp = decodeGuzzleResponse((new Client())->post($url, ['headers' => Inputs::headers(), 'json' => $data, 'http_errors' => false]));
-       dd($resp);
+        $url     = "$this->baseUrl/$this->contextPath/api/dfs/check-out/initialize/$this->merchantId/$transactionId";
+        $data    = Inputs::init($transactionId);
+        $request = (new TPRequest())->setMethod(TPRequest::METHOD_POST)->setHeaders(Inputs::headers())->setInput($data)->setUrl($url);
+        $resp    = $this->client->call($request);
         return new Initialize($resp);
     }
 
-//    /**
-//     * @param            $transactionId
-//     * @param Initialize $resp
-//     * @param            $amount
-//     * @param            $callbackUrl
-//     * @return CheckoutComplete
-//     * @throws Exception\EncryptionFailed
-//     * @throws TPProxyServerError
-//     */
-//    public function placeOrder($transactionId, Initialize $resp, $amount, $callbackUrl)
-//    {
-//        $paymentRefId = $resp->getPaymentReferenceId();
-//        $url          = "$this->baseUrl/$this->contextPath/api/dfs/check-out/complete/$paymentRefId";
-//        $data         = Inputs::complete($transactionId, $resp, $amount, $callbackUrl);
-//        $request      = (new TPRequest())->setUrl($url)->setMethod(TPRequest::METHOD_POST)->setHeaders(Inputs::headers())->setInput($data);
-//        $resp         = $this->client->call($request);
-//        return new CheckoutComplete($resp);
-//    }
-//
-//    /**
-//     * @param $refId
-//     * @return Validator
-//     * @throws Exception\InvalidOrderId
-//     * @throws TPProxyServerError
-//     */
-//    public function validate($refId)
-//    {
-//        $url     = "$this->baseUrl/$this->contextPath/api/dfs/verify/payment/$refId";
-//        $request = (new TPRequest())->setUrl($url)->setMethod(TPRequest::METHOD_GET)->setHeaders(Inputs::headers());
-//        $resp    = $this->client->call($request);
-//        return new Validator($resp, true);
-//
-//    }
+    /**
+     * @param            $transactionId
+     * @param Initialize $resp
+     * @param            $amount
+     * @param            $callbackUrl
+     * @return CheckoutComplete
+     * @throws Exception\EncryptionFailed
+     * @throws TPProxyServerError
+     */
+    public function placeOrder($transactionId, Initialize $resp, $amount, $callbackUrl)
+    {
+        $paymentRefId = $resp->getPaymentReferenceId();
+        $url          = "$this->baseUrl/$this->contextPath/api/dfs/check-out/complete/$paymentRefId";
+        $data         = Inputs::complete($transactionId, $resp, $amount, $callbackUrl);
+        $request      = (new TPRequest())->setUrl($url)->setMethod(TPRequest::METHOD_POST)->setHeaders(Inputs::headers())->setInput($data);
+        $resp         = $this->client->call($request);
+        return new CheckoutComplete($resp);
+    }
+
+    /**
+     * @param $refId
+     * @return Validator
+     * @throws Exception\InvalidOrderId
+     * @throws TPProxyServerError
+     */
+    public function validate($refId)
+    {
+        $url     = "$this->baseUrl/$this->contextPath/api/dfs/verify/payment/$refId";
+        $request = (new TPRequest())->setUrl($url)->setMethod(TPRequest::METHOD_GET)->setHeaders(Inputs::headers());
+        $resp    = $this->client->call($request);
+        return new Validator($resp, true);
+
+    }
 }
