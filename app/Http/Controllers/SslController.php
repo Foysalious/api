e636c7f401569787d2a15d23d83176526117de1b
arnabrahman <arnabrahman@hotmail.com>
2020-08-20 10:13:15 +0600

    ➕ Redis ssl log
    Type(s): ADD

--- app/Http/Controllers/SslController.php
+++ app/Http/Controllers/SslController.php
@@ -1,39 +1,45 @@
 <?php namespace App\Http\Controllers;
 
 use App\Models\Payment;
+use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Validation\ValidationException;
 use Sheba\Payment\PaymentManager;
 use Sheba\TopUp\Vendor\Internal\SslClient;
+use Throwable;
 
 class SslController extends Controller
 {
     public function validatePayment(Request $request, PaymentManager $payment_manager)
     {
+        Redis::set('ssl_' . Carbon::now()->timestamp, json_encode($request->all()));
         $redirect_url = config('sheba.front_url');
         try {
             /** @var Payment $payment */
             $payment = Payment::where('gateway_transaction_id', $request->tran_id)->first();
             if ($payment) {
                 $redirect_url = $payment->payable->success_url . '?invoice_id=' . $payment->transaction_id;
-                $method       = $payment->paymentDetails->last()->method;
+                $method = $payment->paymentDetails->last()->method;
                 if ($payment->isValid() && !$payment->isComplete()) {
                     $payment_manager->setMethodName($method)->setPayment($payment)->complete();
                 }
             } else {
-                throw new \Exception('Payment not found to validate.');
+                throw new Exception('Payment not found to validate.');
             }
-        } catch (\Throwable $e) {
+        } catch (Throwable $e) {
             logError($e);
         }
         return redirect($redirect_url);
     }
 
-    public function validateTopUp(Request $request, SslClient $ssl) {
+    public function validateTopUp(Request $request, SslClient $ssl)
+    {
         try {
             $this->validate($request, [
                 'vr_guid' => 'required',
-                'guid'    => 'required',
+                'guid' => 'required',
             ]);
             $response = $ssl->getRecharge($request->guid, $request->vr_guid);
             return api_response($request, $response, 200, ['data' => $response]);
@@ -41,17 +47,18 @@ class SslController extends Controller
             $message = getValidationErrorMessage($e->validator->errors()->all());
             logError($e, $request, $message);
             return api_response($request, $message, 400, ['message' => $message]);
-        } catch (\Throwable $e) {
+        } catch (Throwable $e) {
             logError($e);
             return api_response($request, null, 500);
         }
     }
 
-    public function checkBalance(Request $request, SslClient $ssl) {
+    public function checkBalance(Request $request, SslClient $ssl)
+    {
         try {
             $response = $ssl->getBalance();
             return api_response($request, $response, 200, ['data' => $response]);
-        } catch (\Throwable $e) {
+        } catch (Throwable $e) {
             logError($e);
             return api_response($request, null, 500);
         }
