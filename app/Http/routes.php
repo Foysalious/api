35f511e1dbf899b3c4e8bb5811deee9300ada11c
Pasha <pasha.startern@gmail.com>
2020-08-23 18:48:57 +0600

    AWS EC2 INSTANCE ID
    Type(s): ADD

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,11 @@
 <?php
 
 Route::get('/', function () {
-    return ['code' => 200, 'message' => "Success. This project will hold the api's"];
+    $instance_id = 'demo_instance_id';
+    try {
+        $instance_id = file_get_contents("http://instance-data/latest/meta-data/instance-id");
+    } catch (Exception $e) {};
+    return ['code' => 200, 'message' => "Success. This project will hold the api's. Instance ID: " . $instance_id];
 });
 $api = app('Dingo\Api\Routing\Router');
 
