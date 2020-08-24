2bb73bb61de368f30d16053c67cef389b27b7761
tonmoy <shtonmoy007@gmail.com>
2020-08-24 14:08:23 +0600

    ⬆️  NAGAD HTTP CLIENT
    Type(s): UPDATE

--- app/Sheba/Payment/Methods/Nagad/NagadHttpClient.php
+++ app/Sheba/Payment/Methods/Nagad/NagadHttpClient.php
@@ -8,18 +8,21 @@ use Sheba\TPProxy\TPRequest;
 
 class NagadHttpClient
 {
-    public function call(TPRequest $request){
-        $url = curl_init($request->getUrl());
+    public function call(TPRequest $request)
+    {
+        $url    = curl_init($request->getUrl());
+        $inputs = $request->getInput();
+        if (is_array($inputs)) $inputs = json_encode($inputs);
         curl_setopt($url, CURLOPT_HTTPHEADER, $request->getHeaders());
         curl_setopt($url, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($url, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($url, CURLOPT_POSTFIELDS, $request->getInput());
+        curl_setopt($url, CURLOPT_POSTFIELDS, $inputs);
         curl_setopt($url, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($url, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($url, CURLOPT_SSL_VERIFYPEER, 0);
 
-        $resultdata = curl_exec($url);
-        $ResultArray = json_decode($resultdata, true);
+        $resultData  = curl_exec($url);
+        $ResultArray = json_decode($resultData, true);
         curl_close($url);
         return $ResultArray;
     }
