482968da9421091fa8993ee916e7c11cd84daf68
arnabrahman <arnabrahman@hotmail.com>
2020-09-08 16:42:44 +0600

    🚑 Sp log on order
    Type(s): HOTFIX

--- app/Sheba/OrderPlace/OrderPlace.php
+++ app/Sheba/OrderPlace/OrderPlace.php
@@ -386,7 +386,7 @@ class OrderPlace
                 $this->jobDeliveryChargeCalculator->setJob($job)->setPartnerOrder($partner_order)->getCalculatedJob();
                 if ($this->action->canSendPartnerOrderRequest())
                     dispatch(new InitiateAutoSpAssign($partner_order, $this->customer, $this->partnersFromList->pluck('id')->toArray()));
-                if (!$this->action->canAssignPartner())
+                if (!$partner_order->partner_id && $this->selectedPartner && $this->action->isLateNightOrder())
                     $this->jobUpdateLogCreator->setJob($job)->setMessage($this->getMessageForPreferredSp())
                         ->setUserAgentInformation($this->userAgentInformation)->setCreatedBy($this->customer)->create();
             });
