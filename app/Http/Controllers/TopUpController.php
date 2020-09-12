7e3d8d264c1a481f0b49acc0540b022e362f8f57
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-09-12 17:32:42 +0600

    ⏪ 🔥 Queue to redis
    Type(s): REVERT
    
    Why:
    To Test TopUp

--- app/Http/Controllers/TopUpController.php
+++ app/Http/Controllers/TopUpController.php
@@ -109,7 +109,6 @@ class TopUpController extends Controller
                 return api_response($request, null, 500);
             }
         } catch (Throwable $e) {
-            dd($e);
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
         }
