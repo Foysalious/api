679752f5ab7aa565bddf15a0dc6fc9196497c19c
tonmoy <shtonmoy007@gmail.com>
2020-09-16 14:24:55 +0600

    ⬆️  NAGAD CONFIG
    Type(s): UPDATE

--- config/nagad.php
+++ config/nagad.php
@@ -11,8 +11,8 @@ return [
         'affiliate' => [
             'base_url'         => env('NAGAD_BASE_URL', 'http://sandbox.mynagad.com:10080'),
             'merchant_id'      => env('NAGAD_MERCHANT_ID', '683002007104225'),
-            'private_key_path' => resource_path(env('NAGAD_MERCHANT_PRIVATE_KEY', 'assets/nagad/merchantPrivate.key')),
-            'public_key_path'  => resource_path(env('NAGAD_PUBLIC_KEY_PATH', 'assets/nagad/pgPublic.key')),
+            'private_key_path' => resource_path(env('NAGAD_MERCHANT_PRIVATE_KEY', 'assets/nagad/Bondhu.merchantPrivate.key')),
+            'public_key_path'  => resource_path(env('NAGAD_PUBLIC_KEY_PATH', 'assets/nagad/Bondhu.pgPublic.key')),
             'context_path'     => 'remote-payment-gateway-1.0',
         ]
     ]
