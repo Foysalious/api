c3687f4765d955030d8442004a7f15c95276eb84
tonmoy <shtonmoy007@gmail.com>
2020-09-16 14:47:03 +0600

    ⬆️  UPDATE CLIENT
    Type(s): UPDATE

--- app/Sheba/Payment/Methods/Nagad/NagadClient.php
+++ app/Sheba/Payment/Methods/Nagad/NagadClient.php
@@ -12,10 +12,6 @@ class NagadClient
 {
     private $client;
     private $baseUrl;
-    private $merchantId;
-    private $publicKey;
-    private $privateKey;
-    private $contextPath;
     /**
      * @var NagadStore
      */
@@ -23,12 +19,7 @@ class NagadClient
 
     public function __construct(TPProxyClient $client)
     {
-        $this->client      = $client;
-        $this->baseUrl     = config('nagad.base_url');
-        $this->merchantId  = config('nagad.merchant_id');
-        $this->publicKey   = file_get_contents(config('nagad.public_key_path'));
-        $this->privateKey  = file_get_contents(config('nagad.private_key_path'));
-        $this->contextPath = config('nagad.context_path');
+
 
     }
 
