7a0a289e67452f2b99f6c9b7b412f5a6ab13e211
Saiful <saiful.sheba@gmail.com>
2020-09-17 14:28:32 +0600

    🐛  Quick Perchase Address Update
    Type(s): FIX
    
    Issue(s):
    - JIRA: B2BBE-385

--- app/Sheba/Business/MemberManager.php
+++ app/Sheba/Business/MemberManager.php
@@ -9,6 +9,10 @@ use Sheba\Location\Geo;
 
 class MemberManager
 {
+    /**
+     * @param Member $member
+     * @return Customer
+     */
     public function createCustomerFromMember(Member $member)
     {
         $customer = new Customer();
@@ -18,17 +22,34 @@ class MemberManager
         return $customer;
     }
 
-    public function createAddress(Member $member, $delivery_address, Geo $geo, Business $business)
+
+    /**
+     * @param Member $member
+     * @param Business $business
+     * @param $delivery_address
+     * @param Geo $geo
+     * @return CustomerDeliveryAddress
+     */
+    public function createAddress(Member $member, Business $business, $delivery_address, Geo $geo)
     {
-        $location = HyperLocal::insidePolygon($geo->getLat(), $geo->getLng())->with('location')->first()->location;
+        $hyper_location = HyperLocal::insidePolygon($geo->getLat(), $geo->getLng())->with('location')->first();
+        $geo_informations = json_encode(['lat' => $geo->getLat(), 'lng' => $geo->getLng()]);
+        if (!$hyper_location) {
+            $business_geo_info = json_decode($business->geo_informations);
+            $hyper_location = HyperLocal::insidePolygon($business_geo_info->lat, $business_geo_info->lng)->with('location')->first();
+            $geo_informations = json_encode(['lat' => $business_geo_info->lat, 'lng' => $business_geo_info->lng]);
+        }
+        $location = $hyper_location->location;
+
         $address = new CustomerDeliveryAddress();
         $address->address = $delivery_address;
         $address->name = $business->name;
-        $address->geo_informations = json_encode(['lat' => $geo->getLat(), 'lng' => $geo->getLng()]);
-        $address->location_id = $location ? $location->id : 4;
+        $address->geo_informations = $geo_informations;
+        $address->location_id = $location->id;
         $address->customer_id = $member->profile->customer->id;
         $address->mobile = $member->profile->mobile;
         $address->save();
+
         return $address;
     }
 }
