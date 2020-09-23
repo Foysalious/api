5eb4a580ee995ebdf0d5c5c73e081b9f4191b9af
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-09-23 18:01:40 +0600

    ⬆️ Is Rent A Car Check
    Type(s): UPDATE

--- app/Sheba/ServiceRequest/ServiceRequest.php
+++ app/Sheba/ServiceRequest/ServiceRequest.php
@@ -9,6 +9,7 @@ use App\Exceptions\Service\OptionIsNotAvailableException;
 use App\Exceptions\ServiceRequest\MultipleCategoryServiceRequestException;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Validation\ValidationException;
+use Sheba\Dal\Category\Category;
 use Sheba\Location\Geo;
 use Sheba\LocationService\CorruptedPriceStructureException;
 use Sheba\Map\MapClientNoResultException;
@@ -69,7 +70,7 @@ class ServiceRequest
             $serviceRequestObject->build();
             array_push($final, $serviceRequestObject);
         }
-        $this->checkForServiceValidation($final);
+        if (isset($this->services[0]['category_id']) && !Category::find($this->services[0]['category_id'])->isRentACar()) $this->checkForServiceValidation($final);
         return $final;
     }
 
