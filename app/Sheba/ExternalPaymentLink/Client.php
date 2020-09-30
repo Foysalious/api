8a7463de29fffab1645410ced145a4132b1b422f
Arafat <yeamin@sheba.xyz>
2020-09-30 13:57:15 +0600

    ➕ Client create api added for external payment link
    Type(s): ADD
    
    Issue(s):
    - JIRA: MSMEBE-1198

---
+++ app/Sheba/ExternalPaymentLink/Client.php
@@ -0,0 +1,92 @@
+<?php namespace Sheba\ExternalPaymentLink;
+
+use Sheba\Dal\PaymentClientAuthentication\Contract as PaymentClientAuthenticationRepo;
+use Sheba\ModificationFields;
+
+class Client
+{
+    use ModificationFields;
+
+    private $name, $details, $whitelisted_ips, $partner_id;
+
+    private $client_id;
+
+    private $client_secret;
+
+    /**
+     * @var PaymentClientAuthenticationRepo
+     */
+    private $repository;
+
+    const CLIENT_ID_LENGTH = 9;
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setPartnerId($partner_id)
+    {
+        $this->partner_id = $partner_id;
+        return $this;
+    }
+
+    public function setDetails($details)
+    {
+        $this->details = $details;
+        return $this;
+    }
+
+    public function setWhitelistedIp($whitelisted_ip)
+    {
+        $this->whitelisted_ips = json_encode($whitelisted_ip);
+        return $this;
+    }
+
+    public function setClientId()
+    {
+        $this->client_id = $this->generateID();
+        return $this;
+    }
+
+    public function setClientSecret()
+    {
+        $this->client_secret = $this->generateSecret();
+        return $this;
+    }
+
+    public function setRepository($repository)
+    {
+        $this->repository = $repository;
+        return $this;
+    }
+
+    private function generateID()
+    {
+        return str_pad(mt_rand(1,999999999),self::CLIENT_ID_LENGTH,'0',STR_PAD_LEFT);
+    }
+
+    private function generateSecret()
+    {
+        return str_random(120);
+    }
+
+    public function store()
+    {
+        $this->repository->create($this->withBothModificationFields($this->processData()));
+    }
+
+    private function processData()
+    {
+        return [
+            "client_id" => $this->client_id,
+            "name"      => $this->name,
+            "client_secret" => $this->client_secret,
+            "details" => $this->details,
+            "whitelisted_ips" => $this->whitelisted_ips,
+            "partner_id" => $this->partner_id,
+            "status" => "published"
+        ];
+    }
+}
