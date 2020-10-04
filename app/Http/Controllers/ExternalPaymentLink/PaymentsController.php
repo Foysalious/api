b229b69a9937be00ad4c353bc4aad4121285e583
tonmoy <shtonmoy007@gmail.com>
2020-10-04 18:51:01 +0600

    ⬆️  INITIATE
    Type(s): UPDATE

---
+++ app/Http/Controllers/ExternalPaymentLink/PaymentsController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Http\Controllers\ExternalPaymentLink;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PaymentsController extends Controller
+{
+    public function create(Request $request){
+
+    }
+}
