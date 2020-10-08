94370e01267396437033462f4dcbb9ae90e9000b
tonmoy <shtonmoy007@gmail.com>
2020-10-08 11:02:51 +0600

    ⬆️  PAYMENT LINK INITIATE METHOD
    Type(s): UPDATE

--- app/Sheba/ExternalPaymentLink/Exceptions/InvalidTransactionIDException.php
+++ app/Sheba/ExternalPaymentLink/Exceptions/InvalidTransactionIDException.php
@@ -8,6 +8,6 @@ use Throwable;
 
 class InvalidTransactionIDException extends ExternalPaymentLinkException
 {
-    public function __construct($message = "Invalid Transaction ID,Please provide a unique transaction ID", $code = 504, Throwable $previous = null) { parent::__construct($message, $code, $previous); }
+    public function __construct($message = "Invalid Transaction ID,Please provide a unique transaction ID", $code = 505, Throwable $previous = null) { parent::__construct($message, $code, $previous); }
 
 }
