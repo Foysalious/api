8b78cae80a8ee67b86ca6a42c721f42bf0c66780
tonmoy <shtonmoy007@gmail.com>
2020-10-11 17:21:21 +0600

    🐛  EX_PAYMENT_LINK PROFILE CREATE
    Type(s): FIX
    
    Issue(s):
    - JIRA: MSMEBE-1226
    - SENTRY: 1950409103

--- app/Sheba/ExternalPaymentLink/ExternalPayments.php
+++ app/Sheba/ExternalPaymentLink/ExternalPayments.php
@@ -214,7 +214,7 @@ class ExternalPayments
 
     private function createPosCustomer()
     {
-        $profile = Profile::where('mobile', $this->data['customer_mobile']);
+        $profile = Profile::where('mobile', $this->data['customer_mobile'])->first();
         if (!$profile) {
             $profile = $this->profileRepo->store($this->withCreateModificationField(['mobile' => $this->data['customer_mobile'], 'name' => isset($this->data['customer_name']) ? $this->data['customer_name'] : '']));
         }
