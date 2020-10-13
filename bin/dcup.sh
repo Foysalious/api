c6502b6e7232ad339c61cbbe8b084456bf6c1cdf
Pasha <pasha.startern@gmail.com>
2020-10-13 14:31:16 +0600

    ➕ LOCAL DEVELOPMENT FIX
    Type(s): ADD

--- bin/dcup.sh
+++ bin/dcup.sh
@@ -4,16 +4,20 @@ compose_folder="./docker/composes"
 
 compose="docker-compose --env-file=./.env -f $compose_folder/docker-compose.yml"
 
-if [[ $1 = "prod" ]]; then
-    extra="-f $compose_folder/docker-compose.prod.yml"
+if [[ $1 = "local" ]]; then
+  extra="-f $compose_folder/docker-compose.local.yml"
+elif [[ $1 = "prod" ]]; then
+  extra="-f $compose_folder/docker-compose.prod.yml"
 else
-    extra="-f $compose_folder/docker-compose.dev.yml"
+  extra="-f $compose_folder/docker-compose.dev.yml"
 fi
 
 compose="$compose $extra up"
 
+# shellcheck disable=SC2206
 all_args=($@)
 len=${#all_args[@]}
+# shellcheck disable=SC2124
 other_args=${all_args[@]:1:${len}-1}
 compose="$compose $other_args"
 
--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,32 +1,40 @@
 #!/bin/sh
 
 get_git_branch() {
-    echo "$(git symbolic-ref --short -q HEAD 2>/dev/null)"
+  # shellcheck disable=SC2005
+  echo "$(git symbolic-ref --short -q HEAD 2>/dev/null)"
 }
 
 pull_from_git() {
-    git fetch origin
-    reset="sudo git reset --hard origin/"
-    reset_branch="$reset$1"
-    eval ${reset_branch}
+  git fetch origin
+  reset="sudo git reset --hard origin/"
+  reset_branch="$reset$1"
+  eval "${reset_branch}"
 
-    ./bin/dcup.sh dev -d
+  ./bin/dcup.sh dev -d
 }
 
 pull_from_docker_registry() {
-    . ./bin/parse_env.sh
-    docker pull registry.sheba.xyz/${CONTAINER_NAME}
+  . ./bin/parse_env.sh
+  docker pull registry.sheba.xyz/"${CONTAINER_NAME}"
 
-    ./bin/dcup.sh prod -d
+  ./bin/dcup.sh prod -d
+}
+
+# USE ON LOCAL DEVELOPMENT
+run_local() {
+  ./bin/dcup.sh local -d
 }
 
 branch=$1
-if [ -z ${branch} ]; then
-    branch="$(get_git_branch)"
+if [ -z "${branch}" ]; then
+  branch="$(get_git_branch)"
 fi
 
-if [ ${branch} = "development" ]; then
-    pull_from_git ${branch}
-elif [ ${branch} = "master" ]; then
-    pull_from_docker_registry
+if [ "${branch}" = "development" ]; then
+  pull_from_git "${branch}"
+elif [ "${branch}" = "master" ]; then
+  pull_from_docker_registry
+elif [ "${branch}" = "local" ]; then
+  run_local
 fi
