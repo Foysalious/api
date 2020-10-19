f9525f283454ecaa5e823dd7ed9dbdcb8f7aeb34
Pasha <pasha.startern@gmail.com>
2020-10-20 00:30:10 +0600

    TYPO
    Type(s): UPDATE

--- bin/parse_env.sh
+++ bin/parse_env.sh
@@ -2,10 +2,12 @@
 
 # CHECK ANY ENV FILE LIKE master.env OR development.env FILE PRESENT
 # IF EXIST, REMOVE
-count=`ls -1 *.env 2>/dev/null | wc -l`
-if [ $count != 0 ]
-then
+# shellcheck disable=SC2012
+# shellcheck disable=SC2035
+count=$(ls -1 *.env 2>/dev/null | wc -l)
+if [ "$count" != 0 ]; then
   sudo mv *.env .env
 fi
 
+# shellcheck disable=SC2046
 export $(egrep -v '^#' .env | xargs)
--- bin/remove_build.sh
+++ bin/remove_build.sh
@@ -2,5 +2,5 @@
 
 . ./bin/parse_env.sh
 
-docker rmi registry.sheba.xyz/${CONTAINER_NAME}:latest
-docker rmi ${CONTAINER_NAME}:latest
+docker rmi registry.sheba.xyz/"${CONTAINER_NAME}":latest
+docker rmi "${CONTAINER_NAME}":latest
