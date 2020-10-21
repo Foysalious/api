bcd33ca4f9d04ec5266ca5f925159b8dcdcf30b8
Pasha <pasha.startern@gmail.com>
2020-10-22 00:55:51 +0600

    OPCACHE WITH SENTRY CLI
    Type(s): FIX

--- bin/build.sh
+++ bin/build.sh
@@ -2,6 +2,6 @@
 
 . ./bin/parse_env.sh
 
-docker build -t "${CONTAINER_NAME}" -f ./docker/Dockerfile .
+docker build -t "${CONTAINER_NAME}" -f ./docker/Dockerfileproduction . --build-arg APP_ENV="${APP_ENV}"
 docker tag "${CONTAINER_NAME}":latest registry.sheba.xyz/"${CONTAINER_NAME}":latest
 docker push registry.sheba.xyz/"${CONTAINER_NAME}":latest
