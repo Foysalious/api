04fa7a870a702f3014805f2283537a2c9d60b48a
Saiful <saiful.sheba@gmail.com>
2020-10-28 08:37:05 +0600

    ➕ Leave prorate Store Done
    Type(s): ADD
    
    Issue(s):
    - JIRA: B2BBE-441

--- app/Http/Controllers/B2b/ProrateController.php
+++ app/Http/Controllers/B2b/ProrateController.php
@@ -18,6 +18,11 @@ class ProrateController extends Controller
     /** @var Creator $creator */
     private $creator;
 
+    /**
+     * ProrateController constructor.
+     * @param ProrateRequester $prorate_requester
+     * @param Creator $creator
+     */
     public function __construct(ProrateRequester $prorate_requester, Creator $creator)
     {
         $this->requester = $prorate_requester;
@@ -27,17 +32,21 @@ class ProrateController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'business_member_ids' => 'required',
+            'business_member_ids' => 'required|array',
             'leave_type_id' => 'required',
-            'total_days' => 'required',
-            'note' => 'string',
+            'total_days' => 'required'
         ]);
-        /**@var Business $business */
-        $business = $request->business;
+
         /** @var Member $manager_member */
         $manager_member = $request->manager_member;
-        dd($request->all());
-        $this->requester->setNote();
+        $this->setModifier($manager_member);
+
+        $this->requester->setBusinessMemberIds($request->business_member_ids)
+            ->setTotalDays($request->total_days)
+            ->setLeaveTypeId($request->leave_type_id)
+            ->setNote($request->note);
+
+        $this->creator->setRequester($this->requester)->create();
         return api_response($request, null, 200);
     }
 
--- app/Sheba/Business/Prorate/Creator.php
+++ app/Sheba/Business/Prorate/Creator.php
@@ -1,13 +1,26 @@
 <?php namespace Sheba\Business\Prorate;
 
-use Sheba\Dal\BusinessMemberLeaveType\Model as BusinessMemberLeaveType;
-use Sheba\Business\Prorate\Requester;
+use Sheba\Dal\BusinessMemberLeaveType\Contract as BusinessMemberLeaveTypeInterface;
+use Sheba\ModificationFields;
 
 class Creator
 {
+    use ModificationFields;
+
+    /** @var BusinessMemberLeaveTypeInterface $businessMemberLeaveTypeRepo */
+    private $businessMemberLeaveTypeRepo;
     /** @var Requester $requester */
     private $requester;
+    private $data;
 
+    /**
+     * Creator constructor.
+     * @param BusinessMemberLeaveTypeInterface $business_member_leave_type_repo
+     */
+    public function __construct(BusinessMemberLeaveTypeInterface $business_member_leave_type_repo)
+    {
+        $this->businessMemberLeaveTypeRepo = $business_member_leave_type_repo;
+    }
 
     /**
      * @param Requester $requester
@@ -17,5 +30,18 @@ class Creator
     {
         $this->requester = $requester;
         return $this;
+    }
+
+    public function create()
+    {
+        foreach ($this->requester->getBusinessMemberIds() as $business_member_id) {
+            $this->data[] = [
+                'business_member_id' => $business_member_id,
+                'leave_type_id' => $this->requester->getLeaveTypeId(),
+                'total_days' => $this->requester->getTotalDays(),
+                'note' => $this->requester->getNote()
+            ];
+        }
+        $this->businessMemberLeaveTypeRepo->insert($this->data);
     }
 }
