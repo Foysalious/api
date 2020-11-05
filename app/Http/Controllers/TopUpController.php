cc4691c8675f68dec1eb26862505f3e55d17aece
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-11-05 16:03:23 +0600

    🔥 Remove dd
    Type(s): REMOVE

--- app/Http/Controllers/TopUpController.php
+++ app/Http/Controllers/TopUpController.php
@@ -135,7 +135,6 @@ class TopUpController extends Controller
             $profileid = $aff->profile_id;
             $profile = $profileRepository->where('id', $profileid)->first();
             $userAgentInformation->setRequest($request);
-            dd($userAgentInformation->getIp());
 
             if(!Hash::check($request->password, $profile->password)){
                 $data = [
