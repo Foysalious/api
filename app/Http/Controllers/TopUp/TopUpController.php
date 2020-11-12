892b4e43c4e3243cda39e71974bf505075399a8f
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2020-11-12 19:47:00 +0600

    [ADD] bulk top up fail email

--- app/Http/Controllers/TopUp/TopUpController.php
+++ app/Http/Controllers/TopUp/TopUpController.php
@@ -226,7 +226,6 @@ class TopUpController extends Controller
             $message = getValidationErrorMessage($e->validator->errors()->all());
             return api_response($request, $message, 400, ['message' => $message]);
         } catch (Throwable $e) {
-            dd($e->getMessage());
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
         }
