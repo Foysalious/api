13f97950b7f6bcabd7e78a5c4bffcd7114e51140
Jahid <md.jahidul@shebaplatform.xyz>
2020-11-22 11:24:53 +0600

    ⬆️ API UPDATE
    Type(s): UPDATE
    
    Why:
    Homepage api success message added
    Gigatech api base url added
    
    Issue(s):
    - JIRA:

--- app/Sheba/NeoBanking/Banks/PrimeBank/PrimeBank.php
+++ app/Sheba/NeoBanking/Banks/PrimeBank/PrimeBank.php
@@ -150,17 +150,17 @@ class PrimeBank extends Bank
         $data = [];
         if($status->cpv === 'cpv_pending') {
             $data['message'] = config('neo_banking.cpv_pending_message');
-            $data['type'] = 'Warning';
+            $data['type'] = config('neo_banking.message_type.cpv_pending');
         } else if($status->cpv === 'cpv_unverified') {
             $data['message'] = config('neo_banking.cpv_unverified_message');
-            $data['type'] = 'Warning';
+            $data['type'] = config('neo_banking.message_type.cpv_unverified');
         } else if($status->cpv === 'cpv_verified') {
             if($status->sign === 'signed') {
-                $data['message'] = config('neo_banking.cpv_verified_message');
-                $data['type'] = 'Success';
+                $data['message'] = config('neo_banking.signed_verified_message');
+                $data['type'] = config('neo_banking.message_type.cpv_verified');;
             } else {
                 $data['message'] = config('neo_banking.unsigned_message');
-                $data['type'] = 'Primary';
+                $data['type'] = config('neo_banking.message_type.cpv_unsigned');
             }
         }
         return $data;
