2698314593c3c56bb1a9c1e15ed4aa72b0f2301b
Shafiqul Islam <shafiq.startern@gmail.com>
2020-11-26 12:52:25 +0600

    🚑  Add resource app ios  app
    Type(s): HOTFIX

--- app/Sheba/AppVersion/Apps.php
+++ app/Sheba/AppVersion/Apps.php
@@ -10,6 +10,7 @@ class Apps
     const CUSTOMER_APP_IOS = 'customer_app_ios';
     const MANAGER_APP_ANDROID = 'manager_app_android';
     const RESOURCE_APP_ANDROID = 'resource_app_android';
+    const RESOURCE_APP_IOS = 'resource_app_ios';
     const BONDHU_APP_ANDROID = 'bondhu_app_android';
     const RIDER_APP_ANDROID = 'rider_app_android';
     const EMPLOYEE_APP_ANDROID = 'employee_app_android';
