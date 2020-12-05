4aff32975adcff029aea67b907aef32101b92426
Md. Rokonuzzaman <rzrokon@gmail.com>
2020-12-05 17:14:15 +0600

    Bondhu topup history fix

--- app/Http/Controllers/AffiliateController.php
+++ app/Http/Controllers/AffiliateController.php
@@ -797,7 +797,6 @@ GROUP BY affiliate_transactions.affiliate_id', [$affiliate->id, $agent_id]));
 
             return response()->json(['code' => 200, 'data' => $topup_data, 'total_topups' => $total_topups, 'offset' => $offset]);
         } catch (Throwable $e) {
-            dd($e);
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
         }
