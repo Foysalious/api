ee9968659555853e4df50ad5e61be4c4a3739d6d
Irteza Asad <irtezaasad.sheba@gmail.com>
2020-12-06 18:40:25 +0600

    ⬆️ can collect
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MPBE-1163

--- app/Sheba/Resource/Jobs/JobList.php
+++ app/Sheba/Resource/Jobs/JobList.php
@@ -229,7 +229,9 @@ class JobList
 
             $formatted_job->put('due', (double) $job->partnerOrder->due);
             $formatted_job->put('has_pending_due', $this->hasDueJob($job) ? 1 : 0);
-
+            $formatted_job->put('can_process', 0);
+            $formatted_job->put('can_serve', 0);
+            $formatted_job->put('can_collect', 0);
             $latest_pending_due_of_partner = $this->latestDueJob($job);
             $formatted_job->put('pending_due', $latest_pending_due_of_partner
                 ? [
@@ -241,11 +243,6 @@ class JobList
 
             $formatted_job->put('is_b2b', $this->isB2BJob($job) ? 1 : 0);
             if ($this->firstJobFromList && $this->shouldICheckActions($this->firstJobFromList, $job)) $formatted_job = $this->actionCalculator->calculateActionsForThisJob($formatted_job, $job);
-            else {
-                $formatted_job->put('can_process', 0);
-                $formatted_job->put('can_serve', 0);
-                $formatted_job->put('can_collect', 0);
-            }
             $formatted_jobs->push($formatted_job);
         }
         return $formatted_jobs;
@@ -338,8 +335,8 @@ class JobList
         $this->setFirstJobFromList($jobs->first());
         $next_jobs_count = $jobs->where('schedule_date', $this->firstJobFromList->schedule_date)
             ->where('preferred_time', $this->firstJobFromList->preferred_time)->count();
-        $preferred_time = Carbon::parse($this->firstJobFromList->preferred_time_start)->format('h:i A') . ' - ' . Carbon::parse($this->firstJobFromList->preferred_time_end)->format('h:i A');
         if ($next_jobs_count == 0) return null;
+        $preferred_time = Carbon::parse($this->firstJobFromList->preferred_time_start)->format('h:i A') . ' - ' . Carbon::parse($this->firstJobFromList->preferred_time_end)->format('h:i A');
         return ['preferred_time' => $preferred_time, 'jobs_count' => $next_jobs_count];
     }
 }
