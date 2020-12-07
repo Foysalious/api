63c5fb9cf4528d9ea04ec5f06632a7f29459e0b2
Saiful <miajee@sheba.xyz>
2020-12-07 21:03:55 +0600

    [ADD][B2BBE-569] Daily Attendance Report

--- app/Sheba/Business/Attendance/Daily/DailyExcel.php
+++ app/Sheba/Business/Attendance/Daily/DailyExcel.php
@@ -7,6 +7,41 @@ class DailyExcel
 {
     private $dailyData;
     private $data = [];
+    private $date;
+    private $employeeId;
+    private $employeeName;
+    private $department;
+    private $status;
+    private $checkInTime;
+    private $checkInStatus;
+    private $checkInLocation;
+    private $checkInAddress;
+    private $checkOutTime;
+    private $checkOutStatus;
+    private $checkOutLocation;
+    private $checkOutAddress;
+    private $totalHours;
+    private $leftEarlyNote;
+
+    public function __construct()
+    {
+        $this->date = null;
+        $this->employeeId = null;
+        $this->employeeName = null;
+        $this->department = null;
+        $this->status = null;
+
+        $this->checkInTime = '-';
+        $this->checkInStatus = '-';
+        $this->checkInLocation = '-';
+        $this->checkInAddress = '-';
+        $this->checkOutTime = '-';
+        $this->checkOutStatus = '-';
+        $this->checkOutLocation = '-';
+        $this->checkOutAddress = '-';
+        $this->totalHours = '-';
+        $this->leftEarlyNote = null;
+    }
 
     public function setData(array $daily_data)
     {
@@ -34,21 +69,77 @@ class DailyExcel
     private function makeData()
     {
         foreach ($this->dailyData as $attendance) {
+            if (!is_null($attendance['check_in']) && !$attendance['is_absent']) {
+                if ($attendance['is_half_day_leave']) {
+                    $this->status = "On leave: half day";
+                } else {
+                    $this->status = 'Present';
+                }
+
+                $this->checkInTime = $attendance['check_in']['checkin_time'];
+                if ($attendance['check_in']['status'] == 'late') {
+                    $this->checkInStatus = "Late";
+                }
+                if ($attendance['check_in']['status'] == 'on_time') {
+                    $this->checkInStatus = "On time";
+                }
+                if ($attendance['check_in']['is_remote']) {
+                    $this->checkInLocation = "Remote";
+                } else {
+                    $this->checkInLocation = "Office IP";
+                }
+
+                $this->checkInAddress = $attendance['check_in']['address'];
+                if (!is_null($attendance['check_out'])) {
+                    $this->checkOutTime = $attendance['check_out']['checkout_time'];
+
+                    if ($attendance['check_out']['status'] == 'left_early') {
+                        $this->checkOutStatus = 'Left early';
+                    }
+                    if ($attendance['check_out']['status'] == 'left_timely') {
+                        $this->checkOutStatus = 'Left timely';
+                    }
+                    if ($attendance['check_out']['is_remote']) {
+                        $this->checkOutLocation = "Remote";
+                    } else {
+                        $this->checkOutLocation = "Office IP";
+                    }
+                    $this->checkOutAddress = $attendance['check_out']['is_remote'];
+                }
+
+                $this->totalHours = $attendance['active_hours'];
+                $this->leftEarlyNote = $attendance['check_out']['note'];
+            }
+
+            if ($attendance['is_absent']) {
+                $this->status = "Absent";
+            }
+            if ($attendance['is_on_leave']) {
+                if (!$attendance['is_half_day_leave']) {
+                    $this->status = "On leave: full day";
+                } else {
+                    $this->status = "On leave: half day";
+                }
+            }
+
             array_push($this->data, [
-                'date' => "one",
-                'employee_id' => "one// Set auto size for sheetone// Set auto size for sheet",
-                'employee_name' => "one",
-                'department' => "one",
-                'status' => "one",
-                'check_in_time' => "one",
-                'check_in_status' => "one",
-                'check_in_location' => "one",
-                'check_in_address' => "one",
-                'check_out_time' => "one",
-                'check_out_status' => "one",
-                'check_out_location' => "one",
-                'check_out_address' => "one",
-                'total_hours' => "one",
+                'date' => $attendance['date'],
+                'employee_id' => $attendance['employee_id'],
+                'employee_name' => $attendance['member']['name'],
+                'department' => $attendance['department']['name'],
+
+                'status' => $this->status,
+                'check_in_time' => $this->checkInTime,
+                'check_in_status' => $this->checkInStatus,
+                'check_in_location' => $this->checkInLocation,
+                'check_in_address' => $this->checkInAddress,
+                'check_out_time' => $this->checkOutTime,
+                'check_out_status' => $this->checkOutStatus,
+                'check_out_location' => $this->checkOutLocation,
+                'check_out_address' => $this->checkOutAddress,
+
+                'total_hours' => $this->totalHours,
+                'left_early_note' => $this->leftEarlyNote,
             ]);
         }
     }
@@ -58,6 +149,6 @@ class DailyExcel
         return ['Date', 'Employee ID', 'Employee Name', 'Department',
             'Status', 'Check in time', 'Check in status', 'Check in location',
             'Check in address', 'Check out time', 'Check out status',
-            'Check out location', 'Check out address', 'Total Hours'];
+            'Check out location', 'Check out address', 'Total Hours', 'Left early note'];
     }
 }
