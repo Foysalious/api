84b632792fd718c30fe58a4378eb3c40762c0e51
shovan <shovan.sheba@gmail.com>
2020-12-07 18:10:32 +0600

    ➕ Store webstore banner settings
    Type(s): ADD
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-74?atlOrigin=eyJpIjoiNzFkZjZhNWUyOWViNGUzMjk0ODE3MTJhZGFkYjg5MjAiLCJwIjoiaiJ9

--- app/Http/Controllers/Partner/Webstore/WebstoreSettingsController.php
+++ app/Http/Controllers/Partner/Webstore/WebstoreSettingsController.php
@@ -1,20 +1,25 @@
 <?php namespace App\Http\Controllers\Partner\Webstore;
 
 use App\Models\Partner;
+use App\Sheba\Partner\Webstore\WebstoreBannerSettings;
 use App\Transformers\CustomSerializer;
 use App\Transformers\Partner\WebstoreSettingsTransformer;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\ValidationException;
 use League\Fractal\Manager;
 use League\Fractal\Resource\Item;
+use Sheba\ModificationFields;
 use Sheba\Partner\Webstore\WebstoreSettingsUpdateRequest;
 use Sheba\Subscription\Partner\Access\AccessManager;
 use Sheba\Subscription\Partner\Access\Exceptions\AccessRestrictedExceptionForPackage;
 
 class WebstoreSettingsController extends Controller
 {
+    use ModificationFields;
+
     public function index($partner, Request $request)
     {
         $partner = $request->partner;
@@ -59,6 +64,43 @@ class WebstoreSettingsController extends Controller
     {
         if (Partner::where('sub_domain', $sub_domain)->exists()) return true;
         return false;
+    }
+
+    /**
+     * @param Request $request
+     * @param $partner
+     * @param WebstoreBannerSettings $webstoreBannerSettings
+     * @return JsonResponse
+     */
+    public function storeBanner(Request $request, $partner, WebstoreBannerSettings $webstoreBannerSettings)
+    {
+        try {
+            $this->validate($request, [
+                'banner_id' => 'required',
+                'title' => 'string',
+                'description' => 'string',
+                'is_published' => 'sometimes|in:1,0',
+            ]);
+
+            $this->setModifier($request->manager_resource);
+            $data = [
+                'partner_id' => $request->partner->id,
+                'banner_id' => $request->banner_id,
+                'title' => $request->title ?: null,
+                'description' => $request->description ?: null,
+                'is_published' => $request->is_published ?: 0,
+            ];
+
+            $webstoreBannerSettings->setData($data)->store();
+            return api_response($request, null, 200, ['msg' => 'Banner Settings Created Successfully']);
+
+        } catch (ValidationException $e) {
+            $message = getValidationErrorMessage($e->validator->errors()->all());
+            return api_response($request, $message, 400, ['message' => $message]);
+        } catch (\Throwable $e) {
+            app('sentry')->captureException($e);
+            return api_response($request, null, 500);
+        }
     }
 
 }
