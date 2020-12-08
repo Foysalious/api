59ecc29b0656a2d14ed8daa91df92c0991250743
shovan <shovan.sheba@gmail.com>
2020-12-08 13:42:33 +0600

    🚧 Image gallery for pos service
    Type(s): WIP
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-81?atlOrigin=eyJpIjoiZjA2ODk1YjZlMDRiNDFmZjhmZTY5MjVkMjFhN2IzZmEiLCJwIjoiaiJ9

--- app/Http/Controllers/Pos/ServiceController.php
+++ app/Http/Controllers/Pos/ServiceController.php
@@ -162,6 +162,7 @@ class ServiceController extends Controller
             $partner_pos_service->master_category_id = $partner_pos_service_model->category->parent_id;
             $partner_pos_service->master_category_name = $partner_pos_service_model->category->parent->name;
             $partner_pos_service->sub_category_id = $partner_pos_service_model->category->id;
+          //  $partner_pos_service->image_gallery = $partner_pos_service_model->image_gallery;
 
             app()->make(ActionRewardDispatcher::class)->run('pos_inventory_create', $request->partner, $request->partner, $partner_pos_service);
             /**
