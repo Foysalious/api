12d4fb44011c98469d98f048ec86967be2eee026
Rashad Akhtar <rashad@sheba.xyz>
2020-12-09 18:16:22 +0600

    :heavy_plus_sign: Attendance Details Excel File Functionality
    Type(s): ADD
    
    Issue(s):
    - JIRA: B2BBE-574

---
+++ app/Sheba/Business/Attendance/Detail/DetailsExcel.php
@@ -0,0 +1,186 @@
+<?php namespace Sheba\Business\Attendance\Detail;
+
+use Carbon\Carbon;
+use Excel;
+
+class DetailsExcel
+{
+    private $breakdownData;
+    private $data = [];
+    private $date;
+    private $status;
+    private $checkInTime;
+    private $checkInStatus;
+    private $checkInLocation;
+    private $checkInAddress;
+    private $checkOutTime;
+    private $checkOutStatus;
+    private $checkOutLocation;
+    private $checkOutAddress;
+    private $totalHours;
+    private $leftEarlyNote;
+    private $businessMember;
+    private $department;
+
+    public function __construct()
+    {
+    }
+
+    public function setBreakDownData(array $detailed_data)
+    {
+        $this->breakdownData = $detailed_data;
+        return $this;
+    }
+
+    public function setBusinessMember($business_member)
+    {
+        $this->businessMember = $business_member;
+        return $this;
+    }
+
+    public function setDepartment($department)
+    {
+        $this->department = $department;
+        return $this;
+    }
+
+    public function download()
+    {
+        $this->makeData();
+        $employee_id = $this->businessMember->employee_id ? $this->businessMember->employee_id : $this->businessMember->id;
+        $department_name = $this->department;
+        $file_name = $employee_id . '_' . $department_name . '_' . 'Attendance';
+        Excel::create($file_name, function ($excel) {
+            $excel->sheet('data', function ($sheet) {
+                $sheet->fromArray($this->data, null, 'A1', false, false);
+                $sheet->prependRow($this->getHeaders());
+                $sheet->freezeFirstRow();
+                $sheet->cell('A1:L1', function ($cells) {
+                    $cells->setFontWeight('bold');
+                });
+                $sheet->setAutoSize(true);
+            });
+        })->export('xlsx');
+    }
+
+    private function makeData()
+    {
+        foreach ($this->breakdownData as $attendance) {
+            $this->date = null;
+            $this->status = null;
+
+            $this->checkInTime = '-';
+            $this->checkInStatus = '-';
+            $this->checkInLocation = '-';
+            $this->checkInAddress = '-';
+
+            $this->checkOutTime = '-';
+            $this->checkOutStatus = '-';
+            $this->checkOutLocation = '-';
+            $this->checkOutAddress = '-';
+
+            $this->totalHours = '-';
+            $this->leftEarlyNote = null;
+            if (!$attendance['weekend_or_holiday_tag']) {
+                if ($attendance['show_attendance'] == 1) {
+                    $this->date = $attendance['date'];
+                    $this->checkInOutLogics($attendance);
+                    $this->status = 'Present';
+                }
+                if ($attendance['show_attendance'] == 0) {
+                    if ($attendance['is_absent'] == 1) {
+                        $this->date = $attendance['date'];
+                        $this->status = 'Absent';
+                    }
+                }
+            }
+            if ($attendance['weekend_or_holiday_tag']) {
+                $this->date = $attendance['date'];
+                if ($attendance['show_attendance'] == 1) {
+                    $this->checkInOutLogics($attendance);
+                }
+                if ($attendance['weekend_or_holiday_tag'] === 'weekend') {
+                    $this->status = 'Weekend';
+                } else if ($attendance['weekend_or_holiday_tag'] === 'holiday') {
+                   $this->status = 'Holiday';
+                } else if ($attendance['weekend_or_holiday_tag'] === 'full_day') {
+                    $this->status = 'On leave: full day';
+                } else if ($attendance['weekend_or_holiday_tag'] === 'first_half' || $attendance['weekend_or_holiday_tag'] === 'second_half') {
+                    $this->status = "On leave: half day";
+                }
+            }
+            array_push($this->data, [
+                'date' => $this->date,
+                'status' => $this->status,
+
+                'check_in_time' => $this->checkInTime,
+                'check_in_status' => $this->checkInStatus,
+                'check_in_location' => $this->checkInLocation,
+                'check_in_address' => $this->checkInAddress,
+
+                'check_out_time' => $this->checkOutTime,
+                'check_out_status' => $this->checkOutStatus,
+                'check_out_location' => $this->checkOutLocation,
+                'check_out_address' => $this->checkOutAddress,
+
+                'total_hours' => $this->totalHours,
+                'left_early_note' => $this->leftEarlyNote,
+            ]);
+        }
+    }
+
+    private function getHeaders()
+    {
+        return ['Date', 'Status', 'Check in time', 'Check in status', 'Check in location',
+            'Check in address', 'Check out time', 'Check out status',
+            'Check out location', 'Check out address', 'Total Hours', 'Left early note'];
+    }
+
+    private function checkInOutLogics($attendance)
+    {
+        $attendance_check_in = $attendance['attendance']['check_in'];
+        $attendance_check_out = $attendance['attendance']['check_out'];
+
+        $this->checkInTime = $attendance_check_in['time'];
+        if ($attendance_check_in['status'] === 'late') {
+            $this->checkInStatus = 'Late';
+        }
+        if ($attendance_check_in['status'] === 'on_time') {
+            $this->checkInStatus = 'On time';
+        }
+        if ($attendance_check_in['is_remote']) {
+            $this->checkInLocation = "Remote";
+        } else {
+            $this->checkInLocation = "Office IP";
+        }
+        if($attendance_check_in['address']) {
+            $this->checkInAddress = $attendance_check_in['address'];
+        }
+
+        if (!is_null($attendance_check_out)) {
+            $this->checkOutTime = $attendance_check_out['time'];
+
+            if ($attendance_check_out['status'] === 'left_early') {
+                $this->checkOutStatus = 'Left early';
+            }
+
+            if ($attendance_check_out['status'] === 'left_timely') {
+                $this->checkOutStatus = 'Left timely';
+            }
+
+            if ($attendance_check_out['is_remote']) {
+                $this->checkOutLocation = 'Remote';
+            } else {
+                $this->checkOutLocation = 'Office IP';
+            }
+            if ($attendance_check_out['address']) {
+                $this->checkOutAddress = $attendance_check_out['address'];
+            }
+        }
+
+        if ($attendance['attendance']['active_hours']) {
+            $this->totalHours = $attendance['attendance']['active_hours'];
+        }
+        $this->leftEarlyNote = $attendance['attendance']['note'];
+    }
+}
