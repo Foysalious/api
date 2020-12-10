d28f4e183945089dca1b7f3b2a863bb9591317b9
shovan <shovan.sheba@gmail.com>
2020-12-10 19:06:35 +0600

    test

--- app/Sheba/Pos/Product/Creator.php
+++ app/Sheba/Pos/Product/Creator.php
@@ -39,6 +39,7 @@ class Creator
         $image_gallery = $this->data['image_gallery'];
         $this->data = array_except($this->data, ['remember_token', 'discount_amount', 'end_date', 'manager_resource', 'partner', 'category_id', 'image_gallery']);
         if (isset($image_gallery)) $image_gallery = $this->saveImageGallery($image_gallery);
+        dd($image_gallery);
         $partner_pos_service =  $this->serviceRepo->save($this->data + (new RequestIdentification())->get());
         $this->storeImageGallery($partner_pos_service,$image_gallery);
         return $partner_pos_service;
