5a51083e3841501b07c83986ec6c85502d7ce6ee
shovan <shovan.sheba@gmail.com>
2020-12-13 20:19:21 +0600

    ➕ Banner list
    Type(s): ADD
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-86?atlOrigin=eyJpIjoiMzNkZjhlYzVjMjhkNGQ5ZDhhMDg5ODI3MTQ5OGM1ZWUiLCJwIjoiaiJ9
    
    # Conflicts:
    #	app/Http/Route/Prefix/V2/Partner/ID/Auth/IndexRoute.php
    #	app/Sheba/Partner/Webstore/WebstoreBannerSettings.php

--- app/Http/Controllers/Pos/ServiceController.php
+++ app/Http/Controllers/Pos/ServiceController.php
@@ -126,12 +126,10 @@ class ServiceController extends Controller
             $secondaries_categories = PosCategory::child()->pluck('id')->toArray();
             $this->validate($request, [
                 'name'        => 'required',
-
                 'category_id' => 'required_without:master_category_id',
                 'master_category_id' => 'required_without:category_id|in:' . implode(',', $master_categories),
                 'unit'        => 'sometimes|in:' . implode(',', array_keys(constants('POS_SERVICE_UNITS'))),
                 'image_gallery' => 'sometimes|file|image'
-
             ]);
             $this->setModifier($request->manager_resource);
             $partner_pos_service = $creator->setData($request->all())->create();
