f1295175b8baf61b7c4569612438c6e10df681f3
Saiful <miajee@sheba.xyz>
2020-12-14 19:56:11 +0600

    ⬆️ Leave Substitute Update
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: B2BBE-613

--- app/Sheba/Business/Leave/Updater.php
+++ app/Sheba/Business/Leave/Updater.php
@@ -244,7 +244,7 @@ class Updater
     {
         if ($this->note) $this->data['note'] = $this->note;
         if ($this->substitute) $this->data['substitute_id'] = $this->substitute;
-        if ($this->substitute == "") $this->data['substitute_id'] = null;
+        if ($this->substitute == "null") $this->data['substitute_id'] = null;
     }
 
     private function getSubstituteName($substitute_id)
@@ -272,8 +272,8 @@ class Updater
             $this->leaveLogRepo->create($this->withCreateModificationField($data));
         }
 
-        if ($this->substitute || $this->substitute == "") {
-            if ($this->substitute == "") {
+        if ($this->substitute || $this->substitute == "null") {
+            if ($this->substitute == "null") {
                 $data['log'] = $this->member->profile->name . ' changed substitute from ' . $this->previousSubstituteName . ' to n/s';
             } else {
                 $data['log'] = $this->member->profile->name . ' changed substitute from ' . $this->previousSubstituteName . ' to ' . $this->getSubstituteName($this->substitute);
