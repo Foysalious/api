97e81a8db816c16edc20a724dbbb8043785b7fd4
Arafat <syedyeaminarafat@gmail.com>
2020-12-15 18:49:44 +0600

    ⬆️ Remaining is 0 for all packages
    Type(s): UPDATE

--- app/Models/Partner.php
+++ app/Models/Partner.php
@@ -901,12 +901,12 @@ class Partner extends BaseModel implements Rewardable, TopUpAgent, HasWallet, Tr
         return round($bonus_wallet + $wallet + $remaining) - $threshold;
     }
 
-    /** @return array
-     * @throws InvalidPreviousSubscriptionRules
+    /**
+     * @return array
      */
     public function getCreditBreakdown()
     {
-        $remaining             = (double)$this->subscriber()->periodicBillingHandler()->remainingCredit();
+        $remaining             = (double)0;
         $wallet                = (double)$this->wallet;
         $bonus_wallet          = (double)$this->bonusWallet();
         $threshold             = $this->walletSetting ? (double)$this->walletSetting->min_wallet_threshold : 0;
