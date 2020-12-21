821cbdd5d6bd0a4c26b52c0e42ecbd0ab25d4a1b
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2020-12-21 11:07:41 +0600

    ⬆️ Payment link web URL in CORS
    Type(s): UPDATE

--- app/Http/Middleware/Cors2MiddleWare.php
+++ app/Http/Middleware/Cors2MiddleWare.php
@@ -113,7 +113,8 @@ class Cors2MiddleWare
             "https://bkash-client.dev-sheba.xyz",
             "https://business.stage.sheba.xyz",
             "https://partners.smanager.xyz",
-            "https://smanager-webstore.stage.sheba.xyz"
+            "https://smanager-webstore.stage.sheba.xyz",
+            "http://paymentlink-web.stage.sheba.xyz"
         ];
         // ALLOW OPTIONS METHOD
         $headers['Access-Control-Allow-Credentials'] = 'true';
