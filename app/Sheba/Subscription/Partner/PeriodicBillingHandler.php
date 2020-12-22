20aecf3edfa844ed853ec4c00557964ce81f0afe
Arafat <syedyeaminarafat@gmail.com>
2020-12-22 12:27:10 +0600

    ⬆️ If null calculate next billing date
    Type(s): UPDATE

--- app/Sheba/Subscription/Partner/PeriodicBillingHandler.php
+++ app/Sheba/Subscription/Partner/PeriodicBillingHandler.php
@@ -59,13 +59,47 @@ class PeriodicBillingHandler
     }
 
     /**
+     * CALCULATE PARTNER'S NEXT BILLING DATE
+     *
+     * @return Carbon
+     */
+    public function calculateNextBillingDate()
+    {
+        $new_bill_date = '';
+        $last_billed_date = $this->partner->last_billed_date;
+        if (isset($last_billed_date) ) {
+            if ($this->partner->billing_type == BillingType::MONTHLY) {
+                $next_billed_date_month = (($last_billed_date->month + 1) % 12) ?: 12;
+                $next_billed_date_year = $last_billed_date->year + ($last_billed_date->month == 12);
+                $new_bill_date = Carbon::createFromDate($next_billed_date_year, $next_billed_date_month, 1);
+
+                if ($this->partner->billing_start_date->day <= $new_bill_date->daysInMonth) {
+                    $new_bill_date->day($this->partner->billing_start_date->day);
+                } else {
+                    $new_bill_date->day = $new_bill_date->daysInMonth;
+                }
+            } elseif ($this->partner->billing_type == BillingType::HALF_YEARLY) {
+                $new_bill_date = $last_billed_date->copy()->addMonths(6);
+            } elseif ($this->partner->billing_type == BillingType::YEARLY) {
+                $new_bill_date = $last_billed_date->copy()->addYear(1);
+                /** @var $new_bill_date Carbon */
+                if ($last_billed_date->isLeapYear() && $new_bill_date->month == 3 && $new_bill_date->day == 1) $new_bill_date->subDay(1);
+                if ($new_bill_date->isLeapYear() && $last_billed_date->month == 2 && $last_billed_date->day == 28) $new_bill_date->addDay(1);
+            }
+
+            return $new_bill_date;
+        }
+        return null;
+    }
+
+    /**
      * FINDING PARTNER'S NEXT BILLING DATE
      *
      * @return Carbon
      */
     public function nextBillingDate()
     {
-        return  $this->partner->next_billing_date ? Carbon::parse($this->partner->next_billing_date) : null;
+        return  $this->partner->next_billing_date ? Carbon::parse($this->partner->next_billing_date) : $this->calculateNextBillingDate();
     }
 
     /**
