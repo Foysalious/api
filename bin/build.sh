d6ada40a81c2f4e2a4a228f77def3be1a0aeb528
Hasan Hafiz Pasha <mach.pasha@gmail.com>
2020-12-23 16:54:01 +0600

    PHP FPM LOGGING CHANGE
    Type(s): FIX

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 . ./bin/parse_env.sh
 
-docker build -t "${CONTAINER_NAME}" -f ./docker/Dockerfileproduction . --build-arg APP_ENV="${APP_ENV}"
+docker build -t "${CONTAINER_NAME}" -f ./docker/Dockerfile.production . --build-arg APP_ENV="${APP_ENV}"
 docker tag "${CONTAINER_NAME}":latest registry.sheba.xyz/"${CONTAINER_NAME}":latest
 docker push registry.sheba.xyz/"${CONTAINER_NAME}":latest
