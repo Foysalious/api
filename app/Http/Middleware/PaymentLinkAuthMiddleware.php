be9011934da2ed3998831cf2b886858d98ab252b
arnabrahman <arnabrahman@hotmail.com>
2020-12-24 12:04:37 +0600

    🚧 Avatar deprecate from middlewares
    Type(s): WIP
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/SHEBA-6287

--- app/Http/Middleware/PaymentLinkAuthMiddleware.php
+++ app/Http/Middleware/PaymentLinkAuthMiddleware.php
@@ -10,8 +10,9 @@ class PaymentLinkAuthMiddleware extends AccessTokenMiddleware
 {
     public function setExtraDataToRequest($request)
     {
+        /** @var AuthUser $auth_user */
         $auth_user = $request->auth_user;
-        $user = $auth_user->getAvatar();
+        $user = $auth_user->getPartner();
         if (!$user) return;
         $type = strtolower(class_basename($user));
         $request->merge([$type => $user, 'type' => $type, 'user' => $user]);
