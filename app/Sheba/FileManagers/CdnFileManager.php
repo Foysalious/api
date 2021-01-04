6ab31cabc6549f36ab01ccfc67d9083765775cd8
shafiq_startern <shafiqul@sheba.xyz>
2021-01-04 23:55:44 +0600

    🐛 Sync as admin
    Type(s): FIX

--- app/Sheba/FileManagers/CdnFileManager.php
+++ app/Sheba/FileManagers/CdnFileManager.php
@@ -9,6 +9,37 @@ trait CdnFileManager
         return $this->putFileToCDNAndGetPath((string)$file, $folder, $filename);
     }
 
+    protected function saveFileToCDN($file, $folder, $filename)
+    {
+        return $this->putFileToCDNAndGetPath(file_get_contents($file), $folder, $filename);
+    }
+
+    protected function saveFileStringToCDN($file, $folder, $filename)
+    {
+        return $this->putFileToCDNAndGetPath($file, $folder, $filename);
+    }
+
+    protected function savePrivateFileToCDN($file, $folder, $filename)
+    {
+        return $this->putFileToCDNAndGetPath(file_get_contents($file), $folder, $filename, 'private');
+    }
+
+    protected function deleteImageFromCDN($filename)
+    {
+        $this->deleteFileFromCDN($filename);
+    }
+
+    protected function deleteFileFromCDN($filename)
+    {
+        $this->getCDN()->delete($filename);
+    }
+
+    public function deleteFile($full_s3_link)
+    {
+        $file_name = substr($full_s3_link, strlen(config('s3.url')));
+        $this->deleteFileFromCDN($file_name);
+    }
+
     public function getFullFileUrl($folder, $filename)
     {
         return config('sheba.s3_url') . $this->makeFullFilePath($folder, $filename);
@@ -29,31 +60,12 @@ trait CdnFileManager
     private function makeFullFilePath($folder, $filename)
     {
         $filename = clean($filename, '_', ['.', '-']);
-        return $folder . $filename;
+        $folder = trim($folder, '/');
+        return $folder . '/' . $filename;
     }
 
     private function getCDN()
     {
         return Storage::disk('s3');
-    }
-
-    protected function saveFileToCDN($file, $folder, $filename)
-    {
-        return $this->putFileToCDNAndGetPath(file_get_contents($file), $folder, $filename);
-    }
-
-    protected function savePrivateFileToCDN($file, $folder, $filename)
-    {
-        return $this->putFileToCDNAndGetPath(file_get_contents($file), $folder, $filename, 'private');
-    }
-
-    protected function deleteImageFromCDN($filename)
-    {
-        $this->deleteFileFromCDN($filename);
-    }
-
-    protected function deleteFileFromCDN($filename)
-    {
-        $this->getCDN()->delete($filename);
     }
 }
