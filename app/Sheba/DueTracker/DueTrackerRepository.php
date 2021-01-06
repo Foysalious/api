8586e4d42d1e53786e9e3f299ca4ef536c59de9e
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-01-06 16:33:26 +0600

    ➕ Due Tracker Head and Source for Webstore Order
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-128

--- app/Sheba/DueTracker/DueTrackerRepository.php
+++ app/Sheba/DueTracker/DueTrackerRepository.php
@@ -16,6 +16,7 @@ use App\Sheba\DueTracker\Exceptions\InsufficientBalance;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
+use Sheba\Dal\POSOrder\SalesChannels;
 use Sheba\DueTracker\Exceptions\InvalidPartnerPosCustomer;
 use Sheba\ExpenseTracker\AutomaticIncomes;
 use Sheba\ExpenseTracker\Exceptions\ExpenseTrackingServerError;
@@ -137,6 +138,12 @@ class DueTrackerRepository extends BaseRepository
             $item['created_at'] = Carbon::parse($item['created_at'])->format('Y-m-d h:i A');
             $item['entry_at']   = Carbon::parse($item['entry_at'])->format('Y-m-d h:i A');
             $item['partner_wise_order_id'] = $item['source_type'] === 'PosOrder' ? PosOrder::getPartnerWiseOrderId($item['source_id']) : null;
+            $pos_order = PosOrder::find($item['source_id']);
+            if ($pos_order && $pos_order->sales_channel === SalesChannels::WEBSTORE) {
+                $item['source_type'] = 'WebstoreOrder';
+                $item['head'] = 'Webstore sales';
+                $item['head_bn'] = 'ওয়েবস্টোর সেলস';
+            }
             return $item;
         });
 
