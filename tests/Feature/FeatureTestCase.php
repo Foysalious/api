500e6ced902910cc0c1223c687333b7633041d5c
Tisha <tisha@sheba.xyz>
2021-01-10 14:46:37 +0600

    ✅ ♻️  Centralize login
    Type(s): TEST, REFACTORING

--- tests/Feature/FeatureTestCase.php
+++ tests/Feature/FeatureTestCase.php
@@ -1,12 +1,22 @@
 <?php namespace Tests\Feature;
 
+use App\Models\Affiliate;
+use App\Models\Profile;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Sheba\Dal\AuthorizationRequest\AuthorizationRequest;
+use Sheba\Dal\AuthorizationToken\AuthorizationToken;
 use TestCase;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class FeatureTestCase extends TestCase
 {
+
     use DatabaseMigrations;
+
+    protected $token;
+    protected $profile;
 
     public function setUp()
     {
@@ -37,5 +47,59 @@ class FeatureTestCase extends TestCase
         $this->beforeApplicationDestroyed(function () {
             DB::unprepared(file_get_contents('database/seeds/sheba_testing.sql'));
         });*/
+    }
+
+    protected function logIn()
+    {
+        $this->truncateTable(Profile::class);
+        $this->profile = factory(Profile::class)->create();
+        $affiliate = factory(Affiliate::class)->create([
+            'profile_id' => $this->profile->id
+        ]);
+
+        $authorization_request = factory(AuthorizationRequest::class)->create([
+            'profile_id' => $this->profile->id
+        ]);
+        $this->token = JWTAuth::fromUser($this->profile, [
+            'name' => $this->profile->name,
+            'image' => $this->profile->pro_pic,
+            'profile' => [
+                'id' => $this->profile->id,
+                'name' => $this->profile->name,
+                'email_verified' => $this->profile->email_verified
+            ],
+            'customer' => null,
+            'resource' => null,
+            'member' => null,
+            'business_member' => null,
+            'affiliate' => [
+                'id' => $affiliate->id
+            ],
+            'logistic_user' => null,
+            'bank_user' => null,
+            'strategic_partner_member' => null,
+            'avatar' => null,
+            "exp" => Carbon::now()->addDay()->timestamp
+        ]);
+        factory(AuthorizationToken::class)->create([
+            'authorization_request_id' => $authorization_request->id,
+            'token' => $this->token
+        ]);
+    }
+
+    protected  function truncateTable($table)
+    {
+        $this->truncateTables([
+            $table
+        ]);
+    }
+
+    protected function truncateTables(array $tables)
+    {
+        Schema::disableForeignKeyConstraints();
+        foreach ($tables as $table) {
+            $table::truncate();
+        }
+        Schema::enableForeignKeyConstraints();
     }
 }
--- tests/Feature/TopUp/SingleTopUpTest.php
+++ tests/Feature/TopUp/SingleTopUpTest.php
@@ -1,77 +1,36 @@
 <?php namespace Tests\Feature\TopUp;
 
 
-use App\Models\Affiliate;
-use App\Models\Profile;
 use App\Models\TopUpOrder;
 use App\Models\TopUpVendor;
 use App\Models\TopUpVendorCommission;
-use Carbon\Carbon;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-use Mockery;
-use Sheba\Dal\AuthorizationRequest\AuthorizationRequest;
-use Sheba\Dal\AuthorizationToken\AuthorizationToken;
 use Sheba\OAuth2\AccountServer;
 use Sheba\TopUp\Verification\VerifyPin;
 use Tests\Feature\FeatureTestCase;
-use Tymon\JWTAuth\Facades\JWTAuth;
 use Sheba\Dal\TopUpOTFSettings\Model as TopUpOTFSettings;
 use Sheba\Dal\TopUpVendorOTF\Model as TopUpVendorOTF;
 use Sheba\Dal\TopUpVendorOTFChangeLog\Model as TopUpVendorOTFChangeLog;
 
 class SingleTopUpTest extends FeatureTestCase
 {
-    private $profile;
     private $affiliate;
-    private $authorizationRequest;
-    private $authrorizationToken;
     private $topUpVendor;
-    private $token;
     private $topUpVendorCommission;
     private $topUpOtfSettings;
     private $topUpVendorOtf;
     private $topUpStatusChangeLog;
 
-
     public function setUp()
     {
         parent::setUp();
-        Profile::where('mobile', '+8801678242955')->delete();
-        $this->profile = factory(Profile::class)->create();
-        $this->affiliate = factory(Affiliate::class)->create([
-            'profile_id' => $this->profile->id
+        $this->truncateTables([
+            TopUpVendor::class,
+            TopUpVendorCommission::class,
+            TopUpOTFSettings::class,
+            TopUpOrder::class
         ]);
-
-        $this->authorizationRequest = factory(AuthorizationRequest::class)->create([
-            'profile_id' => $this->profile->id
-        ]);
-        $this->token = JWTAuth::fromUser($this->profile, [
-            'name' => $this->profile->name,
-            'image' => $this->profile->pro_pic,
-            'profile' => ['id' => $this->profile->id, 'name' => $this->profile->name, 'email_verified' => $this->profile->email_verified],
-            'customer' => null,
-            'resource' => null,
-            'member' => null,
-            'business_member' => null,
-            'affiliate' => ['id' => $this->affiliate->id],
-            'logistic_user' => null,
-            'bank_user' => null,
-            'strategic_partner_member' => null,
-            'avatar' => null,
-            "exp" => Carbon::now()->addDay()->timestamp
-        ]);
-        $this->authrorizationToken = factory(AuthorizationToken::class)->create([
-            'authorization_request_id' => $this->authorizationRequest->id,
-            'token' => $this->token
-        ]);
-
-        Schema::disableForeignKeyConstraints();
-        TopUpVendor::truncate();
-        TopUpVendorCommission::truncate();
-        TopUpOTFSettings::truncate();
-        TopUpOrder::truncate();
-        Schema::enableForeignKeyConstraints();
+        $this->logIn();
 
         $this->topUpVendor = factory(TopUpVendor::class)->create();
         $this->topUpVendorCommission = factory(TopUpVendorCommission::class)->create([
@@ -89,14 +48,21 @@ class SingleTopUpTest extends FeatureTestCase
         $this->topUpStatusChangeLog= factory(TopUpVendorOTFChangeLog::class)->create([
             'otf_id' => $this->topUpVendorOtf->id
         ]);
+        $verify_pin_mock = $this->getMockBuilder(VerifyPin::class)
+            ->setConstructorArgs([$this->app->make(AccountServer::class)])
+            ->setMethods(['verify'])
+            ->getMock();
+        $verify_pin_mock->method('setAgent')->will($this->returnSelf());
+        $verify_pin_mock->method('setProfile')->will($this->returnSelf());
+        $verify_pin_mock->method('setRequest')->will($this->returnSelf());
 
-
+        $this->app->instance(VerifyPin::class, $verify_pin_mock);
     }
 
     public function testInvalidMobileNumberIsRejected()
     {
         $response = $this->post('/v2/top-up/affiliate', [
-            'mobile' => 0171404731411,
+            'mobile' => '016782429559',
             'vendor_id' => $this->topUpVendor->id,
             'connection_type' => 'prepaid',
             'amount' => 112,
@@ -188,15 +154,6 @@ class SingleTopUpTest extends FeatureTestCase
 
     public function testOneTopUpRequestCreateOneTopUpOrder()
     {
-        $verify_pin_mock = $this->getMockBuilder(VerifyPin::class)
-            ->setConstructorArgs([$this->app->make(AccountServer::class)])
-            ->setMethods(['verify'])
-            ->getMock();
-        $verify_pin_mock->method('setAgent')->will($this->returnSelf());
-        $verify_pin_mock->method('setProfile')->will($this->returnSelf());
-        $verify_pin_mock->method('setRequest')->will($this->returnSelf());
-
-        $this->app->instance(VerifyPin::class, $verify_pin_mock);
 
         $this->post('/v2/top-up/affiliate', [
             'mobile' => '01678242955',
@@ -213,16 +170,6 @@ class SingleTopUpTest extends FeatureTestCase
 
     public function testTopUpOrderDataMatchesTopUpRequestData()
     {
-        $verify_pin_mock= $this->getMockBuilder(VerifyPin::class)
-            ->setConstructorArgs([$this->app->make(AccountServer::class)])
-            ->setMethods(['verify'])
-            ->getMock();
-        $verify_pin_mock->method('setAgent')->will($this->returnSelf());
-        $verify_pin_mock->method('setProfile')->will($this->returnSelf());
-        $verify_pin_mock->method('setRequest')->will($this->returnSelf());
-
-        $this->app->instance(VerifyPin::class, $verify_pin_mock);
-
         $response = $this->post('/v2/top-up/affiliate', [
             'mobile' => '01678242955',
             'vendor_id' => $this->topUpVendor->id,
@@ -248,15 +195,6 @@ class SingleTopUpTest extends FeatureTestCase
 
     public function testTopUpOrderSuccessfulResponseCodeAndMessage()
     {
-        $verify_pin_mock = $this->getMockBuilder(VerifyPin::class)
-            ->setConstructorArgs([$this->app->make(AccountServer::class)])
-            ->setMethods(['verify'])
-            ->getMock();
-        $verify_pin_mock->method('setAgent')->will($this->returnSelf());
-        $verify_pin_mock->method('setProfile')->will($this->returnSelf());
-        $verify_pin_mock->method('setRequest')->will($this->returnSelf());
-
-        $this->app->instance(VerifyPin::class, $verify_pin_mock);
 
         $response = $this->post('/v2/top-up/affiliate', [
             'mobile' => '01678242955',
