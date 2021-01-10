aab602196fc022ced975c077fb34e3134cc607db
Raqibul Hasan <raqibul@sheba.xyz>
2021-01-10 10:48:35 +0600

    ⬆️ Entry note for payment link
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSMEBE-1744

--- app/Sheba/ExpenseTracker/Repository/AutomaticEntryRepository.php
+++ app/Sheba/ExpenseTracker/Repository/AutomaticEntryRepository.php
@@ -258,6 +258,7 @@ class AutomaticEntryRepository extends BaseRepository
             $data['amount_cleared'] = $data['amount'];
         if ($this->profileId)
             $data['profile_id'] = $this->profileId;
+        \Log::info('real data -----------   '.json_encode($data).'  ---------');
         return $data;
     }
 
