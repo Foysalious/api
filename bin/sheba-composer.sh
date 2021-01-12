cb23adf3698e764acb72f5ca30157b40cd105681
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-01-12 12:40:37 +0600

    ⬆️ Update Specific Package
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/SHEBA-6323

--- bin/sheba-composer.sh
+++ bin/sheba-composer.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+other_args="--no-interaction --ignore-platform-reqs"
+
 get_git_branch() {
   echo "$(git symbolic-ref --short -q HEAD 2>/dev/null)"
 }
@@ -7,36 +9,37 @@ get_git_branch() {
 get_composer_command_for_development() {
   FILE=composer-dev.json
   if test -f "$FILE"; then
-    echo "sudo env COMPOSER=$FILE composer ${action} --no-interaction --ignore-platform-reqs"
+    echo "sudo env COMPOSER=$FILE composer ${args} ${other_args}"
   else
-    echo "sudo composer ${action} --no-interaction --ignore-platform-reqs"
+    echo "sudo composer ${args} ${other_args}"
   fi
 }
 
 get_composer_command_for_production() {
-  echo "sudo composer ${action} --no-interaction --ignore-platform-reqs"
+  echo "sudo composer ${args} ${other_args}"
 }
 
-if [ $1 = "install" ]; then
-  action="install"
-elif [ $1 = "update" ]; then
-  action="update"
-else
-  return
-fi
+echo "${package_name}"
 
-branch=$2
+all_args="$@"
+
+#only the last word
+branch="${all_args##* }"
+
+#all but the last word
+args="${all_args% *}"
+
 if [ -z "${branch}" ]; then
   branch="$(get_git_branch)"
 fi
 
 if [ "${branch}" = "master" ]; then
-  composer_command="$(get_composer_command_for_production)"
+  compose="$(get_composer_command_for_production)"
 elif [ "${branch}" = "release" ]; then
-  composer_command="$(get_composer_command_for_production)"
+  compose="$(get_composer_command_for_production)"
 else
-  composer_command="$(get_composer_command_for_development)"
+  compose="$(get_composer_command_for_development)"
 fi
 
-eval "${composer_command}"
+eval "${compose}"
 
