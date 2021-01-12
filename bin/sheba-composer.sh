981b74aa1f0bbcdc928a8040b189ff6f7cba7efc
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-01-12 17:39:01 +0600

    ⬆️ Separate Dev Prod Version
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/SHEBA-6323

--- bin/sheba-composer.sh
+++ bin/sheba-composer.sh
@@ -2,10 +2,6 @@
 
 other_args="--no-interaction --ignore-platform-reqs"
 
-get_git_branch() {
-  echo "$(git symbolic-ref --short -q HEAD 2>/dev/null)"
-}
-
 get_composer_command_for_development() {
   FILE=composer-dev.json
   if test -f "$FILE"; then
@@ -19,25 +15,17 @@ get_composer_command_for_production() {
   echo "sudo composer ${args} ${other_args}"
 }
 
-echo "${package_name}"
-
 all_args="$@"
 
 #only the last word
-branch="${all_args##* }"
+composer_version="${all_args##* }"
 
 #all but the last word
 args="${all_args% *}"
 
-if [ -z "${branch}" ]; then
-  branch="$(get_git_branch)"
-fi
-
-if [ "${branch}" = "master" ]; then
+if [ "${composer_version}" = "prod" ]; then
   compose="$(get_composer_command_for_production)"
-elif [ "${branch}" = "release" ]; then
-  compose="$(get_composer_command_for_production)"
-else
+elif [ "${composer_version}" = "dev" ]; then
   compose="$(get_composer_command_for_development)"
 fi
 
