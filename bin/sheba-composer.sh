9f327680102768ed8b5b6ce631f1c4868c7aedae
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-01-17 17:57:57 +0600

    ⬆️ Copy Prod Composer .lock for dev
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/SHEBA-6323

--- bin/sheba-composer.sh
+++ bin/sheba-composer.sh
@@ -5,14 +5,14 @@ other_args="--no-interaction --ignore-platform-reqs"
 get_composer_command_for_development() {
   FILE=composer-dev.json
   if test -f "$FILE"; then
-    echo "sudo env COMPOSER=$FILE composer ${args} ${other_args}"
+    echo "env COMPOSER=$FILE composer ${args} ${other_args}"
   else
-    echo "sudo composer ${args} ${other_args}"
+    echo "composer ${args} ${other_args}"
   fi
 }
 
 get_composer_command_for_production() {
-  echo "sudo composer ${args} ${other_args}"
+  echo "composer ${args} ${other_args}"
 }
 
 all_args="$@"
