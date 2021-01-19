0e6105faa57f61eae36780568a176ee1dabcc8ab
Irteza Asad <irtezaasad.sheba@gmail.com>
2021-01-19 12:34:47 +0600

    ⬆️ Vat_percentage in invoice
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MPBE-1627

--- resources/views/pdfs/_invoice_v2.blade.php
+++ resources/views/pdfs/_invoice_v2.blade.php
@@ -103,6 +103,12 @@
 
     <tr>
         <td colspan="2"></td>
+        <td class="text-left" colspan="1">VAT</td>
+        <td class="s-price">{{ $partner_order->vat }}</td>
+    </tr>
+
+    <tr>
+        <td colspan="2"></td>
         @if($partner_order->due>0 && $type !== "QUOTATION")
             <td class="text-left" colspan="1">DUE AMOUNT @if($type=="QUOTATION") *** @endif</td>
             <td class="s-price">{{ $partner_order->dueWithLogistic }}</td>
