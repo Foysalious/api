2dccaf69b1323c3cdf900fe02736265a3cba1fae
Shafiqul Islam <shafiq.startern@gmail.com>
2021-02-02 11:28:02 +0600

    ➕ Error support in Mock vendor
    Type(s): ADD
    
    Issue(s):
    - JIRA: SHEBA-6456

--- app/Sheba/TopUp/Vendor/Mock.php
+++ app/Sheba/TopUp/Vendor/Mock.php
@@ -1,23 +1,31 @@
 <?php namespace Sheba\TopUp\Vendor;
 
 use App\Models\TopUpOrder;
+use Sheba\Dal\TopupOrder\Statuses;
+use Sheba\TopUp\Exception\GatewayTimeout;
 use Sheba\TopUp\TopUpRequest;
+use Sheba\TopUp\Vendor\Response\GenericGatewayErrorResponse;
 use Sheba\TopUp\Vendor\Response\MockResponse;
+use Sheba\TopUp\Vendor\Response\TopUpGatewayTimeoutResponse;
 use Sheba\TopUp\Vendor\Response\TopUpResponse;
 
 class Mock extends Vendor
 {
-    function recharge(TopUpOrder $topup_order): TopUpResponse
+    public function recharge(TopUpOrder $topup_order): TopUpResponse
     {
+        if ($topup_order->payee_mobile == "+8801700999999") return $this->handleGatewayTimeout();
+
+        $is_success = $topup_order->payee_mobile != "+8801700888888";
+
         return (new MockResponse())->setResponse(json_decode(json_encode([
-            'TXNSTATUS' => 200,
+            'TXNSTATUS' => $is_success ? 200 : 500,
             'TXNID' => 123456,
             'MESSAGE' => "Mocking"
         ])));
     }
 
-    function getTopUpInitialStatus()
+    public function getTopUpInitialStatus()
     {
-        return "Successful";
+        return Statuses::SUCCESSFUL;
     }
 }
