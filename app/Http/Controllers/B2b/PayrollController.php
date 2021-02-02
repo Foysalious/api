89dde16edf69ab371ab862a165e7d210c66d22f0
Saiful <miajee@sheba.xyz>
2021-02-02 19:11:29 +0600

    Payroll Setting Typo

--- app/Http/Controllers/B2b/PayrollController.php
+++ app/Http/Controllers/B2b/PayrollController.php
@@ -1,13 +1,17 @@
 <?php namespace App\Http\Controllers\B2b;
 
+use App\Models\BusinessMember;
 use Sheba\Business\PayrollSetting\Requester as PayrollSettingRequester;
 use Sheba\Business\PayrollSetting\Updater as PayrollSettingUpdater;
 use Sheba\Dal\PayrollSetting\PayrollSettingRepository;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Sheba\ModificationFields;
 
 class PayrollController extends Controller
 {
+    use ModificationFields;
+
     private $payrollSettingRepository;
     private $payrollSettingRequester;
     private $payrollSettingUpdater;
@@ -27,7 +31,12 @@ class PayrollController extends Controller
             'is_enable' => 'required|integer',
             'pay_day' => 'required|integer'
         ]);
+        /** @var BusinessMember $business_member */
+        $business_member = $request->business_member;
+        $this->setModifier($business_member->member);
+
         $payroll_setting = $this->payrollSettingRepository->find((int)$payroll_setting);
+        if (!$payroll_setting) return api_response($request, null, 404);
         $this->payrollSettingRequester->setIsEnable($request->is_enable)->setPayDay($request->pay_day);
         $this->payrollSettingUpdater->setPayrollSetting($payroll_setting)->setPayrollSettingRequest($this->payrollSettingRequester)->update();
         return api_response($request, null, 200);
@@ -35,11 +44,11 @@ class PayrollController extends Controller
 
     public function updateSalaryBreakdown($business, $payroll_setting, Request $request)
     {
-        $this->validate($request, [
-            'is_enable' => 'required|integer',
-            'pay_day' => 'required|integer'
-        ]);
+        /** @var BusinessMember $business_member */
+        $business_member = $request->business_member;
+        $this->setModifier($business_member->member);
         $payroll_setting = $this->payrollSettingRepository->find((int)$payroll_setting);
+        if (!$payroll_setting) return api_response($request, null, 404);
 
         return api_response($request, null, 200);
     }
--- app/Sheba/Business/PayrollComponent/Updater.php
+++ app/Sheba/Business/PayrollComponent/Updater.php
@@ -1,6 +1,53 @@
 <?php namespace Sheba\Business\PayrollComponent;
 
+use Sheba\Business\PayrollComponent\Requester as PayrollComponentRequest;
+use Sheba\Dal\PayrollComponent\Components;
+use Sheba\Dal\PayrollComponent\PayrollComponentRepository;
+use Sheba\Dal\PayrollSetting\PayrollSetting;
+
 class Updater
 {
+    /**
+     * @var PayrollSetting
+     */
+    private $payrollSetting;
+    private $payrollComponentRepository;
+    private $payrollComponentRequester;
+    private $payrollComponentData = [];
 
+
+    public function __construct(PayrollComponentRepository $payroll_component_repository)
+    {
+        $this->payrollComponentRepository = $payroll_component_repository;
+    }
+
+    public function setPayrollSetting(PayrollSetting $payroll_setting)
+    {
+        $this->payrollSetting = $payroll_setting;
+        return $this;
+    }
+
+    public function setPayrollComponentRequester(PayrollComponentRequest $payroll_component_requester)
+    {
+        $this->payrollComponentRequester = $payroll_component_requester;
+        return $this;
+    }
+
+    public function create()
+    {
+        $this->payrollComponentData();
+        $this->payrollComponentRepository->insert($this->payrollComponentData);
+    }
+
+    private function payrollComponentData()
+    {
+        foreach (Components::get() as $key => $component) {
+            $this->payrollComponentData[] = [
+                'payroll_setting_id' => $this->payrollSetting->id,
+                'name' => $component,
+                'setting' => json_encode(['percentage' => 0]),
+            ];
+        }
+        return $this->payrollComponentData;
+    }
 }
