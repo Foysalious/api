cee886c43316058514ef9e1b120fe8ebe09c65dc
shovan <shovan.sheba@gmail.com>
2021-02-04 18:14:23 +0600

    ♻️ Trade fair shop list
    Type(s): REFACTORING
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-228?atlOrigin=eyJpIjoiNjQ3Yzk2ZDNlMjE3NDc4MTlmY2JmNjk4MjBiMDI3NGQiLCJwIjoiaiJ9
    https://blabol.atlassian.net/browse/MSBE-229?atlOrigin=eyJpIjoiMDhiMzhkMDUwM2M2NDNhNzg5Zjk3Njg2MWRlYjI0ZmQiLCJwIjoiaiJ9

---
+++ app/Sheba/Partner/TradeFair/TradeFair.php
@@ -0,0 +1,89 @@
+<?php namespace App\Sheba\Partner\TradeFair;
+
+
+use App\Models\Partner;
+use Illuminate\Support\Facades\DB;
+use Sheba\Dal\TradeFair\Model as TradeFairModel;
+
+class TradeFair
+{
+
+    /**
+     * @return mixed
+     */
+    public function getBusinessTypeWisePartner()
+    {
+        return DB::select('SELECT partners.id,partners.business_type
+                                FROM   partners  JOIN (
+                                SELECT  business_type, GROUP_CONCAT(id) grouped_partner
+                                FROM  partners
+                                where is_webstore_published = 1
+                                GROUP BY business_type) group_max 
+                                ON partners.business_type = group_max.business_type
+                                AND FIND_IN_SET(id, grouped_partner) BETWEEN 1 AND 3
+                                ORDER BY   partners.business_type DESC');
+    }
+
+    /**
+     * @param $index
+     * @return array
+     */
+    public function convertPartnerBusinessType($index)
+    {
+        $business_types = constants('PARTNER_BUSINESS_TYPE');
+        $converted_business_types = [];
+        foreach ($business_types as $business_type) {
+            if ($index == 'bn')
+                $converted_business_types[$business_type['bn']] = $business_type['en'];
+            else
+                $converted_business_types[$business_type['en']] = $business_type['bn'];
+        }
+        return $converted_business_types;
+    }
+
+    /**
+     * @param $partners
+     * @param $mapped_partner_business_type
+     * @return mixed
+     */
+    public function makeData($partners, $mapped_partner_business_type)
+    {
+        $converted_business_types = $this->convertPartnerBusinessType('bn');
+        return $trade_fair_data = TradeFairModel::whereIn('partner_id', $partners)->get()->map(function ($shop) use ($mapped_partner_business_type, $converted_business_types) {
+            return [
+                'stall_id' => $shop->stall_id,
+                'partner_id' => $shop->partner_id,
+                'desciption' => $shop->description,
+                'discount' => $shop->discount,
+                'is_published' => $shop->is_published,
+                'business_type' => $converted_business_types[$mapped_partner_business_type[$shop->partner_id]]
+            ];
+        });
+    }
+
+    /**
+     * @param $business_type
+     * @return array
+     */
+    public function getStoresByBusinessType($business_type)
+    {
+        $converted_business_types = $this->convertPartnerBusinessType('en');
+        $partners = Partner::has('tradeFair')->with('tradeFair')->where('is_webstore_published', 1)
+            ->where('business_type', $converted_business_types[$business_type])
+            ->select('id')->get();
+
+        $stores = [];
+        $partners->each(function ($partner) use (&$stores) {
+            array_push($stores, [
+                'partner_id' => $partner->id,
+                'stall_id' => $partner->tradeFair->stall_id,
+                'description' => $partner->tradeFair->description,
+                'discount' => $partner->tradeFair->discount,
+                'is_published' => $partner->tradeFair->is_published
+            ]);
+        });
+        return $stores;
+    }
+
+
+}
