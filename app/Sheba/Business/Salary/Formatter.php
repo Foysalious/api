51dedda99e1035ec32f116c9e26f16dd563de61e
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2021-02-07 15:28:18 +0600

    Gross Type Salary
    Type(s): ADD

--- app/Sheba/Business/Salary/Formatter.php
+++ app/Sheba/Business/Salary/Formatter.php
@@ -1,13 +1,16 @@
 <?php namespace App\Sheba\Business\Salary;
 
 
+use Sheba\Dal\PayrollSetting\PayrollSetting;
+use Sheba\Dal\PayrollComponent\Type;
+
 class Formatter
 {
     private $payrollSetting;
     private $salary;
     private $grossSalaryBreakdown = [];
 
-    public function setPayrollSetting($payroll_setting)
+    public function setPayrollSetting(PayrollSetting $payroll_setting)
     {
         $this->payrollSetting = $payroll_setting;
         return $this;
@@ -22,8 +25,9 @@ class Formatter
     public function calculate()
     {
         $gross_salary = $this->salary->gross_salary;
+        $gross_type_components = $this->payrollSetting->components()->where('type', Type::GROSS);
         $this->grossSalaryBreakdown['salary_breakdown']['gross_salary'] = floatval($gross_salary);
-        foreach ($this->payrollSetting->components as $components)
+        foreach ($gross_type_components as $components)
         {
             $percentage = json_decode($components->setting, 1)['percentage'];
             $this->grossSalaryBreakdown['salary_breakdown'][$components->name] = ($gross_salary * $percentage) / 100;
