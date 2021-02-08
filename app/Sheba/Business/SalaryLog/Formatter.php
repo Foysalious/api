d136a35a05195b69c3243a753c85136cf1400a7e
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2021-02-08 12:25:49 +0600

    Salary Log Format
    Type(s): UPDATE

--- app/Sheba/Business/SalaryLog/Formatter.php
+++ app/Sheba/Business/SalaryLog/Formatter.php
@@ -20,9 +20,12 @@ class Formatter
      */
     public function format()
     {
+        $x = 0;
         foreach ($this->salaryLogs as $log)
         {
-            $this->grossSalaryLogs[] = $log->log;
+            $this->grossSalaryLogs[$x]['dateTime'] = $log->created_at->format('h:i A - d M, Y');
+            $this->grossSalaryLogs[$x]['log'] = $log->log;
+            $x++;
         }
         return $this->grossSalaryLogs;
     }
