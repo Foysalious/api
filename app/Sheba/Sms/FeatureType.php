1e4930d86126d0be7afc60260616e62a5a75cc73
Raqibul Hasan <raqibul@sheba.xyz>
2021-02-17 12:43:33 +0600

    ➕ Business type and feater type
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSMEBE-1949

---
+++ app/Sheba/Sms/FeatureType.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Sheba\Sms;
+
+
+use Sheba\Helpers\ConstGetter;
+
+class FeatureType
+{
+    use ConstGetter;
+
+    const TOP_UP         = 'topup';
+    const PAYMENT_LINK  = 'payment_link';
+    const DUE_TRACKER   = 'due_tracker';
+    const PARTNER_REGISTRATION       = 'partner_registration';
+}
