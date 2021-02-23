f006686705135750ae7ff13e9073bf45687baf73
Khairun Nahar <khairun@sheba.xyz>
2021-02-23 13:20:56 +0600

    Merge branch 'master' into master-test
    # Conflicts:
    #	app/Http/Controllers/Auth/PartnerRegistrationController.php
    #	app/Sheba/Exceptions/Handlers/Handler.php
    #	app/Sheba/Helpers/Miscellaneous/functions.php
    #	app/Sheba/TopUp/TopUp.php
    #	config/queue.php

--- database/Factory/PartnerFactory.php
+++ database/Factory/PartnerFactory.php
@@ -1,7 +1,4 @@
-<?php
-
-
-namespace Factory;
+<?php namespace Factory;
 
 
 use App\Models\Partner;
@@ -18,6 +15,7 @@ class PartnerFactory extends Factory
     {
         return array_merge($this->commonSeeds, [
             'name'=> $this->faker->name,
+            'sub_domain'=>"test-sp",
             'package_id'=>2,
             'mobile' => '+8801666777555',
             'password' => bcrypt(89079),
