661ef474131515208e73586364d01eb9d5c8e485
shovan <shovan.sheba@gmail.com>
2021-02-24 12:30:45 +0600

    ⬆️ Partners category list
    Type(s): UPDATE
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-214?atlOrigin=eyJpIjoiYWU4NWRmOWEzY2MxNGZjN2I4MmI5ZGM1ZWMzZDE5MTIiLCJwIjoiaiJ9

--- app/Http/Route/Prefix/POS/V1/Route.php
+++ app/Http/Route/Prefix/POS/V1/Route.php
@@ -6,7 +6,7 @@ class Route
     public function set($api)
     {
         $api->group(['prefix' => 'pos/v1', 'namespace' => 'App\Http\Controllers'], function ($api) {
-            $api->group(['prefix' => 'partners/{partner}', 'middleware' => ['jwtGlobalAuth']], function ($api) {
+            $api->group(['prefix' => 'partners/{partner}', 'middleware' => ['accessToken']], function ($api) {
                 $api->group(['prefix' => 'products'], function ($api) {
                     $api->get('/', 'Inventory\ProductController@index');
                 });
