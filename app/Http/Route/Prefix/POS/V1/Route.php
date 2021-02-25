104f14c867032da69a482fac8ba87872494783b6
foysal <foysal@iamsheba.xyz>
2021-02-25 15:39:42 +0600

    ➕ Category and Sub Category API
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-341

--- app/Http/Route/Prefix/POS/V1/Route.php
+++ app/Http/Route/Prefix/POS/V1/Route.php
@@ -6,6 +6,7 @@ class Route
     public function set($api)
     {
         $api->group(['prefix' => 'pos/v1', 'namespace' => 'App\Http\Controllers'], function ($api) {
+
             $api->group(['prefix' => 'partners/{partner}', 'middleware' => ['accessToken']], function ($api) {
                 $api->group(['prefix' => 'products'], function ($api) {
                     $api->get('/', 'Inventory\ProductController@index');
@@ -18,6 +19,7 @@ class Route
                 });
                 $api->group(['prefix' => 'categories'], function ($api) {
                     $api->get('/', 'Inventory\CategoryController@index');
+                    $api->get('/allCategory', 'Inventory\CategoryController@allCategory');
                     $api->post('/', 'Inventory\CategoryController@store');
                     $api->post('/{category_id}', 'Inventory\CategoryController@update');
                     $api->delete('/{category_id}', 'Inventory\CategoryController@delete');
