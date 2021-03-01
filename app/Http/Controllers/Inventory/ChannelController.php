f2152145bf28e3288cc9e6e642dd593251c969e2
foysal <foysal@iamsheba.xyz>
2021-03-01 13:17:30 +0600

    ➕
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-346

---
+++ app/Http/Controllers/Inventory/ChannelController.php
@@ -0,0 +1,29 @@
+<?php namespace App\Http\Controllers\Inventory;
+
+use App\Http\Controllers\Controller;
+use App\Sheba\InventoryService\Services\ChannelService;
+use Illuminate\Http\Request;
+
+
+
+
+class ChannelController extends Controller
+{
+
+    private $channelService;
+
+    public function __construct(ChannelService $channelService)
+    {
+        $this->channelService = $channelService;
+    }
+
+    public function index(Request $request)
+    {
+
+        $units = $this->channelService->getAll();
+        return api_response($request, null, 200, $units);
+
+    }
+}
+
+
