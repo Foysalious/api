bf6c458866b01f327a0bd2da6aa16cb7157e3d3a
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2021-03-03 19:29:19 +0600

    is_default added in payroll component
    Type(S): ADD

--- app/Sheba/Business/PayrollComponent/Creator.php
+++ app/Sheba/Business/PayrollComponent/Creator.php
@@ -46,6 +46,7 @@ class Creator
                 'payroll_setting_id' => $this->payrollSetting->id,
                 'name' => $component['key'],
                 'type' => $component['type'],
+                'is_default' => 1,
                 'setting' => json_encode(['percentage' => 0]),
             ];
         }
