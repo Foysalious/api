be7b72b66f6524d27db29a82375cceae999935b6
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2021-03-03 21:33:43 +0600

    Payroll setting API Update
    Type(s): UPDATE

--- app/Transformers/Business/PayrollSettingsTransformer.php
+++ app/Transformers/Business/PayrollSettingsTransformer.php
@@ -98,8 +98,8 @@ class PayrollSettingsTransformer extends TransformerAbstract
     {
         $data = [];
         foreach ($addition_components as $addition) {
-            if (!$addition->is_default) $data['addition'][] = ['name' => implode(" ", explode("_",$addition->name)), 'is_default' => 0];
-            if ($addition->is_default) $data['addition'][] = ['name' => Components::getComponents($addition->name)['value'], 'is_default' => 1];
+            if (!$addition->is_default) $data['addition'][] = ['id' => $addition->id, 'name' => implode(" ", explode("_",$addition->name)), 'is_default' => 0];
+            if ($addition->is_default) $data['addition'][] = ['id' => $addition->id, 'name' => Components::getComponents($addition->name)['value'], 'is_default' => 1];
         }
         return $data;
     }
@@ -108,8 +108,8 @@ class PayrollSettingsTransformer extends TransformerAbstract
     {
         $data = [];
         foreach ($deduction_components as $deduction) {
-            if (!$deduction->is_default) $data['deduction'][] = ['name' => implode(" ", explode("_",$deduction->name)), 'is_default' => 0];
-            if ($deduction->is_default) $data['deduction'][] = ['name' => Components::getComponents($deduction->name)['value'], 'is_default' => 1];
+            if (!$deduction->is_default) $data['deduction'][] = ['id' => $deduction->id, 'name' => implode(" ", explode("_",$deduction->name)), 'is_default' => 0];
+            if ($deduction->is_default) $data['deduction'][] = ['id' => $deduction->id, 'name' => Components::getComponents($deduction->name)['value'], 'is_default' => 1];
         }
         return $data;
     }
