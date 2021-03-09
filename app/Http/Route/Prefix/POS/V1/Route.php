435ec6b2b4de255fb66b6d40e7fc013518aba65b
foysal <foysal@iamsheba.xyz>
2021-03-09 12:11:17 +0600

    ➕
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-344

--- app/Http/Route/Prefix/POS/V1/Route.php
+++ app/Http/Route/Prefix/POS/V1/Route.php
@@ -7,12 +7,15 @@ class Route
     {
         $api->group(['prefix' => 'pos/v1', 'namespace' => 'App\Http\Controllers'], function ($api) {
 
+
             $api->get('/channels', "Inventory\ChannelController@index");
-            $api->get('/allCategory', 'Inventory\CategoryController@allCategory');
             $api->get('/units', "Inventory\UnitController@index");
 
+        });
 
-            $api->group(['prefix' => 'partners', 'middleware' => ['accessToken']], function ($api) {
+            $api->group(['prefix' => 'pos/v1', 'middleware' => ['accessToken']], function ($api) {
+                $api->get('/tree', 'Inventory\CategoryController@allCategory');
+
                 $api->group(['prefix' => 'products'], function ($api) {
                     $api->get('/', 'Inventory\ProductController@index');
                     $api->post('/', 'Inventory\ProductController@store');
@@ -29,9 +32,7 @@ class Route
                     $api->post('/{category_id}', 'Inventory\CategoryController@update');
                     $api->delete('/{category_id}', 'Inventory\CategoryController@delete');
                 });
-//                $api->group(['prefix' => 'units'], function ($api) {
-//                    $api->get('/', "Inventory\UnitController@index");
-//                });
+
                 $api->group(['prefix' => 'options'], function ($api) {
                     $api->get('/', "Inventory\OptionController@index");
                     $api->post('/', "Inventory\OptionController@store");
@@ -43,12 +44,39 @@ class Route
                 $api->group(['prefix' => 'values'], function ($api) {
                     $api->group(['prefix' => '{values}'], function ($api) {
                         $api->put('/', "Inventory\ValueController@update");
-                    });
+                    }); });
+            $api->group(['prefix' => 'products'], function ($api) {
+                $api->get('/', 'Inventory\ProductController@index');
+                $api->post('/', 'Inventory\ProductController@store');
+                $api->group(['prefix' => '{products}'], function ($api) {
+                    $api->get('/', 'Inventory\ProductController@show');
+                    $api->put('/', 'Inventory\ProductController@update');
+                    $api->delete('/', 'Inventory\ProductController@destroy');
                 });
             });
-            $api->group(['prefix' => 'collections'], function($api){
+            $api->group(['prefix' => 'categories'], function ($api) {
+                $api->get('/', 'Inventory\CategoryController@index');
+                $api->get('/allCategory', 'Inventory\CategoryController@allCategory');
+                $api->post('/', 'Inventory\CategoryController@store');
+                $api->put('/{category_id}', 'Inventory\CategoryController@update');
+                $api->delete('/{category_id}', 'Inventory\CategoryController@delete');
+            });
+            $api->group(['prefix' => 'collections'], function ($api) {
                 $api->get('/', 'Inventory\CollectionController@index');
+                $api->post('/', 'Inventory\CollectionController@store');
+                $api->get('/{collection}', 'Inventory\CollectionController@show');
+                $api->put('/{collection}', 'Inventory\CollectionController@update');
+                $api->delete('/{collection}', 'Inventory\CollectionController@destroy');
+            });
+
+
+            $api->group(['prefix' => 'values'], function ($api) {
+                $api->group(['prefix' => '{values}'], function ($api) {
+                    $api->put('/', "Inventory\ValueController@update");
+                });
             });
         });
     }
 }
+
+
