5f728b435e3f3750d8007880d83d2de00bfdb151
Shafiqul Islam <shafiqul@sheba.xyz>
2021-03-09 15:10:37 +0600

    ♻️  Move lots of code
    Type(s): REFACTORING
    
    Issue(s):
    - JIRA: SHEBA-6704

--- app/Sheba/TopUp/Vendor/Response/Ipn/Paywell/PaywellSuccessResponse.php
+++ app/Sheba/TopUp/Vendor/Response/Ipn/Paywell/PaywellSuccessResponse.php
@@ -1,21 +1,10 @@
-<?php namespace Sheba\TopUp\Vendor\Response\Paywell;
+<?php namespace Sheba\TopUp\Vendor\Response\Ipn\Paywell;
 
 use App\Models\TopUpOrder;
 use Sheba\TopUp\Vendor\Response\Ipn\SuccessResponse;
 
 class PaywellSuccessResponse extends SuccessResponse
 {
-
-    public function getSuccessfulTransactionDetails()
-    {
-        return $this->response;
-    }
-
-    public function setResponse($response)
-    {
-        $this->response = $response;
-    }
-
     public function getTopUpOrder(): TopUpOrder
     {
         return TopUpOrder::where('id', $this->response->tran_id)->first();
