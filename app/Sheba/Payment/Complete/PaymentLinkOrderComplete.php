bdbc8a86bc745025ca78bba5d9037e1b6e9266b0
tonmoy <tonmoy@sheba.xyz>
2021-03-10 15:45:33 +0600

    :arrow_up:  PAYMENT LINK TRANSACTION DETAILS
    Type(s): UPDATE

--- app/Sheba/Payment/Complete/PaymentLinkOrderComplete.php
+++ app/Sheba/Payment/Complete/PaymentLinkOrderComplete.php
@@ -62,7 +62,6 @@ class PaymentLinkOrderComplete extends PaymentComplete
                 $this->paymentRepository->setPayment($this->payment);
                 $payable = $this->payment->payable;
                 $this->setModifier($customer = $payable->user);
-                $this->payment->transaction_details = null;
                 $this->completePayment();
                 $this->processTransactions($this->payment_receiver);
                 $this->clearTarget();
