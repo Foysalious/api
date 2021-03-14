47106e36e6df7efd5281a787ba9b5f11d6bdf595
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2021-03-14 20:33:27 +0600

    ➕ Is_payroll_enable added in list
    Type(s): ADD
    
    Issue(s):
    - JIRA: B2BBE-885

--- app/Http/Controllers/B2b/MemberController.php
+++ app/Http/Controllers/B2b/MemberController.php
@@ -180,6 +180,7 @@ class MemberController extends Controller
             'business_id' => $business ? $business->id : null,
             'remember_token' => $member->remember_token,
             'is_super' => $member->businessMember ? $member->businessMember->is_super : null,
+            'is_payroll_enable' => $business->is_payroll_enable,
             'access' => [
                 'support' => $business ? (in_array($business->id, config('business.WHITELISTED_BUSINESS_IDS')) && $access_control->hasAccess('support.rw') ? 1 : 0) : 0,
                 'expense' => $business ? (in_array($business->id, config('business.WHITELISTED_BUSINESS_IDS')) && $access_control->hasAccess('expense.rw') ? 1 : 0) : 0,
