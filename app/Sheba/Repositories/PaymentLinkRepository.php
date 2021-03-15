ac632e96faf2d5b8394a4a974d749c30ebe679e6
Arafat <syedyeaminarafat@gmail.com>
2021-03-15 11:58:41 +0600

    ⬆️ Payment link url updated
    Type(s): UPDATE

--- app/Sheba/Repositories/PaymentLinkRepository.php
+++ app/Sheba/Repositories/PaymentLinkRepository.php
@@ -2,8 +2,10 @@
 
 use App\Models\Payable;
 use App\Models\Payment;
+use App\Transformers\PaymentLinkTransactionDetailsTransformer;
 use GuzzleHttp\Exception\GuzzleException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Sheba\Payment\Exceptions\PayableNotFound;
 use Sheba\PaymentLink\PaymentLinkClient;
 use Sheba\PaymentLink\PaymentLinkTransformer;
@@ -16,6 +18,10 @@ class PaymentLinkRepository extends BaseRepository implements PaymentLinkReposit
     private $paymentLinkClient;
     private $paymentLinkTransformer;
     private $urlTransformer;
+    /**
+     * @var PaymentLinkTransactionDetailsTransformer
+     */
+    private $paymentLinkTransactionDetailTransformer;
 
     /**
      * PaymentLinkRepository constructor.
@@ -28,6 +34,7 @@ class PaymentLinkRepository extends BaseRepository implements PaymentLinkReposit
         $this->paymentLinkClient = new PaymentLinkClient();
         $this->paymentLinkTransformer = $paymentLinkTransformer;
         $this->urlTransformer = $urlTransformer;
+        $this->paymentLinkTransactionDetailTransformer = new PaymentLinkTransactionDetailsTransformer();
 
     }
 
@@ -131,4 +138,58 @@ class PaymentLinkRepository extends BaseRepository implements PaymentLinkReposit
         $response = json_decode(json_encode($this->paymentLinkClient->createShortUrl($url)));
         return $response && $response->code == 200 ? $this->urlTransformer->setResponse($response->url) : null;
     }
+
+    public function getPaymentList(Request $request)
+    {
+        $filter = $search_value = $request->transaction_search;
+        $payment_links_list = $this->getPartnerPaymentLinkList($request);
+        if (is_array($payment_links_list) && count($payment_links_list) > 0) {
+            $transactionList = [];
+            $linkIds = array_column($payment_links_list, 'linkId');
+            $links = [];
+            array_walk($payment_links_list, function ($val, $key) use (&$links) {
+                $links[$val['linkId']] = $val;
+            });
+            $transactionQuery = DB::table('payments as pa')
+                ->select('pa.id as payment_id', 'pb.id as payable_id', 'customerProfile.name', 'pb.type_id')
+                ->join('payables as pb', 'pa.payable_id', '=', 'pb.id')
+                ->leftJoin('customers as cus', function ($join) {
+                    $join->on('pb.user_id', '=', 'cus.id')
+                        ->on('pb.user_type', '=', DB::raw('"App\\\Models\\\Customer"'));
+                })
+                ->leftJoin('profiles as customerProfile', function ($join) {
+                    $join->on('cus.profile_id', '=', 'customerProfile.id')
+                        ->on('pb.user_type', '=', DB::raw('"App\\\Models\\\Customer"'));
+                })
+                ->whereIn('type_id', $linkIds)
+                ->where('type', 'payment_link');
+
+            $transactions = $transactionQuery->get();
+
+            foreach ($transactions as $transaction) {
+                $payment = $this->payment($transaction->payment_id);
+                $transactionFormatted = $this->paymentLinkTransactionDetailTransformer->transform($payment, $links[$transaction->type_id]);
+                array_push($transactionList, $transactionFormatted);
+            }
+
+
+            if ($filter) {
+                $transactionList = array_filter($transactionList, function ($item) use ($filter) {
+                    return preg_match("/$filter/i", (string)$item['link_id']) || preg_match("/$filter/i", $item['customer_name']);
+                });
+            }
+
+            usort($transactionList, function ($a, $b) {
+                return $b['payment_id'] - $a['payment_id'];
+            });
+
+            $limit = $request->transaction_limit;
+            $offset = $request->transaction_offset;
+            $links = collect($transactionList)->slice($offset)->take($limit);
+            return array_values($links->toArray());
+
+        }
+        return null;
+    }
+
 }
