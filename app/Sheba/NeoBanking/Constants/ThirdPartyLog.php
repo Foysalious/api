bc50970d87a6598c9e62409eeda279044b7030d7
Rakib <rakib.rumi09@gmail.com>
2021-03-16 16:27:35 +0600

    ➕ added constant values
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSMEBE-2043

--- app/Sheba/NeoBanking/Constants/ThirdPartyLog.php
+++ app/Sheba/NeoBanking/Constants/ThirdPartyLog.php
@@ -5,11 +5,21 @@ namespace App\Sheba\NeoBanking\Constants;
 
 final class ThirdPartyLog
 {
-    public const DATA_TYPE_LIST = [
-       'request', 'response'
+    //data type list
+    const REQUEST_DATA_TYPE = 'request';
+    const RESPONSE_DATA_TYPE = 'response';
+
+    //from list
+    const PBL_REQUEST = 'pbl_request';
+    const PBL_RESPONSE = 'pbl_response';
+
+    const DATA_TYPE_LIST = [
+        self::RESPONSE_DATA_TYPE,
+        self::REQUEST_DATA_TYPE
     ];
 
-    public const THIRD_PARTY_FROM_LIST = [
-        'pbl_request', 'pbl_response'
+    const THIRD_PARTY_FROM_LIST = [
+        self::PBL_REQUEST,
+        self::PBL_RESPONSE
     ];
 }
