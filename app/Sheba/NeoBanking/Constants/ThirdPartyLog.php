2d74590c3c1c12ef4134308f6ee7eafc5736906e
Rakib <rakib.rumi09@gmail.com>
2021-03-16 16:14:00 +0600

    ➕ Added repository for neo banking third party log
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSMEBE-2043

---
+++ app/Sheba/NeoBanking/Constants/ThirdPartyLog.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Sheba\NeoBanking\Constants;
+
+
+final class ThirdPartyLog
+{
+    public const DATA_TYPE_LIST = [
+       'request', 'response'
+    ];
+
+    public const THIRD_PARTY_FROM_LIST = [
+        'pbl_request', 'pbl_response'
+    ];
+}
---
+++ app/Sheba/NeoBanking/Repositories/NeoBankingThirdPartyLogRepository.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Sheba\NeoBanking\Repositories;
+
+use App\Sheba\NeoBanking\Constants\ThirdPartyLog;
+use Sheba\Dal\NeoBankingThirdPartyLog\Contract as Repository;
+use Sheba\ModificationFields;
+
+class NeoBankingThirdPartyLogRepository
+{
+    use ModificationFields;
+
+    /** @var Repository $repository */
+    private $repository;
+
+    private $data, $dataType, $from;
+
+    public function __construct(Repository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function setFrom($from)
+    {
+        if (!in_array($from,ThirdPartyLog::THIRD_PARTY_FROM_LIST)) {
+            throw new \Exception('Sorry! Invalid Third Party Name.');
+        }
+
+        $this->from = $from;
+        return $this;
+    }
+
+    public function setDataType($dataType)
+    {
+        if (!in_array($dataType,ThirdPartyLog::DATA_TYPE_LIST)) {
+           throw new \Exception('Sorry! Invalid Data Type.');
+        }
+        $this->dataType = $dataType;
+        return $this;
+    }
+
+    public function setData($data)
+    {
+        $this->data = $data;
+        return $this;
+    }
+
+    public function store()
+    {
+        return $this->repository->create(
+            $this->withCreateModificationField(self::getData($this->data, $this->dataType, $this->from))
+        );
+    }
+
+    private static function getData($data, $dataType, $from)
+    {
+        return [
+            "data" => $data,
+            "data_type" => $dataType,
+            "from" => $from,
+        ];
+    }
+}
