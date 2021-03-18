43d6c5d73b05653f284c64e6ca802cf02d89b4e6
Ebne Faiz Al-Amin <faiz@iamsheba.xyz>
2021-03-18 19:07:41 +0600

    ➕ Added reward-history controller
    Type(s): ADD

--- app/Http/Controllers/Affiliate/BondhuRewardController.php
+++ app/Http/Controllers/Affiliate/BondhuRewardController.php
@@ -6,7 +6,11 @@ namespace App\Http\Controllers\Affiliate;
 
 use App\Http\Controllers\Controller;
 use App\Models\Affiliate;
+use App\Models\Reward;
+use App\Models\RewardCampaign;
+use Illuminate\Support\Facades\Artisan;
 use Sheba\Dal\RewardAffiliates\Contract as RewardAffiliatesRepo;
+use Sheba\Reward\CompletedCampaignHandler;
 
 class BondhuRewardController extends Controller
 {
@@ -17,10 +21,21 @@ class BondhuRewardController extends Controller
         $this->rewardAffiliateRepo = $rewardAffiliateRepo;
     }
 
-    public function rewardHistory($affiliate)
+    public function rewardHistory($affiliate_id)
     {
-//        $affiliate_model = Affiliate::where('id', $affiliate)->get();
-        $rewards = $this->rewardAffiliateRepo->where('affiliate', $affiliate)->get();
-
+        $affiliate_rewards = $this->getAffiliateRewards($affiliate_id);
+        return $affiliate_rewards;
     }
+
+    private function getAffiliateRewards($affiliate_id){
+
+        $rewards_of_affiliate = $this->rewardAffiliateRepo->where('affiliate', $affiliate_id)->get();
+        $rewards = Reward::whereIn('id', $rewards_of_affiliate->pluck('reward') )->get();
+
+        foreach ( $rewards_of_affiliate as $each ) {
+            $each->reward_details = $rewards->where('id', $each->reward)->values();
+        }
+        return $rewards_of_affiliate;
+    }
+
 }
