82599be29ffef8e790d58934d255d9197b43eea6
Saiful <miajee@sheba.xyz>
2021-03-18 16:54:30 +0600

    Terminating Middleware Check

--- app/Http/Middleware/B2B/TerminatingMiddleware.php
+++ app/Http/Middleware/B2B/TerminatingMiddleware.php
@@ -1,8 +1,10 @@
 <?php namespace App\Http\Middleware\B2B;
 
+use Carbon\Carbon;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Closure;
+use Illuminate\Support\Facades\Redis;
 
 class TerminatingMiddleware
 {
@@ -27,6 +29,7 @@ class TerminatingMiddleware
      */
     public function terminate($request, $response)
     {
-        // ...
+        $key = 'BusinessRequestResponse::'.Carbon::now()->timestamp;
+        Redis::set($key, json_encode('Request_Uri:'. $request->getUri(), 'Request_All:'. json_encode($request->all()), '_Response:' .json_encode($response)));
     }
 }
