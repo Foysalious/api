7c6b3929c831c04568095a96636939c4797bd394
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2021-03-18 11:25:45 +0600

    ➕ And sign instead of amp; in frontend for department
    Type(s): ADD
    
    Issue(s):
    - JIRA: B2BBE-906

--- app/Http/Middleware/XSS.php
+++ app/Http/Middleware/XSS.php
@@ -20,7 +20,9 @@ class XSS
         'v2/partners/*/pos/services',
         'v2/partners/*/pos/services/*',
         'v2/partners/*/webstore-settings',
-        'v2/partners/*/pos/products/orders'
+        'v2/partners/*/pos/products/orders',
+        'v2/businesses/*/departments',
+        'v2/businesses/*/departments/*'
     ];
 
     /**
