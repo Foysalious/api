f432d5a7286838c73ed15bc099f32dcfa34ec1dd
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2021-04-01 14:02:14 +0600

    typo fix
    Type(s): UPDATE

--- app/Transformers/Business/LeaveRequestDetailsTransformer.php
+++ app/Transformers/Business/LeaveRequestDetailsTransformer.php
@@ -73,7 +73,6 @@ class LeaveRequestDetailsTransformer extends TransformerAbstract
             'created_at' => $approval_request->created_at->format('M d, Y'),
             'super_admin_section_show' => $this->isLeaveCancelled($requestable),
             'super_admin_action_reason' => (new ApproverWithReason())->getRejectReason($approval_request, self::SUPER_ADMIN, null),
-            'show_approve_reject_buttons' => $approval_request->approver_id == $requestable->business_member_id ? $this->isLeaveApprovedOrRejected($requestable) : 0,
             'show_approve_reject_buttons' => $this->isLeaveApprovedOrRejected($requestable),
             'show_normal_approver_approve_reject_buttons' => $approval_request->approver_id == $requestable->business_member_id ? 1 : 0,
             'leave' => [
