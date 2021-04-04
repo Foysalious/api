978c53bbcaabd6dbb249886b1c61e4fb50c2a958
Fariha Nuzhat <fariha@iamsheba.xyz>
2021-04-04 19:44:52 +0600

    ➕ New API for spro info call submit request
    Type(s): ADD
    
    Issue(s):
    - JIRA: MPBE-1892

--- app/Http/Controllers/Resource/InfoCallController.php
+++ app/Http/Controllers/Resource/InfoCallController.php
@@ -3,9 +3,22 @@
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\InfoCallCreateRequest;
+use Sheba\Dal\InfoCall\InfoCallRepository;
+use Sheba\ModificationFields;
+use Sheba\OAuth2\AuthUser;
 
 class InfoCallController extends Controller
 {
+    use ModificationFields;
+
+    /** @var InfoCallRepository  */
+    private $infoCallRepository;
+
+    public function __construct(InfoCallRepository $repo)
+    {
+        $this->infoCallRepository = $repo;
+    }
+
     public function index()
     {
 
@@ -13,7 +26,22 @@ class InfoCallController extends Controller
 
     public function store(InfoCallCreateRequest $request)
     {
-        dd($request->service_name);
+        /** @var AuthUser $auth_user */
+        $auth_user = $request->auth_user;
+        $resource = $auth_user->getResource();
+        $this->setModifier($resource);
+        $data = [
+            'priority' => 'High',
+            'flag' => 'Red',
+            'info_category' => 'not_available',
+            'customer_mobile' => $request->mobile,
+            'location_id' => $request->location_id,
+            'service_id' => $request->service_id,
+            'service_name' => $request->service_name,
+            'created_by_type'=> get_class($resource)
+        ];
+        $info_call = $this->infoCallRepository->create($data);
+        return api_response($request, $info_call, 200, ['info_call' => $info_call]);
     }
 
     public function show($id)
