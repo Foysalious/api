45586c24535fc3fd18e93ad5d79447e38af48ffe
Nadim Shakil <nadim@iamsheba.xyz>
2021-04-07 18:56:04 +0600

    ➕ Gateway class initial design
    Type(s): ADD
    
    Issue(s):
    - JIRA: BONBE-282

---
+++ app/Sheba/TopUp/Gateway/BdRecharge.php
@@ -0,0 +1,54 @@
+<?php
+
+
+namespace Sheba\TopUp\Gateway;
+
+
+use App\Models\TopUpOrder;
+use App\Sheba\TopUp\Vendor\BdRechargeClient;
+use Exception;
+use Sheba\Dal\TopupOrder\Statuses;
+use Sheba\TopUp\Exception\GatewayTimeout;
+use Sheba\TopUp\Vendor\Response\BdRechargeResponse;
+use Sheba\TopUp\Vendor\Response\TopUpResponse;
+
+class BdRecharge implements Gateway
+{
+    CONST SHEBA_COMMISSION = 0.0;
+    private $client;
+
+    public function __construct(BdRechargeClient $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function recharge(TopUpOrder $topup_order): TopUpResponse
+    {
+        $response = new BdRechargeResponse();
+        $response->setResponse($this->client->recharge($topup_order));
+        dd($response);
+    }
+
+    public function getInitialStatus()
+    {
+        return self::getInitialStatusStatically();
+    }
+
+    public static function getInitialStatusStatically()
+    {
+        return Statuses::PENDING;
+    }
+
+    public function getShebaCommission()
+    {
+        return self::SHEBA_COMMISSION;
+    }
+
+    public function getName()
+    {
+        return Names::BD_RECHARGE;
+    }
+}
---
+++ app/Sheba/TopUp/Vendor/Response/BdRechargeResponse.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace Sheba\TopUp\Vendor\Response;
+
+
+class BdRechargeResponse extends TopUpResponse
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function hasSuccess(): bool
+    {
+        // TODO: Implement hasSuccess() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getTransactionId()
+    {
+        // TODO: Implement getTransactionId() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getErrorCode()
+    {
+        // TODO: Implement getErrorCode() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getErrorMessage()
+    {
+        // TODO: Implement getErrorMessage() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function resolveTopUpSuccessStatus()
+    {
+        // TODO: Implement resolveTopUpSuccessStatus() method.
+    }
+}
