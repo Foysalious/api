0386eb37109a10008105bb3a1549c6e6494943e0
foysalious <foysal@iamsheba.xyz>
2021-04-13 21:06:39 +0600

    ➕ Update
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-517

--- app/Models/CanTopUpUpdateLog.php
+++ app/Models/CanTopUpUpdateLog.php
@@ -1,10 +1,18 @@
-<?php
+<?php namespace App\Models;
 
 
-namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
 
-
-class CanTopUpUpdateLog
+class CanTopUpUpdateLog extends Model
 {
+    protected $guarded = ['id'];
+    public $timestamps = false;
+    protected $dates = ['created_at'];
+    protected $table = 'can_top_up_update_logs';
 
+
+    public function partner()
+    {
+        return $this->belongsTo(Partner::class);
+    }
 }
