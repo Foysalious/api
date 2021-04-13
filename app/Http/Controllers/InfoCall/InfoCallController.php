745f791fd9a638c17f49fa8bcb9591b53a1adfe4
Fariha Nuzhat <fariha@iamsheba.xyz>
2021-04-13 09:57:58 +0600

    ⬆️ Store function in InfoCall/InfoCallController
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: MPBE-1956

--- app/Http/Controllers/InfoCall/InfoCallController.php
+++ app/Http/Controllers/InfoCall/InfoCallController.php
@@ -2,24 +2,27 @@
 
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
+use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Validation\ValidationException;
 use Sheba\Dal\InfoCall\Statuses;
 use Sheba\ModificationFields;
 use Illuminate\Http\Request;
+
 class InfoCallController extends Controller
 {
     use ModificationFields;
 
     public function store($customer, Request $request)
     {
+        $customer = Customer::findOrFail($request->customer);
         try {
+            $this->setModifier($customer);
             $this->validate($request, [
                 'service_name' => 'required|string',
                 'mobile' => 'required|string|mobile:bd',
                 'location_id' => 'numeric',
             ]);
-            $customer = Customer::find($request->customer);
             $profile = $customer->profile;
             $data = [
                 'service_name' => $request->service_name,
@@ -33,6 +36,7 @@ class InfoCallController extends Controller
                 'intended_closing_date' => Carbon::now()->addMinutes(30)
             ];
             $info_call = $customer->infoCalls()->create($this->withCreateModificationField($data));
+            $this->sendNotificationToSD($info_call);
             return api_response($request, 1, 200);
         } catch (ValidationException $e) {
             $message = getValidationErrorMessage($e->validator->errors()->all());
@@ -42,5 +46,20 @@ class InfoCallController extends Controller
             return api_response($request, $e, 500);
         }
     }
+
+    public function sendNotificationToSD($info_call)
+    {
+        try {
+            $sd_but_not_crm = User::where('department_id', 5)->where('is_cm', 0)->pluck('id');
+            notify()->users($sd_but_not_crm)->send([
+                "title" => 'New Info Call Created by Customer',
+                'link' => config('sheba.admin_url') . '/info-call/' . $info_call->id,
+                "type" => notificationType('Info')
+            ]);
+        } catch (\Throwable $e) {
+            app('sentry')->captureException($e);
+        }
+    }
+
 
 }
