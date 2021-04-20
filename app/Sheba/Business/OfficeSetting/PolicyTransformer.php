a8af0c0b879baccfec17b5e30d4b8ad3de5a0b0c
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2021-04-21 00:42:37 +0600

    Grace Policy Rules
    Type(s): UPDATE

--- app/Sheba/Business/OfficeSetting/PolicyTransformer.php
+++ app/Sheba/Business/OfficeSetting/PolicyTransformer.php
@@ -4,15 +4,15 @@ use League\Fractal\TransformerAbstract;
 
 class PolicyTransformer extends TransformerAbstract
 {
-    public function transform($grace_policy)
+    public function transform($policy)
     {
         return [
-            'id' => $grace_policy->id,
-            'from_days' => $grace_policy->from_days,
-            'to_days' => $grace_policy->to_days,
-            'action_name' => $grace_policy->action,
-            'penalty_type' => $grace_policy->penalty_type,
-            'penalty_amount' => $grace_policy->penalty_amount
+            'id' => $policy->id,
+            'from_days' => $policy->from_days,
+            'to_days' => $policy->to_days,
+            'action_name' => $policy->action,
+            'penalty_type' => $policy->penalty_type,
+            'penalty_amount' => $policy->penalty_amount
         ];
     }
 
