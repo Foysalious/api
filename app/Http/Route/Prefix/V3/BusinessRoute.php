421d34feab53f764a8a1901faa38f34650a34008
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2021-04-21 15:26:29 +0600

    Office Settings Grace Policy Store
    Type(s): ADD
    
    Issue(s):
    - JIRA: B2BBE-942

--- app/Http/Route/Prefix/V3/BusinessRoute.php
+++ app/Http/Route/Prefix/V3/BusinessRoute.php
@@ -29,6 +29,7 @@ class BusinessRoute
                     $api->get('/attendance', 'B2b\AttendanceController@getAttendanceOfficeSettings');
                     $api->post('/attendance', 'B2b\AttendanceController@updateAttendanceOfficeSettings');
                     $api->get('/grace-policy', 'B2b\AttendanceController@getGracePolicy');
+                    $api->post('/grace-policy', 'B2b\AttendanceController@createGracePolicy');
                 });
             });
         });
