ddc5ae477af917f88de0d7e07de5195a08a2b08c
rakib-09 <rakib.rumi09@gmail.com>
2021-04-28 15:38:00 +0600

    ➕ Completed api for account create update delete Type(s): ADD Issue(s): - JIRA: https://blabol.atlassian.net/browse/MSMEBE-2308

--- app/Sheba/AccountingEntry/Repository/UserAccountRepository.php
+++ app/Sheba/AccountingEntry/Repository/UserAccountRepository.php
@@ -9,11 +9,92 @@ use Sheba\AccountingEntry\Repository\AccountingEntryClient;
 class UserAccountRepository extends BaseRepository
 {
     private $api;
+    private $name;
+    private $name_bn;
+    private $root_account;
+    private $account_type;
+    private $icon;
+    private $editable = true;
+    private $deletable = true;
+    private $visible = true;
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @param mixed $name_bn
+     */
+    public function setNameBn($name_bn)
+    {
+        $this->name_bn = $name_bn;
+        return $this;
+    }
+
+    /**
+     * @param mixed $root_account
+     */
+    public function setRootAccount($root_account)
+    {
+        $this->root_account = $root_account;
+        return $this;
+    }
+
+    /**
+     * @param mixed $account_type
+     */
+    public function setAccountType($account_type)
+    {
+        $this->account_type = $account_type;
+        return $this;
+    }
+
+    /**
+     * @param mixed $icon
+     */
+    public function setIcon($icon)
+    {
+        $this->icon = $icon;
+        return $this;
+    }
+
+    /**
+     * @param mixed $editable
+     */
+    public function setEditable($editable)
+    {
+        $this->editable = $editable;
+        return $this;
+    }
+
+    /**
+     * @param mixed $deletable
+     */
+    public function setDeletable($deletable)
+    {
+        $this->deletable = $deletable;
+        return $this;
+    }
+
+    /**
+     * @param mixed $visible
+     */
+    public function setVisible($visible)
+    {
+        $this->visible = $visible;
+        return $this;
+    }
+
 
     public function __construct(AccountingEntryClient $client)
     {
         parent::__construct($client);
-        $this->api = 'api/accounts';
+        $this->api = 'api/accounts/';
     }
 
     public function getAccountType($userId, array $request = [], $userType = UserType::PARTNER)
@@ -24,10 +105,10 @@ class UserAccountRepository extends BaseRepository
         }
         try {
             return $this->client->setUserType($userType)->setUserId($userId)->get(
-                $this->api . '/account-types' . $query
+                $this->api . 'account-types' . $query
             );
         } catch (AccountingEntryServerError $e) {
-            return $e->getMessage();
+            throw new AccountingEntryServerError($e->getMessage(), $e->getCode());
         }
     }
 
@@ -45,18 +126,62 @@ class UserAccountRepository extends BaseRepository
                 $this->api . $query
             );
         } catch (AccountingEntryServerError $e) {
-            return $e->getMessage();
+            throw new AccountingEntryServerError($e->getMessage(), $e->getCode());
         }
     }
 
     public function getCashAccounts($userId, $userType = UserType::PARTNER)
     {
         try {
-            return $this->client->setUserType($userType)->setUserId($userId)->get(
-                $this->api . '/cash-accounts'
-            );
+            return $this->client->setUserType($userType)->setUserId($userId)
+                ->get($this->api . 'cash-accounts');
         } catch (AccountingEntryServerError $e) {
-            return $e->getMessage();
+            throw new AccountingEntryServerError($e->getMessage(), $e->getCode());
         }
+    }
+
+    public function storeAccount($userId, $userType = UserType::PARTNER)
+    {
+        try {
+            $payload = $this->makeData();
+            return $this->client->setUserType($userType)->setUserId($userId)
+                ->post($this->api, $payload);
+        } catch (AccountingEntryServerError $e) {
+            throw new AccountingEntryServerError($e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function updateAccount($accountId, $userId, $userType = UserType::PARTNER)
+    {
+        try {
+            $payload = $this->makeData();
+            return $this->client->setUserType($userType)->setUserId($userId)
+                ->put($this->api . $accountId . '/', $payload);
+        } catch (AccountingEntryServerError $e) {
+            throw new AccountingEntryServerError($e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function deleteAccount($accountId, $userId, $userType = UserType::PARTNER)
+    {
+        try {
+            return $this->client->setUserType($userType)->setUserId($userId)
+                ->delete($this->api . $accountId . '/');
+        } catch (AccountingEntryServerError $e) {
+            throw new AccountingEntryServerError($e->getMessage(), $e->getCode());
+        }
+    }
+
+    private function makeData()
+    {
+        $data['name'] = $this->name;
+        $data['name_bn'] = $this->name_bn;
+        $data['root_account'] = $this->root_account;
+        $data['account_type'] = $this->account_type;
+        $data['icon'] = $this->icon;
+        $data['editable'] = $this->editable;
+        $data['deletable'] = $this->deletable;
+        $data['visible'] = $this->visible;
+        return $data;
     }
 }
