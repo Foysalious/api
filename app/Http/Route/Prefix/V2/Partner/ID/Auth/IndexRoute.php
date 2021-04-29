e96dd5724b95f1392cf42f37593d0871f9b442df
shovan <shovan.sheba@gmail.com>
2021-04-29 10:33:10 +0600

    ➕ Delivery initail commit
    Type(s): ADD

--- app/Http/Route/Prefix/V2/Partner/ID/Auth/IndexRoute.php
+++ app/Http/Route/Prefix/V2/Partner/ID/Auth/IndexRoute.php
@@ -290,6 +290,7 @@ class IndexRoute
             (new BidRoute())->set($api);
             (new DueTrackerRoute())->set($api);
             (new ReferralRoute())->individuals($api);
+            (new DeliveryRoute())->set($api);
         });
     }
 }
