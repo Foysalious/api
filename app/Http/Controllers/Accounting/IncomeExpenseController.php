3de9bbb177aad1ee5ad7cce103797ccbd2b8d41d
Raqibul Hasan <raqibul@sheba.xyz>
2021-04-29 13:52:24 +0600

    ⬆️ Expense entry with inventory
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSMEBE-2291

--- app/Http/Controllers/Accounting/IncomeExpenseController.php
+++ app/Http/Controllers/Accounting/IncomeExpenseController.php
@@ -42,7 +42,7 @@ class IncomeExpenseController extends Controller
                 'amount' => 'required|numeric',
                 'from_account_key' => 'required',
                 'to_account_key' => 'required',
-                'date' => 'required|date_format:Y-m-d',
+                'date' => 'required|date_format:Y-m-d H:i:s',
                 'amount_cleared' => 'sometimes|required|numeric',
                 'customer_id' => 'required_with:amount_cleared'
             ]);
