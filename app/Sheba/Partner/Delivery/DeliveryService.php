3c1921d9ad833ef9e4a5e4712e6f82dd0c990e8e
foysalious <foysal@iamsheba.xyz>
2021-04-29 16:25:01 +0600

    ➕ order information customer and partner order information
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-613

--- app/Sheba/Partner/Delivery/DeliveryService.php
+++ app/Sheba/Partner/Delivery/DeliveryService.php
@@ -1,6 +1,8 @@
 <?php namespace App\Sheba\Partner\Delivery;
 
 
+use App\Exceptions\DoNotReportException;
+use App\Http\Requests\Request;
 use App\Models\Partner;
 use App\Models\PosOrder;
 
@@ -20,6 +22,22 @@ class DeliveryService
         return $this;
     }
 
+    /**
+     * @return mixed
+     */
+    public function vendorList()
+    {
+
+
+        $vendor_list = [];
+        $all_vendor_list = config('pos_delivery.vendor_list');
+        foreach ($all_vendor_list as $key => $list) {
+            array_push($vendor_list, $list);
+        }
+        return $vendor_list;
+
+    }
+
     public function getRegistrationInfo()
     {
         return [
@@ -29,27 +47,47 @@ class DeliveryService
             'mobile' => $this->partner->getContactNumber(),
             'email' => $this->partner->getContactEmail(),
             'business_type' => $this->partner->business_type,
-            'address' => $this->partner->address,
+            'address' => [
+                'full_address' => $this->partner->deliveryInformation->address,
+                'thana' => $this->partner->deliveryInformation->thana,
+                'zilla' => $this->partner->deliveryInformation->district
+            ],
         ];
     }
 
     public function getOrderInfo($order_id)
     {
-        $order= PosOrder::where('id',$order_id)->partner_id->get();
-        if ($this->partner->id != $order){
-            return response(' Order Does not exist', 400);
+        $order = PosOrder::where('id', $order_id)->with('postCustomer', 'posCustomer.profile')->first();
+        if ($this->partner->id != $order->partner_id) {
+            throw new DoNotReportException("Order does not belongs to this partner", 400);
         }
-        else{
-            return [
+        return [
+            'partner_pickup_information' => [
                 'merchant_name' => $this->partner->name,
                 'contact_person' => $this->partner->getContactPerson(),
                 'mobile' => $this->partner->getContactNumber(),
                 'email' => $this->partner->getContactEmail(),
                 'business_type' => $this->partner->business_type,
-                'address' => $this->partner->address,
-            ];
-        }
+                'address' => [
+                    'full_address' => $this->partner->deliveryInformation->address,
+                    'thana' => $this->partner->deliveryInformation->thana,
+                    'zilla' => $this->partner->deliveryInformation->district
+                ],
+            ],
+            'customer-delivery_information' => [
+                'name' => $order->posCustomer->profile->name,
+                'number' => $order->posCustomer->profile->mobile,
+                'address' => [
+                    'full_address' => $order->address,
+                    'thana' => $order->delivery_thana,
+                    'zilla' => $order->delivery_zilla
+                ],
+                'payment_method' => 'bkash',
+                'cash_amount' => 5680
 
+            ]
+        ];
     }
+
 
 }
