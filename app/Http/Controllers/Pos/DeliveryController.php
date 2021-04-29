648d0ebbbd45ed0cced0f8a23b65242bc75bf5fa
shovan <shovan.sheba@gmail.com>
2021-04-29 10:33:29 +0600

    ➕ Delivery initail commit
    Type(s): ADD

---
+++ app/Http/Controllers/Pos/DeliveryController.php
@@ -0,0 +1,14 @@
+<?php namespace App\Http\Controllers\Pos;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class DeliveryController extends Controller
+{
+    public function getInfoForRegistration(Request $request)
+    {
+
+    }
+
+}
