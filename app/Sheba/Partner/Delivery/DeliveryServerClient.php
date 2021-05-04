4be5c312760d0a72a2330296e3572ab04cac0df6
foysalious <foysal@iamsheba.xyz>
2021-05-04 10:23:05 +0600

    ➕ testing of https request
    Type(s): Update
    
    Issue(s):
    - https://blabol.atlassian.net/browse/MSBE-613

--- app/Sheba/Partner/Delivery/DeliveryServerClient.php
+++ app/Sheba/Partner/Delivery/DeliveryServerClient.php
@@ -27,10 +27,11 @@ class DeliveryServerClient
             return json_decode($this->client->request(strtoupper($method), $this->makeUrl($uri), $this->getOptions($data, $multipart))->getBody()->getContents(), true);
         }
         catch (GuzzleException $e) {
+            dd($e);
             $res = $e->getResponse();
             $http_code = $res->getStatusCode();
             $message = $res->getBody()->getContents();
-            dd($message);
+
             if ($http_code > 399 && $http_code < 500) throw new DeliveryServiceServerError($message, $http_code);
             throw new DeliveryServiceServerError($e->getMessage(), $http_code);
         }
