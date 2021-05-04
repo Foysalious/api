2e2f1276e6099f7a9685f80e7fe6ec7b381a01e6
foysalious <foysal@iamsheba.xyz>
2021-05-04 10:58:13 +0600

    ➕ District and Upazillas remember token removed
    Type(s): Update
    
    Issue(s):
    - https://blabol.atlassian.net/browse/MSBE-613

--- app/Sheba/Partner/Delivery/DeliveryServerClient.php
+++ app/Sheba/Partner/Delivery/DeliveryServerClient.php
@@ -12,7 +12,7 @@ class DeliveryServerClient
     public function __construct(Client $client)
     {
         $this->client = $client;
-        $this->baseUrl = rtrim(config('pos_delivery.api_url'),'/');
+        $this->baseUrl = rtrim(config('pos_delivery.api_url'), '/');
     }
 
     public function get($uri)
@@ -25,20 +25,20 @@ class DeliveryServerClient
         try {
 //            dd($this->makeUrl($uri));
             return json_decode($this->client->request(strtoupper($method), $this->makeUrl($uri), $this->getOptions($data, $multipart))->getBody()->getContents(), true);
-        }
-        catch (GuzzleException $e) {
+        } catch (GuzzleException $e) {
             $res = $e->getResponse();
             $http_code = $res->getStatusCode();
             $message = $res->getBody()->getContents();
-            dd($message);
             if ($http_code > 399 && $http_code < 500) throw new DeliveryServiceServerError($message, $http_code);
             throw new DeliveryServiceServerError($e->getMessage(), $http_code);
         }
     }
+
     private function makeUrl($uri)
     {
         return $this->baseUrl . "/" . $uri;
     }
+
     private function getOptions($data = null, $multipart = false)
     {
         $options['headers'] = [
@@ -53,6 +53,7 @@ class DeliveryServerClient
         }
         return $options;
     }
+
     public function post($uri, $data, $multipart = false)
     {
         return $this->call('post', $uri, $data, $multipart);
