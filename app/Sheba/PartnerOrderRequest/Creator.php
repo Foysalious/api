e477797ebf3dab86180849be44b2d6fc9e6311a9
tonmoy <shtonmoy007@gmail.com>
2021-05-05 17:58:15 +0600

    ⬆️  TEST NOTIFICATION
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: MSMEBE-2370

--- app/Sheba/PartnerOrderRequest/Creator.php
+++ app/Sheba/PartnerOrderRequest/Creator.php
@@ -8,11 +8,13 @@ use App\Sheba\Sms\BusinessType;
 use App\Sheba\Sms\FeatureType;
 use App\Transformers\CustomSerializer;
 use App\Transformers\Partner\OrderRequestTransformer;
+use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use League\Fractal\Manager;
 use League\Fractal\Resource\Item;
 use Sheba\Dal\PartnerOrderRequest\PartnerOrderRequest;
 use Sheba\Dal\PartnerOrderRequest\PartnerOrderRequestRepositoryInterface;
+use Sheba\Dal\PushNotificationMonitoring\PushNotificationMonitoringItemRepository;
 use Sheba\Partner\ImpressionManager;
 use Sheba\PartnerOrderRequest\Events\OrderRequestEvent;
 use Sheba\PartnerOrderRequest\Validators\CreateValidator;
@@ -37,15 +39,17 @@ class Creator
     private $partnerOrderRequestId;
     /** @var ImpressionManager ImpressionManager */
     private $impressionManager;
+    private $pushNotificationMonitoringRepo;
 
     public function __construct(PartnerOrderRequestRepositoryInterface $partner_order_request_repo,
                                 CreateValidator $create_validator,
-                                PushNotificationHandler $push_notification_handler, ImpressionManager $impressionManager)
+                                PushNotificationHandler $push_notification_handler, ImpressionManager $impressionManager, PushNotificationMonitoringItemRepository $pushNotificationMonitoringRepo)
     {
-        $this->partnerOrderRequestRepo = $partner_order_request_repo;
-        $this->createValidator         = $create_validator;
-        $this->pushNotificationHandler = $push_notification_handler;
-        $this->impressionManager       = $impressionManager;
+        $this->partnerOrderRequestRepo        = $partner_order_request_repo;
+        $this->createValidator                = $create_validator;
+        $this->pushNotificationHandler        = $push_notification_handler;
+        $this->impressionManager              = $impressionManager;
+        $this->pushNotificationMonitoringRepo = $pushNotificationMonitoringRepo;
     }
 
     /**
@@ -88,8 +92,8 @@ class Creator
             $this->sendOrderRequestPushNotificationToPartner($partner_id);
             $job = $this->partnerOrder->jobs->first();
             $this->impressionManager->setLocationId($this->partnerOrder->order->location_id)->setCategoryId($job->category_id)
-                ->setCustomerId($this->partnerOrder->order->customer_id)->setPortalName(request()->header('portal-name'))
-                ->setServices($this->getServices($job))->setImpressionToDeduct(10)->deduct([$partner_id]);
+                                    ->setCustomerId($this->partnerOrder->order->customer_id)->setPortalName(request()->header('portal-name'))
+                                    ->setServices($this->getServices($job))->setImpressionToDeduct(10)->deduct([$partner_id]);
         }
     }
 
@@ -102,19 +106,23 @@ class Creator
             /** @var Partner $partner */
             $partner = $this->partners->keyBy('id')->get($partner_id);
             $topic   = config('sheba.push_notification_topic_name.manager') . $partner->id;
-            $channel = config('sheba.push_notification_channel_name.manager');
+            $channel = config('sheba.push_notification_topic_name.manager_new');
             $sound   = config('sheba.push_notification_sound.manager');
 
-            $payload = [
-                "title"      => 'New Order',
-                "message"    => "প্রিয় $partner->name আপনার একটি নতুন অর্ডার রয়েছে, অনুগ্রহ করে ম্যানেজার অ্যাপ থেকে অর্ডারটি একসেপ্ট করুন",
-                "sound"      => "notification_sound",
-                "event_type" => 'PartnerOrder',
-                "event_id"   => $this->partnerOrderRequestId->id,
-                "link"       => "new_order",
-                "order"      => $this->getOrderRequestData()
+            $payload                               = [
+                "title"                      => 'New Order',
+                "message"                    => "প্রিয় $partner->name আপনার একটি নতুন অর্ডার রয়েছে, অনুগ্রহ করে ম্যানেজার অ্যাপ থেকে অর্ডারটি একসেপ্ট করুন",
+                "sound"                      => "notification_sound",
+                "event_type"                 => 'PartnerOrder',
+                "event_id"                   => $this->partnerOrderRequestId->id,
+                "link"                       => "new_order",
+                "order"                      => $this->getOrderRequestData(),
+                'notification_monitoring_id' => null,
+                'create_time'                => Carbon::now()->format('Y-m-d H:i:s')
             ];
-            $this->pushNotificationHandler->send($payload, $topic, $channel, $sound);
+            $notification                          = $this->pushNotificationMonitoringRepo->create(['partner_id' => $partner->id, 'sent_payload' => json_encode($payload)]);
+            $payload['notification_monitoring_id'] = $notification ? $notification->id : null;
+            $this->pushNotificationHandler->setPriority(1)->send($payload, $topic, $channel, $sound);
             event(new OrderRequestEvent(['user_type' => 'partner', 'user_id' => $partner->id, 'payload' => $payload]));
         } catch (Throwable $e) {
             logError($e);
@@ -127,12 +135,12 @@ class Creator
             $order_request = $this->partnerOrderRequestId;
             $manager       = new Manager();
             $manager->setSerializer(new CustomSerializer());
-            $resource = new Item($order_request, new OrderRequestTransformer());
-            $data= $manager->createData($resource)->toArray()['data'];
-            $data['time_left_to_accept_in_seconds']=config('partner.order.request_accept_time_limit_in_seconds');
+            $resource                               = new Item($order_request, new OrderRequestTransformer());
+            $data                                   = $manager->createData($resource)->toArray()['data'];
+            $data['time_left_to_accept_in_seconds'] = config('partner.order.request_accept_time_limit_in_seconds');
             return $data;
-        }catch (Throwable $e){
-            return [$this->partnerOrderRequestId,$e->getMessage(),$e->getLine()];
+        } catch (Throwable $e) {
+            return [$this->partnerOrderRequestId, $e->getMessage(), $e->getLine()];
         }
     }
 
@@ -142,11 +150,11 @@ class Creator
             /** @var Partner $partner */
             $partner = $this->partners->keyBy('id')->get($partner_id);
             (new SmsHandlerRepo('partner-order-request'))->setVendor('sslwireless')
-                ->setBusinessType(BusinessType::SMANAGER)
-                ->setFeatureType(FeatureType::PARTNER_SUBSCRIPTION_ORDER_REQUEST)
-                ->send($partner->getContactNumber(), [
-                'partner_name' => $partner->name
-            ]);
+                                                         ->setBusinessType(BusinessType::SMANAGER)
+                                                         ->setFeatureType(FeatureType::PARTNER_SUBSCRIPTION_ORDER_REQUEST)
+                                                         ->send($partner->getContactNumber(), [
+                                                             'partner_name' => $partner->name
+                                                         ]);
         } catch (Throwable $e) {
             logError($e);
         }
