07fc43e2c14cf8c7337f998b9b82979fb149a240
tonmoy <shtonmoy007@gmail.com>
2021-05-17 17:06:49 +0600

    ⬆️  PAYMENT LINK BY IDENTIFIER
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: MSMEBE-2389

--- app/Sheba/Repositories/PaymentLinkRepository.php
+++ app/Sheba/Repositories/PaymentLinkRepository.php
@@ -108,13 +108,16 @@ class PaymentLinkRepository extends BaseRepository implements PaymentLinkReposit
     public function find($linkId)
     {
 
-        $response = $this->isIdentifier($linkId)?$this->paymentLinkClient->getPaymentLinkByIdentifier($linkId):$this->paymentLinkClient->getPaymentLinkByLinkId($linkId);
+        $response = $this->isIdentifier($linkId) ? $this->paymentLinkClient->getPaymentLinkByIdentifier($linkId) : $this->paymentLinkClient->getPaymentLinkByLinkId($linkId);
         $response = json_decode(json_encode($response['links'][0]));
         return $response ? $this->paymentLinkTransformer->setResponse($response) : null;
     }
-    private function isIdentifier($linkId){
-        return preg_match('/^@.+/',(string)$linkId);
+
+    private function isIdentifier($linkId)
+    {
+        return preg_match('/^@.+/', (string)$linkId);
     }
+
     /**
      * @param        $id
      * @param string $type
