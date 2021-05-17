fdbf9ef605c8da8c4088b8dffa37b4f44885f673
Nadim Shakil <nadim@iamsheba.xyz>
2021-05-17 16:01:02 +0600

    Revert "➕ added address radio button for nid selfie"
    This reverts commit 2af17baf2fd583af558467a39e0681673200a0a1.

--- config/neo_banking.php
+++ config/neo_banking.php
@@ -1135,35 +1135,10 @@ return [
                 'title'       => 'জন্ম তারিখ'
             ],
             [
-                'field_type' => 'radioGroup',
-                'title'      => 'ঠিকানা',
-                'name'       => 'address',
-                'id'         => 'address',
-                'mandatory'  => true,
-                'views'      => [
-                    [
-                        'field_type' => 'radioButton',
-                        'name'       => 'permanent_address',
-                        'id'         => 'permanent_address',
-                        'title'      => 'স্থায়ী ঠিকানা',
-                        'mandatory'  => false,
-                        'value'      => 'permanent'
-                    ],
-                    [
-                        'field_type' => 'radioButton',
-                        'name'       => 'present_address',
-                        'id'         => 'female',
-                        'title'      => 'বর্তমান ঠিকানা',
-                        'mandatory'  => false,
-                        'value'      => 'present'
-                    ]
-                ]
-            ],
-            [
                 'field_type'  => 'text',
-                'name'        => 'address',
+                'name'        => 'pres_address',
                 'is_editable' => false,
-                'title'       => ''
+                'title'       => 'ঠিকানা'
             ]
         ]
     ],
