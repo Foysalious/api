7d5c9a5e47edcdb587424e4315e01c679e9914d5
Khairun Nahar <khairun@sheba.xyz>
2021-05-19 18:36:18 +0600

    PosOrderFactory Update

--- database/Factory/PosOrderFactory.php
+++ database/Factory/PosOrderFactory.php
@@ -8,26 +8,21 @@ class PosOrderFactory extends Factory
 
     protected function getModelClass()
     {
-        return PosOrder::class;// TODO: Implement getModelClass() method.
+        return PosOrder::class;
     }
 
     protected function getData()
     {
         return array_merge($this->commonSeeds, [
-            'partner_wise_order_id' =>1,
-            'partner_id' =>1,
-            'customer_id' =>1,
-            'payment_status'=> "Due",
-            'delivery_charge'=>60,
-            'delivery_vendor_name'=>"Paperfly",
-            'delivery_request_id'=>0,
-            'delivery_thana'=>"Banari Para",
-            'delivery_district'=>"Barisal",
-            'delivery_status'=>"Created",
+            'partner_wise_order_id'=>'1',
+            'partner_id'=>'1',
+            'payment_status'=>'Due',
+            'delivery_charge'=>'50',
             'address'=>$this->faker->address,
             'status'=>'Pending',
-            'sales_channel'=>'webstore',
-
+            'sales_channel'=>'pos'
         ]);
     }
 }
+
+  
