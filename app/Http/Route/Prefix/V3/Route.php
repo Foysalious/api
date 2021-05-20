885867acd50bf8cbde7b02bb8bdc4aa70cc7fd3a
Hasan Hafiz Pasha <mach.pasha@gmail.com>
2021-05-20 19:15:39 +0600

    🎨  CODE INDENTING
    Type(s): STYLE
    
    Issue(s):
    - JIRA: SHEBA-6968

--- app/Http/Route/Prefix/V3/Route.php
+++ app/Http/Route/Prefix/V3/Route.php
@@ -69,10 +69,7 @@ class Route
                 $api->get('/{id}', 'SubscriptionController@details');
             });
             $api->get('payment-gateways/{service_type}', 'PaymentGatewayController@getPaymentGateways');
-//            emi-info with static info
             $api->get('emi-info', 'ShebaController@getEmiInfoV3');
         });
-
-
     }
 }
