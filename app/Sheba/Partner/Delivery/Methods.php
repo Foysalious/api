220bfa52e0676944d183f4b1db46d970f3168dc6
shovan <shovan.sheba@gmail.com>
2021-05-24 18:20:08 +0600

    ⬆️ Dlivery vendor list api
    Type(s): UPDATE
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-769?atlOrigin=eyJpIjoiNjEwZTgzMjRhMWMzNDFjYmFhNmRiZTJkY2MyNTdkMTAiLCJwIjoiaiJ9

---
+++ app/Sheba/Partner/Delivery/Methods.php
@@ -0,0 +1,12 @@
+<?php namespace App\Sheba\Partner\Delivery;
+
+use Sheba\Helpers\ConstGetter;
+
+class Methods
+{
+    use ConstGetter;
+
+    const OWN_DELIVERY = 'own_delivery';
+    const PAPERFLY = 'paperfly';
+
+}
