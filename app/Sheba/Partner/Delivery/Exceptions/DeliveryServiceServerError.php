b1ebd58de6e6d6f959ad9660c0fb3cdef10f30d6
shovan <shovan.sheba@gmail.com>
2021-05-26 13:22:25 +0600

    ⬆️ Sdelivery  exception handle
    Type(s): UPDATE
    
    Issue(s):
    - JIRA:

--- app/Sheba/Partner/Delivery/Exceptions/DeliveryServiceServerError.php
+++ app/Sheba/Partner/Delivery/Exceptions/DeliveryServiceServerError.php
@@ -1,9 +1,10 @@
 <?php namespace App\Sheba\Partner\Delivery\Exceptions;
 
+use App\Exceptions\DoNotReportException;
 use App\Exceptions\HttpException;
 use Throwable;
 
-class DeliveryServiceServerError extends HttpException
+class DeliveryServiceServerError extends DoNotReportException
 {
     public function __construct($message = "", $code = 402, Throwable $previous = null)
     {
