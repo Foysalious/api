99fc3cc9ff8e17726f487aa93208ab8b6a295b1c
rakib-09 <rakib.rumi09@gmail.com>
2021-05-27 19:42:04 +0600

    added test log

--- app/Http/Controllers/Pos/OrderController.php
+++ app/Http/Controllers/Pos/OrderController.php
@@ -235,7 +235,7 @@ class OrderController extends Controller
             'order' => $order,
             'payment' => $link
         ]);
-        } catch (\Exception $e) {
+        } catch (\Throwable $e) {
             Log::error(["error in pos order", $e->getCode(), $e->getMessage(), $e->getTrace()]);
             return api_response($request, null, $e->getCode(), ['message' => $e->getMessage()]);
         }
