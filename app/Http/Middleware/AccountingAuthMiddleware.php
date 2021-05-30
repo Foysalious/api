11667af2d37ef10ce66eedfdb900592d7195dd23
Raqibul Hasan <raqibul@sheba.xyz>
2021-05-30 23:52:20 +0600

    ➕ accounting middleware
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSMEBE-2501

--- app/Http/Middleware/AccountingAuthMiddleware.php
+++ app/Http/Middleware/AccountingAuthMiddleware.php
@@ -1,12 +1,14 @@
 <?php namespace App\Http\Middleware;
 
+use Sheba\OAuth2\AuthUser;
 
 class AccountingAuthMiddleware extends AccessTokenMiddleware
 {
     public function setExtraDataToRequest($request)
     {
+        /** @var AuthUser $auth_user */
         $auth_user = $request->auth_user;
-        $user = $auth_user->getAvatar();
+        $user = $auth_user->getPartner();
         if (!$user) return;
         $type = strtolower(class_basename($user));
         $request->merge([$type => $user, 'type' => $type, 'user' => $user]);
