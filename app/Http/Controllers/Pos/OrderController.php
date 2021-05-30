2288813d4c4c84fef44161f0d531393c4af52c7e
rakib-09 <rakib.rumi09@gmail.com>
2021-05-30 14:11:28 +0600

    added test log

--- app/Http/Controllers/Pos/OrderController.php
+++ app/Http/Controllers/Pos/OrderController.php
@@ -134,7 +134,6 @@ class OrderController extends Controller
      */
     public function store($partner, Request $request, Creator $creator, ProfileCreator $profileCreator, PosCustomerCreator $posCustomerCreator, PartnerRepository $partnerRepository, PaymentLinkCreator $paymentLinkCreator)
     {
-        try {
         $this->validate($request, [
             'services' => 'required|string',
             'paid_amount' => 'sometimes|required|numeric',
@@ -235,9 +234,6 @@ class OrderController extends Controller
             'order' => $order,
             'payment' => $link
         ]);
-        } catch (\Throwable $e) {
-            return api_response($request, null, $e->getCode(), ['message' => $e->getMessage(), 'trace' => $e->getTrace()]);
-        }
     }
 
     private function sendCustomerEmail($order)
