aa1a412cf55cfa0602e4e06679a35cea632ea613
shovan <shovan.sheba@gmail.com>
2021-06-01 17:06:10 +0600

    ⬆️ Pos_delivery configuration
    Type(s): UPDATE
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-796?atlOrigin=eyJpIjoiN2JmMWVjZmVlMDYyNDg5ODhhODRkNTNjZWIzYTgwYmYiLCJwIjoiaiJ9

--- config/pos_delivery.php
+++ config/pos_delivery.php
@@ -2,14 +2,14 @@
 
 
 return [
-    'mobile_banking_providers' => ['bKash', 'rocket'],
+    'mobile_banking_providers' => ['bkash', 'rocket'],
     'payment_method_for_bank' => ['beftn'],
     'vendor_list' => [
         'own_delivery' => ['bn' => 'নিজস্ব পরিবহন ', 'en' => 'Own Delivery'], 'paperfly' => ['bn' => 'পেপারফ্লাই', 'en' => 'Paperfly']
     ],
     'api_url' => env('S_DELIVERY_API_URL'),
 
-    'payment_method' => ['beftn','bKash','rocket','nagad'],
+    'payment_method' => ['beftn','bkash','rocket','nagad'],
     'account_type'  => ['bank','mobile'],
     'paperfly_charge' => [
         'inside_city' => [
