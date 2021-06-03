28c2ed2ec847c746cb905b16bf601e34a9377e28
rakib-09 <rakib.rumi09@gmail.com>
2021-06-03 17:47:40 +0600

    ➕ ADD: added test case for due tracker list Type(s): ADD Issue(s): - JIRA: https://blabol.atlassian.net/browse/MSMEBE-2489

--- tests/Feature/Accounting/DueTrackerListApiTest.php
+++ tests/Feature/Accounting/DueTrackerListApiTest.php
@@ -4,18 +4,17 @@
 namespace Tests\Feature\Accounting;
 
 
+use GuzzleHttp\Client;
 use Tests\Feature\FeatureTestCase;
 
 class DueTrackerListApiTest extends FeatureTestCase
 {
-    private $token = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoicmVhbCIsImltYWdlIjoiaHR0cHM6Ly9zMy5hcC1zb3V0aC0xLmFtYXpvbmF3cy5jb20vY2RuLXNoZWJhZGV2L2ltYWdlcy9wcm9maWxlcy9hdmF0YXIvMTU5OTYzMjMxM19yZWFsLmpwZWciLCJwcm9maWxlIjp7ImlkIjoyNTk5MzMsIm5hbWUiOiJyZWFsIiwiZW1haWxfdmVyaWZpZWQiOjB9LCJjdXN0b21lciI6eyJpZCI6MTg5ODk4fSwicmVzb3VyY2UiOnsiaWQiOjQ1MTc5LCJwYXJ0bmVyIjp7ImlkIjozNzkwMCwibmFtZSI6IiIsInN1Yl9kb21haW4iOiJwYXJ0bmVyLWRldiIsImxvZ28iOiJodHRwczovL3MzLmFwLXNvdXRoLTEuYW1hem9uYXdzLmNvbS9jZG4tc2hlYmFkZXYvaW1hZ2VzL3BhcnRuZXJzL2xvZ29zLzE1ODk5OTQzMjNfcGFydG5lcmRldi5wbmciLCJpc19tYW5hZ2VyIjp0cnVlfX0sInBhcnRuZXIiOm51bGwsIm1lbWJlciI6bnVsbCwiYnVzaW5lc3NfbWVtYmVyIjpudWxsLCJhZmZpbGlhdGUiOnsiaWQiOjM5MzI0fSwibG9naXN0aWNfdXNlciI6bnVsbCwiYmFua191c2VyIjpudWxsLCJzdHJhdGVnaWNfcGFydG5lcl9tZW1iZXIiOm51bGwsImF2YXRhciI6eyJ0eXBlIjoicGFydG5lciIsInR5cGVfaWQiOjM3OTAwfSwiZXhwIjoxNjIzMzIxMTM0LCJzdWIiOjI1OTkzMywiaXNzIjoiaHR0cDovL2FjY291bnRzLmRldi1zaGViYS54eXovYXBpL3YzL3Rva2VuL2dlbmVyYXRlIiwiaWF0IjoxNjIyNzE2MzM1LCJuYmYiOjE2MjI3MTYzMzUsImp0aSI6InBYeERYNjh5RUJ6NDRGanoifQ.ED1ln6pDSUla1pR96GhigvXSPt5_unRrvVzE5V6edgs';
-    private $base_url = 'http://api.sheba.test';
     public function test_due_tracker_list()
     {
         $response = $this->get(
-            $this->base_url . '/v2/accounting/due-tracker/due-list',
+            config('sheba.api_url'). '/v2/accounting/due-tracker/due-list',
             [
-                'Authorization' => $this->token
+                'Authorization' => $this->generateToken()
             ]
         );
         $response->assertResponseOk();
@@ -36,5 +35,12 @@ class DueTrackerListApiTest extends FeatureTestCase
                 ]
             ]
         );
+    }
+
+    private function generateToken(): string
+    {
+        $client = new Client();
+        $response = $client->get('https://accounts.dev-sheba.xyz/api/v3/token/generate?type=resource&token=TemAMQbHo8NES7nlEielwNw1EGTOKcQTC6jImGLNP4MLbFCjtvbeziGwlMd7&type_id=45320');
+        return 'Bearer ' . \GuzzleHttp\json_decode($response->getBody())->token;
     }
 }
