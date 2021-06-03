cb9c93f74e8d51d7672d3f263bc5b1fd1a37eb06
Nadim Shakil <nadim@iamsheba.xyz>
2021-06-03 18:29:09 +0600

    ➕ Expense inventory api test case
    Type(s): ADD
    
    Issue(s):
    - JIRA: MSMEBE-2480

--- tests/Feature/Accounting/IncomeExpenseEntryApiTest.php
+++ tests/Feature/Accounting/IncomeExpenseEntryApiTest.php
@@ -43,18 +43,50 @@ class IncomeExpenseEntryApiTest extends AccountingFeatureTest
         ]);
     }
 
-//    public function test_expense_entry_with_inventory_products(){
-//
-//    }
+    public function test_expense_entry_with_inventory_products(){
+        $additionalData = [
+            'amount_cleared' => 900,
+            'customer_id' => 568,
+            'inventory_products' => json_encode([
+                [
+                    "id" => "1",
+                    "name" => "name-1",
+                    "unit_price" => 50,
+                    "quantity" => 5
+                ],
+                [
+                    "id" => "5",
+                    "name" => "name-5",
+                    "unit_price" => 40,
+                    "quantity" => 15
+                ]
+            ])
+        ];
+        $response = $this->post(config('sheba.api_url').'/v2/accounting/expense', $this->getFormData($additionalData), [
+            'Authorization' => $this->token ?? $this->generateToken()
+        ]);
 
-    private function getFormData() : array {
-        return [
+        $id = json_decode($response->response->getContent())->data->id;
+        $response->assertResponseOk();
+        $response->seeJson([
+            "code" => 200,
+            "message" => "Successful",
+            "data" => [
+                "id" => $id,
+                "amount" => 1111
+            ]
+        ]);
+    }
+
+    private function getFormData(array $additionalFields=[]) : array {
+        $data = [
             'amount' => 1111,
             'from_account_key' => 'cash',
             'to_account_key' => 'sheba_account',
             'date' => '2020-12-25 15:49:59',
             'note' => 'test case from api project',
         ];
+        return array_merge($data,$additionalFields);
     }
 
 }
