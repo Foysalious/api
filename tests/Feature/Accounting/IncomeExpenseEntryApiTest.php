c3a8e2042e747a83b60bdf0ca06cd35c14950d69
Nadim Shakil <nadim@iamsheba.xyz>
2021-06-03 18:11:27 +0600

    some refactoring

--- tests/Feature/Accounting/IncomeExpenseEntryApiTest.php
+++ tests/Feature/Accounting/IncomeExpenseEntryApiTest.php
@@ -4,10 +4,8 @@
 use GuzzleHttp\Client;
 use Tests\Feature\FeatureTestCase;
 
-class IncomeExpenseEntryApiTest extends FeatureTestCase
+class IncomeExpenseEntryApiTest extends AccountingFeatureTest
 {
-    private $token;
-
     public function test_income_entry_api()
     {
         $response = $this->post(config('sheba.api_url').'/v2/accounting/income', $this->getFormData(), [
@@ -45,13 +43,9 @@ class IncomeExpenseEntryApiTest extends FeatureTestCase
         ]);
     }
 
-    private function generateToken(): string
-    {
-        $client = new Client();
-        $response = $client->get('https://accounts.dev-sheba.xyz/api/v3/token/generate?type=resource&token=TemAMQbHo8NES7nlEielwNw1EGTOKcQTC6jImGLNP4MLbFCjtvbeziGwlMd7&type_id=45320');
-        $this->token = 'Bearer ' . \GuzzleHttp\json_decode($response->getBody())->token;
-        return $this->token;
-    }
+//    public function test_expense_entry_with_inventory_products(){
+//
+//    }
 
     private function getFormData() : array {
         return [
