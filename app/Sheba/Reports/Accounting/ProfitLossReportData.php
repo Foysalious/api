890038038ca59ae874ba562cacd422ffae017275
Arafat <yeamin@sheba.xyz>
2021-06-10 12:42:37 +0600

    ➕ Profit loss report data from api end with discount
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSMEBE-2607

--- app/Sheba/Reports/Accounting/ProfitLossReportData.php
+++ app/Sheba/Reports/Accounting/ProfitLossReportData.php
@@ -12,9 +12,12 @@ class ProfitLossReportData
     {
         $formatted_data = array();
         $formatted_data['sales'] = $data['operating_earning'];
-        $formatted_data['total_sales'] = $this->getSum($data['operating_earning']);
+        $formatted_data['total_sales'] = ($this->getSum($data['operating_earning']));
+        $formatted_data['equity_discount'] = $data['equity_discount'];
+        $total_operating_income = ($formatted_data['total_sales'] - $formatted_data['equity_discount']);
+        $formatted_data['total_operating_income'] = $this->getPreciseRound($total_operating_income, 2);
         $formatted_data['cost_of_goods_sold'] = $data['cost_of_goods_sold'];
-        $formatted_data['full_profit'] = $formatted_data['total_sales'] - $formatted_data['cost_of_goods_sold'];
+        $formatted_data['full_profit'] = $formatted_data['total_operating_income'] - $formatted_data['cost_of_goods_sold'];
         $formatted_data['operating_cost'] = $data['business_cost'];
         $formatted_data['total_operating_cost'] = $this->getSum($data['business_cost']);
         $formatted_data['non_operating_income'] = $data['non_operating_income'];
@@ -34,8 +37,16 @@ class ProfitLossReportData
     {
         $total_earning = 0.0;
         foreach ($data as $earning)
-            $total_earning += $earning['balance'];
+            $total_earning += ($earning['balance']);
 
         return $total_earning;
+    }
+
+    private function getPreciseRound($number, $decimal)
+    {
+        $number = $number * pow(10, $decimal);
+        $number = intval($number);
+        $number = $number / pow(10, $decimal);
+        return $number;
     }
 }
