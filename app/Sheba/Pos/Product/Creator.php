a52567afb99cae56e2b14c5bfcc6b581ca4df42f
shovan <shovan.sheba@gmail.com>
2021-06-13 13:00:38 +0600

    ⬆️ Accounting info add in pos service details
    Type(s): UPDATE
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-863?atlOrigin=eyJpIjoiMDBjOGFlYWIzOTdhNGFmZjkwNDRjZmExNGFjOTkxOGIiLCJwIjoiaiJ9

--- app/Sheba/Pos/Product/Creator.php
+++ app/Sheba/Pos/Product/Creator.php
@@ -51,11 +51,11 @@ class Creator
         if (isset($this->data['image_gallery']))
             $image_gallery = $this->data['image_gallery'];
         $cloned_data = $this->data;
-        $this->data = array_except($this->data, ['remember_token', 'discount_amount', 'end_date', 'manager_resource', 'partner', 'category_id', 'image_gallery','accounting_info']);
+        $this->data = array_except($this->data, ['remember_token', 'discount_amount', 'end_date', 'manager_resource', 'partner', 'category_id', 'image_gallery']);
         $partner_pos_service = $this->serviceRepo->save($this->data + (new RequestIdentification())->get());
         $this->storeImageGallery($partner_pos_service, json_decode($image_gallery,true));
-      //  if (isset($cloned_data['accounting_info']) && !empty($cloned_data['accounting_info']))
-        //    $this->createExpenseEntry($partner_pos_service, json_decode($cloned_data['accounting_info'], true));
+        if (isset($cloned_data['accounting_info']) && !empty($cloned_data['accounting_info']))
+            $this->createExpenseEntry($partner_pos_service, json_decode($cloned_data['accounting_info'], true));
         return $partner_pos_service;
     }
 
