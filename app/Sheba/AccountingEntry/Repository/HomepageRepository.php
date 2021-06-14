e18ba89ed31f3d3d2e8c058b98c101d364143b6f
Rakib <rakib.rumi09@gmail.com>
2021-06-14 20:23:01 +0600

    UPDATE: added log

--- app/Sheba/AccountingEntry/Repository/HomepageRepository.php
+++ app/Sheba/AccountingEntry/Repository/HomepageRepository.php
@@ -68,7 +68,7 @@ class HomepageRepository extends BaseRepository
 
     public function getAccountListBalance($userId, $startDate, $endDate, $limit, $offset, $rootAccount, $userType = UserType::PARTNER) {
         try {
-            dd($this->api . "account-list-balance?" . ($limit ? "limit={$limit}" : "") . "&offset={$offset}" . ($startDate ? "&start_date={$startDate}" : "") . ($endDate ? "&end_date={$endDate}" : ""). ($rootAccount ? "&root_account={$rootAccount}" : ""))
+            dd($this->api . "account-list-balance?" . ($limit ? "limit={$limit}" : "") . "&offset={$offset}" . ($startDate ? "&start_date={$startDate}" : "") . ($endDate ? "&end_date={$endDate}" : ""). ($rootAccount ? "&root_account={$rootAccount}" : ""));
             return $this->client->setUserType($userType)->setUserId($userId)
                 ->get($this->api . "account-list-balance?" . ($limit ? "limit={$limit}" : "") . "&offset={$offset}" . ($startDate ? "&start_date={$startDate}" : "") . ($endDate ? "&end_date={$endDate}" : ""). ($rootAccount ? "&root_account={$rootAccount}" : ""));
         } catch (AccountingEntryServerError $e) {
