2a28abc70f857a25ed175ce5035c0714fe0257c7
Fariha Nuzhat <fariha@iamsheba.xyz>
2021-06-14 11:45:14 +0600

    ⬆️ _order_details, _invoice, _invoice_v1, _invoice_v2 blade files
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: MPBE-2123

--- resources/views/pdfs/_invoice_v2.blade.php
+++ resources/views/pdfs/_invoice_v2.blade.php
@@ -107,7 +107,7 @@
             <td class="text-left" colspan="1">DUE AMOUNT @if($type=="QUOTATION") *** @endif</td>
             <td class="s-price">{{ $partner_order->dueWithLogistic }}</td>
         @else
-            <td class="text-left" colspan="1">GRAND TOTAL @if($type=="QUOTATION") *** @endif</td>
+            <td class="text-left" colspan="1">GRAND TOTAL @if($type=="QUOTATION") @endif</td>
             <td class="s-price">{{ number_format($partner_order->grossAmountWithLogistic, 2) }}</td>
         @endif
     </tr>
