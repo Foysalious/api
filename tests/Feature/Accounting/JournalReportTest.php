f04dd94495cfc362bc8daa243f41e71868c26418
Rakib <rakib.rumi09@gmail.com>
2021-06-15 10:47:05 +0600

    ADD: added journal report test case in acocunting

--- tests/Feature/Accounting/JournalReportTest.php
+++ tests/Feature/Accounting/JournalReportTest.php
@@ -6,4 +6,23 @@ namespace Tests\Feature\Accounting;
 
 class JournalReportTest extends AccountingFeatureTest
 {
+    public function test_response()
+    {
+        $response = $this->get(
+            config(
+                'sheba.api_url'
+            ) . '/v2/accounting/reports/journal_report?start_date=2021-05-10&end_date=2021-07-15',
+            [
+                'Authorization' => $this->token ?? $this->generateToken()
+            ]
+        );
+        $response->assertResponseOk();
+        $response->seeJsonStructure(
+            [
+                'code',
+                'message',
+                'data'
+            ]
+        );
+    }
 }
