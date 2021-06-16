a52abd71796ca116152e796fa1e257c9ad0613f9
Rashad Akhtar <rashad@sheba.xyz>
2021-06-16 16:41:29 +0600

    :heavy_plus_sign: All top up data with waiting time
    Type(s): ADD
    
    Issue(s):
    - JIRA: B2BBE-1074

--- app/Http/Route/Prefix/V2/Route.php
+++ app/Http/Route/Prefix/V2/Route.php
@@ -162,10 +162,11 @@ class Route
                 $api->post('/get-topup-token', 'TopUp\TopUpController@generateJwt');
                 $api->post('/{user?}', 'TopUp\TopUpController@topUp')->where('user', "(business|partner|affiliate)");
                 $api->post('/bulk', 'TopUp\TopUpController@bulkTopUp');
-                $api->get('/history', 'TopUp\TopUpController@topUpHistory');
+                $api->get('/business/history', 'TopUp\TopUpController@topUpHistory');
                 $api->get('/active-bulk', 'TopUp\TopUpController@activeBulkTopUps');
                 $api->get('/special-amount-data', 'TopUp\TopUpController@specialAmount');
                 $api->get('bulk-list', 'TopUp\TopUpController@bulkList');
+                $api->get('/all-top-up', 'TopUp\TopUpController@allTopUps');
                 /**
                  * FOR TEST
                  * $api->post('top-up-test', 'TopUp\\TopUpController@topUpTest');
