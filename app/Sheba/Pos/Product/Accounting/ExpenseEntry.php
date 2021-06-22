09abc52eb946f1a57554d2dd25a0bc5a39ebf0ba
Foysal <foysal@iamsheba.xyz>
2021-06-22 18:48:29 +0600

    ⬆️ From Account Check
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-916

--- app/Sheba/Pos/Product/Accounting/ExpenseEntry.php
+++ app/Sheba/Pos/Product/Accounting/ExpenseEntry.php
@@ -88,7 +88,7 @@ class ExpenseEntry
         $data = collect();
         $data->partner = $this->partner;
         $data->amount = $this->stock * $this->costPerUnit;
-        $data->from_account_key = $this->accountingInfo['from_account'];
+        $data->from_account_key = $this->accountingInfo['from_account']?$this->accountingInfo['from_account']:null;
         $data->to_account_key = $this->id;
         $data->customer_id = $this->accountingInfo['supplier_id']?$this->accountingInfo['supplier_id']:null;
         $data->inventory_products = [['id' => $this->id, 'unit_price' => $this->costPerUnit, 'name' => $this->name, 'quantity' => $this->stock]];
