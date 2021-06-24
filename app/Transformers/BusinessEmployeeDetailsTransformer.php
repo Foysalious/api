2a6d125dd354d9ec06b3707d649dcafa1dc984dd
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2021-06-24 19:17:30 +0600

    ➕ Phonebook Data Update
    Type(s): ADD
    
    Issue(s):
    - JIRA: B2BBE-1109

--- app/Transformers/BusinessEmployeeDetailsTransformer.php
+++ app/Transformers/BusinessEmployeeDetailsTransformer.php
@@ -1,6 +1,7 @@
 <?php namespace App\Transformers;
 
 use App\Models\Member;
+use Carbon\Carbon;
 use League\Fractal\TransformerAbstract;
 
 class BusinessEmployeeDetailsTransformer extends TransformerAbstract
@@ -21,7 +22,24 @@ class BusinessEmployeeDetailsTransformer extends TransformerAbstract
             'email'         => $profile->email,
             'image'         => $profile->pro_pic,
             'designation'   => $role ? $role->name : null,
-            'department'    => $role ? $role->businessDepartment->name : null
+            'department'    => $role ? $role->businessDepartment->name : null,
+            'blood_group'   => $profile->blood_group,
+            'dob'           => Carbon::parse($profile->dob)->format('jS F'),
+            'social_link'   => $this->getSocialLinks($member)
         ];
+    }
+
+    private function getSocialLinks($member)
+    {
+        $social_links = json_decode($member->social_links, 1);
+        if (!$social_links) return null;
+        $data = [];
+        foreach ($social_links as $type => $link){
+            array_push($data, [
+               'link' =>  $link,
+               'type' => $type
+            ]);
+        }
+        return $data;
     }
 }
