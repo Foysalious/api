7d78f140b288a6849bcee86628698b68f5bc4d0a
Hasan Hafiz Pasha <mach.pasha@gmail.com>
2021-06-24 14:32:52 +0600

    DB CONNECTION CHANGE FROM SINGLETON TO RESOLVED
    Type(s): UPDATE

--- app/Sheba/Database/DatabaseServiceProvider.php
+++ app/Sheba/Database/DatabaseServiceProvider.php
@@ -1,22 +1,28 @@
 <?php namespace Sheba\Database;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Eloquent\Model;
+use Sheba\Database\Connectors\ConnectionFactory;
 
 class DatabaseServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap the application events.
+     */
+    public function boot()
+    {
+        Model::setConnectionResolver($this->app['db']);
+
+        Model::setEventDispatcher($this->app['events']);
+    }
+
     public function register()
     {
-        // Register the MySql connection class as a singleton
-        // because we only want to have one, and only one,
-        // MySql database connection at the same time.
-        $this->app->bind('db.connection.mysql', function ($app, $parameters) {
-            // First, we list the passes parameters into single
-            // variables. I do this because it is far easier
-            // to read than using it as eg $parameters[0].
-            list($connection, $database, $prefix, $config) = $parameters;
-
-            // Next we can initialize the connection.
-            return new MySqlConnection($connection, $database, $prefix, $config);
+        // The connection factory is used to create the actual connection instances on
+        // the database. We will inject the factory into the manager so that it may
+        // make the connections while they are actually needed and not of before.
+        $this->app->singleton('db.factory', function ($app) {
+            return new ConnectionFactory($app);
         });
     }
 }
