bed505722567294800a56deacf450ec6af1e492e
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2021-06-24 19:19:57 +0600

    Profile Pic Added
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: B2BBE-1109

--- app/Transformers/Business/EmergencyContactInfoTransformer.php
+++ app/Transformers/Business/EmergencyContactInfoTransformer.php
@@ -9,8 +9,10 @@ class EmergencyContactInfoTransformer extends TransformerAbstract
     public function transform(BusinessMember $business_member)
     {
         $member = $business_member->member;
+        $profile = $member->profile;
 
         return [
+            'pro_pic' => $profile->pro_pic,
             'emergency_name' => $member->emergency_contract_person_name,
             'emergency_number' => $member->emergency_contract_person_number,
             'emergency_person_relationship' => $member->emergency_contract_person_relationship,
