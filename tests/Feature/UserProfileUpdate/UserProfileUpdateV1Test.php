c184a667b326bda88d903d2d0dafdf5f80b0529d
Zakwan <k.fahd@iamsheba.xyz>
2021-06-29 19:50:36 +0000

    2nd commit v2.0 [Working Version] [Added two Test classes for User Profile Update V1 and V3 with test suites]

---
+++ tests/Feature/UserProfileUpdate/UserProfileUpdateV1Test.php
@@ -0,0 +1,270 @@
+<?php namespace Tests\Feature\UserProfileUpdate;
+
+use App\Models\Affiliate;
+use App\Models\Customer;
+use App\Models\Profile;
+use Carbon\Carbon;
+use Tests\Feature\FeatureTestCase;
+
+class UserProfileUpdateV1Test extends FeatureTestCase
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->truncateTable(Profile::class,Customer::class);
+
+        $this->logIn();
+
+    }
+
+    public function testUserProfileUpdateAPIV1ByUpdatingNameGenderBirthdayAddress()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v1/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'field' => 'name',
+            'value' => 'John Doe',
+
+            'field' => 'gender',
+            'value' => 'Male',
+
+            'field' => 'birthday',
+            'value' => '2007-06-27',
+
+            'field' => 'address',
+            'value' => 'Dhaka, Bangladesh'
+        ]);
+        $data = $response->decodeResponseJson();
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV1ByUpdatingNameOnly()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v1/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'field' => 'name',
+            'value' => 'John Doe',
+        ]);
+        $data = $response->decodeResponseJson();
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV1ByUpdatingGenderOnly()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v1/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'field' => 'gender',
+            'value' => 'Male',
+        ]);
+        $data = $response->decodeResponseJson();
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV1ByUpdatingBirthdayOnly()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v1/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'field' => 'birthday',
+            'value' => '2007-06-27'
+        ]);
+        $data = $response->decodeResponseJson();
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV1ByUpdatingAddressOnly()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v1/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'field' => 'address',
+            'value' => 'Dhaka, Bangladesh'
+        ]);
+        $data = $response->decodeResponseJson();
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV1ByGivingNullValueOfName()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v1/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'field' => 'name',
+            'value' => '',
+        ]);
+        $data = $response->decodeResponseJson();
+
+        $this->assertEquals(400, $data["code"]);
+        $this->assertEquals("The value field is required.", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV1ByGivingNullValueOfGender()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v1/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'field' => 'gender',
+            'value' => ''
+        ]);
+        $data = $response->decodeResponseJson();
+
+        $this->assertEquals(400, $data["code"]);
+        $this->assertEquals("The value field is required.", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV1ByGivingNullValueOfBirthday()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v1/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'field' => 'birthday',
+            'value' => ''
+        ]);
+        $data = $response->decodeResponseJson();
+
+        $this->assertEquals(400, $data["code"]);
+        $this->assertEquals("The value field is required.", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV1ByGivingNullValueOfAddress()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v1/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'field' => 'address',
+            'value' => ''
+        ]);
+        $data = $response->decodeResponseJson();
+
+        $this->assertEquals(400, $data["code"]);
+        $this->assertEquals("The value field is required.", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV1ByGivingInvalidBirthday()
+    {
+
+        //arrange
+
+        $today = Carbon::now()->toDateString();
+
+        //act
+
+        $response = $this->put("/v1/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'field' => 'birthday',
+            'value' => '2222-06-29'
+        ]);
+        $data = $response->decodeResponseJson();
+
+        $this->assertEquals(400, $data["code"]);
+        $this->assertEquals("The value must be a date before " . $today . ".", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV1ByGivingCharacterInputInBirthday()
+    {
+
+        //arrange
+
+        $today = Carbon::now()->toDateString();
+
+        //act
+
+        $response = $this->put("/v1/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'field' => 'birthday',
+            'value' => 'jgfffs'
+        ]);
+        $data = $response->decodeResponseJson();
+
+        $this->assertEquals(400, $data["code"]);
+        $this->assertEquals("The value is not a valid date.The value does not match the format Y-m-d.The value must be a date before " . $today . ".", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV1ByGivingInvalidGender()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v1/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'field' => 'gender',
+            'value' => 'hjsfgaf'
+        ]);
+        $data = $response->decodeResponseJson();
+
+        $this->assertEquals(400, $data["code"]);
+        $this->assertEquals("The selected value is invalid.", $data["message"]);
+
+    }
+
+//    public function testUserProfileUpdateAPIV1ByGivingInvalidName()
+//    {
+//
+//        //arrange
+//
+//        //act
+//
+//        $response = $this->put("/v1/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+//            'field' => 'name',
+//            'value' => '123456'
+//        ]);
+//        $data = $response->decodeResponseJson();
+//
+//        $this->assertEquals(400, $data["code"]);
+//        $this->assertEquals("The selected value is invalid.", $data["message"]);
+//
+//    }
+
+
+}
---
+++ tests/Feature/UserProfileUpdate/UserProfileUpdateV3Test.php
@@ -0,0 +1,558 @@
+<?php namespace Tests\Feature\UserProfileUpdate;
+
+use App\Models\Affiliate;
+use App\Models\Customer;
+use App\Models\Profile;
+use Carbon\Carbon;
+use Tests\Feature\FeatureTestCase;
+
+class UserProfileUpdateV3Test extends FeatureTestCase
+{
+    private $today;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->truncateTable(Profile::class,Customer::class);
+
+        $this->logIn();
+
+    }
+
+//    public function testUserProfileUpdateAPIV3ByCreatingProfileGivingBirthdayDateEmailGenderName()
+//    {
+//
+//        //arrange
+//
+//        //act
+//
+//        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+//            'dob' => '2009-6-29',
+//            'email'=> 'johndoe@gmail.com',
+//            'gender' => 'Male',
+//            'is_old_user' => 0,
+//            'name' => 'John Doe'
+//        ]);
+//
+//        $data = $response->decodeResponseJson();
+//
+//        //assert
+//
+//        $this->assertEquals(200, $data["code"]);
+//        $this->assertEquals("Successful", $data["message"]);
+//
+//    }
+//
+//    public function testUserProfileUpdateAPIV3ByCreatingProfileGivingDateOfBirthGenderName()
+//    {
+//
+//        //arrange
+//
+//        //act
+//
+//        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+//            'dob' => '2009-6-29',
+//            'gender' => 'Male',
+//            'is_old_user' => 0,
+//            'name' => 'John Doe'
+//        ]);
+//
+//        $data = $response->decodeResponseJson();
+//
+//        //assert
+//
+//        $this->assertEquals(200, $data["code"]);
+//        $this->assertEquals("Successful", $data["message"]);
+//
+//    }
+//
+//    public function testUserProfileUpdateAPIV3ByCreatingProfileGivingDateOfBirthGender()
+//    {
+//
+//        //arrange
+//
+//        //act
+//
+//        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+//            'dob' => '2009-6-29',
+//            'gender' => 'Male',
+//            'is_old_user' => 0
+//        ]);
+//
+//        $data = $response->decodeResponseJson();
+//
+//        //assert
+//
+//        $this->assertEquals(200, $data["code"]);
+//        $this->assertEquals("Successful", $data["message"]);
+//
+//    }
+
+    public function testUserProfileUpdateAPIV3ByUpdatingName()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'is_old_user' => 1,
+            'name' => 'John Doe'
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByUpdatingEmail()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'email'=> 'johndoe@gmail.com',
+            'is_old_user' => 1
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByUpdatingDateOfBirth()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'dob' => '2009-6-29',
+            'is_old_user' => 1
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByUpdatingGender()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'gender' => 'Male',
+            'is_old_user' => 1
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByUpdatingAddress()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'address' => 'Azimpur Khoborsthan',
+            'is_old_user' => 1
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByUpdatingDateOfBirthGender()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'dob' => '2009-6-29',
+            'gender' => 'Male',
+            'is_old_user' => 1,
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByUpdatingDateOfBirthName()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'dob' => '2009-6-29',
+            'is_old_user' => 1,
+            'name' => 'John Doe'
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByUpdatingDateOfBirthGenderName()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'dob' => '2009-6-29',
+            'gender' => 'Male',
+            'is_old_user' => 1,
+            'name' => 'John Doe'
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByUpdatingDateOfBirthEmailGenderName()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'dob' => '2009-6-29',
+            'email'=> 'johndoe@gmail.com',
+            'gender' => 'Male',
+            'is_old_user' => 1,
+            'name' => 'John Doe'
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByUpdatingDateOfBirthEmailGenderNameAddress()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'dob' => '2009-6-29',
+            'email'=> 'johndoe@gmail.com',
+            'gender' => 'Male',
+            'is_old_user' => 1,
+            'name' => 'John Doe',
+            'address' => 'Dhaka, Bangladesh'
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(200, $data["code"]);
+        $this->assertEquals("Successful", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByNotGivingIsOldUser()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'dob' => '2009-6-29',
+            'email'=> 'johndoe@gmail.com',
+            'gender' => 'Male',
+            'name' => 'John Doe'
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(400, $data["code"]);
+        $this->assertEquals("The is old user field is required.", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByGivingInvalidGender()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'dob' => '2009-6-29',
+            'is_old_user' => 1,
+            'email'=> 'johndoe@gmail.com',
+            'gender' => 'dkjsfshkdf',
+            'name' => 'John Doe'
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(400, $data["code"]);
+        $this->assertEquals("The selected gender is invalid.", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByGivingInvalidRememberToken()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=dhksueislbdnf",[
+            'dob' => '2009-6-29',
+            'is_old_user' => 1,
+            'email'=> 'johndoe@gmail.com',
+            'gender' => 'Male',
+            'name' => 'John Doe'
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(404, $data["code"]);
+        $this->assertEquals("User not found.", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByNotGivingRememberToken()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?",[
+            'dob' => '2009-6-29',
+            'is_old_user' => 1,
+            'email'=> 'johndoe@gmail.com',
+            'gender' => 'Male',
+            'name' => 'John Doe'
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(400, $data["code"]);
+        $this->assertEquals("Authentication token is missing from the request.", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByGivingInvalidCustomerId()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/123456/edit?remember_token=" . $this->customer->remember_token,[
+            'dob' => '2009-6-29',
+            'email'=> 'johndoe@gmail.com',
+            'gender' => 'Male',
+            'is_old_user' => 1,
+            'name' => 'John Doe'
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(403, $data["code"]);
+        $this->assertEquals("You're not authorized to access this user.", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByGivingInvalidDateOfBirth()
+    {
+
+        //arrange
+
+        $today = Carbon::now()->toDateString();
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'dob' => '2028-06-25',
+            'email'=> 'johndoe@gmail.com',
+            'gender' => 'Male',
+            'is_old_user' => 1,
+            'name' => 'John Doe'
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(400, $data["code"]);
+        $this->assertEquals("The dob must be a date before " . $today . ".", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByGivingCharacterInputInDateOfBirth()
+    {
+
+        //arrange
+
+        $today = Carbon::now()->toDateString();
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'dob' => 'sbhfjkfs',
+            'email'=> 'johndoe@gmail.com',
+            'gender' => 'Male',
+            'is_old_user' => 1,
+            'name' => 'John Doe'
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(400, $data["code"]);
+        $this->assertEquals("The dob is not a valid date.The dob does not match the format Y-m-d.The dob must be a date before " . $today . ".", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByGivingInvalidEmail()
+    {
+
+        //arrange
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'dob' => '2009-6-29',
+            'email'=> 'johndoe@gmail',
+            'gender' => 'Male',
+            'is_old_user' => 1,
+            'name' => 'John Doe'
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(400, $data["code"]);
+        $this->assertEquals("The email must be a valid email address.", $data["message"]);
+
+    }
+
+    public function testUserProfileUpdateAPIV3ByGivingInvalidDateOfBirthGenderEmail()
+    {
+
+        //arrange
+
+        $today = Carbon::now()->toDateString();
+
+        //act
+
+        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+            'dob' => 'hfdkhf',
+            'email'=> 'johndoe@gmail',
+            'gender' => 'Others',
+            'is_old_user' => 1,
+            'name' => 'John Doe'
+        ]);
+
+        $data = $response->decodeResponseJson();
+
+        //assert
+
+        $this->assertEquals(400, $data["code"]);
+        $this->assertEquals("The selected gender is invalid.The dob is not a valid date.The dob does not match the format Y-m-d.The dob must be a date before " . $today . ".The email must be a valid email address.", $data["message"]);
+
+    }
+
+//    public function testUserProfileUpdateAPIV3ByGivingInvalidName()
+//    {
+//
+//        //arrange
+//
+//        //act
+//
+//        $response = $this->put("/v3/customers/" . $this->customer->id . "/edit?remember_token=" . $this->customer->remember_token,[
+//            'dob' => '2009-6-29',
+//            'email'=> 'johndoe@gmail.com',
+//            'gender' => 'Male',
+//            'is_old_user' => 1,
+//            'name' => '123456'
+//        ]);
+//
+//        $data = $response->decodeResponseJson();
+//
+//        //assert
+//
+//        $this->assertEquals(400, $data["code"]);
+//        $this->assertEquals("The name must be a valid name.", $data["message"]);
+//
+//    }
+
+}
