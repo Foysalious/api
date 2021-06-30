dc392b7661de4fd3eed4a70152e6a36b7b43b47c
Fariha Nuzhat <fariha@iamsheba.xyz>
2021-06-30 10:56:03 +0600

    ⬆️ EventDataConverter
    Type(s): UPDATE

--- app/Sheba/Reward/EventDataConverter.php
+++ app/Sheba/Reward/EventDataConverter.php
@@ -181,7 +181,7 @@ class EventDataConverter
                             ]
                         ]
                     ]
-                ,
+                    ],
                 'campaign' => [
                     'order_serve' => [
                         'name' => 'Order Serve',
@@ -410,7 +410,6 @@ class EventDataConverter
                     ],
                 ],
             ]
-
         ]);
     }
 
