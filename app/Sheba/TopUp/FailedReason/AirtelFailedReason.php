f125c5abb479a9cec4f108ed7ed82b1bbe480d7c
Rashad Akhtar <rashad@sheba.xyz>
2021-07-03 19:16:50 +0600

    :arrow_up: Top up failed reason
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: B2BBE-1099

--- app/Sheba/TopUp/FailedReason/AirtelFailedReason.php
+++ app/Sheba/TopUp/FailedReason/AirtelFailedReason.php
@@ -8,10 +8,11 @@ class AirtelFailedReason extends PretupsFailedReason
     {
         try {
             $transaction_details = json_decode($this->transaction, true);
-            if ($transaction_details_response = $transaction_details['response']) {
-                return $transaction_details_response['MESSAGE'];
+            if (array_key_exists('response', $transaction_details)) {
+                if (!$transaction_details['response']) return "The Recharge could not be processed due to a technical issue. Pls try again later.";
+                if (array_key_exists('message', $transaction_details['response'])) return $transaction_details['response']['message'];
+                return $transaction_details['response']['MESSAGE'];
             }
-            return $transaction_details['message'];
         } catch (Throwable $e) {
             logError($e);
         }
