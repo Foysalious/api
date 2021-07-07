7eca8ff7b7e5fdbc81ca580ef4e3dff1a8921119
Hasan Hafiz Pasha <mach.pasha@gmail.com>
2021-07-07 19:30:34 +0600

    AUTO BUILD TEST BUILD
    Type(s): UPDATE

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -56,10 +56,12 @@ class TestController extends Controller
         /*$mailer->send('emails.notify-vendor-balance', ['current_balance' => 100, 'vendor_name' => 'test'], function ($m)  {
            $m->from('yourEmail@domain.com', 'Sheba.xyz');
            $m->to('shovan@sheba.xyz')->subject('Low Balance for testvendor');
-       });*/ // $movie_ticket_order = MovieTicketOrder::find(1);
-        //$vendor = $vendor->getById(1);
+        });*/
+        
+        // $movie_ticket_order = MovieTicketOrder::find(1);
+        // $vendor = $vendor->getById(1);
         // $movie_ticket_order->vendor = $vendor;
-        //dispatch(new SendEmailToNotifyVendorBalance('transport_ticket',1));
+        // dispatch(new SendEmailToNotifyVendorBalance('transport_ticket',1));
         try {
             (new SendEmailToNotifyVendorBalance('transport_ticket', 1))->handle($mailer);
 
