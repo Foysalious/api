380302b013a0293cc652ddf42cb3f8c9c21758eb
Arafat <yeamin@sheba.xyz>
2021-07-08 14:12:32 +0600

    ⬆️ Update payment link list with query param
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSMEBE-2746

--- app/Sheba/PaymentLink/PaymentLink.php
+++ app/Sheba/PaymentLink/PaymentLink.php
@@ -53,4 +53,28 @@ class PaymentLink
             "payment_link_charge_percentage" => PaymentLinkStatics::get_payment_link_commission()
         ];
     }
+
+    /**
+     * @param $payment_links
+     * @param $link_type
+     * @return array
+     */
+    public function filterPaymentLinkList($payment_links, $link_type): array
+    {
+        $result = array();
+        if($link_type == PaymentLinkStatics::PAYMENT_LINK_TYPE_EMI) {
+            foreach ($payment_links as $emi_link) {
+                if ($emi_link['emi'] === 1)
+                    $result[] = $emi_link;
+            }
+        }
+        elseif ($link_type == PaymentLinkStatics::PAYMENT_LINK_TYPE_DIGITAL_COLLECTION) {
+            foreach ($payment_links as $digital_collection_link) {
+                if ($digital_collection_link['emi'] === 0)
+                    $result[] = $digital_collection_link;
+            }
+        }
+
+        return $result;
+    }
 }
