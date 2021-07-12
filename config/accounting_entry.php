89bf24da0ec378675d0d217b652eddf3195dffc2
nafeeur <nafeeur@iamsheba.xyz>
2021-07-12 12:23:55 +0600

    ⬆️ Inventory Update
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-891

---
+++ config/accounting_entry.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'api_url' => env('SHEBA_ACCOUNTING_URL'),
+    'api_key' => env('SHEBA_ACCOUNTING_KEY','sheba.xyz'),
+    'icon_url' => env('SHEBA_ACCOUNTING_ICON_URL','https://cdn-shebaxyz.s3.ap-south-1.amazonaws.com/partner/accounting/icons')
+];
