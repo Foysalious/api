d0589db54e09bee8d3580b4c59692746262ef2fc
Arafat <yeamin@sheba.xyz>
2021-07-14 12:49:31 +0600

    ⬆️ Topup settings url updated for master
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSMEBE-2760

--- app/Http/Route/Prefix/V2/Route.php
+++ app/Http/Route/Prefix/V2/Route.php
@@ -158,7 +158,7 @@ class Route
                 $api->get('current', 'LocationController@getCurrent');
             });
             $api->group(['prefix' => 'top-up', 'middleware' => ['topUp.auth']], function ($api) {
-                $api->get('/vendor/{user}', 'TopUp\TopUpController@getVendor')->where('user', "(business|partner|affiliate)");
+                $api->get('/vendor/{user?}', 'TopUp\TopUpController@getVendor')->where('user', "(business|partner|affiliate)");
                 $api->post('/get-topup-token', 'TopUp\TopUpController@generateJwt');
                 $api->post('/{user?}', 'TopUp\TopUpController@topUp')->where('user', "(business|partner|affiliate)");
                 $api->post('/bulk', 'TopUp\TopUpController@bulkTopUp');
