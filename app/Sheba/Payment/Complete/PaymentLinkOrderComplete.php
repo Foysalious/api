aad1194e18682b9e495ba8e94d2ed54cd10b2723
rakib-09 <rakib.rumi09@gmail.com>
2021-07-28 20:31:21 +0600

    UPDATE: added log for payment link check

--- app/Sheba/Payment/Complete/PaymentLinkOrderComplete.php
+++ app/Sheba/Payment/Complete/PaymentLinkOrderComplete.php
@@ -81,6 +81,7 @@ class PaymentLinkOrderComplete extends PaymentComplete
             DB::transaction(function () {
                 $this->paymentRepository->setPayment($this->payment);
                 $payable = $this->payment->payable;
+                Log::info(['payment link total information', $payable, $payable->user, $this->payment_receiver]);
                 $this->setModifier($customer = $payable->user);
                 $this->completePayment();
                 $this->processTransactions($this->payment_receiver);
