ea64470a4512d5daa13f3fae77dad470e01fb616
Zakwan <k.fahd@iamsheba.xyz>
2021-08-02 06:23:02 +0000

    v3.0

---
+++ app/Sheba/PosCustomerService/SmanagerUserServerClient.php
@@ -0,0 +1,94 @@
+<?php namespace App\Sheba\PosCustomerService;
+
+
+use App\Sheba\PosCustomerService\Exceptions\SmanagerUserServiceServerError;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+class SmanagerUserServerClient
+{
+    protected $client;
+    protected $baseUrl;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+        $this->baseUrl = rtrim(config('smanager_user_service.api_url'), '/');
+    }
+
+    public function get($uri)
+    {
+        return $this->call('get', $uri);
+    }
+
+
+    /**
+     * @param $method
+     * @param $uri
+     * @param null $data
+     * @param bool $multipart
+     * @return mixed
+     * @throws SmanagerUserServiceServerError
+     */
+    private function call($method, $uri, $data = null, $multipart = false)
+    {
+        try {
+            return json_decode($this->client->request(strtoupper($method), $this->makeUrl($uri), $this->getOptions($data, $multipart))->getBody()->getContents(), true);
+        } catch (GuzzleException $e) {
+            $res = $e->getResponse();
+            $http_code = $res->getStatusCode();
+            $message = $res->getBody()->getContents();
+            if ($http_code > 399 && $http_code < 500) throw new SmanagerUserServiceServerError($message, $http_code);
+            throw new SmanagerUserServiceServerError($e->getMessage(), $http_code);
+        }
+    }
+
+    private function makeUrl($uri)
+    {
+        return $this->baseUrl . "/" . $uri;
+    }
+
+    private function getOptions($data = null, $multipart = false)
+    {
+        $options['headers'] = [
+            'Accept' => 'application/x-www-form-urlencoded'
+        ];
+        if (!$data) return $options;
+        if ($multipart) {
+            $options['multipart'] = $data;
+        } else {
+            $options['form_params'] = $data;
+            $options['json'] = $data;
+        }
+        return $options;
+    }
+
+    public function post($uri, $data, $multipart = false)
+    {
+        return $this->call('post', $uri, $data, $multipart);
+    }
+
+
+    /**
+     * @param $uri
+     * @param $data
+     * @param false $multipart
+     * @return mixed
+     * @throws SmanagerUserServiceServerError
+     */
+    public function put($uri, $data, $multipart = false)
+    {
+        return $this->call('put', $uri, $data, $multipart);
+    }
+
+
+    /**
+     * @param $uri
+     * @return mixed
+     * @throws SmanagerUserServiceServerError
+     */
+    public function delete($uri)
+    {
+        return $this->call('DELETE', $uri);
+    }
+}
---
+++ app/Sheba/Voucher/VoucherValidate.php
@@ -0,0 +1,65 @@
+<?php namespace App\Sheba\Voucher;
+
+
+use App\Models\PosCustomer;
+use Illuminate\Database\Eloquent\Model;
+use Sheba\Voucher\DTO\Params\CheckParamsForPosOrder;
+
+class VoucherValidate
+{
+    private $real_pos_customer;
+    private $customer;
+
+    public function setPartner($partner_id)
+    {
+        $this->partnerId = $partner_id;
+        return $this;
+    }
+
+    public function setRealPosCustomer($real_pos_customer)
+    {
+        $this->real_pos_customer = $real_pos_customer;
+        return $this;
+    }
+
+    public function setPosCustomer($customer)
+    {
+        $this->customer = $customer;
+        return $this;
+    }
+
+    public function posOrderParams($request)
+    {
+        $pos_order_params = (new CheckParamsForPosOrder());
+        $pos_order_params->setOrderAmount($request->amount);
+        $pos_order_params = $this->real_pos_customer ? $pos_order_params->setApplicant($this->customer) : $pos_order_params->setApplicant(new PosCustomer());
+        return $pos_order_params->setPartnerPosService($request->pos_services);
+    }
+
+    public function OrderVoucherResult($request, $pos_order_params)
+    {
+        $result = voucher($request->code)->checkForPosOrder($pos_order_params);
+        return $this->real_pos_customer ? $result->reveal() : $result->checkMobile($this->customer['mobile'])->reveal();
+    }
+
+    public function voucherValidate($request)
+    {
+        $pos_order_params = $this->posOrderParams($request);
+        $result = $this->OrderVoucherResult($request, $pos_order_params);
+
+        if (!$result['is_valid'])
+            return api_response($request, null, 403, ['message' => 'Invalid Promo']);
+
+        $voucher = $result['voucher'];
+        $voucher = [
+            'amount' => (double)$result['amount'],
+            'code' => $voucher->code,
+            'id' => $voucher->id,
+            'title' => $voucher->title
+        ];
+        return api_response($request, null, 200, ['voucher' => $voucher]);
+
+    }
+
+
+}
