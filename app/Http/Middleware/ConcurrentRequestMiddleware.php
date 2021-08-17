e6c3e76ac436648455f07a1bf89dd0cf947d2458
nadim imtiaz shakil <nadim@iamsheba.xyz>
2021-08-17 13:49:30 +0600

    ⬆️ Implemented ConcurrentRequestMiddleware
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MPBE-2346

--- app/Http/Middleware/ConcurrentRequestMiddleware.php
+++ app/Http/Middleware/ConcurrentRequestMiddleware.php
@@ -14,11 +14,11 @@ class ConcurrentRequestMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next, $duration)
+    public function handle($request, Closure $next, $paramName, $duration=1, $maxHit=1)
     {
         $rateLimiter = app(RateLimiter::class);
-        $key = $request->route('job')->id;
-        if($rateLimiter->attempts($key) > 0) {
+        $key = $paramName . '-' .$request->route($paramName)->id;
+        if($rateLimiter->retriesLeft($key, $maxHit) <= 0) {
             return response()->json(['code' => 403, 'message' => 'Too many requests']);
         }
         $rateLimiter->hit($key, $duration);
