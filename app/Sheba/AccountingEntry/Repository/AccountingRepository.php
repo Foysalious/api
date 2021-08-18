70b10bb2fb5e2af4d7a553ca15c2268e5e4ccd96
rakib-09 <rakib.rumi09@gmail.com>
2021-08-18 10:58:37 +0600

    UPDATE: commented pos order reconcile in store entry

--- app/Sheba/AccountingEntry/Repository/AccountingRepository.php
+++ app/Sheba/AccountingEntry/Repository/AccountingRepository.php
@@ -27,9 +27,10 @@ class AccountingRepository extends BaseRepository
         $url = "api/entries/";
         try {
             $datum = $this->client->setUserType(UserType::PARTNER)->setUserId($partner->id)->post($url, $data);
-            if ($datum['source_type'] == 'pos' && $datum['amount_cleared'] > 0) {
-                $this->createPosOrderPayment($datum['amount_cleared'], $datum['source_id'], 'cod');
-            }
+            // May need pos order reconcile while storing entry
+//            if ($datum['source_type'] == 'pos' && $datum['amount_cleared'] > 0) {
+//                $this->createPosOrderPayment($datum['amount_cleared'], $datum['source_id'], 'cod');
+//            }
             return $datum;
         } catch (AccountingEntryServerError $e) {
             logError($e);
