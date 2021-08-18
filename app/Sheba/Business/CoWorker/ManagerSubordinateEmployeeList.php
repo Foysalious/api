437ede279cc56386b5e582263d6506f9cc07c66f
miajee <miajee@sheba.xyz>
2021-08-18 13:33:15 +0600

    ➕ Visits , My Visit, with Filter & search
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/B2BBE-1229,
    https://blabol.atlassian.net/browse/B2BBE-1231

--- app/Sheba/Business/CoWorker/ManagerSubordinateEmployeeList.php
+++ app/Sheba/Business/CoWorker/ManagerSubordinateEmployeeList.php
@@ -1,8 +1,8 @@
 <?php namespace App\Sheba\Business\CoWorker;
 
-
 use App\Transformers\Business\CoWorkerManagerListTransformer;
 use App\Transformers\CustomSerializer;
+use Illuminate\Support\Collection;
 use League\Fractal\Manager;
 use League\Fractal\Resource\Item;
 use Sheba\Repositories\Business\BusinessMemberRepository;
@@ -16,21 +16,32 @@ class ManagerSubordinateEmployeeList
     {
         $this->businessMemberRepository = app(BusinessMemberRepository::class);
     }
+
+    /**
+     * @param $business_member
+     * @return array
+     */
     public function get($business_member)
     {
-        $first_level_managers = $this->getCoWorkersUnderSpecificManager($business_member->id);
+        $managers_data = [];
         $manager = new Manager();
         $manager->setSerializer(new CustomSerializer());
-        $managers_data = [];
+
+        /** @var  $first_level_managers */
+        $first_level_managers = $this->getCoWorkersUnderSpecificManager($business_member->id);
         if ($first_level_managers->count() > 0)
             foreach ($first_level_managers as $first_level_manager) {
                 $resource = new Item($first_level_manager, new CoWorkerManagerListTransformer());
                 $managers_data[] = $manager->createData($resource)->toArray()['data'];
+
+                /** @var  $second_level_managers */
                 $second_level_managers = $this->getCoWorkersUnderSpecificManager($first_level_manager->id);
                 if ($second_level_managers->count() > 0)
                     foreach ($second_level_managers as $second_level_manager) {
                         $resource = new Item($second_level_manager, new CoWorkerManagerListTransformer());
                         $managers_data[] = $manager->createData($resource)->toArray()['data'];
+
+                        /** @var  $third_level_managers */
                         $third_level_managers = $this->getCoWorkersUnderSpecificManager($second_level_manager->id);
                         if ($third_level_managers->count() > 0)
                             foreach ($third_level_managers as $third_level_manager) {
@@ -42,6 +53,10 @@ class ManagerSubordinateEmployeeList
         return $managers_data;
     }
 
+    /**
+     * @param $business_member_id
+     * @return Collection
+     */
     private function getCoWorkersUnderSpecificManager($business_member_id)
     {
         return $this->businessMemberRepository->where('manager_id', $business_member_id)->get();
