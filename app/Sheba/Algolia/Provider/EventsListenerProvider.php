9288bf22478bfe40332e86754634864b469ff0b7
Shafiqul Islam <shafiqul@sheba.xyz>
2021-08-22 18:31:06 +0600

    📦  Update dal
    Type(s): PACKAGE

--- app/Sheba/Algolia/Provider/EventsListenerProvider.php
+++ app/Sheba/Algolia/Provider/EventsListenerProvider.php
@@ -1,28 +1,18 @@
 <?php namespace Sheba\Algolia\Provider;
 
 use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Sheba\Dal\PartnerPosService\Events\PartnerPosServiceCreated;
 use Sheba\Dal\PartnerPosService\Events\PartnerPosServiceSaved as PartnerPosServiceSaved;
-use App\Sheba\Algolia\Listeners\PartnerPosServiceSaved as PartnerPosServiceSavedListener;
+use Sheba\Algolia\Listeners\PartnerPosServiceSaved as PartnerPosServiceSavedListener;
 use Sheba\Dal\PartnerPosService\Events\PartnerPosServiceUpdated;
 
 class EventsListenerProvider extends EventServiceProvider
 {
     public function boot()
     {
-        Event::listen(PartnerPosServiceSavedEvent::class, PartnerPosServiceSavedListener::class);
+        Event::listen(PartnerPosServiceSaved::class, PartnerPosServiceSavedListener::class);
     }
-}
-
-    /**
-     * @param Dispatcher $events
-     */
-    public function boot(Dispatcher $events)
-    {
-        parent::boot($events);
-        $events->listen(PartnerPosServiceSaved::class, PartnerPosServiceSavedListener::class);
-    }
-
-
 }
