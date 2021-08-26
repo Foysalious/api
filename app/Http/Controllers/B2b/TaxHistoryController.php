9ef145638d81b30050d427add3fb001f6ba89a5b
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2021-08-26 12:16:50 +0600

    Tax Report Download Show Banner Update
    Type(s): ADD
    
    Issue(s):
    - JIRA: B2BBE-1268

--- app/Http/Controllers/B2b/TaxHistoryController.php
+++ app/Http/Controllers/B2b/TaxHistoryController.php
@@ -5,16 +5,20 @@ use Illuminate\Http\Request;
 use App\Models\Business;
 use App\Models\BusinessMember;
 use App\Sheba\Business\Payslip\TaxHistoryList;
+use Sheba\Dal\PayrollSetting\PayrollSettingRepository;
 use Sheba\Helpers\TimeFrame;
 
 class TaxHistoryController extends Controller
 {
     /*** @var TimeFrame $timeFrame*/
     private $timeFrame;
+    /** @var PayrollSettingRepository $payrollSettingsRepo*/
+    private $payrollSettingsRepo;
 
-    public function __construct(TimeFrame $time_frame)
+    public function __construct(TimeFrame $time_frame, PayrollSettingRepository $payroll_settings_repo)
     {
         $this->timeFrame = $time_frame;
+        $this->payrollSettingsRepo = $payroll_settings_repo;
     }
 
     public function index(Request $request, TaxHistoryList $tax_history_list)
@@ -31,6 +35,22 @@ class TaxHistoryController extends Controller
         $total_tax_amount = $tax_report->sum('total_tax_amount_monthly');
         $tax_report = collect($tax_report)->splice($offset, $limit);
         return api_response($request, null, 200, ['tax_history' => $tax_report, 'total_tax_amount' => $total_tax_amount, 'show_download_report_banner' => 1, 'total' => $total_report_count]);
+    }
+
+    public function updateReportShowBanner($payroll_settings_id, Request $request)
+    {
+        $this->validate($request, [
+            'show_banner' => 'required'
+        ]);
+        /** @var Business $business */
+        $business = $request->business;
+        /** @var BusinessMember $business_member */
+        $business_member = $request->business_member;
+        if (!$business_member) return api_response($request, null, 401);
+        $payroll_settings = $this->payrollSettingsRepo->find($payroll_settings_id);
+        if (!$payroll_settings) return api_response($request, null, 401);
+        $this->payrollSettingsRepo->update($payroll_settings, ['show_tax_report_download_banner' => $request->show_banner]);
+        return api_response($request, null, 200);
     }
 
 }
