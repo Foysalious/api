9f659285f941c179009bc39e684ff373be888144
nadim imtiaz shakil <nadim@iamsheba.xyz>
2021-08-26 14:27:32 +0600

    ➕ Blocking customer and resource for concurrent money collect
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MPBE-2354

--- app/Http/Middleware/ConcurrentRequestMiddleware.php
+++ app/Http/Middleware/ConcurrentRequestMiddleware.php
@@ -2,27 +2,47 @@
 
 namespace App\Http\Middleware;
 
+use Carbon\Carbon;
 use Closure;
 use Illuminate\Cache\RateLimiter;
+use Illuminate\Contracts\Cache\Repository as Cache;
+use Illuminate\Support\Facades\Redis;
 
 class ConcurrentRequestMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next, $paramName, $duration=1, $maxHit=1)
+    private $paramNames = ['job'];
+    private $actions = ['collect', 'pay'];
+
+    public function handle($request, Closure $next, $user='resource', $paramName = 'job', $action = 'collect')
     {
+        if (!in_array($paramName, $this->paramNames) || !in_array($action, $this->actions)) return $next($request);
+
+        $duration = constants('MAX_CONCURRENT_TIME');
+        $key = 'job_' . $this->getJobId($request, $paramName);
+
+        if ($data = Redis::get($key)){
+            $data = json_decode($data, true);
+            return response()->json(['code' => 429, 'message' =>  $this->generateMsg($data) . ' You need to wait at least ' . $duration/60 . ' minutes before requesting again.']);
+        }
+
+        $value = ['created_at' => Carbon::now(), 'action' => $action, 'created_by' => $user];
+        Redis::set($key, json_encode($value));
+        Redis::expire($key, $duration);
         return $next($request);
-//        $rateLimiter = app(RateLimiter::class);
-//        $key = $paramName . '-' .$request->route($paramName)->id;
-//        if($rateLimiter->retriesLeft($key, $maxHit) <= 0) {
-//            return response()->json(['code' => 403, 'message' => 'You need to wait at least ' . $duration . 'minutes before requesting again.']);
-//        }
-//        $rateLimiter->hit($key, $duration);
-//        return $next($request);
+    }
+
+    private function getJobId($request, $paramName)
+    {
+        if ($paramName === 'job') {
+            return $request->route($paramName)->id;
+        }
+    }
+
+    private function generateMsg($data)
+    {
+        if ($data['action'] === 'pay' || $data['action'] === 'collect'){
+            return $data['created_by'] . ' requested to ' . $data['action'] . ' for this order a few seconds ago.';
+        }
+        return $data['created_by'] . ' requested to update the order a few seconds ago.';
     }
 }
--- app/Http/Route/Prefix/V2/CustomerRoute.php
+++ app/Http/Route/Prefix/V2/CustomerRoute.php
@@ -87,7 +87,7 @@ class CustomerRoute
                         $api->get('/', 'JobController@show');
                         $api->get('bills', 'JobController@getBills');
                         $api->get('invoice', 'JobController@getInvoice');
-                        $api->get('bills/clear', 'JobController@clearBills');
+                        $api->get('bills/clear', 'JobController@clearBills')->middleware('concurrent_request:customer');
                         $api->post('reschedule', 'JobController@rescheduleJob');
                         $api->get('logs', 'JobController@getLogs');
                         $api->get('logs/order', 'JobController@getOrderLogs');
