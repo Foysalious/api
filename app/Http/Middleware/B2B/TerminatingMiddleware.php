6bfdaa96933ace4a0ae6d3daa528e5d7dc0e2a28
miajee <miajee@sheba.xyz>
2021-08-31 19:15:18 +0600

    Server Log v2

--- app/Http/Middleware/B2B/TerminatingMiddleware.php
+++ app/Http/Middleware/B2B/TerminatingMiddleware.php
@@ -19,7 +19,6 @@ class TerminatingMiddleware
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-
         // Add response time as an HTTP header. For better accuracy ensure this middleware
         if (defined('LARAVEL_START') and $response instanceof Response) {
             $response->headers->add(['X-RESPONSE-TIME' => microtime(true) - LARAVEL_START]);
@@ -39,12 +38,18 @@ class TerminatingMiddleware
     {
         if (defined('LARAVEL_START') and $request instanceof Request) {
             $method = $request->getMethod();
+            $url = $request->url();
             $full_url = $request->fullUrl();
             $port = $request->getPort();
+            $host = $request->getHost();
             $response_time = microtime(true) - LARAVEL_START;
-           # $req = $request->except(['token', 'access_token', 'auth_user', 'manager_member', 'business', 'business_member']);
+            $req = $request->getContent();
             $res = $response->getContent();
-            app('log')->debug("'Response time', 'method: '$method, 'full_url: '$full_url,  'port: '$port, 'response_time: '$response_time, 'res: '$res");
+
+            app('log')->debug("'Response time', 'method': $method, 'url': $url, 'host': $host, 'full_url': $full_url, 'port': $port, 'response_time': $response_time, 'req': $req, 'res': $res", [
+                'req' => $req,
+                'res' => $res,
+            ]);
         }
     }
 }
