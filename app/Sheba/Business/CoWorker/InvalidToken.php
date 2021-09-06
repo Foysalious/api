476dcd7f1fe55d6dd6e202b1a62211a100ad50aa
miajee <miajee@sheba.xyz>
2021-09-06 13:36:35 +0600

    ➕ When Deactive User Logout for all Device
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/B2BBE-1310

--- app/Sheba/Business/CoWorker/InvalidToken.php
+++ app/Sheba/Business/CoWorker/InvalidToken.php
@@ -1,20 +1,28 @@
 <?php namespace Sheba\Business\CoWorker;
 
-use Sheba\Dal\AuthorizationToken\AuthorizationTokenRepositoryInterface;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class InvalidToken
 {
-    /** @var AuthorizationTokenRepositoryInterface $authorizeTokenRepo */
-    private $authorizeTokenRepo;
-
-    public function __construct()
+    public function invalidTheTokens($email)
     {
-        $this->authorizeTokenRepo = app(AuthorizationTokenRepositoryInterface::class);
-    }
+        try {
+            $today = Carbon::now()->format('Y-m-d h:i:s');
+            $sub_minute = Carbon::now()->subMinute();
 
-    public function invalidTheTokens($email = 'b2btest@sheba.xyz')
-    {
-        $invalid_tokens = $this->authorizeTokenRepo->builder()->
-            dd($invalid_tokens);
+            $authorization_tokens = DB::select("SELECT id FROM authorization_tokens WHERE valid_till >= '$today' AND is_blacklisted = 0 
+	   AND authorization_request_id IN ( SELECT id FROM authorization_requests 
+	   WHERE profile_id IN ( SELECT id FROM `profiles` WHERE email = '{$email}') ORDER BY id DESC ) ORDER BY id DESC");
+
+            $authorization_tokens_id = collect($authorization_tokens)->pluck('id')->toArray();
+            $split_data_authorization_tokens_ids = implode(",", $authorization_tokens_id);
+
+            DB::statement("UPDATE authorization_tokens SET authorization_tokens.valid_till = '$sub_minute', authorization_tokens.refresh_valid_till = '$sub_minute' 
+          where authorization_tokens.id  in ($split_data_authorization_tokens_ids)");
+        } catch (\Throwable $e) {
+            app('sentry')->captureException($e);
+            return null;
+        }
     }
 }
