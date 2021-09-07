629b7c55dadf81a51964cf05aaa350138d01ae2c
Foysal <foysal@iamsheba.xyz>
2021-09-07 11:02:18 +0600

    ➕ get pos user url

--- app/Sheba/PosCustomerService/SmanagerUserServerClient.php
+++ app/Sheba/PosCustomerService/SmanagerUserServerClient.php
@@ -31,7 +31,7 @@ class SmanagerUserServerClient
      * @throws SmanagerUserServiceServerError
      */
     private function call($method, $uri, $data = null, $multipart = false)
-    {dd($this->makeUrl($uri));
+    {
         try {
             return json_decode($this->client->request(strtoupper($method), $this->makeUrl($uri), $this->getOptions($data, $multipart))->getBody()->getContents(), true);
         } catch (GuzzleException $e) {
