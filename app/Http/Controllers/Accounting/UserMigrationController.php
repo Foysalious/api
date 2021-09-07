ee78cad27499e9d7967a77a0c21f2e95f1f2dd82
rakib-09 <rakib.rumi09@gmail.com>
2021-09-07 14:29:12 +0600

    ADD: try catch while creating user migration

--- app/Http/Controllers/Accounting/UserMigrationController.php
+++ app/Http/Controllers/Accounting/UserMigrationController.php
@@ -20,24 +20,28 @@ class UserMigrationController extends Controller
 
     public function create(Request $request)
     {
-        $this->validate($request, [
-            'status' => 'required',
-            'user_id' => 'required',
-            'user_type' => 'required'
-        ]);
-        if (!in_array($request->status, UserStatus::get())) {
-            throw new Exception('Invalid Status', 404);
+        try{
+            $this->validate($request, [
+                'status' => 'required',
+                'user_id' => 'required',
+                'user_type' => 'required'
+            ]);
+            if (!in_array($request->status, UserStatus::get())) {
+                throw new Exception('Invalid Status', 404);
+            }
+            if ($request->partner->id != $request->user_id) {
+                throw new Exception('You are not an admin of this partner', 401);
+            }
+            $data = [
+                'status'      => $request->status,
+                'user_id'     => $request->user_id,
+                'user_type'   => strtolower($request->user_type),
+            ];
+            $user = $this->userMigrationRepo->create($data);
+            return api_response($request, $user, 200, ['data' => $user]);
+        } catch (Exception $e) {
+            return api_response($request, null, $e->getCode() == 0 ? 400 : $e->getCode(), ['message' => $e->getMessage()]);
         }
-        if ($request->partner->id != $request->user_id) {
-            throw new Exception('You are not an admin of this partner', 401);
-        }
-        $data = [
-          'status'      => $request->status,
-          'user_id'     => $request->user_id,
-          'user_type'   => strtolower($request->user_type),
-        ];
-        $user = $this->userMigrationRepo->create($data);
-        return api_response($request, $user, 200, ['data' => $user]);
     }
 
     public function show(Request $request, $userId)
