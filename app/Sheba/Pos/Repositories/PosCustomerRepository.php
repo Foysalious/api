54f58f59e59ffd47f89a1a158247e6a62ce61a40
rakib-09 <rakib.rumi09@gmail.com>
2021-09-09 20:13:37 +0600

    ➕ ADD: added user migration check for customer apis Type(s): ADD Issue(s): - JIRA: https://blabol.atlassian.net/browse/MSMEBE-2978

--- app/Sheba/Pos/Repositories/PosCustomerRepository.php
+++ app/Sheba/Pos/Repositories/PosCustomerRepository.php
@@ -4,6 +4,7 @@ use App\Models\Partner;
 use App\Models\PosCustomer;
 use App\Sheba\AccountingEntry\Repository\AccountingDueTrackerRepository;
 use Sheba\AccountingEntry\Exceptions\AccountingEntryServerError;
+use Sheba\DueTracker\DueTrackerRepository;
 use Sheba\DueTracker\Exceptions\InvalidPartnerPosCustomer;
 use Sheba\Repositories\BaseRepository;
 
@@ -19,18 +20,31 @@ class PosCustomerRepository extends BaseRepository
     }
 
     /**
-     * @throws InvalidPartnerPosCustomer
+     * @param Partner $partner
+     * @param $customerId
+     * @param $request
+     * @return int[]
      * @throws AccountingEntryServerError
+     * @throws InvalidPartnerPosCustomer
+     * @throws \Sheba\ExpenseTracker\Exceptions\ExpenseTrackingServerError
      */
-    public function getDueAmountFromDueTracker(Partner $partner, $customerId)
+    public function getDueAmountFromDueTracker(Partner $partner, $customerId, $request)
     {
         $response = [
             'due' => 0,
             'payable' => 0
         ];
+        $request->merge(['customer_id' => $customerId]);
         /** @var AccountingDueTrackerRepository $accDueTrackerRepository */
         $accDueTrackerRepository = app(AccountingDueTrackerRepository::class);
-        $data = $accDueTrackerRepository->setPartner($partner)->dueListBalanceByCustomer($customerId);
+        // checking the partner is migrated to accounting
+        if ($accDueTrackerRepository->isMigratedToAccounting($partner->id)) {
+            $data = $accDueTrackerRepository->setPartner($partner)->dueListBalanceByCustomer($customerId);
+        } else {
+            /** @var DueTrackerRepository $dueTrackerRepo */
+            $dueTrackerRepo = app(DueTrackerRepository::class);
+            $data = $dueTrackerRepo->getDueListByProfile($partner, $request);
+        }
         if ($data['balance']['type'] == 'receivable') {
             $response['due'] = $data['balance']['amount'];
         }
