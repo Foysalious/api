0910cf16ef62a0d99d8dca120c627a64942d4005
Rashad Akhtar <rashad@sheba.xyz>
2021-09-09 12:49:46 +0600

    :heavy_plus_sign: Attenance summary common functions
    Type(s): ADD
    
    Issue(s):
    - JIRA: B2BBE-1299

---
+++ app/Sheba/Business/MyTeamDashboard/CommonFunctions.php
@@ -0,0 +1,70 @@
+<?php namespace Sheba\Business\MyTeamDashboard;
+
+use App\Models\Business;
+use Carbon\Carbon;
+use Sheba\Dal\BusinessHoliday\Contract as BusinessHolidayRepoInterface;
+use Sheba\Dal\BusinessWeekend\Contract as BusinessWeekendRepoInterface;
+use Sheba\Helpers\TimeFrame;
+
+class CommonFunctions
+{
+    /** @var Business */
+    private $business;
+    /** @var Carbon */
+    private $startDate;
+    /** @var Carbon */
+    private $endDate;
+    /** @var BusinessHolidayRepoInterface $businessHoliday */
+    private $businessHoliday;
+    /** @var BusinessWeekendRepoInterface $businessWeekend */
+    private $businessWeekend;
+
+    public function __construct(BusinessHolidayRepoInterface  $business_holiday_repo,
+                                BusinessWeekendRepoInterface  $business_weekend_repo)
+    {
+        $this->businessHoliday = $business_holiday_repo;
+        $this->businessWeekend = $business_weekend_repo;
+    }
+
+    public function setBusiness(Business $business)
+    {
+        $this->business = $business;
+        return $this;
+    }
+
+    public function setSelectedDate(TimeFrame $selected_date)
+    {
+        $this->startDate = $selected_date->start;
+        $this->endDate = $selected_date->end;
+        return $this;
+    }
+
+    public function isWeekendHoliday()
+    {
+        $business_weekend = $this->businessWeekend->getAllByBusiness($this->business);
+        $business_holiday = $this->businessHoliday->getAllByBusiness($this->business);
+
+        $dates_of_holidays_formatted = [];
+        $weekend_day = $business_weekend->pluck('weekday_name')->toArray();
+        foreach ($business_holiday as $holiday) {
+            $start_date = Carbon::parse($holiday->start_date);
+            $end_date = Carbon::parse($holiday->end_date);
+            for ($d = $start_date; $d->lte($end_date); $d->addDay()) {
+                $dates_of_holidays_formatted[] = $d->format('Y-m-d');
+            }
+        }
+
+        return $this->isWeekend($this->startDate, $weekend_day)
+            || $this->isHoliday($this->startDate, $dates_of_holidays_formatted);
+    }
+
+    private function isWeekend(Carbon $date, $weekend_day)
+    {
+        return in_array(strtolower($date->format('l')), $weekend_day);
+    }
+
+    private function isHoliday(Carbon $date, $holidays)
+    {
+        return in_array($date->format('Y-m-d'), $holidays);
+    }
+}
