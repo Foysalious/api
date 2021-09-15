932348d9927c00ab3264af1055f672a2504d751d
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-09-15 11:53:30 +0600

    🚑 Queue Connection For Invoice Generation
    Type(s): HOTFIX

--- app/Jobs/Partner/Pos/GenerateOrderInvoice.php
+++ app/Jobs/Partner/Pos/GenerateOrderInvoice.php
@@ -2,6 +2,7 @@
 
 use App\Jobs\Job;
 use App\Sheba\Pos\Order\Invoice\InvoiceService;
+use Exception;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
@@ -14,16 +15,19 @@ class GenerateOrderInvoice  extends Job implements ShouldQueue
     public function __construct($model)
     {
         $this->model = $model;
+        $this->connection = 'invoice_generation';
         $this->queue = 'invoice_generation';
     }
 
     public function handle()
     {
         try {
-            /** @var InvoiceService $invoiceService */
-            $invoiceService= app(InvoiceService::class);
-            $invoiceService  =  $invoiceService->setPosOrder($this->model);
-            $invoiceService->generateInvoice()->saveInvoiceLink();
+            if ($this->attempts() <= 1) {
+                /** @var InvoiceService $invoiceService */
+                $invoiceService= app(InvoiceService::class);
+                $invoiceService  =  $invoiceService->setPosOrder($this->model);
+                $invoiceService->generateInvoice()->saveInvoiceLink();
+            }
         } catch (Exception $e) {
             logError($e->getMessage());
         }
