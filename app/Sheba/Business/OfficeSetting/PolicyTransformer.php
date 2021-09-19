1d2be7eb4f7d925b4f2f60c2c1f304e25b04678c
Rashad Akhtar <rashad@sheba.xyz>
2021-09-19 12:22:12 +0600

    :heavy_plus_sign: App - Payroll Policy
    Type(s): ADD
    
    Issue(s):
    - JIRA: B2BBE-1333

--- app/Sheba/Business/OfficeSetting/PolicyTransformer.php
+++ app/Sheba/Business/OfficeSetting/PolicyTransformer.php
@@ -1,7 +1,9 @@
 <?php namespace App\Sheba\Business\OfficeSetting;
 
 use League\Fractal\TransformerAbstract;
+use Sheba\Dal\LeaveType\Model as LeaveType;
 use Sheba\Dal\OfficePolicyRule\ActionType;
+use Sheba\Dal\PayrollComponent\PayrollComponent;
 
 class PolicyTransformer extends TransformerAbstract
 {
@@ -12,12 +14,26 @@ class PolicyTransformer extends TransformerAbstract
             'from_days' => $policy->from_days,
             'to_days' => $policy->to_days,
             'action_name' => $policy->action,
+            'action_title' => ucwords(str_replace('_', ' ', $policy->action)),
             'leave_penalty_type' => $policy->action == ActionType::LEAVE_ADJUSTMENT ? (int)$policy->penalty_type : null,
+            'leave_penalty_name' => $policy->action == ActionType::LEAVE_ADJUSTMENT ? $this->getLeavePenaltyName((int)$policy->penalty_type) : null,
             'salary_penalty_type' => $policy->action == ActionType::SALARY_ADJUSTMENT ? $policy->penalty_type : null,
+            'salary_penalty_name' => $policy->action == ActionType::SALARY_ADJUSTMENT ? $this->getSalaryPenaltyName($policy->penalty_type) : null,
             'cash_penalty_amount' => $policy->action == ActionType::CASH_PENALTY ? floatval($policy->penalty_amount) : null,
             'leave_penalty_amount' => $policy->action == ActionType::LEAVE_ADJUSTMENT ? floatval($policy->penalty_amount) : null,
             'salary_penalty_amount' => $policy->action == ActionType::SALARY_ADJUSTMENT ? floatval($policy->penalty_amount) : null,
         ];
+    }
+
+    private function getLeavePenaltyName($leave_type_id)
+    {
+        $leave_type = LeaveType::withTrashed()->findOrFail($leave_type_id);
+        return $leave_type->title;
+    }
+
+    private function getSalaryPenaltyName($component)
+    {
+        return ucwords(str_replace('_', ' ', $component));
     }
 
 }
