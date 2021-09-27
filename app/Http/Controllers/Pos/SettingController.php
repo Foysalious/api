88844d8c6ad560ad62e4593b9220c45ae42d1fc5
Shafiqul Islam <shafiqul@sheba.xyz>
2021-09-27 10:55:12 +0600

    🐛  Use explicit table name
    Type(s): FIX
    
    Issue(s):
    - JIRA: SHEBA-7709

--- app/Http/Controllers/Pos/SettingController.php
+++ app/Http/Controllers/Pos/SettingController.php
@@ -16,7 +16,6 @@ use Sheba\Pos\Repositories\PosSettingRepository;
 use Sheba\Pos\Setting\Creator;
 use Sheba\Transactions\Types;
 use Sheba\Transactions\Wallet\WalletTransactionHandler;
-use Throwable;
 
 class SettingController extends Controller
 {
@@ -30,66 +29,52 @@ class SettingController extends Controller
      */
     public function getSettings(Request $request, Creator $creator, PosSettingRepository $repository)
     {
-        try {
-            /** @var Partner $partner */
-            $partner = $request->partner;
+        /** @var Partner $partner */
+        $partner = $request->partner;
+        $settings = PartnerPosSetting::byPartner($partner->id)->select('id', 'partner_id', 'vat_percentage', 'auto_printing', 'sms_invoice')->first();
+        if (!$settings) {
+            $data = ['partner_id' => $partner->id];
+            $creator->setData($data)->create();
             $settings = PartnerPosSetting::byPartner($partner->id)->select('id', 'partner_id', 'vat_percentage', 'auto_printing', 'sms_invoice')->first();
-            if (!$settings) {
-                $data = ['partner_id' => $partner->id];
-                $creator->setData($data)->create();
-                $settings = PartnerPosSetting::byPartner($partner->id)->select('id', 'partner_id', 'vat_percentage', 'auto_printing', 'sms_invoice')->first();
-            }
-            $settings->vat_registration_number = $partner->basicInformations->vat_registration_number;
-            $settings['has_qr_code'] = ($partner->qr_code_image && $partner->qr_code_account_type) ? 1 : 0;
-            removeRelationsAndFields($settings);
-            return api_response($request, $settings,200, ['settings' => $settings]);
-        } catch (Throwable $e) {
-            app('sentry')->captureException($e);
-            return api_response($request, null, 500);
         }
+        $settings->vat_registration_number = $partner->basicInformations->vat_registration_number;
+        $settings['has_qr_code'] = ($partner->qr_code_image && $partner->qr_code_account_type) ? 1 : 0;
+        removeRelationsAndFields($settings);
+        return api_response($request, $settings,200, ['settings' => $settings]);
     }
 
     public function getPrinterSettings(Request $request, Creator $creator, PosSettingRepository $repository)
     {
-        try {
-            /** @var Partner $partner */
-            $partner = $request->partner;
+        /** @var Partner $partner */
+        $partner = $request->partner;
+        $settings = PartnerPosSetting::byPartner($partner->id)->select('partner_id', 'printer_model', 'printer_name', 'auto_printing')->first();
+        if (!$settings) {
+            $data = ['partner_id' => $partner->id,];
+            $creator->setData($data)->create();
             $settings = PartnerPosSetting::byPartner($partner->id)->select('partner_id', 'printer_model', 'printer_name', 'auto_printing')->first();
-            if (!$settings) {
-                $data = ['partner_id' => $partner->id,];
-                $creator->setData($data)->create();
-                $settings = PartnerPosSetting::byPartner($partner->id)->select('partner_id', 'printer_model', 'printer_name', 'auto_printing')->first();
-            }
-            removeRelationsAndFields($settings);
-            $repository->getTrainingVideoData($settings);
-            return api_response($request, $settings,200, ['data' => $settings]);
-        } catch (Throwable $e) {
-            app('sentry')->captureException($e);
-            return api_response($request, null, 500);
         }
+        removeRelationsAndFields($settings);
+        $repository->getTrainingVideoData($settings);
+        return api_response($request, $settings,200, ['data' => $settings]);
     }
 
-    public function storePosSetting(Request $request, Creator $creator) {
-        try {
-            /** @var Partner $partner */
-            $partner = $request->partner;
-            $partnerPosSetting = PartnerPosSetting::where('partner_id', $partner->id)->first();
-            if (!$partnerPosSetting) $partnerPosSetting = $creator->createPartnerPosSettings($partner);
-            $data = [];
-            $this->setModifier($request->manager_resource);
+    public function storePosSetting(Request $request, Creator $creator)
+    {
+        /** @var Partner $partner */
+        $partner = $request->partner;
+        $partnerPosSetting = PartnerPosSetting::where('partner_id', $partner->id)->first();
+        if (!$partnerPosSetting) $partnerPosSetting = $creator->createPartnerPosSettings($partner);
+        $data = [];
+        $this->setModifier($request->manager_resource);
 
-            if($request->has('vat_percentage')) $data["vat_percentage"] = $request->vat_percentage;
-            if($request->has('sms_invoice')) $data["sms_invoice"] = $request->sms_invoice;
-            if($request->has('auto_printing')) $data["auto_printing"] = $request->auto_printing;
-            if($request->has('printer_name')) $data["printer_name"] = $request->printer_name;
-            if($request->has('printer_model')) $data["printer_model"] = $request->printer_model;
+        if($request->has('vat_percentage')) $data["vat_percentage"] = $request->vat_percentage;
+        if($request->has('sms_invoice')) $data["sms_invoice"] = $request->sms_invoice;
+        if($request->has('auto_printing')) $data["auto_printing"] = $request->auto_printing;
+        if($request->has('printer_name')) $data["printer_name"] = $request->printer_name;
+        if($request->has('printer_model')) $data["printer_model"] = $request->printer_model;
 
-            $partnerPosSetting->update($this->withUpdateModificationField($data));
-            return api_response($request, null, 200);
-        } catch (Throwable $e) {
-            app('sentry')->captureException($e);
-            return api_response($request, null, 500);
-        }
+        $partnerPosSetting->update($this->withUpdateModificationField($data));
+        return api_response($request, null, 200);
     }
 
 
