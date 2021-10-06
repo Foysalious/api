aab98ee4d946a06572a75643bc9e747a578b367d
Rakib <rakib.rumi09@gmail.com>
2021-10-06 16:44:10 +0600

    UPDATE: updated user migration key for acc

--- app/Sheba/Payment/Complete/PaymentLinkOrderComplete.php
+++ app/Sheba/Payment/Complete/PaymentLinkOrderComplete.php
@@ -197,22 +197,22 @@ class PaymentLinkOrderComplete extends PaymentComplete
     private function clearTarget()
     {
         $this->target = $this->paymentLink->getTarget();
-        if ($this->target instanceof PosOrderObject) {
-            $payment_data    = [
-                'pos_order_id' => $this->target->getId(),
-                'amount'       => $this->transaction->getEntryAmount(),
-                'method'       => $this->payment->payable->type,
-                'emi_month'    => $this->transaction->getEmiMonth(),
-                'interest'     => $this->transaction->isPaidByPartner() ? $this->transaction->getInterest() : 0
-            ];
-            /** @var PaymentCreator $payment_creator */
-            $payment_creator = app(PaymentCreator::class);
-            $payment_creator->credit($payment_data, $this->target->getType());
-            if ($this->transaction->isPaidByCustomer()) {
-                $this->target->update(['interest' => 0, 'bank_transaction_charge' => 0]);
-            }
-//            $this->storeAccountingJournal($payment_data);
-        }
+//        if ($this->target instanceof PosOrderObject) {
+//            $payment_data    = [
+//                'pos_order_id' => $this->target->getId(),
+//                'amount'       => $this->transaction->getEntryAmount(),
+//                'method'       => $this->payment->payable->type,
+//                'emi_month'    => $this->transaction->getEmiMonth(),
+//                'interest'     => $this->transaction->isPaidByPartner() ? $this->transaction->getInterest() : 0
+//            ];
+//            /** @var PaymentCreator $payment_creator */
+//            $payment_creator = app(PaymentCreator::class);
+//            $payment_creator->credit($payment_data, $this->target->getType());
+//            if ($this->transaction->isPaidByCustomer()) {
+//                $this->target->update(['interest' => 0, 'bank_transaction_charge' => 0]);
+//            }
+////            $this->storeAccountingJournal($payment_data);
+//        }
         if ($this->target instanceof ExternalPayment) {
             $this->target->payment_id = $this->payment->id;
             $this->target->update();
