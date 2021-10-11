5f3c73f04cff9eababd1f9356a789fedda9eea59
Rakib <rakib.rumi09@gmail.com>
2021-10-11 11:16:29 +0600

    ➕ ADD: added setter for handling partner and order id Type(s): ADD Issue(s): - JIRA: https://blabol.atlassian.net/browse/MSMEBE-3131

--- app/Sheba/Pos/Repositories/PosClientRepository.php
+++ app/Sheba/Pos/Repositories/PosClientRepository.php
@@ -6,8 +6,31 @@ use App\Sheba\Pos\PosClient;
 
 class PosClientRepository extends PosClient
 {
-    public function addOnlinePayment()
+    private $partnerId;
+    private $orderId;
+
+    public function addOnlinePayment(array $data)
     {
 
+    }
+
+    /**
+     * @param mixed $partnerId
+     * @return PosClientRepository
+     */
+    public function setPartnerId($partnerId): PosClientRepository
+    {
+        $this->partnerId = $partnerId;
+        return $this;
+    }
+
+    /**
+     * @param mixed $orderId
+     * @return PosClientRepository
+     */
+    public function setOrderId($orderId): PosClientRepository
+    {
+        $this->orderId = $orderId;
+        return $this;
     }
 }
