4eca4ad3ffa54aebdee0be56cc52612120fdecd5
Foysal <foysal@iamsheba.xyz>
2021-10-13 10:34:11 +0600

    ➕ http_response throw
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-1375

--- app/Http/Controllers/Pos/SettingController.php
+++ app/Http/Controllers/Pos/SettingController.php
@@ -42,7 +42,7 @@ class SettingController extends Controller
             $settings->vat_reg_number = $partner->basicInformations->is_show_vat_reg_number;
             $settings['has_qr_code'] = ($partner->qr_code_image && $partner->qr_code_account_type) ? 1 : 0;
             removeRelationsAndFields($settings);
-            return api_response($request, $settings, 200, ['settings' => $settings]);
+            return http_response($request, $settings, 200, ['settings' => $settings]);
         } catch (Throwable $e) {
             app('sentry')->captureException($e);
             return api_response($request, null, 500);
