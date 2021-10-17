50e14736fe208d6341f75aa6f67d7f14408a7f78
Rakib <rakib.rumi09@gmail.com>
2021-10-17 12:32:50 +0600

    UPDATE: resolved customer from payment link

--- app/Sheba/Pos/Customer/PosCustomerResolver.php
+++ app/Sheba/Pos/Customer/PosCustomerResolver.php
@@ -55,10 +55,15 @@ class PosCustomerResolver
             ->with(['customer' => function($q) {
                 $q->with('profile');
             }])->first();
-        return $this->posCustomerObject->setId($partner_pos_customer->customer_id)->setPartnerId($partner_pos_customer->partner_id)
-            ->setName($partner_pos_customer->nick_name ?: $partner_pos_customer->customer->profile->name)->setIsSupplier($partner_pos_customer->is_supplier)
-            ->setMobile($partner_pos_customer->customer->profile->name)->setEmail($partner_pos_customer->customer->profile->email)
-            ->setGender($partner_pos_customer->customer->profile->gender)->setDob($partner_pos_customer->customer->profile->dob)
+        return $this->posCustomerObject
+            ->setId($partner_pos_customer->customer_id)
+            ->setPartnerId($partner_pos_customer->partner_id)
+            ->setName($partner_pos_customer->nick_name ?: $partner_pos_customer->customer->profile->name)
+            ->setIsSupplier($partner_pos_customer->is_supplier)
+            ->setMobile($partner_pos_customer->customer->profile->name)
+            ->setEmail($partner_pos_customer->customer->profile->email)
+            ->setGender($partner_pos_customer->customer->profile->gender)
+            ->setDob($partner_pos_customer->customer->profile->dob)
             ->setProPic($partner_pos_customer->customer->profile->pro_pic);
 
     }
