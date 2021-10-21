b186761f96d7e993341ee406c639253c10bea05d
Shovan Chowdhury <shovan@sheba.xyz>
2021-10-21 16:49:31 +0600

    [fix] show_vat_registration_number response
    Type(s): FIX
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-1462?atlOrigin=eyJpIjoiODZjN2M4NjAzNjdiNDEwYmIwNmYwMGRiNWQxNTM0ZmUiLCJwIjoiaiJ9

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -1062,10 +1062,10 @@ class PartnerController extends Controller
         $partner->basicInformations()->update($this->withUpdateModificationField(
             [
                 'vat_registration_number' => $request->vat_registration_number,
-                'show_vat_registration_number' => (int) $request->show_vat_registration_number ?: 0
+                'show_vat_registration_number' => (int)$request->show_vat_registration_number ?: 0
             ]
         ));
-        return make_response($request, null, 200, ['msg' => 'Vat Registration Number Update Successfully']);
+        return http_response($request, null, 200, ['msg' => 'Vat Registration Number Update Successfully']);
     }
 
     public function changeLogo(Request $request)
