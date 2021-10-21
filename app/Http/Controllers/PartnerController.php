da7c41685fe63a3ba7e75ca7b67d1fcb37ed471b
Shovan Chowdhury <shovan@sheba.xyz>
2021-10-21 18:16:31 +0600

    [fix] show_vat_registration_number response
    Type(s): FIX
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSBE-1462?atlOrigin=eyJpIjoiODZjN2M4NjAzNjdiNDEwYmIwNmYwMGRiNWQxNTM0ZmUiLCJwIjoiaiJ9

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -1065,7 +1065,7 @@ class PartnerController extends Controller
                 'show_vat_registration_number' => (int)$request->show_vat_registration_number ?: 0
             ]
         ));
-        return make_response($request, null, 200, ['msg' => 'Vat Registration Number Update Successfully']);
+        return api_response($request, null, 200, ['msg' => 'Vat Registration Number Update Successfully']);
     }
 
     public function changeLogo(Request $request)
