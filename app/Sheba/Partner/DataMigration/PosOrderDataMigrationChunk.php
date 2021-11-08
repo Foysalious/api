325a7b7512ba158cb9e9c96ccb71dd8d59abcfb0
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-11-08 20:32:22 +0600

    ⬆️ Pos Order Chunk Queue
    Type(s): UPDATE

---
+++ app/Sheba/Partner/DataMigration/PosOrderDataMigrationChunk.php
@@ -0,0 +1,56 @@
+<?php namespace Sheba\Partner\DataMigration;
+
+
+use App\Models\PosOrder;
+use Illuminate\Support\Facades\Redis;
+use Sheba\Partner\DataMigration\Jobs\PartnerDataMigrationToPosOrderChunk;
+use Sheba\Partner\DataMigration\Jobs\PartnerMigrationCompleteJob;
+use Sheba\Pos\Repositories\PosOrderRepository;
+
+class PosOrderDataMigrationChunk
+{
+    private $partner;
+    const CHUNK_SIZE = 5000;
+    /**
+     * @var PosOrderRepository
+     */
+    private $posOrderRepository;
+    private $currentQueue = 1;
+
+    public function __construct(PosOrderRepository $posOrderRepository)
+    {
+        $this->posOrderRepository = $posOrderRepository;
+    }
+
+    /**
+     * @param $partner
+     * @return PosOrderDataMigrationChunk
+     */
+    public function setPartner($partner)
+    {
+        $this->partner = $partner;
+        return $this;
+    }
+
+    public function generate()
+    {
+        $posOrderCount = PosOrder::withTrashed()->where('partner_id', $this->partner->id)->count();
+        for($i=1 ; $i<=$posOrderCount/self::CHUNK_SIZE; $i++) {
+            $this->setRedisKey();
+            dispatch(new PartnerDataMigrationToPosOrderChunk($i*self::CHUNK_SIZE, self::CHUNK_SIZE, $this->partner, $this->currentQueue));
+            $this->increaseCurrentQueueValue();
+        }
+    }
+
+    private function setRedisKey()
+    {
+        Redis::set('DataMigration::Partner::' . $this->partner->id . '::PosOrderChunk::Queue::' . $this->currentQueue, 'initiated');
+    }
+
+    private function increaseCurrentQueueValue()
+    {
+        $this->currentQueue += 1;
+    }
+
+
+}
