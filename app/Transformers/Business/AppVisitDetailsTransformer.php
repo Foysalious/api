8d20e6c1f1ab26f398b115a88e76b260c18fe27b
miajee <miajee@sheba.xyz>
2021-11-10 17:46:55 +0600

    🐛 Cancel Note  with Date
    Type(s): FIX
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/B2BBE-1494

--- app/Transformers/Business/AppVisitDetailsTransformer.php
+++ app/Transformers/Business/AppVisitDetailsTransformer.php
@@ -185,7 +185,10 @@ class AppVisitDetailsTransformer extends TransformerAbstract
     {
         if ($visit->status === Status::CANCELLED) {
             $visit_note = $visit->visitNotes()->where('status', Status::CANCELLED)->select('note')->orderBy('id', 'DESC')->first();
-            return $visit_note ? $visit_note->note : null;
+            return $visit_note ? [
+                'note' => $visit_note->note,
+                'date' => Carbon::parse($visit_note->date)->format('F d,Y')
+            ] : null;
         } else {
             return null;
         }
