e40443500330bbcb87b859fd4e7ff5209cc12df3
Fariha Nuzhat <fariha@iamsheba.xyz>
2021-11-14 14:44:41 +0600

    ⬆️ Trim email if field is in request
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: MPBE-2733

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -92,7 +92,7 @@ class CustomerController extends Controller
         /** @var Customer $customer */
         $customer = $request->customer;
         $profile = $customer->profile;
-        $request['email'] = trim($request->email);
+        if ($request->has('email')) $request['email'] = trim($request->email);
         $this->validate($request, [
             'name' => 'string',
             'gender'=>'string|in:Male,Female,Other',
