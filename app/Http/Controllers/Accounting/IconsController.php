8e3310579c1e629f34dfbe32d41f51de1e1ed2e8
Rakib <rakib.rumi09@gmail.com>
2021-11-16 09:41:50 +0600

    ♻️ Refactored accounting controllers
    Type(s): REFACTORING
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSMEBE-3253

--- app/Http/Controllers/Accounting/IconsController.php
+++ app/Http/Controllers/Accounting/IconsController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers\Accounting;
 use Exception;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Sheba\AccountingEntry\Exceptions\AccountingEntryServerError;
 use Sheba\AccountingEntry\Repository\IconsRepository;
 use Sheba\ModificationFields;
 
@@ -21,18 +22,12 @@ class IconsController extends Controller
         $this->iconsRepo = $iconsRepo;
     }
 
+    /**
+     * @throws AccountingEntryServerError
+     */
     public function getIcons(Request $request)
     {
-        try {
             $response = $this->iconsRepo->getIconList($request->partner->id);
             return api_response($request, $response, 200, ['data' => $response]);
-        } catch (Exception $e) {
-            return api_response(
-                $request,
-                null,
-                $e->getCode() == 0 ? 400 : $e->getCode(),
-                ['message' => $e->getMessage()]
-            );
-        }
     }
 }
--- app/Http/Controllers/Accounting/ReportsController.php
+++ app/Http/Controllers/Accounting/ReportsController.php
@@ -4,8 +4,10 @@
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
+use Sheba\AccountingEntry\Exceptions\AccountingEntryServerError;
 use Sheba\Reports\Accounting\AccountingReportRepository;
 use Sheba\Reports\Pos\PosReportRepository;
 use Sheba\Reports\Accounting\AccountingReportValidation;
@@ -25,7 +27,8 @@ class ReportsController extends Controller
         $this->accountingReportRepository = $accountingReportRepository;
     }
 
-    public function getCustomerWiseReport(Request $request) {
+    public function getCustomerWiseReport(Request $request)
+    {
         try {
             if ($request->has('download_excel')) {
                 $name = 'Customer Wise Sales Report';
@@ -48,7 +51,8 @@ class ReportsController extends Controller
 
     }
 
-    public function getProductWiseReport(Request $request) {
+    public function getProductWiseReport(Request $request)
+    {
         try {
             if ($request->has('download_excel')) {
                 $name = 'Product Wise Sales Report';
@@ -70,49 +74,35 @@ class ReportsController extends Controller
         }
     }
 
-    public function getAccountingReport(Request $request, $reportType)
+    /**
+     * @throws AccountingEntryServerError
+     */
+    public function getAccountingReport(Request $request, $reportType): JsonResponse
     {
         if ($reportType == "details_ledger_report") {
             $this->validate($request, ['account_id' => 'required']);
         }
 
-        $report_types = [ "profit_loss_report", "journal_report", "balance_sheet_report", "general_ledger_report", "details_ledger_report", "general_accounting_report" ];
+        $report_types = ["profit_loss_report", "journal_report", "balance_sheet_report", "general_ledger_report", "details_ledger_report", "general_accounting_report"];
         $startDate = $request->start_date ? $request->start_date . ' 0:00:00' : Carbon::now()->format('Y-m-d') . ' 0:00:00';
         $endDate = $request->end_date ? $request->end_date . ' 23:59:59' : Carbon::now()->format('Y-m-d') . ' 23:59:59';
-        $acc_id = (int) $request->account_id;
+        $acc_id = (int)$request->account_id;
 
-        if ($endDate < $startDate){
-            return api_response($request,null, 400, ['message' => 'End date can not smaller than start date']);
+        if ($endDate < $startDate) {
+            return api_response($request, null, 400, ['message' => 'End date can not smaller than start date']);
         }
 
         if (in_array($reportType, $report_types)) {
-            try {
-                $response = $this->accountingReportRepository->getAccountingReport($reportType, $request->partner->id, $startDate, $endDate, $acc_id, $request->account_type);
-                return api_response($request, $response, 200, ['data' => $response]);
-            } catch (Exception $e) {
-                return api_response(
-                    $request,
-                    null,
-                    $e->getCode() == 0 ? 400 : $e->getCode(),
-                    ['message' => $e->getMessage()]
-                );
-            }
+            $response = $this->accountingReportRepository->getAccountingReport($reportType, $request->partner->id, $startDate, $endDate, $acc_id, $request->account_type);
+            return api_response($request, $response, 200, ['data' => $response]);
         }
         return api_response($request, null, 402, ['message' => 'Please apply with correct report type.']);
     }
 
-    public function getAccountingReportsList(Request $request)
+    public function getAccountingReportsList(Request $request): JsonResponse
     {
-        try {
-            $response = $this->accountingReportRepository->getAccountingReportsList();
-            return api_response($request, $response, 200, ['data' => $response]);
-        } catch (Exception $e) {
-            return api_response(
-                $request,
-                null,
-                $e->getCode() == 0 ? 400 : $e->getCode(),
-                ['message' => $e->getMessage()]
-            );
-        }
+        $response = $this->accountingReportRepository->getAccountingReportsList();
+        return api_response($request, $response, 200, ['data' => $response]);
+
     }
 }
