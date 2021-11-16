ea4e45b644ce1140f678b2d9f00dfb892b0a7813
Sayed Yeamin Arafat <yeamin@sheba.xyz>
2021-11-16 16:59:12 +0600

    ⬆️ Reseller payment name added for payment store ssl
    Type(s): UPDATE

--- app/Sheba/Payment/Methods/Ssl/Stores/DynamicSslStore.php
+++ app/Sheba/Payment/Methods/Ssl/Stores/DynamicSslStore.php
@@ -34,7 +34,7 @@ class DynamicSslStore extends SslStore
 
     public function getName()
     {
-//        $storeAccount        = $this->getStoreAccount(PaymentStrategy::SSL);
-//        return $storeAccount->name;
+        $storeAccount        = $this->getStoreAccount(PaymentStrategy::SSL);
+        return $storeAccount->name;
     }
 }
