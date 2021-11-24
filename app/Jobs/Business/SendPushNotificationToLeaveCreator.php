e23da0e968eb4e8bda0cb75e7635633a9c6f4310
miajee <miajee@sheba.xyz>
2021-11-24 18:41:59 +0600

    ♻️ Notification Add In Queue [continue]
    Type(s): REFACTORING
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/B2BBE-1525

---
+++ app/Jobs/Business/SendPushNotificationToLeaveCreator.php
@@ -0,0 +1,33 @@
+<?php namespace App\Jobs\Business;
+
+use App\Sheba\Business\BusinessQueue;
+use Sheba\PushNotificationHandler;
+
+class SendPushNotificationToLeaveCreator extends BusinessQueue
+{
+    private $pushNotification;
+
+    public function __construct()
+    {
+        $this->pushNotification = new PushNotificationHandler();
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        $topic = config('sheba.push_notification_topic_name.employee') . $business_member->member->id;
+        $channel = config('sheba.push_notification_channel_name.employee');
+        $sound = config('sheba.push_notification_sound.employee');
+        $push_notification_data = [
+            "title" => 'Leave request update',
+            "message" => "Your leave request has been $status",
+            "event_type" => 'leave',
+            "event_id" => $this->leave->id,
+            "sound" => "notification_sound",
+            "channel_id" => $channel,
+            "click_action" => "FLUTTER_NOTIFICATION_CLICK"
+        ];
+
+        $this->pushNotification->send($push_notification_data, $topic, $channel, $sound);
+    }
+}
