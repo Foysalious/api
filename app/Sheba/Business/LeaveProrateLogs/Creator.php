f2dbe583e890f9a655ccf6c4363be8ac0954f5d3
Asad Ahmed <asad@sheba.xyz>
2021-11-25 12:50:05 +0600

    Manual Leave Prorate Log Create
    Type(s): ADD
    
    Issue(s):
    - JIRA: B2BBE-1515

--- app/Sheba/Business/LeaveProrateLogs/Creator.php
+++ app/Sheba/Business/LeaveProrateLogs/Creator.php
@@ -5,12 +5,12 @@ use Sheba\Dal\LeaveProrateLog\Contract as LeaveProrateLogRepository;
 class Creator
 {
     private $prorateTypeArr = [
-                                'fiscal_year' => 'Fiscal year',
-                                'leave_policy' => 'Leave Policy',
-                                'manual' => 'Manual Prorate',
-                                'leave_type_create' => 'Leave Type Create',
-                                'leave_type_update' => 'Leave Type Update'
-                            ];
+        'fiscal_year' => 'Fiscal year',
+        'leave_policy' => 'Leave Policy',
+        'manual' => 'Manual Prorate',
+        'leave_type_create' => 'Leave Type Create',
+        'leave_type_update' => 'Leave Type Update'
+    ];
     private $businessMember;
     private $prorateType;
     private $proratedLeaveDays;
@@ -75,7 +75,7 @@ class Creator
             'leave_type_target_id' => $this->leaveType->id,
             'leave_type_total_days' => $this->previousLeaveTypeTotalDays,
             'prorated_leave_type_total_days' => $this->proratedLeaveDays,
-            'log' => $this->leaveType->title.' '.$this->proratedLeaveDays.' days has been prorated according to '.$this->prorateTypeArr[$this->prorateType]
+            'log' => $this->leaveType->title . ' ' . $this->proratedLeaveDays . ' days has been prorated according to ' . $this->prorateTypeArr[$this->prorateType]
         ];
     }
 
