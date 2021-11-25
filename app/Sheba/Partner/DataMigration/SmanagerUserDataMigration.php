5d4b7272739641d7f6a2562ec3c9a924b4adb4ce
Ebne Faiz Al-Amin <faiz@sheba.xyz>
2021-11-25 15:47:35 +0600

    ⬆️ Data migration - time subtract for date_time fields updated
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-1629

--- app/Sheba/Partner/DataMigration/SmanagerUserDataMigration.php
+++ app/Sheba/Partner/DataMigration/SmanagerUserDataMigration.php
@@ -103,8 +103,8 @@ class SmanagerUserDataMigration
             ->select('partner_pos_customers.id', 'partner_pos_customers.customer_id as previous_id', 'partner_pos_customers.partner_id', $query,
                 'partner_pos_customers.is_supplier', 'partner_pos_customers.note', 'profiles.mobile', 'profiles.email', 'profiles.fb_id', 'profiles.google_id',
                 'profiles.mobile_verified', 'profiles.email_verified', 'profiles.email_verified_at', 'profiles.address', 'profiles.gender',
-                'profiles.dob', 'profiles.pro_pic', 'profiles.created_by_name', 'profiles.updated_by_name', 'profiles.created_at',
-                'profiles.updated_at')
+                'profiles.dob', 'profiles.pro_pic', 'profiles.created_by_name', 'profiles.updated_by_name',
+                DB::raw('SUBTIME(profiles.created_at,"6:00:00") as created_at, SUBTIME(profiles.updated_at,"6:00:00") as updated_at'))
             ->get();
     }
 
