b2ea8c6d58fbf10be772b564daa18582094a7eed
miajee <miajee@sheba.xyz>
2021-11-25 12:07:54 +0600

    ♻️ New Queue Add Approval Request
    Type(s): REFACTORING
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/B2BBE-1525

--- app/Jobs/Business/SendPushNotificationToLeaveCreator.php
+++ app/Jobs/Business/SendPushNotificationToLeaveCreator.php
@@ -1,5 +1,6 @@
 <?php namespace App\Jobs\Business;
 
+use Sheba\Dal\Leave\LeaveStatusPresenter as LeaveStatusPresenter;
 use App\Sheba\Business\BusinessQueue;
 use Sheba\PushNotificationHandler;
 
@@ -7,14 +8,18 @@ class SendPushNotificationToLeaveCreator extends BusinessQueue
 {
     private $pushNotification;
 
-    public function __construct()
+    public function __construct($leave)
     {
+        $this->leave = $leave;
         $this->pushNotification = new PushNotificationHandler();
         parent::__construct();
     }
 
     public function handle()
     {
+        $status = LeaveStatusPresenter::statuses()[$this->leave->status];
+        $business_member = $this->leave->businessMember;
+
         $topic = config('sheba.push_notification_topic_name.employee') . $business_member->member->id;
         $channel = config('sheba.push_notification_channel_name.employee');
         $sound = config('sheba.push_notification_sound.employee');
