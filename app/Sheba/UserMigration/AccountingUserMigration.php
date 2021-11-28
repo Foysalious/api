bdda3993bfc38bff4d232e473d6b28f9ccda95ef
Rakib <rakib.rumi09@gmail.com>
2021-11-28 17:50:06 +0600

    ➕ Added access code
    Type(s): ADD

--- app/Sheba/UserMigration/AccountingUserMigration.php
+++ app/Sheba/UserMigration/AccountingUserMigration.php
@@ -98,13 +98,13 @@ class AccountingUserMigration extends UserMigrationRepository
     {
         if ((int)$appVersion >= $modulePayload['app_version']) {
             return [
-                'code' => 200,
+                "access" => true,
                 'message' => 'You are allowed to use.'
             ];
 
         }
         return [
-            "code" => 403,
+            "access" => false,
             "icon" => Constants::$accounting_migration_url . '/accounting_pending.png',
             "header" => "নতুন হিসাব খাতায় আপগ্রেড করেছেন।",
             "message" => "<center>নতুন হিসাব খাতা ব্যবহার করতে নতুন সিস্টেম <br />
