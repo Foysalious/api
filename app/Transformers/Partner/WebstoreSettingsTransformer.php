a7fdd14eef31110c5d9ecd95bdfa7ffd4c509f0e
Ebne Faiz Al-Amin <faiz@sheba.xyz>
2021-11-29 18:20:46 +0600

    ⬆️ Webstore url field add in webstore settings api
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSBE-1867

--- app/Transformers/Partner/WebstoreSettingsTransformer.php
+++ app/Transformers/Partner/WebstoreSettingsTransformer.php
@@ -14,6 +14,7 @@ class WebstoreSettingsTransformer extends TransformerAbstract
         return [
             'name' => $partner->name,
             'sub_domain' => $partner->sub_domain,
+            'url' => $this->getMigratedPartnerSubDomain($partner),
             'has_webstore' => $partner->has_webstore,
             'is_webstore_published' => $partner->is_webstore_published,
             'logo' => $partner->logo,
@@ -41,5 +42,14 @@ class WebstoreSettingsTransformer extends TransformerAbstract
         /** @var OrderService $orderService */
         $orderService = app(OrderService::class);
         return $orderService->setPartnerId($partner->id)->getPartnerDetails()['partner']['delivery_charge'];
+    }
+
+    public function getMigratedPartnerSubDomain(Partner $partner)
+    {
+        if($partner->isMigrated(Modules::POS)) {
+            return config('sheba.webstore_url_new');
+        } else {
+            return config('sheba.webstore_url');
+        }
     }
 }
