a7f433e097d53a589cecbe36fc627deef05d7eb4
Rakib <rakib.rumi09@gmail.com>
2021-11-30 19:51:57 +0600

    ➕ Added pos in user migration list
    Type(s): ADD

--- config/user_migration.php
+++ config/user_migration.php
@@ -8,11 +8,11 @@ return [
             'status'        => null,
             'priority'      => 1
         ],
-//        [
-//            'key'           => 'pos',
-//            'app_version'   => 230600,
-//            'status'        => null,
-//            'priority'      => 2
-//        ]
+        [
+            'key'           => 'pos',
+            'app_version'   => 230600,
+            'status'        => null,
+            'priority'      => 2
+        ]
     ]
 ];
