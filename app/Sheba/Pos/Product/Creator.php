e59e703deae138487bda8d5a5ecaf2d8e1dab027
Abdullah Arnab <abdullah.arnab@sheba.xyz>
2021-12-05 10:27:13 +0600

    ⬆️ Fix Missing Accounting Info Code
    Type(s): UPDATE

--- app/Sheba/Pos/Product/Creator.php
+++ app/Sheba/Pos/Product/Creator.php
@@ -60,9 +60,10 @@ class Creator
         $this->data['cost'] = (double)$this->data['cost'];
         $this->format();
         $image_gallery = isset($this->data['image_gallery']) ? $this->data['image_gallery'] : null;
-        $this->data = array_except($this->data, ['remember_token', 'discount_amount', 'end_date', 'manager_resource', 'partner', 'category_id', 'image_gallery']);
+        $this->data = array_except($this->data, ['remember_token', 'discount_amount', 'end_date', 'manager_resource', 'partner', 'category_id', 'image_gallery','accounting_info']);
         $partner_pos_service = $this->serviceRepo->save($this->data + (new RequestIdentification())->get());
-        if ($image_gallery) $this->storeImageGallery($partner_pos_service, json_decode($image_gallery, true));
+        $this->savePartnerPosServiceBatch($partner_pos_service, $this->data['stock'], $this->data['cost']);
+        if ($image_gallery) $this->storeImageGallery($partner_pos_service, json_decode($image_gallery,true));
         return $partner_pos_service;
     }
 
