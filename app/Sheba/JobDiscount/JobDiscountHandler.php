970aa358ce847fd8eb68df88062dd412086a241f
Fariha Nuzhat <fariha@iamsheba.xyz>
2021-12-12 13:11:02 +0600

    ⬆️ Check job discount
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: MPBE-2488

--- app/Sheba/JobDiscount/JobDiscountHandler.php
+++ app/Sheba/JobDiscount/JobDiscountHandler.php
@@ -105,7 +105,7 @@ class JobDiscountHandler
 
     public function hasDiscount()
     {
-        return !is_null($this->discount) && (double) $this->discount !== 0;
+        return !is_null($this->discount) && $this->discount !== 0;
     }
 
     public function create(Job $job)
