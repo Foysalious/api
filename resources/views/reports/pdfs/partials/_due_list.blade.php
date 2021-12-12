ee28e361bb2b1df92a6b511d7593e9acf086c1d3
Rakib <rakib.rumi09@gmail.com>
2021-12-12 10:24:35 +0600

    🐛  Fix due tracker pdf
    Type(s): FIX
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSMEBE-3465

--- resources/views/reports/pdfs/partials/_due_list.blade.php
+++ resources/views/reports/pdfs/partials/_due_list.blade.php
@@ -14,7 +14,7 @@
             <td style="width: 8%">{{++$key}}</td>
             <td style="width: 40%">{{$item['customer_name']}}</td>
             <td style="text-align: center; width: 20%">{{$item['customer_mobile']}}</td>
-            @if($item['balance_type'] === 'account_receivable')
+            @if($item['balance_type'] === 'account_receivable' || $item['balance_type'] === 'due')
                 <td style="text-align: center;color: #219653; width: 16%">0</td>
                 <td style="text-align: center;color: #DC1E1E; width: 16%">{{$item['balance'] }}</td>
             @else
