ec112960e11e35a8f3da62e25485edd59c7b4358
nadim imtiaz shakil <nadim@sheba.xyz>
2021-12-14 16:56:11 +0600

    ➕ Blocking order status and collection concurrency
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MPBE-2694

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -8,6 +8,7 @@ use App\Http\Middleware\Authenticate;
 use App\Http\Middleware\B2B\OrderMiddleware;
 use App\Http\Middleware\B2B\TerminatingMiddleware;
 use App\Http\Middleware\BusinessManagerAuthMiddleware;
+use App\Http\Middleware\ConcurrentOrderServeCollectMiddleware;
 use App\Http\Middleware\ConcurrentRequestMiddleware;
 use App\Http\Middleware\CheckUserMigrationRunningMiddleware;
 use App\Http\Middleware\Cors2MiddleWare;
@@ -128,6 +129,7 @@ class Kernel extends HttpKernel
         'partner.status'=> PartnerStatusAuthMiddleware::class,
         'concurrent_request' => ConcurrentRequestMiddleware::class,
         'userMigration.auth' => UserMigrationMiddleware::class,
-        'userMigration.check_status' => CheckUserMigrationRunningMiddleware::class
+        'userMigration.check_status' => CheckUserMigrationRunningMiddleware::class,
+        'job.concurrent_serve_collect' => ConcurrentOrderServeCollectMiddleware::class,
     ];
 }
