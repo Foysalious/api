e6b3755b7447b1687c66ff922417ce215e78d6f8
miajee <miajee@sheba.xyz>
2021-12-14 16:29:33 +0600

    ➕ New Column Add in Team Visit Excel
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/B2BBE-1563

--- app/Sheba/Business/EmployeeTracking/EmployeeVisit/Excel.php
+++ app/Sheba/Business/EmployeeTracking/EmployeeVisit/Excel.php
@@ -19,7 +19,7 @@ class Excel
     {
         $header = $this->getHeaders();
         $this->makeData();
-        $file_name = 'Employee_visit_report_'. Carbon::now()->timestamp;
+        $file_name = 'Employee_visit_report_' . Carbon::now()->timestamp;
         EmployeeVisitExcel::create($file_name, function ($excel) use ($header) {
             $excel->sheet('data', function ($sheet) use ($header) {
                 $sheet->fromArray($this->data, null, 'A1', false, false);
@@ -40,13 +40,26 @@ class Excel
     {
         foreach ($this->employeesVisitData as $visit) {
             array_push($this->data, [
-                'schedule_date' => $visit['schedule_date'],
-                'employee_name' => $visit['profile']['name'],
-                'department' => $visit['profile']['department'],
-                'title' => $visit['title'],
-                'description' => $visit['description'],
-                'status' => $visit['status'],
-            ]);
+                    'schedule_date' => $visit['schedule_date'],
+                    'employee_id' => $visit['profile']['employee_id'],
+                    'employee_name' => $visit['profile']['name'],
+                    'department' => $visit['profile']['department'],
+                    'title' => $visit['title'],
+                    'description' => $visit['description'],
+                    'status' => $visit['status'],
+                    'picture' => $visit['photos'],
+                    'started_location' => $visit['visit_started_location'],
+                    'started_time' => $visit['visit_started_date'],
+                    'reached_location' => $visit['visit_reached_location'],
+                    'reached_date' => $visit['visit_reached_date'],
+                    'completed_location' => $visit['visit_completed_location'],
+                    'complete_date' => $visit['visit_complete_date'],
+                    'total_hours' => $visit['total_hours'],
+                    'cancelled_at' => $visit['visit_cancelled_at'],
+                    'cancelled_note' => $visit['visit_cancelled_note'],
+                    'rescheduled_at' => $visit['visit_reschedule_dates'],
+                    'rescheduled_notes' => $visit['visit_reschedule_notes'],
+                ] + $visit['all_notes']);
         }
 
     }
@@ -57,7 +70,9 @@ class Excel
     private function getHeaders()
     {
         return [
-            'Visit Date', 'Employee Name', 'Department', 'Title', 'Description', 'Status',
+            'Visit Date', 'Employee Id', 'Employee Name', 'Department', 'Title', 'Description', 'Status',
+            'Pictures', 'Start Location', 'Start Time', 'Reached Location', 'Reached Time', 'End Location', 'End Time',
+            'Total Visit Time', 'Cancelled At', 'Cancelled Reason', 'Rescheduled At', 'Rescheduled Reason'
         ];
     }
 }
