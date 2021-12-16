653a556ef4bb1a6e1bffa7656cae854a9c1535e7
Shovan Chowdhury <shovan@sheba.xyz>
2021-12-16 18:17:35 +0600

    event listner

--- app/Sheba/Algolia/Listeners/PartnerSaved.php
+++ app/Sheba/Algolia/Listeners/PartnerSaved.php
@@ -6,15 +6,15 @@ use Sheba\Dal\Partner\Events\PartnerSaved as PartnerSavedEvent;
 class PartnerSaved
 {
 
-//
-//    public function handle(PartnerSavedEvent $event)
-//    {
-//        /** @var Partner $partner */
-//        $partner = $event->model;
-//        if ($partner->is_webstore_published == 1)
-//            $partner->pushToIndex();
-//        if ($partner->is_webstore_published == 0)
-//            $partner->removeFromIndex();
-//    }
+
+   public function handle(PartnerSavedEvent $event)
+   {
+      /* /** @var Partner $partner */
+       /* $partner = $event->model;
+         if ($partner->is_webstore_published == 1)
+             $partner->pushToIndex();
+        if ($partner->is_webstore_published == 0)
+             $partner->removeFromIndex();*/
+   }
 
 }
