1e88cc7ec309764064c785dfa3c3a52f92a7406c
Ebne Faiz Al-Amin <faiz@sheba.xyz>
2021-12-21 16:18:08 +0600

    ⬆️ partner sync  attempt added
    Type(s): UPDATE

--- app/Jobs/Partner/SyncPartnersSetting.php
+++ app/Jobs/Partner/SyncPartnersSetting.php
@@ -3,6 +3,7 @@
 use App\Jobs\Job;
 use App\Sheba\InventoryService\Services\SyncService\PartnerInventorySetting;
 use App\Sheba\PosOrderService\Services\SyncService\PartnerPosSetting;
+use Exception;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
@@ -33,12 +34,13 @@ class SyncPartnersSetting extends Job implements ShouldQueue
 
     public function handle()
     {
+        if ($this->attempts() > 1) return;
         try {
             /** @var PartnerInventorySetting|PartnerPosSetting $sync_service */
             $sync_service = App::make($this->syncServiceClass);
             $sync_service->setModel($this->model)->syncSettings();
-        } catch (\Exception $e) {
-            logError($e->getMessage());
+        } catch (Exception $e) {
+            app('sentry')->captureException($e);
         }
     }
 }
