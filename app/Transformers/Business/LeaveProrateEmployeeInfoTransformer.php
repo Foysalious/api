d7c8c438d9dfb196053786d4987fde3e4abcd62d
Asad Ahmed <asad@sheba.xyz>
2021-12-21 10:32:51 +0600

    Leave Type Find with Trashed
    Type(s): ADD
    
    Issue(s):
    - JIRA B2BBE-1631

--- app/Transformers/Business/LeaveProrateEmployeeInfoTransformer.php
+++ app/Transformers/Business/LeaveProrateEmployeeInfoTransformer.php
@@ -1,16 +1,20 @@
 <?php namespace App\Transformers\Business;
 
 use League\Fractal\TransformerAbstract;
+use Sheba\Dal\LeaveType\Contract as LeaveTypeRepository;
 use Sheba\Repositories\Interfaces\MemberRepositoryInterface;
 
 class LeaveProrateEmployeeInfoTransformer extends TransformerAbstract
 {
     /*** @var MemberRepositoryInterface $memberRepository*/
     private $memberRepository;
+    /*** @var LeaveTypeRepository $leaveTypeRepository*/
+    private $leaveTypeRepository;
 
     public function __construct()
     {
         $this->memberRepository = app(MemberRepositoryInterface::class);
+        $this->leaveTypeRepository = app(LeaveTypeRepository::class);
     }
 
     public function transform($prorate)
@@ -21,18 +25,19 @@ class LeaveProrateEmployeeInfoTransformer extends TransformerAbstract
         $profile = $member->profile;
         $department = $business_member->department();
         $updated_by = $prorate->updated_by_name ? $this->memberRepository->find($prorate->updated_by) : null;
+        $leave_type = $this->leaveTypeRepository->findWithTrashed($prorate->leave_type_id);
         return [
             'id' => $prorate->id,
             'employee_id' => $business_member->employee_id,
             'profile_id' => $profile->id,
             'business_member_id' => $business_member->id,
-            'leave_type_id' => $prorate->leaveType->id,
+            'leave_type_id' => $leave_type->id,
             'employee_department_id' => $department ? $department->id : null,
             'employee_name' => $profile->name,
             'employee_pro_pic' => $profile->pro_pic,
             'employee_department_name' => $department ? $department->name : null,
             'business_member_joined_date' => $business_member_join_date ? $business_member_join_date->format('F Y') : null,
-            'leave_type' => $prorate->leaveType->title,
+            'leave_type' => $leave_type->title,
             'total_days' => $prorate->total_days,
             'is_auto_prorated' => $prorate->is_auto_prorated,
             'created_at' => $prorate->created_at->format('Y-m-d H:i:s'),
