8472fc40cf9588ec8f3353e204916bbaf9d7fed9
Arafat <yeamin@sheba.xyz>
2022-01-01 18:47:49 +0600

    [UPDATE] #payment link tax is set to 0

--- config/payment_link.php
+++ config/payment_link.php
@@ -2,7 +2,7 @@
 
 return [
     "payment_link_commission" => 2,
-    "payment_link_tax"        => 3,
+    "payment_link_tax"        => 0,
     "step_margin"             => .25,
     "minimum_percentage"      => 2,
     "maximum_percentage"      => 3
