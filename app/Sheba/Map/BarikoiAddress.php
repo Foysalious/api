f2fd4e0a9bc2122bf18f4292adfbc8880c498165
nadim imtiaz shakil <nadim@sheba.xyz>
2022-01-05 21:23:47 +0600

    🐛  Fixed setter calling errors
    Type(s): FIX
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MPBE-3116

--- app/Sheba/Map/BarikoiAddress.php
+++ app/Sheba/Map/BarikoiAddress.php
@@ -19,14 +19,14 @@ class BarikoiAddress extends Address
         return $this;
     }
 
-    public function setHouseNo(string $houseNo)
+    public function setHouseNo($houseNo)
     {
         $houseNo = trim($houseNo);
         if (substr($houseNo, 0, 5) === 'House') $this->houseNo = trim(substr($houseNo, 5));
         else $this->houseNo = $houseNo;
     }
 
-    public function setRoadNo(string $roadNo)
+    public function setRoadNo($roadNo)
     {
         $roadNo = trim($roadNo);
         if (substr($roadNo, 0, 4) === 'Road') $this->roadNo = trim(substr($roadNo, 4));
@@ -37,7 +37,7 @@ class BarikoiAddress extends Address
     {
         if (!is_null($areaComponents->sub_area)) $this->area = $areaComponents->sub_area . ', ';
 
-        $this->area .= $areaComponents->area;
+        $this->area .= trim($areaComponents->area);
     }
 
     public function getRoadNo()
