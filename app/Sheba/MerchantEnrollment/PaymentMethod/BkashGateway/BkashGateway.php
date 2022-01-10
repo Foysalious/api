2aa6cd66251058c1a136e287b52b7d64181ce924
Sayed Yeamin Arafat <yeamin@sheba.xyz>
2022-01-10 16:41:34 +0600

    [UPDATE] #spelling mistake

--- app/Sheba/MerchantEnrollment/PaymentMethod/BkashGateway/BkashGateway.php
+++ app/Sheba/MerchantEnrollment/PaymentMethod/BkashGateway/BkashGateway.php
@@ -2,14 +2,32 @@
 
 namespace Sheba\MerchantEnrollment\PaymentMethod\BkashGateway;
 
+use App\Sheba\MerchantEnrollment\PaymentMethod\Completion;
+use Sheba\MerchantEnrollment\Exceptions\InvalidListInsertionException;
 use Sheba\MerchantEnrollment\MEFFormCategory\CategoryGetter;
 use Sheba\MerchantEnrollment\MEFFormCategory\MEFFormCategory;
 use Sheba\MerchantEnrollment\PaymentMethod\PaymentMethod;
+use Sheba\MerchantEnrollment\PaymentMethod\PaymentMethodCompletion;
+use Sheba\ResellerPayment\Exceptions\InvalidKeyException;
 
 class BkashGateway extends PaymentMethod
 {
     public function categoryDetails(MEFFormCategory $category): CategoryGetter
     {
         return $category->get();
+    }
+
+    public function getStaticCategoryFormData(MEFFormCategory $category)
+    {
+        return $category->getFormFields();
+    }
+
+    /**
+     * @throws InvalidListInsertionException
+     * @throws InvalidKeyException
+     */
+    public function completion(): PaymentMethodCompletion
+    {
+        return (new Completion())->setPartner($this->partner)->setPaymentGateway($this->payment_method)->get();
     }
 }
