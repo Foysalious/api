d61993e0bc2c76e02250e9f371b23094f822c49a
Shovan Chowdhury <shovan@sheba.xyz>
2022-01-11 18:44:56 +0600

    personal info api
    Type(s): WIP
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSMEBE-3669?atlOrigin=eyJpIjoiZWFiZWVkYjFmOWVlNGRkYWFjNGRmNTNiMmVkOTFhYTMiLCJwIjoiaiJ9
    https://blabol.atlassian.net/browse/MSMEBE-3670?atlOrigin=eyJpIjoiODVmNjlkZmRkZDJmNGZlOWEzZDFjNjQ3NTk4ZWQ1ODkiLCJwIjoiaiJ9

---
+++ app/Sheba/MerchantEnrollment/PersonalInformation.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Sheba\MerchantEnrollment;
+
+use Sheba\MerchantEnrollment\PartnerAllInformation;
+use Sheba\ModificationFields;
+
+class PersonalInformation extends PartnerAllInformation
+{
+    use ModificationFields;
+
+    public function personal_get(): array
+    {
+        return $this->getFormFieldValues();
+    }
+
+    public function personal_post($post_data)
+    {
+        foreach ($this->formItems as $item) {
+            if ($item['is_editable']) {
+                if (isset($post_data[$item['id']])) {
+                    $key = $item['id'];
+                    if (isset($item['data_source']))
+                        $this->{$item['data_source']}->{$item['data_source_id']} = $post_data[$key];
+                }
+            }
+        }
+        $this->partner_resource_profile->save();
+    }
+
+    /**
+     * @param $category_code = "institution" | "personal" | ""
+     * @return array
+     */
+    public function getByCode($category_code): array
+    {
+        return $this->personal_get();
+    }
+
+    public function postByCode($category_code, $post_data)
+    {
+        $this->personal_post($post_data);
+    }
+
+
+}
