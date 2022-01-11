d4ec59aad589691258404ff89d661f38918deaa6
tonmoy <tonmoy@sheba.xyz>
2022-01-11 14:32:41 +0600

    ➕ USAGE LOG ACCOUNTING
    Type(s): ADD

--- app/Sheba/Usage/Partner.php
+++ app/Sheba/Usage/Partner.php
@@ -11,12 +11,18 @@ class Partner
     const PRODUCT_LINK                = 'product_link';
     const PAYMENT_LINK                = 'payment_link';
     const DUE_TRACKER_TRANSACTION     = 'due_tracker_transaction';
-
     const TRANSACTION_COMPLETE        = 'transaction_complete';
     const TOPUP_COMPLETE              = 'topup_complete';
-
-
     const CREATE_CUSTOMER             = 'customer_create';
-
     const PAYMENT_COLLECT             = 'payment_collect';
+    const DUE_ENTRY_UPDATE            = 'due_entry_update';
+    const DUE_ENTRY_DELETE            = 'due_entry_delete';
+    const EXPENSE_ENTRY_UPDATE        = 'expense_entry_update';
+    const EXPENSE_ENTRY_DELETE        = 'expense_entry_delete';
+    const TRANSFER_ENTRY_UPDATE       = 'transfer_entry_update';
+    const REMINDER_SET                = 'due_reminder_set';
+    const DUE_SMS_SEND                = 'due_sms_send';
+    const TRANSFER_ENTRY              = 'account_transfer_entry';
+    const ENTRY_DELETE                = 'entry_delete';
+    const REPORT_DOWNLOAD             = 'accounting_report_download';
 }
