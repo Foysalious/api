46b19d10950f74f1f9567cdf37cf73bdee42ac52
Asad Ahmed <asad@sheba.xyz>
2022-01-12 21:32:23 +0600

    Gross Salary Breakdown Histor
    Type(s): ADD
    Issue(s):
    - JIRA: B2BBE-1673

--- app/Sheba/Business/PayrollComponent/Components/GrossComponents/GrossBreakdownHistory/Creator.php
+++ app/Sheba/Business/PayrollComponent/Components/GrossComponents/GrossBreakdownHistory/Creator.php
@@ -1,9 +1,22 @@
 <?php namespace App\Sheba\Business\PayrollComponent\Components\GrossComponents\GrossBreakdownHistory;
 
+use Carbon\Carbon;
+use Sheba\Dal\GrossSalaryBreakdownHistory\GrossSalaryBreakdownHistoryRepository;
+
 class Creator
 {
+
+    const INDIVIDUAL_SALARY = 'individual_salary';
+    const BREAKDOWN_GROSS_SALARY = 'breakdown_gross_salary';
     private $businessMember;
     private $grossSalaryBreakdown;
+    /*** @var GrossSalaryBreakdownHistoryRepository $grossSalaryBreakdownHistoryRepository*/
+    private $grossSalaryBreakdownHistoryRepository;
+
+    public function __construct()
+    {
+        $this->grossSalaryBreakdownHistoryRepository = app(GrossSalaryBreakdownHistoryRepository::class);
+    }
 
     public function setBusinessMember($business_member)
     {
@@ -13,13 +26,30 @@ class Creator
 
     public function setGrossSalaryBreakdown($gross_salary)
     {
-        $this->grossSalaryBreakdown = $gross_salary;
+        $this->grossSalaryBreakdown = json_decode($gross_salary,1);
         return $this;
     }
 
     public function update()
     {
-        
+        $data = [];
+        foreach ($this->grossSalaryBreakdown as $breakdown) {
+            $data [] = [
+                'id' => $breakdown['id'],
+                'name' => $breakdown['name'],
+                'value' => $breakdown['value'],
+                'is_taxable' => $breakdown['is_default'] ? 1 : $breakdown['is_taxable'],
+                'is_active' => $breakdown['is_default'] ? 1 : $breakdown['is_active']
+            ];
+        }
+        $existing_setting = $this->grossSalaryBreakdownHistoryRepository->where('business_member_id', $this->businessMember->id)->where('end_date', null)->first();
+        $this->grossSalaryBreakdownHistoryRepository->update($existing_setting, ['end_date' => Carbon::now()->toDateString()]);
+        $this->grossSalaryBreakdownHistoryRepository->create([
+            'business_member_id' => $this->businessMember->id,
+            'setting_form_where' => self::INDIVIDUAL_SALARY,
+            'settings' => json_encode($data),
+            'start_date' => Carbon::now()->toDateString(),
+        ]);
     }
 
 }
