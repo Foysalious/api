f4b0a13d2e4c4a15019aac44d8f64d34e5149e46
Shovan Chowdhury <shovan@sheba.xyz>
2022-01-13 14:43:53 +0600

    payment gateway details
    Type(s): WIP

--- app/Sheba/ResellerPayment/PaymentGateway/PaymentGateway.php
+++ app/Sheba/ResellerPayment/PaymentGateway/PaymentGateway.php
@@ -18,7 +18,13 @@ class PaymentGateway
     public function getDetails()
     {
         return [
-          'banner' =>''
+          'banner' =>'https://cdn-shebadev.s3.ap-south-1.amazonaws.com/reseller_payment/payment_gateway_banner/app-banner+(1)+2.png',
+            'faq' => [
+                'আপনার ব্যবসার প্রোফাইল সম্পন্ন করুন',
+                'পেমেন্ট সার্ভিসের জন্য আবেদন করুন',
+                'পেমেন্ট সার্ভিস কনফিগার করুন'
+            ],
+            'status' => ''
         ];
 
     }
