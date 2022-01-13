e7f248dd7076ab4e2dea432bd926e9375bc1e6c2
Shovan Chowdhury <shovan@sheba.xyz>
2022-01-13 10:26:52 +0600

    survey post
    Type(s): UPDATE
    
    Issue(s):
    - JIRA:
    https://blabol.atlassian.net/browse/MSMEBE-3745?atlOrigin=eyJpIjoiMDczZTliMGQ0N2Q4NDUxZWIwNTcyM2RmOTNjZmVlZDkiLCJwIjoiaiJ9

---
+++ app/Sheba/Survey/Exception/SurveyException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Sheba\Survey\Exception;
+
+use Throwable;
+
+class SurveyException extends \Exception
+{
+    public function __construct($message = "", $code = 403, Throwable $previous = null)
+    {
+        if ($message == "") $message = "Survey already exist";
+        parent::__construct($message, $code, $previous);
+    }
+}
