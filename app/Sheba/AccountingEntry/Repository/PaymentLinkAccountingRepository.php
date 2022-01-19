8456f7e7eae7155d8e6207d41cf2ae8e4ad27de2
Sayed Yeamin Arafat <yeamin@sheba.xyz>
2022-01-19 17:36:20 +0600

    ⬆️ Payment method added to calculate amount
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MSMEBE-3691

--- app/Sheba/AccountingEntry/Repository/PaymentLinkAccountingRepository.php
+++ app/Sheba/AccountingEntry/Repository/PaymentLinkAccountingRepository.php
@@ -261,7 +261,7 @@ class PaymentLinkAccountingRepository extends AccountingRepository
         $data['amount'] = $this->amount;
         $data['amount_cleared'] = $this->amount_cleared;
         $data['entry_at'] = Carbon::now()->format('Y-m-d H:i:s');
-        $data['bank_transaction_charge'] = 0;
+        $data['bank_transaction_charge'] = $this->bank_transaction_charge;
         $data['interest'] = 0;
         $data['source_id'] = $this->source_id;
         $data['source_type'] = $this->source_type;
