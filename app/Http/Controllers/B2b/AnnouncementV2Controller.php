27a7600d24f991abd11c0c3b068c99096e30d321
miajee <miajee@sheba.xyz>
2022-01-23 14:27:56 +0600

    ➕ Announcement Sorting
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/B2BBE-1708

--- app/Http/Controllers/B2b/AnnouncementV2Controller.php
+++ app/Http/Controllers/B2b/AnnouncementV2Controller.php
@@ -52,14 +52,60 @@ class AnnouncementV2Controller extends Controller
         $business = $request->business;
         /** @var BusinessMember $business_member */
         $business_member = $request->business_member;
+        list($offset, $limit) = calculatePagination($request);
 
-        $announcements = $business->announcements()->orderBy('id', 'desc')->get();
+        $announcements = $business->announcements()->orderBy('id', 'desc');
 
+        if ($request->has('type')) $announcements = $announcements->where('type', $request->type);
+
+        $announcements = $announcements->get();
         $manager = new Manager();
         $manager->setSerializer(new ArraySerializer());
         $announcements = new Collection($announcements, new AnnouncementListTransformer());
         $announcements = collect($manager->createData($announcements)->toArray()['data']);
 
-        return api_response($request, $announcements, 200, ['announcements'=>$announcements]);
+        if ($request->has('search')) $announcements = $this->searchTitle($announcements, $request);
+
+        if ($request->has('sort_by_created_on')) $announcements = $this->sortByCreatedOn($announcements, $request->sort_by_created_on)->values();
+        if ($request->has('sort_by_target_type')) $announcements = $this->sortByTargetType($announcements, $request->sort_by_target_type)->values();
+
+
+        $total_announcements = $announcements->count();
+        #$limit = $this->getLimit($request, $limit, $total_announcements);
+        if ($request->has('limit')) $announcements = collect($announcements)->splice($offset, $limit);
+
+        return api_response($request, $announcements, 200, [
+            'announcements' => $announcements,
+            'total_announcements' => $total_announcements
+        ]);
+    }
+
+    private function getLimit(Request $request, $limit, $total_announcements)
+    {
+        if ($request->has('limit') && $request->limit == 'all') return $total_announcements;
+        return $limit;
+    }
+
+    private function searchTitle($announcements, Request $request)
+    {
+        return $announcements->filter(function ($announcement) use ($request) {
+            return str_contains(strtoupper($announcement['title']), strtoupper($request->search));
+        });
+    }
+
+    private function sortByCreatedOn($announcements, $sort = 'asc')
+    {
+        $sort_by = ($sort === 'asc') ? 'sortBy' : 'sortByDesc';
+        return collect($announcements)->$sort_by(function ($announcements, $key) {
+            return strtoupper($announcements['created_at']);
+        });
+    }
+
+    private function sortByTargetType($announcements, $sort = 'asc')
+    {
+        $sort_by = ($sort === 'asc') ? 'sortBy' : 'sortByDesc';
+        return collect($announcements)->$sort_by(function ($announcements, $key) {
+            return strtoupper($announcements['target_type']);
+        });
     }
 }
