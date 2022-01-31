0644ecf03b85e4ebbc49822482ed95c1f30ddde8
Hasan Hafiz Pasha <mach.pasha@gmail.com>
2022-01-31 19:16:10 +0600

    AUTHOR ANNOTATION CHANGES
    Type(s): UPDATE

--- tests/Mocks/MockAccountServerClient.php
+++ tests/Mocks/MockAccountServerClient.php
@@ -5,30 +5,33 @@ namespace Tests\Mocks;
 use Mockery\CountValidator\Exception;
 use Sheba\OAuth2\AccountServerClient;
 
+/**
+ * @author Shafiqul Islam <shafiqul@sheba.xyz>
+ */
 class MockAccountServerClient extends AccountServerClient
 {
     public static $token;
 
     public function post($uri, $data, $headers = null)
     {
-        if ( $uri == 'api/v3/profile/login' ) {
+        if ($uri == 'api/v3/profile/login') {
             return $this->getLoginData($data);
         }
+
         return self::$token;
-
     }
-
 
     private function getLoginData($data)
     {
-        if ( $data['email'] != 'tisha@sheba.xyz' || $data['password'] != '12345' ) {
+        if ($data['email'] != 'tisha@sheba.xyz' || $data['password'] != '12345') {
             throw new Exception("Data Mismatch");
         }
+
         return json_decode(
             '{
             "code": 200,
             "message": "Successful",
-            "token": "' . self::$token . '",
+            "token": "'.self::$token.'",
             "user": {
                 "name": "Tiara Wuckert",
                 "mobile": "+8801678242955",
@@ -37,8 +40,8 @@ class MockAccountServerClient extends AccountServerClient
                 "business_name": "My Company",
                 "is_remote_attendance_enable": true
             }
-            }', true
+            }',
+            true
         );
     }
-
 }
