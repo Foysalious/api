e47c45a075041c67a9b72715a740d6d2cb00c16b
Asad Ahmed <asad@sheba.xyz>
2022-02-02 21:16:23 +0600

    Disbursed Time Store
    Type(s): ADD
    
    Issue(s):
    - JIRA: B2BBE-1744

--- app/Sheba/Business/Payslip/PayRun/Updater.php
+++ app/Sheba/Business/Payslip/PayRun/Updater.php
@@ -4,6 +4,7 @@ use App\Jobs\Business\SendPayslipDisburseNotificationToEmployee;
 use App\Jobs\Business\SendPayslipDisbursePushNotificationToEmployee;
 use App\Sheba\Business\PayrollComponent\Components\GrossSalaryBreakdownCalculate;
 use App\Sheba\Business\Salary\Requester as SalaryRequester;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 use Sheba\Dal\PayrollComponent\Components;
 use Sheba\Dal\PayrollComponent\PayrollComponentRepository;
@@ -100,7 +101,12 @@ class Updater
     public function disburse()
     {
         DB::transaction(function () {
-            $this->payslipRepository->getPaySlipByStatus($this->businessMemberIds, Status::PENDING)->where('schedule_date', 'like', '%' . $this->scheduleDate . '%')->update(['status' => Status::DISBURSED]);
+            $this->payslipRepository->getPaySlipByStatus($this->businessMemberIds, Status::PENDING)
+                ->where('schedule_date', 'like', '%' . $this->scheduleDate . '%')
+                ->update([
+                    'status' => Status::DISBURSED,
+                    'disbursed_at' => Carbon::now()
+                ]);
         });
 
         $this->sendNotifications();
