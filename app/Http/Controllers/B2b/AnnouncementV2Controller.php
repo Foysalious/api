4a19d5495c4b5f1500029a280621051ebd77fe2f
Asad Ahmed <asad@sheba.xyz>
2022-02-07 21:35:04 +0600

    Announcement Edit
    Type(s): ADD
    
    Issue(s):
    - JIRA: B2BBE-1710

--- app/Http/Controllers/B2b/AnnouncementV2Controller.php
+++ app/Http/Controllers/B2b/AnnouncementV2Controller.php
@@ -4,6 +4,7 @@ use App\Sheba\Business\AnnouncementV2\CreatorRequester;
 use App\Sheba\Business\AnnouncementV2\Creator;
 use App\Http\Controllers\Controller;
 use App\Models\BusinessMember;
+use App\Sheba\Business\AnnouncementV2\Updater;
 use App\Transformers\Business\AnnouncementListTransformer;
 use App\Transformers\Business\AnnouncementShowTransformer;
 use App\Transformers\Business\AnnouncementTransformer;
@@ -106,6 +107,36 @@ class AnnouncementV2Controller extends Controller
         $announcement = $manager->createData($resource)->toArray()['data'];
 
         return api_response($request, $announcement, 200, ['announcement' => $announcement]);
+    }
+
+    public function update($business_id, $announcement_id, Request $request, CreatorRequester $creator_requester, Updater $updater)
+    {
+        /** @var  Business $business */
+        $business = $request->business;
+        /** @var BusinessMember $business_member */
+        $business_member = $request->business_member;
+        if (!$business_member) return api_response($request, null, 401);
+        /** @var Member $manager_member */
+        $manager_member = $request->managerMember;
+        $this->setModifier($manager_member);
+        $announcement = $this->announcementRepo->find($announcement_id);
+        if (!$announcement) return api_response($request, null, 404);
+        $creator_requester->setType($request->type)
+            ->setTitle($request->title)
+            ->setDescription($request->description)
+            ->setIsPublished($request->is_published)
+            ->setTargetType($request->target_type)
+            ->setTargetIds($request->target_ids)
+            ->setScheduledFor($request->scheduled_for)
+            ->setStartDate($request->start_date)
+            ->setStartTime($request->start_time)
+            ->setEndDate($request->end_date)
+            ->setEndTime($request->end_time)
+            ->setStatus($request->status)
+            ->setAnnouncement($announcement);
+        $announcement_update = $updater->setRequest($creator_requester)->update();
+        if ($announcement_update == false) return api_response($request, null, 400, ['message' => 'You cannot edit an expired announcement.']);
+        return api_response($request, null, 200);
     }
 
     private function getLimit(Request $request, $limit, $total_announcements)
