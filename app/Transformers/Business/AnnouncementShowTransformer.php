400499e99172943c097381b1bde9bafd80cf55aa
miajee <miajee@sheba.xyz>
2022-02-08 13:10:06 +0600

    ➕ Employee And Department Details Add
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/B2BBE-1714

--- app/Transformers/Business/AnnouncementShowTransformer.php
+++ app/Transformers/Business/AnnouncementShowTransformer.php
@@ -1,11 +1,17 @@
 <?php namespace App\Transformers\Business;
 
+use App\Models\BusinessDepartment;
+use App\Models\BusinessMember;
 use Carbon\Carbon;
 use League\Fractal\TransformerAbstract;
 use Sheba\Dal\Announcement\Announcement;
 
 class AnnouncementShowTransformer extends TransformerAbstract
 {
+    const ALL = "all";
+    const DEPARTMENT = "department";
+    const EMPLOYEE = "employee";
+
     public function transform(Announcement $announcement)
     {
         return [
@@ -15,7 +21,8 @@ class AnnouncementShowTransformer extends TransformerAbstract
             'type' => $announcement->type,
             'target' => [
                 'type' => $announcement->target_type,
-                'count' => 8,
+                'type_ids' => $this->targetTypeInfo($announcement),
+                'count' => $this->getCount($announcement),
             ],
             'active_between' => 'Dec 02,2020-Dec 05,2020',
             'status' => $this->getStatus($announcement->end_date),
@@ -25,6 +32,56 @@ class AnnouncementShowTransformer extends TransformerAbstract
             'updated_by' => $announcement->updated_by ?: 'N/S',
             'updated_at' => $announcement->updated_at->toDateTimeString(),
         ];
+    }
+
+    private function targetTypeInfo($announcement)
+    {
+        if ($announcement->target_type == self::ALL) return 'all';
+        if ($announcement->target_type == self::EMPLOYEE) return $this->getEmployeeInfo($announcement);
+        if ($announcement->target_type == self::DEPARTMENT) return $this->getDepartmentInfo($announcement);
+    }
+
+    private function getCount($announcement)
+    {
+        if ($announcement->target_type == self::ALL) return 0;
+        if ($announcement->target_type == self::EMPLOYEE) return count($this->getDecodedTarget($announcement));
+        if ($announcement->target_type == self::DEPARTMENT) return count($this->getDecodedTarget($announcement));
+    }
+
+
+    private function getDecodedTarget($announcement)
+    {
+        return json_decode($announcement->target_id, 1);
+    }
+
+    private function getEmployeeInfo($announcement)
+    {
+        $employee_info = [];
+        $business_member_ids = $this->getDecodedTarget($announcement);
+        foreach ($business_member_ids as $business_member_id) {
+            $business_member = BusinessMember::find($business_member_id);
+            $employee_info [] = [
+                'business_member_id' => $business_member->id,
+                'name' => $business_member->profile()->name,
+                'pro_pic' => $business_member->profile()->pro_pic,
+                'employee_id' => $business_member->employee_id,
+            ];
+        }
+        return $employee_info;
+    }
+
+    private function getDepartmentInfo($announcement)
+    {
+        $department_info = [];
+        $department_ids = $this->getDecodedTarget($announcement);
+        foreach ($department_ids as $department_id) {
+            $department = BusinessDepartment::find($department_id);
+            $department_info [] = [
+                'department_id' => $department->id,
+                'name' => $department->name,
+            ];
+        }
+        return $department_info;
     }
 
     private function getStatus($end_date)
