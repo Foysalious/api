7606dba6a81bbb42aa91989ff18c7ad732170079
Asad Ahmed <asad@sheba.xyz>
2022-02-17 22:24:09 +0600

    Payslip Summary
    Type(s): ADD
    
    Issue(s):
    - JIRA: B2BBE-1772

--- app/Http/Controllers/B2b/BusinessPayReportSummaryController.php
+++ app/Http/Controllers/B2b/BusinessPayReportSummaryController.php
@@ -1,12 +1,19 @@
 <?php namespace App\Http\Controllers\B2b;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Request;
+use App\Models\Business;
+use App\Models\BusinessMember;
+use Illuminate\Http\Request;
 
-class BusinessPayReportSummaryController  extends Controller
+class BusinessPayReportSummaryController extends Controller
 {
     public function index(Request $request)
     {
-
+        /** @var Business $business */
+        $business = $request->business;
+        /** @var BusinessMember $business_member */
+        $business_member = $request->business_member;
+        if (!$business_member) return api_response($request, null, 401);
+        $payslip_summary = $business->payslipSummary;
     }
 }
