53770a5919c1d1cdda2f171d8e44562cc48fdd98
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2022-02-20 02:19:40 +0600

    Business Payslip Summary List
    Type(s): ADD
    Issue(s):
    - JIRA: B2BBE-1772

--- app/Http/Controllers/B2b/BusinessPayReportSummaryController.php
+++ app/Http/Controllers/B2b/BusinessPayReportSummaryController.php
@@ -3,7 +3,11 @@
 use App\Http\Controllers\Controller;
 use App\Models\Business;
 use App\Models\BusinessMember;
+use App\Transformers\Business\PayReportSummaryListTransformer;
 use Illuminate\Http\Request;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Serializer\ArraySerializer;
 
 class BusinessPayReportSummaryController extends Controller
 {
@@ -15,5 +19,11 @@ class BusinessPayReportSummaryController extends Controller
         $business_member = $request->business_member;
         if (!$business_member) return api_response($request, null, 401);
         $payslip_summary = $business->payslipSummary;
+        $manager = new Manager();
+        $manager->setSerializer(new ArraySerializer());
+        $payreport_summary_list_transformer = new PayReportSummaryListTransformer();
+        $payreport_summary_list = new Collection($payslip_summary, $payreport_summary_list_transformer);
+        $payreport_summary_list = collect($manager->createData($payreport_summary_list)->toArray()['data']);
+        return api_response($request, null, 200, ['pay_report_summary' => $payreport_summary_list]);
     }
 }
---
+++ app/Transformers/Business/PayReportSummaryListTransformer.php
@@ -0,0 +1,75 @@
+<?php namespace App\Transformers\Business;
+
+use Carbon\Carbon;
+use League\Fractal\TransformerAbstract;
+
+class PayReportSummaryListTransformer extends TransformerAbstract
+{
+    public function transform($payslip_summary)
+    {
+        $payslips = $payslip_summary->payslip;
+        return [
+                'id' =>   $payslip_summary->id,
+                'status' => $payslip_summary->status,
+                'disburse_date' => Carbon::parse($payslip_summary->disburse_at)->format('j F'),
+                'cycle' => Carbon::parse($payslip_summary->cycle_start_date)->format('M d').' - '.Carbon::parse($payslip_summary->cycle_end_date)->format('M d'),
+                'total_gross' => $this->getTotalGross($payslips),
+                'addition_total' => $this->getTotalAddition($payslips),
+                'deduction_total' => $this->getTotalDeduction($payslips),
+                'tax_total' => $this->getTaxTotal($payslips)
+        ];
+    }
+
+    private function getTotalGross($payslips)
+    {
+        $gross_salary = 0;
+        foreach ($payslips as $payslip)
+        {
+            $gross_salary += json_decode($payslip->salary_breakdown, 1)['gross_salary_breakdown']['gross_salary'];
+        }
+        return $gross_salary;
+    }
+
+    private function getTotalAddition($payslips)
+    {
+        $total_addition = 0;
+        foreach ($payslips as $payslip)
+        {
+            $additions = json_decode($payslip->salary_breakdown, 1)['payroll_component']['addition'];
+            foreach($additions as $addition)
+            {
+                $total_addition += $addition;
+            }
+        }
+        return $total_addition;
+    }
+
+    private function getTotalDeduction($payslips)
+    {
+        $total_deduction = 0;
+        foreach ($payslips as $payslip)
+        {
+            $deductions = json_decode($payslip->salary_breakdown, 1)['payroll_component']['deduction'];
+            foreach($deductions as $key => $deduction)
+            {
+                if ($key == 'tax') continue;
+                $total_deduction += $deduction;
+            }
+        }
+        return $total_deduction;
+    }
+
+    private function getTaxTotal($payslips)
+    {
+        $total_tax = 0;
+        foreach ($payslips as $payslip)
+        {
+            $deductions = json_decode($payslip->salary_breakdown, 1)['payroll_component']['deduction'];
+            foreach($deductions as $key => $deduction)
+            {
+                if ($key == 'tax') $total_tax += $deduction;
+            }
+        }
+        return $total_tax;
+    }
+}
