3a2ebd708e359d3c7b2144ba33de661faae71c0e
Arafat <yeamin@sheba.xyz>
2022-02-22 18:16:35 +0600

    ⬆️ Qr payment complete
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/OW1-11

---
+++ app/Sheba/QRPayment/Complete/PosOrderComplete.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Sheba\QRPayment\Complete;
+
+use App\Sheba\Pos\Repositories\PosClientRepository;
+use Sheba\Dal\QRGateway\Model as QRGateway;
+use Sheba\Payment\Statuses;
+
+class PosOrderComplete extends QRPaymentComplete
+{
+    public function complete()
+    {
+        $this->qr_payment->reload();
+        if ($this->qr_payment->isComplete())
+            return $this->qr_payment;
+        $this->clearOrder();
+        $this->completePayment();
+        return $this->qr_payment;
+    }
+
+    private function clearOrder()
+    {
+        $payment_data    = $this->makeData();
+        $partner = $this->payable->payee;
+
+
+        /** @var PosClientRepository $posOrderRepo */
+        $posOrderRepo = app(PosClientRepository::class);
+        $posOrderRepo->setPartnerId($partner->id)->setOrderId($this->payable->type_id)->addOnlinePayment($payment_data);
+    }
+
+    private function makeData(): array
+    {
+        $payment_method_detail = QRGateway::where('method_name', $this->method)->first();
+
+        return [
+            'amount'              => $this->payable->amount,
+            'payment_method'      => $this->method,
+            'payment_method_en'   => $payment_method_detail->name,
+            'payment_method_bn'   => $payment_method_detail->name_bn,
+            'payment_method_icon' => $payment_method_detail->icon,
+            'interest'            => 0,
+        ];
+    }
+
+    private function completePayment()
+    {
+        $this->qr_payment->status = Statuses::COMPLETED;
+        $this->qr_payment->save();
+    }
+}
---
+++ app/Sheba/QRPayment/QRPaymentManager.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Sheba\QRPayment;
+
+use App\Models\Payable;
+use Illuminate\Support\Facades\Redis;
+use Sheba\Dal\QRPayment\Model as QRPaymentModel;
+use Sheba\Payment\Exceptions\AlreadyCompletingPayment;
+use Throwable;
+
+class QRPaymentManager
+{
+    /*** @var Payable */
+    private $payable;
+    /*** @var QRPaymentModel */
+    private $qr_payment;
+
+    private $method;
+
+    /**
+     * @param mixed $qr_payment
+     * @return QRPaymentManager
+     */
+    public function setQrPayment(QRPaymentModel $qr_payment): QRPaymentManager
+    {
+        $this->qr_payment = $qr_payment;
+        return $this;
+    }
+
+    /**
+     * @param mixed $payable
+     * @return QRPaymentManager
+     */
+    public function setPayable(Payable $payable): QRPaymentManager
+    {
+        $this->payable = $payable;
+        return $this;
+    }
+
+    /**
+     * @param mixed $method
+     * @return QRPaymentManager
+     */
+    public function setMethod($method): QRPaymentManager
+    {
+        $this->method = $method;
+        return $this;
+    }
+
+    /**
+     * @throws Throwable
+     * @throws AlreadyCompletingPayment
+     */
+    public function complete()
+    {
+        $this->runningCompletionCheckAndSet();
+        try {
+            if ($this->qr_payment->canComplete()) {
+                $completion_class = $this->payable->getQRCompletionClass();
+                $completion_class->setQrPayment($this->qr_payment)->setMethod($this->method);
+                $payment = $completion_class->setPayable($this->payable)->complete();
+            }
+            $this->unsetRunningCompletion();
+            return $payment ? : $this->qr_payment;
+        } catch (Throwable $e) {
+            $this->unsetRunningCompletion();
+            throw $e;
+        }
+    }
+
+    /**
+     * @throws AlreadyCompletingPayment
+     */
+    private function runningCompletionCheckAndSet()
+    {
+        $key = $this->getKey();
+        $already = Redis::get($key);
+        if ($already) {
+            throw new AlreadyCompletingPayment();
+        }
+        Redis::set($key, 1);
+    }
+
+    private function unsetRunningCompletion()
+    {
+        Redis::del($this->getKey());
+    }
+
+    private function getKey()
+    {
+        return 'QR_Payment::Completing::' . $this->qr_payment->id;
+    }
+}
