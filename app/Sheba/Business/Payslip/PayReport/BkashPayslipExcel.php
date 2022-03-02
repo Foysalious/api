05a8616e3ec91077d6811db0ffd67656ddf2f194
Asad Ahmed <asad@sheba.xyz>
2022-03-02 20:32:39 +0600

    Bkash Payslip Excel
    Type(s): ADD
    
    
    Issue(s):
    - JIRA: B2BBE-1827

---
+++ app/Sheba/Business/Payslip/PayReport/BkashPayslipExcel.php
@@ -0,0 +1,18 @@
+<?php namespace App\Sheba\Business\Payslip\PayReport;
+
+class BkashPayslipExcel
+{
+    const FINAL_DISBURSEMENT = "Final Disbursement";
+    const CLIENT = "Client";
+    const FINAL = "Final";
+    const FEE = "Fee";
+    const BKASH = "bKash";
+
+    const SL_NO = "A";
+    const WALLET_NO = "B";
+    const PRINCIPLE_AMOUNT = "C";
+
+    const SL_NO_TITLE = "sl_no";
+    const WALLET_NO_TITLE = "wallet_no";
+    const PRINCIPLE_AMOUNT_TITLE = "principle_amount";
+}
