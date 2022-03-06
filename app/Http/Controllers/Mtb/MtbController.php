63ee5511e6a2cf05a4213377ad905c1502457eb9
Foysal <foysal@iamsheba.xyz>
2022-03-06 18:52:46 +0600

    ⬆️ Save primary information to mtb
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/OW1-57

---
+++ app/Http/Controllers/Mtb/MtbController.php
@@ -0,0 +1,14 @@
+<?php namespace App\Http\Controllers\Mtb;
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class MtbController extends Controller
+{
+    public function index(Request $request)
+    {
+
+    }
+}
