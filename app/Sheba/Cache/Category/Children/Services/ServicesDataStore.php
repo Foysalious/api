1d41229afd738294cfd643baad9d4fcb12de6d2c
nadim imtiaz shakil <nadim@sheba.xyz>
2022-03-06 11:48:20 +0600

    ⬆️  Call getservices controller method instead of api call
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MPBE-3385

--- app/Sheba/Cache/Category/Children/Services/ServicesDataStore.php
+++ app/Sheba/Cache/Category/Children/Services/ServicesDataStore.php
@@ -1,9 +1,19 @@
 <?php namespace Sheba\Cache\Category\Children\Services;
 
+use App\Http\Controllers\CategoryController;
 use GuzzleHttp\Client;
+use Illuminate\Http\Request;
 use Sheba\Cache\CacheRequest;
 use Sheba\Cache\DataStoreObject;
 use Sheba\Cache\Exceptions\CacheGenerationException;
+use Sheba\Checkout\DeliveryCharge;
+use Sheba\JobDiscount\JobDiscountHandler;
+use Sheba\LocationService\PriceCalculation;
+use Sheba\LocationService\StartPriceCalculation;
+use Sheba\LocationService\UpsellCalculation;
+use Sheba\Service\MinMaxPrice;
+use Sheba\Services\ServiceSubscriptionDiscount;
+use Sheba\Subscription\ApproximatePriceCalculator;
 use Throwable;
 
 class ServicesDataStore implements DataStoreObject
@@ -24,9 +34,17 @@ class ServicesDataStore implements DataStoreObject
      */
     public function generate()
     {
-        $client = new Client();
         try {
-            $response = $client->get(config('sheba.api_url') . '/v2/categories/' . $this->servicesCacheRequest->getCategoryId() . '/services?location=' . $this->servicesCacheRequest->getLocationId());
+            $request = new Request();
+            $request->replace(['location' => $this->servicesCacheRequest->getLocationId()]);
+
+            /** @var CategoryController $controller */
+            $controller = app(CategoryController::class);
+            $response = $controller->getServices($this->servicesCacheRequest->getCategoryId(), $request, app(PriceCalculation::class),
+                app(DeliveryCharge::class), app(JobDiscountHandler::class), app(UpsellCalculation::class),
+                app(MinMaxPrice::class), app(ApproximatePriceCalculator::class), app(ServiceSubscriptionDiscount::class),
+                app(StartPriceCalculation::class)
+            );
         } catch (Throwable $e) {
             throw new CacheGenerationException();
         }
