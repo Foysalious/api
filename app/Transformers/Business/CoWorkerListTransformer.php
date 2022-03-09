1d083c704012e2090ecabdf9d00e8f3afef1e75a
Asad Ahmed <asad@sheba.xyz>
2022-03-09 12:08:07 +0600

    Employee List Sorting based on Salary & Salary Configured or not
    Type(s): ADD
    
    Isuse(s):
    - JIRA: B2BBE-1838

--- app/Transformers/Business/CoWorkerListTransformer.php
+++ app/Transformers/Business/CoWorkerListTransformer.php
@@ -2,9 +2,9 @@
 
 use App\Models\BusinessMember;
 use App\Models\BusinessRole;
+use App\Models\Member;
 use App\Models\Profile;
 use League\Fractal\TransformerAbstract;
-use App\Models\Member;
 
 class CoWorkerListTransformer extends TransformerAbstract
 {
@@ -16,7 +16,6 @@ class CoWorkerListTransformer extends TransformerAbstract
         $profile = $member->profile;
         /** @var BusinessRole $role */
         $role = $business_member->role;
-        $business_member_salary = $business_member->salary;
         return [
             'id' => $member->id,
             'employee_id' => $business_member->employee_id,
@@ -33,8 +32,7 @@ class CoWorkerListTransformer extends TransformerAbstract
             'department_id' => $role ? $role->businessDepartment->id : null,
             'department' => $role ? $role->businessDepartment->name : null,
             'designation' => $role ? $role->name : null,
-            'salary_configured' => $business_member_salary ? 1 : 0,
-            'sort_order' => $business_member_salary ? 999 : 1
+            'is_salary_configured' => $business_member->salary ? 1 : 0
         ];
     }
 }
