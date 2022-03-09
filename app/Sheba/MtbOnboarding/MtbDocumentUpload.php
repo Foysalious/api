9dd95d9b012e6d0248d3a2acd49ce868f29c5502
Foysal <foysal@iamsheba.xyz>
2022-03-09 11:08:22 +0600

    ➕ MtbDocumentUpload created
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/OW1-60

---
+++ app/Sheba/MtbOnboarding/MtbDocumentUpload.php
@@ -0,0 +1,42 @@
+<?php namespace App\Sheba\MtbOnboarding;
+
+use App\Models\Partner;
+use App\Models\PartnerMefInformation;
+use App\Sheba\MTB\MtbServerClient;
+
+class MtbDocumentUpload
+{
+    /**
+     * @var MtbServerClient
+     */
+    private $client;
+    /**
+     * @var Partner
+     */
+    private $partner;
+
+    public function __construct(MtbServerClient $client)
+    {
+        $this->client = $client;
+    }
+
+    public function setPartner(Partner $partner)
+    {
+        $this->partner = $partner;
+        return $this;
+    }
+
+    private function makeData()
+    {
+        $partnerMefInformation = PartnerMefInformation::where('partner_id', $this->partner->id)->first();
+        return [
+            'ticketId' => $partnerMefInformation['ticket_id'],
+
+        ];
+    }
+
+    public function uploadDocument()
+    {
+        $data = $this->makeData();
+    }
+}
