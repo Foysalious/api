5ef0c249b420dc5e40b7122f7580e841f2b317d6
Sayed Yeamin Arafat <yeamin@sheba.xyz>
2022-03-30 15:16:55 +0600

    ⬆️ Dynamic form mobile end api changes
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/OW1-134

--- app/Sheba/MerchantEnrollment/Statics/PaymentMethodStatics.php
+++ app/Sheba/MerchantEnrollment/Statics/PaymentMethodStatics.php
@@ -58,4 +58,12 @@ class PaymentMethodStatics
             "completed_message"  => "প্রয়োজনীয় তথ্য দেয়া সম্পন্ন হয়েছ, SSL পেমেন্ট সার্ভিস সচল করতে আবেদন করুন।"
         ];
     }
+
+    public static function mtbCompletionPageMessage(): array
+    {
+        return [
+            "incomplete_message" => "MTB QR সার্ভিস সচল করতে প্রয়োজনীয় তথ্য প্রদান করুন।",
+            "completed_message"  => "প্রয়োজনীয় তথ্য দেয়া সম্পন্ন হয়েছ, MTB QR সার্ভিস সচল করতে আবেদন করুন।"
+        ];
+    }
 }
