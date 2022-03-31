c295f5a9cae838ce8781e2addfb27e2ef0abe2e8
Fariha Nuzhat <fariha@iamsheba.xyz>
2022-03-31 14:11:25 +0600

    ⏪ Total price in job bills
    Type(s): REVERT
    
    Issue(s):
    - JIRA: MPBE-3483

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -372,7 +372,7 @@ class JobController extends Controller
 //        dd();
 
         $bill = collect();
-        $bill['total'] = (double)($partnerOrder->_calculateThisJobsForBillsDetails()->totalPriceForCancelledOrder + $partnerOrder->_calculateThisJobsForBillsDetails()->totalLogisticChargeForCancelledOrder);
+        $bill['total'] = (double)($partnerOrder->totalPrice + $partnerOrder->totalLogisticCharge);
         $bill['total_without_logistic'] = (double)($partnerOrder->totalPrice);
         $bill['original_price'] = (double)$partnerOrder->jobPrices - (double)$job->totalServiceSurcharge;
         $bill['paid'] = (double)$partnerOrder->paidWithLogistic;
