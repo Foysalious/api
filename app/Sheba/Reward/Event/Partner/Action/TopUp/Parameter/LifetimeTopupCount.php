d6ebc292090823552eef8b819530d0ad5b3c53cb
Shafiqul Islam <shafiqul@sheba.xyz>
2022-04-19 18:18:47 +0600

    ➕ Add top up action days parameters
    Type(s): ADD
    
    Issue(s):
    - JIRA: SHEBA-8964

---
+++ app/Sheba/Reward/Event/Partner/Action/TopUp/Parameter/LifetimeTopupCount.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Sheba\Reward\Event\Partner\Action\TopUp\Parameter;
+
+use App\Models\Partner;
+use App\Models\TopUpOrder;
+use Sheba\Reward\Event\ActionEventParameter;
+
+class LifetimeTopupCount extends ActionEventParameter
+{
+    public function validate(){}
+
+    /**
+     * @param array $params
+     * @return bool
+     */
+    public function check(array $params)
+    {
+        if ($this->value == null) return true;
+
+        /** @var TopUpOrder $topup_order */
+        $topup_order = $params[0];
+
+        /** @var Partner $agent */
+        $agent = $topup_order->agent;
+        $lifetime_count = $agent->topUpOrders()->count();
+
+        return $lifetime_count == $this->value;
+    }
+}
