11c7546a38888a799ba294392a85b37fb8e89269
Foysal <foysal@iamsheba.xyz>
2022-04-19 14:00:14 +0600

    ⬆️ Service Charge Settings change
    Type(s): UPDATE
    
    Issue(s): https://blabol.atlassian.net/browse/OW1-141

--- config/payment_link.php
+++ config/payment_link.php
@@ -3,7 +3,7 @@
 return [
     "payment_link_commission" => 2,
     "payment_link_tax"        => 0,
-    "step_margin"             => .25,
-    "minimum_percentage"      => 2,
+    "step_margin"             => .05,
+    "minimum_percentage"      => 0,
     "maximum_percentage"      => 5
 ];
