0a3d54adde98262ea2d76996c8aadb77534d70ff
Rakib <rakib.rumi09@gmail.com>
2022-04-25 09:52:08 +0600

    ➕ Add entry details and delete v2 api
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/SW1-143

---
+++ app/Http/Controllers/Accounting/EntriesControllerV2.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Accounting;
+
+use App\Http\Controllers\Controller;
+use App\Sheba\AccountingEntry\Repository\EntriesRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Sheba\Usage\Usage;
+
+class EntriesControllerV2 extends Controller
+{
+    private EntriesRepository $entriesRepo;
+
+    public function __construct(EntriesRepository $entriesRepo)
+    {
+        $this->entriesRepo = $entriesRepo;
+    }
+
+    public function details(Request $request, int $entry_id): JsonResponse
+    {
+        $data = $this->entriesRepo->setPartner($request->partner)->setEntryId($entry_id)->entryDetails();
+        return http_response($request, null, 200, ['data' => $data]);
+    }
+
+    public function delete(Request $request, int $entry_id): JsonResponse
+    {
+        $this->entriesRepo->setPartner($request->partner)->setEntryId($entry_id)->deleteEntry();
+        (new Usage())->setUser($request->partner)->setType(Usage::Partner()::ENTRY_DELETE)->create($request->auth_user);
+        return http_response($request, null, 200, ['data' => "Entry delete successful"]);
+    }
+}
