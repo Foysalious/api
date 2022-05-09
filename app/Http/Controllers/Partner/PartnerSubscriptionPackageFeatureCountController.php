04c66c99bc78ebe09869efa204278a9768d14985
Jamil <jamil@iamsheba.xyz>
2022-05-09 16:43:42 +0600

    ➕ Incremented sms count
    Type(s): ADD
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MGWB-124

--- app/Http/Controllers/Partner/PartnerSubscriptionPackageFeatureCountController.php
+++ app/Http/Controllers/Partner/PartnerSubscriptionPackageFeatureCountController.php
@@ -13,23 +13,34 @@ class PartnerSubscriptionPackageFeatureCountController extends Controller
         $this->packageFeatureCount = $packageFeatureCount;
     }
 
-    public function increment(Request $request)
+    public function increment(Request $request, $partner)
     {
         $feature = $request->feature;
         $count = $request->count;
 
-        $current_count = $this->currentCount($feature);
-        dd($current_count);
+        $current_count = $this->currentCount($feature, $partner);
+        $updated_count = $current_count + $count;
+        $this->countUpdate($feature, $updated_count, $partner);
+        return api_response($request, null, 200, ['message' => 'SMS count incremented successfully']);
     }
 
-    public function decrement($feature, $count)
+    public function decrement(Request $request, $partner)
     {
+        $feature = $request->feature;
+        $count = $request->count;
 
+        $current_count = $this->currentCount($feature, $partner);
     }
 
-    private function currentCount($feature)
+    private function currentCount($feature, $partner)
     {
         $methodName = $feature . 'CurrentCount';
-        return $this->packageFeatureCount->$methodName();
+        return $this->packageFeatureCount->$methodName($partner);
+    }
+
+    private function countUpdate($feature, $updated_count, $partner)
+    {
+        $methodName = $feature . 'CountUpdate';
+        return $this->packageFeatureCount->$methodName($updated_count, $partner);
     }
 }
--- app/Sheba/Partner/PackageFeatureCount.php
+++ app/Sheba/Partner/PackageFeatureCount.php
@@ -1,19 +1,44 @@
 <?php namespace App\Sheba\Partner;
 
+use Sheba\Dal\PartnerPackageFeatureCounter\EloquentImplementation as PartnerPackageFeatureCounter;
+use Sheba\ModificationFields;
+
 class PackageFeatureCount
 {
+    use ModificationFields;
+
+    protected $partnerPackageFeatureCounter;
+
+    public function __construct(PartnerPackageFeatureCounter $partnerPackageFeatureCounter)
+    {
+        $this->partnerPackageFeatureCounter = $partnerPackageFeatureCounter;
+    }
+
     public function topupCurrentCount(): int
     {
         return 10;
     }
 
-    public function smsCurrentCount(): int
+    public function smsCurrentCount($partner): int
     {
-        return 10;
+        $features_count = $this->featuresCount($partner);
+        return $features_count->sms;
     }
 
     public function deliveryCurrentCount(): int
     {
-        return 10;
+        return 20;
+    }
+
+    public function smsCountUpdate($updated_count, $partner)
+    {
+        $features_count = $this->featuresCount($partner);
+        $data['sms'] = $updated_count;
+        $features_count->update($this->withUpdateModificationField($data));
+    }
+
+    private function featuresCount($partner)
+    {
+        return $this->partnerPackageFeatureCounter->where('partner_id', $partner)->first();
     }
 }
