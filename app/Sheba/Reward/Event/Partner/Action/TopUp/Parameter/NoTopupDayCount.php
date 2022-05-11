f8d76f88fb7a17b6d4446dbd6e39ccd4d2eeab5e
Shafiqul Islam <shafiqul@sheba.xyz>
2022-05-11 11:17:36 +0600

    🚑  Check for first top up today
    Type(s): HOTFIX
    
    Issue(s):
    - JIRA: SHEBA-9079

--- app/Sheba/Reward/Event/Partner/Action/TopUp/Parameter/NoTopupDayCount.php
+++ app/Sheba/Reward/Event/Partner/Action/TopUp/Parameter/NoTopupDayCount.php
@@ -1,11 +1,10 @@
-<?php
-
-namespace Sheba\Reward\Event\Partner\Action\TopUp\Parameter;
+<?php namespace Sheba\Reward\Event\Partner\Action\TopUp\Parameter;
 
 use App\Models\Partner;
 use App\Models\TopUpOrder;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
+use Sheba\Dal\TopupOrder\Statuses;
 use Sheba\Reward\Event\ActionEventParameter;
 
 class NoTopupDayCount extends ActionEventParameter
@@ -23,15 +22,37 @@ class NoTopupDayCount extends ActionEventParameter
         /** @var TopUpOrder $topup_order */
         $topup_order = $params[0];
 
+        if (!$this->isFirstTopUpToday($topup_order)) return false;
+
+        return $this->getNoTopUpDayBeforeToday($topup_order->agent_id) >= $this->value;
+    }
+
+    private function isFirstTopUpToday(TopUpOrder $topup_order)
+    {
+        $count_today = DB::table('topup_orders')
+            ->where('agent_type', Partner::class)
+            ->where('agent_id', $topup_order->agent_id)
+            ->where('status', Statuses::SUCCESSFUL)
+            ->whereDate('created_at', '<', $topup_order->created_at)
+            ->whereDate('created_at', Carbon::today())
+            ->count();
+
+        return $count_today == 0;
+    }
+
+    /**
+     * @param $partner_id
+     * @return int
+     */
+    private function getNoTopUpDayBeforeToday($partner_id)
+    {
         $result = DB::table('topup_orders')
             ->selectRaw('MAX(DATE(created_at)) as last_topup_date')
             ->where('agent_type', Partner::class)
-            ->where('agent_id', $topup_order->agent_id)
+            ->where('agent_id', $partner_id)
             ->whereDate('created_at', '<', Carbon::today())
             ->first();
 
-        $no_topup_day = Carbon::parse($result->last_topup_date)->diffInDays(Carbon::now()) - 1;
-
-        return $no_topup_day >= $this->value;
+        return Carbon::parse($result->last_topup_date)->diffInDays(Carbon::now()) - 1;
     }
 }
