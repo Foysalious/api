d66365e400e512756a3721b9d2815988f135ca04
Jamil <jamil@iamsheba.xyz>
2022-05-11 11:30:32 +0600

    ⬆️ Refactor features count code
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: https://blabol.atlassian.net/browse/MGWB-125

--- app/Sheba/Partner/PackageFeatureCount.php
+++ app/Sheba/Partner/PackageFeatureCount.php
@@ -14,43 +14,35 @@ class PackageFeatureCount
         $this->partnerPackageFeatureCounter = $partnerPackageFeatureCounter;
     }
 
-    public function topupCurrentCount($partner): int
+    /**
+     * @param $feature
+     * @param $partner
+     * @return mixed
+     */
+    public function featureCurrentCount($feature, $partner)
     {
-        return $this->featuresCount($partner)->topup;
+        return $this->allFeaturesCount($partner)->$feature;
     }
 
-    public function smsCurrentCount($partner): int
+    /**
+     * @param $feature
+     * @param $updated_count
+     * @param $partner
+     * @return bool
+     */
+    public function featureCountUpdate($feature, $updated_count, $partner): bool
     {
-        return $this->featuresCount($partner)->sms;
-    }
-
-    public function deliveryCurrentCount($partner): int
-    {
-        return $this->featuresCount($partner)->delivery;
-    }
-
-    public function smsCountUpdate($updated_count, $partner)
-    {
-        $features_count = $this->featuresCount($partner);
-        $data['sms'] = $updated_count;
+        $features_count = $this->allFeaturesCount($partner);
+        $data[$feature] = $updated_count;
         $features_count->update($this->withUpdateModificationField($data));
+        return true;
     }
 
-    public function topupCountUpdate($updated_count, $partner)
-    {
-        $features_count = $this->featuresCount($partner);
-        $data['topup'] = $updated_count;
-        $features_count->update($this->withUpdateModificationField($data));
-    }
-
-    public function deliveryCountUpdate($updated_count, $partner)
-    {
-        $features_count = $this->featuresCount($partner);
-        $data['delivery'] = $updated_count;
-        $features_count->update($this->withUpdateModificationField($data));
-    }
-
-    private function featuresCount($partner)
+    /**
+     * @param $partner
+     * @return mixed
+     */
+    private function allFeaturesCount($partner)
     {
         return $this->partnerPackageFeatureCounter->where('partner_id', $partner)->first();
     }
