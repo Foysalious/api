96cef8c3771428df95e3ecbb5f602752d86a0292
Asad Ahmed <asad.ahmed@iamsheba.xyz>
2022-05-18 01:45:40 +0600

    Shift Calendar
    Type(s): ADD

--- app/Http/Controllers/B2b/ShiftCalenderController.php
+++ app/Http/Controllers/B2b/ShiftCalenderController.php
@@ -3,8 +3,13 @@
 use App\Http\Controllers\Controller;
 use App\Models\Business;
 use App\Models\BusinessMember;
+use App\Transformers\Business\PayReportListTransformer;
+use App\Transformers\Business\ShiftCalenderTransformer;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Collection;
+use League\Fractal\Serializer\ArraySerializer;
 use Sheba\Dal\ShiftCalender\ShiftCalenderRepository;
 
 class ShiftCalenderController extends Controller
@@ -21,5 +26,10 @@ class ShiftCalenderController extends Controller
         $end_date = $request->end_date ?: Carbon::now()->addDays(7)->toDateString();
         $active_business_member_ids = $business->getActiveBusinessMember()->pluck('id')->toArray();
         $shift_calender = $shift_calender_repository->builder()->whereIn('business_member_id', $active_business_member_ids)->whereBetween('date', [$start_date, $end_date]);
+        $manager = new Manager();
+        $manager->setSerializer(new ArraySerializer());
+        $shift_calender_transformer = new ShiftCalenderTransformer();
+        $shift_calender = new Collection($shift_calender, $shift_calender_transformer);
+        $shift_calender = collect($manager->createData($shift_calender)->toArray()['data']);
     }
 }
---
+++ app/Transformers/Business/ShiftCalenderTransformer.php
@@ -0,0 +1,12 @@
+<?php namespace App\Transformers\Business;
+
+use League\Fractal\TransformerAbstract;
+
+class ShiftCalenderTransformer extends TransformerAbstract
+{
+    public function transform($shift_calender)
+    {
+
+    }
+
+}
