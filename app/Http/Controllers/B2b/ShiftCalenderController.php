0bcf4c93f56d05339ecb7cb65f92dc621efd80d9
Asad Ahmed <asad@sheba.xyz>
2022-05-18 20:53:05 +0600

    Shift Calendar List
    Type(s): ADD
    Issue(s):
    - JIRA: B2BBE-1983

--- app/Http/Controllers/B2b/ShiftCalenderController.php
+++ app/Http/Controllers/B2b/ShiftCalenderController.php
@@ -3,16 +3,9 @@
 use App\Http\Controllers\Controller;
 use App\Models\Business;
 use App\Models\BusinessMember;
-use App\Transformers\Business\PayReportListTransformer;
 use App\Transformers\Business\ShiftCalenderTransformer;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use League\Fractal\Resource\Item;
-use App\Transformers\CustomSerializer;
-use App\Transformers\Business\ShiftDetailsTransformer;
-use League\Fractal\Manager;
-use League\Fractal\Resource\Collection;
-use League\Fractal\Serializer\ArraySerializer;
 use Sheba\Business\ShiftSetting\ShiftAssign\Requester;
 use Sheba\Business\ShiftSetting\ShiftAssign\Creator;
 use Sheba\Dal\BusinessShift\BusinessShiftRepository;
@@ -31,15 +24,16 @@ class ShiftCalenderController extends Controller
         /** @var BusinessMember $business_member */
         $business_member = $request->business_member;
         if (!$business_member) return api_response($request, null, 401);
+        list($offset, $limit) = calculatePagination($request);
         $start_date = $request->start_date ?: Carbon::now()->addDay()->toDateString();
         $end_date = $request->end_date ?: Carbon::now()->addDays(7)->toDateString();
         $active_business_member_ids = $business->getActiveBusinessMember()->pluck('id')->toArray();
         $shift_calender = $shift_calender_repository->builder()->whereIn('business_member_id', $active_business_member_ids)->whereBetween('date', [$start_date, $end_date]);
-        $manager = new Manager();
-        $manager->setSerializer(new ArraySerializer());
-        $shift_calender_transformer = new ShiftCalenderTransformer();
-        $shift_calender = new Collection($shift_calender, $shift_calender_transformer);
-        $shift_calender = collect($manager->createData($shift_calender)->toArray()['data']);
+        $shift_calender_data = (new ShiftCalenderTransformer())->transform($shift_calender->get());
+        $total_data = count($shift_calender_data['data']);
+        $shift_calender_employee_data = collect($shift_calender_data['data'])->splice($offset, $limit)->toArray();
+        usort($shift_calender_employee_data, array($this,'employeeSortByPDisplayPriority'));
+        return api_response($request, null, 200, ['shift_calender_employee' => $shift_calender_employee_data, 'shift_calender_header' => $shift_calender_data['header'], 'total' => $total_data]);
     }
 
     public function assignShift($business, $id, Request $request, BusinessShiftRepository $business_shift_repository, ShiftCalenderRepository $shift_calender_repository,
@@ -78,5 +72,11 @@ class ShiftCalenderController extends Controller
 //        return api_response($request, null, 200, ['shift_details' => $shift_calender]);
         $shift_calender_creator->setShiftCalenderRequester($shift_calender_requester)->update($shift_calender);
         return api_response($request, null, 200);
+    }
+
+    private function employeeSortByPDisplayPriority($a, $b)
+    {
+        if ($a['display_priority'] < $b['display_priority']) return 0;
+        return 1;
     }
 }
--- app/Transformers/Business/ShiftCalenderTransformer.php
+++ app/Transformers/Business/ShiftCalenderTransformer.php
@@ -1,12 +1,33 @@
 <?php namespace App\Transformers\Business;
 
-use League\Fractal\TransformerAbstract;
+use Carbon\Carbon;
 
-class ShiftCalenderTransformer extends TransformerAbstract
+class ShiftCalenderTransformer
 {
-    public function transform($shift_calender)
+    public function transform($shift_calenders)
     {
-
+        $data = $header = [];
+        foreach ($shift_calenders as $shift_calender)
+        {
+            $header[$shift_calender->date] =
+            [
+                'date_raw' => $shift_calender->date,
+                'date' =>  Carbon::parse($shift_calender->date)->format('d M'),
+                'day' => Carbon::parse($shift_calender->date)->format('l')
+            ];
+            $data[$shift_calender->business_member_id][$shift_calender->date] = [
+                'id' => $shift_calender->id,
+                'date' => $shift_calender->date,
+                'business_member_id' => $shift_calender->business_member_id,
+                'is_general' => $shift_calender->is_general,
+                'is_unassigned' => $shift_calender->is_unassigned,
+                'is_shift' => $shift_calender->is_shift,
+                'shift_name' => $shift_calender->shift_name
+            ];
+            if (!isset($data[$shift_calender->business_member_id]['display_priority'])) $data[$shift_calender->business_member_id]['display_priority'] = $shift_calender->is_shift == 1 ? 0 : 1;
+            else
+                if ($shift_calender->is_shift) $data[$shift_calender->business_member_id]['display_priority'] = $data[$shift_calender->business_member_id]['display_priority'] == 0 ? 0 : 1;
+        }
+        return ['header' => $header, 'data' => $data];
     }
-
 }
