79050de8b7fbbf5f658af0941c5f703bc7c26ab1
Foysal <foysal@iamsheba.xyz>
2022-05-23 11:08:21 +0600

    ⬆️ Response Change for MTB sdave primary information and merged with that
    Type(s): UPDATE

--- app/Sheba/MtbOnboarding/MtbSavePrimaryInformation.php
+++ app/Sheba/MtbOnboarding/MtbSavePrimaryInformation.php
@@ -202,13 +202,13 @@ class MtbSavePrimaryInformation
             return http_response($request, null, 403, ['message' => 'Please fill Up all the fields, Your form is ' . $data . " completed"]);
         $data = $this->makePrimaryInformation($request->reference, $request->otp);
         $response = $this->client->post(QRPaymentStatics::MTB_SAVE_PRIMARY_INFORMATION, $data, AuthTypes::BARER_TOKEN);
-        if (!isset($response['ticketId'])) {
+        if (!isset($response['Data']['TicketId'])) {
             if (isset($response['responseMessage']))
                 throw new MtbServiceServerError("MTB Account Creation Failed, " . $response['responseMessage']);
             else
                 throw new MtbServiceServerError("MTB Account Creation Failed, " . $response['ResponseMessage']);
         }
-        $this->partner->partnerMefInformation->mtb_ticket_id = $response['ticketId'];
+        $this->partner->partnerMefInformation->mtb_ticket_id = $response['Data']['TicketId'];
         $this->partner->partnerMefInformation->save();
         $this->applyMtb();
         $bannerMtb = (new PaymentService())->setPartner($this->partner)->getBannerForMtb();
