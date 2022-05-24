60cdc5e3bd5345f30f2941c5a22982b9d9e5f184
Shafiqul Islam <shafiqul@sheba.xyz>
2022-05-24 16:35:22 +0600

    ⬆️  Make all manager auth route to support both jwt and remember token
    Type(s): UPDATE
    
    Issue(s):
    - JIRA: SHEBA-9128.

--- app/Http/Middleware/AccessTokenMiddleware.php
+++ app/Http/Middleware/AccessTokenMiddleware.php
@@ -21,6 +21,8 @@ class AccessTokenMiddleware
     protected $authorizationToken;
     /** @var AuthorizationTokenRepositoryInterface */
     private $authorizeTokenRepository;
+    /** @var AuthUser */
+    protected $authUser;
 
     /**
      * AccessTokenMiddleware constructor.
@@ -39,7 +41,7 @@ class AccessTokenMiddleware
         $key_name = 'digigo:debug:' . $now;
 
         try {
-            $token = JWTAuth::getToken();
+            $token = $this->getToken();
             if (!$token) {
                 if ($is_digigo) Redis::set($key_name, "1: $now : null");
                 return $this->formApiResponse($request, null, 401, ['message' => "Your session has expired. Try Login"]);
@@ -55,8 +57,9 @@ class AccessTokenMiddleware
                 throw new AccessTokenNotValidException();
             }
             $this->setAuthorizationToken($access_token);
+            $this->authUser = AuthUser::create();
 
-            $request->merge(['access_token' => $access_token, 'auth_user' => AuthUser::create()]);
+            $request->merge(['access_token' => $access_token, 'auth_user' => $this->authUser]);
             $partner = $request->auth_user->getPartner();
             if (!$this->isRouteAccessAllowed($partner)) {
                 return api_response($request, null, 403, ["message" => "Sorry! Your migration is running. Please be patient."]);
@@ -73,6 +76,11 @@ class AccessTokenMiddleware
         }
 
         return $next($request);
+    }
+
+    protected function getToken()
+    {
+        return JWTAuth::getToken();
     }
 
     /**
