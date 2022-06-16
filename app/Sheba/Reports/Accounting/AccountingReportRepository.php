b0ceebd60495e69263b4c96a5c5008fc028dc0e2
Foysal <foysal@iamsheba.xyz>
2022-06-16 17:18:21 +0600

    ➕ commented code removed Type(s): ADD

--- app/Sheba/Reports/Accounting/AccountingReportRepository.php
+++ app/Sheba/Reports/Accounting/AccountingReportRepository.php
@@ -145,14 +145,7 @@ class AccountingReportRepository extends BaseRepository
 
     public function getAccountingReportsList($version_code): array
     {
-
         $list = [
-//            [
-//                'key' => AccountingReport::DIGITAL_TRANSACTION_LIST,
-//                'report_bangla_name' => 'ডিজিটাল ট্রানজেকশন লিস্ট',
-//                'url' => config('sheba.api_url') . '/v2/accounting/reports/online-transactions',
-//                'icon' => config('accounting_entry.icon_url') . '/' . 'digital_transaction_list_report.png'
-//            ],
             [
                 'key' => AccountingReport::PRODUCT_WISE_SALES_REPORT,
                 'report_bangla_name' => 'পণ্য অনুযায়ী বিক্রয় রিপোর্ট',
