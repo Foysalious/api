832314e65855f00fa2e007f5fd8a16cfe2d78e14
Foysal <foysal@iamsheba.xyz>
2022-06-22 11:29:13 +0600

    ➕ Disclaimer message added
    Type(s): ADD

--- app/Sheba/ResellerPayment/PaymentService.php
+++ app/Sheba/ResellerPayment/PaymentService.php
@@ -108,7 +108,7 @@ class PaymentService
                     'banner' => PaymentMethodStatics::getMtbBannerURL(),
                     'faq' => PaymentMethodStatics::detailsFAQ(),
                     'status' => 'completed' ?? null,
-                    'disclaimer_message' => isset($mtb_status) ? $mtb_status['description'] : 'আবেদন সফল হয়েছে !',
+                    'disclaimer_message' => isset($mtb_status) ? $mtb_status['description'] . $account_details->Description : 'আবেদন সফল হয়েছে !',
                     'how_to_use_link' => PaymentLinkStatics::how_to_use_webview(),
                     'payment_service_info_link' => PaymentLinkStatics::payment_setup_faq_webview(),
                     'details' => [
