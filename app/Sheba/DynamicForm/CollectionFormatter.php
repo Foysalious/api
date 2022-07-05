3b2228dd0bc3485b89602353ee529a087a7c50f8
Foysal <foysal@iamsheba.xyz>
2022-07-05 15:13:23 +0600

    ➕ thana division district change
    Type(s): ADD

--- app/Sheba/DynamicForm/CollectionFormatter.php
+++ app/Sheba/DynamicForm/CollectionFormatter.php
@@ -23,4 +23,45 @@ class CollectionFormatter
         }
         return $this->data;
     }
+
+    public function formatCollectionUpdated()
+    {
+        for ($i = 0; $i < count($this->data); $i++) {
+            $this->data[$i]['key'] = $this->data[$i]['name'];
+            unset($this->data[$i]['id']);
+            $this->data[$i]['value'] = $this->data[$i]['bn_name'];
+            unset($this->data[$i]['bn_name']);
+        }
+        return $this->data;
+    }
+
+    public function formatCollectionDistrict()
+    {
+        for ($i = 0; $i < count($this->data); $i++) {
+            $this->data[$i]->key = $this->data[$i]->district;
+            $this->data[$i]->value = $this->data[$i]->district;
+            $this->data[$i]->name = $this->data[$i]->district;
+            unset($this->data[$i]->thana);
+            unset($this->data[$i]->district);
+            unset($this->data[$i]->division);
+            unset($this->data[$i]->branch_name);
+            unset($this->data[$i]->branch_code);
+        }
+        return $this->data;
+    }
+
+    public function formatCollectionThana()
+    {
+        for ($i = 0; $i < count($this->data); $i++) {
+            $this->data[$i]->key = $this->data[$i]->thana;
+            $this->data[$i]->value = $this->data[$i]->thana;
+            $this->data[$i]->name = $this->data[$i]->thana;
+            unset($this->data[$i]->thana);
+            unset($this->data[$i]->district);
+            unset($this->data[$i]->division);
+            unset($this->data[$i]->branch_name);
+            unset($this->data[$i]->branch_code);
+        }
+        return $this->data;
+    }
 }
