5140ab5887670eea420d7a78c4163bc2487ac2c0
Asad Ahmed <asad@sheba.xyz>
2022-07-24 17:14:02 +0600

    Queue Work Remove
    Type(s): Remove

--- app/Sheba/TopUp/Jobs/QueueConnectionManager.php
+++ app/Sheba/TopUp/Jobs/QueueConnectionManager.php
@@ -60,23 +60,14 @@ class QueueConnectionManager
          * New connection name needs to be listened from supervisor.
          */
 
-        $top_agents = json_decode(Redis::get('topup_top_agents'));
-
-        $first_top_partner_agents = $top_agents->top_agents_partner;
-        $second_top_partner_agents = $top_agents->second_top_agents_partner;
-
-        $first_top_affiliate_agents = $top_agents->top_agents_affiliate;
-        $second_top_affiliate_agents = $top_agents->second_top_agents_affiliate;
-
-        $first_top_business_agents = $top_agents->top_agents_business;
-        $second_top_business_agents = $top_agents->seccond_top_agents_business;
-
         return [
             /**
              * Affiliates
              */
-            "topup_affiliate_1" => $first_top_affiliate_agents,
-            "topup_affiliate_2" => $second_top_affiliate_agents,
+            "topup_affiliate_1" => [52585, 113908, 99291, 56464, 86463, 39662, 95829, 36443],
+            "topup_affiliate_2" => [52807, 130546, 126724, 105278, 35922, 127249, 105929, 105998],
+            "topup_affiliate_3" => [56317, 125090, 39755, 95948, 52376, 104288, 56830, 22355, 128162, 120697],
+            "topup_affiliate_4" => [108618, 123995, 123422, 35751, 83479, 102926, 119102, 97955,2635,112387,125756,123593],
 
             "topup_affiliate_chunk_1" => [ "from" =>      1, "to" =>  40000 ],
             "topup_affiliate_chunk_2" => [ "from" =>  40001, "to" =>  80000 ],
@@ -90,8 +81,8 @@ class QueueConnectionManager
             /**
              * Partners
              */
-            "topup_partner_1" => $first_top_partner_agents,
-            "topup_partner_2" => $second_top_partner_agents,
+            "topup_partner_1" => [470194, 123497, 370604],
+            "topup_partner_2" => [291149, 521301, 89642],
 
             "topup_partner_chunk_1"  => [ "from" =>       1, "to" =>  200000 ],
             "topup_partner_chunk_2"  => [ "from" =>  200001, "to" =>  400000 ],
@@ -110,8 +101,8 @@ class QueueConnectionManager
             /**
              * Businesses
              */
-            "topup_business_1" => $first_top_business_agents,
-            "topup_business_2" => $second_top_business_agents,
+            "topup_business_1" => [113],
+            "topup_business_2" => [1605, 91],
             "topup_business_default" => null,
 
             /**
