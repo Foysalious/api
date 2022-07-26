a1ed17a65effa53a73d2083a51c9ab0f16ba5653
Asad Ahmed <asad@sheba.xyz>
2022-07-26 12:50:10 +0600

    Employee Middleware change
    Type(s): ADD

--- app/Http/Route/Prefix/V2/EmployeeRoute.php
+++ app/Http/Route/Prefix/V2/EmployeeRoute.php
@@ -4,7 +4,7 @@ class EmployeeRoute
 {
     public function set($api)
     {
-        $api->group(['prefix' => 'employee', 'middleware' => ['jwtAuth']], function ($api) {
+        $api->group(['prefix' => 'employee', 'middleware' => ['employee.auth']], function ($api) {
             $api->group(['prefix' => 'employee-visit'], function ($api) {
                 $api->get('own-visit-history', 'Employee\VisitController@ownVisitHistoryV2');
                 $api->get('team-visits', 'Employee\VisitController@teamVisitsListV2');
